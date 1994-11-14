1d88708676e97b4feec38b7e76b194d5a94dc650
Henning Wolf <info@sac-home.org>
1994-11-14 17:51:13 +0000

    added FltnCond FltnFor

--- src/flatten/flatten.h
+++ src/flatten/flatten.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1994/11/10 15:39:42  sbs
+ * Revision 1.3  1994/11/14 17:50:53  hw
+ * added FltnCond FltnFor
+ *
+ * Revision 1.2  1994/11/10  15:39:42  sbs
  * RCS-header inserted
  *
  *
@@ -15,5 +18,7 @@ extern node *Flatten (node *);
 extern node *FltnAssign (node *arg_node, node *arg_info);
 extern node *FltnPrf (node *arg_node, node *arg_info);
 extern node *FltnExprs (node *arg_node, node *arg_info);
+extern node *FltnCond (node *aarg_node, node *arg_info);
+extern node *FltnFor (node *aarg_node, node *arg_info);
 
 #endif /* _flatten_h  */
