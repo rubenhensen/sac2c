faaf4f496d70339335ce243a23112ae922b06bb8
Henning Wolf <info@sac-home.org>
1994-11-17 16:50:16 +0000

    added FltnWhile

--- src/flatten/flatten.h
+++ src/flatten/flatten.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.4  1994/11/15 14:45:31  hw
+ * Revision 1.5  1994/11/17 16:50:16  hw
+ * added FltnWhile
+ *
+ * Revision 1.4  1994/11/15  14:45:31  hw
  * deleted FltnFor
  *
  * Revision 1.3  1994/11/14  17:50:53  hw
@@ -21,6 +24,8 @@ extern node *Flatten (node *);
 extern node *FltnAssign (node *arg_node, node *arg_info);
 extern node *FltnPrf (node *arg_node, node *arg_info);
 extern node *FltnExprs (node *arg_node, node *arg_info);
-extern node *FltnCond (node *aarg_node, node *arg_info);
+extern node *FltnCond (node *arg_node, node *arg_info);
+extern node *FltnWhile (node *arg_node, node *arg_info);
+extern node *FltnWith (node *arg_node, node *arg_info);
 
 #endif /* _flatten_h  */
