3bedae157db235c19f62a69d4c6ba3b05248f112
Sven-Bodo Scholz <sbs@sac-home.org>
1994-11-22 13:47:03 +0000

    Initial revision

---
+++ src/scanparse/scnprs.h
@@ -0,0 +1,21 @@
+#ifndef _scnprs_h
+
+#define _scnprs_h
+/*
+ *
+ * $Log$
+ * Revision 1.1  1994/11/22 13:47:03  sbs
+ * Initial revision
+ *
+ *
+ */
+
+/*
+ * This file contains external declarations for all functions/global variables
+ * provided by the scanner/parser which are used from outside.
+ *
+ */
+
+extern int yyparse ();
+
+#endif /* _scnprs_h */
