d583a6e6e216462bc147ca1cd98740499bdbf7fb
Henning Wolf <info@sac-home.org>
1994-11-29 10:52:01 +0000

    added pointer to struct NODE to struct ids

--- src/global/tree.h
+++ src/global/tree.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1994/11/10 15:44:34  sbs
+ * Revision 1.3  1994/11/29 10:52:01  hw
+ * added pointer to struct NODE to struct ids
+ *
+ * Revision 1.2  1994/11/10  15:44:34  sbs
  * RCS-header inserted
  *
  *
@@ -36,6 +39,7 @@ typedef char id;
 
 typedef struct IDS {
     id *id;
+    struct NODE *node;
     struct IDS *next;
 } ids;
 
