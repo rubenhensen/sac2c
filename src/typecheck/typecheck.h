75ace3bec434dedb2fb9977809842053d8da70b4
Henning Wolf <info@sac-home.org>
1994-12-02 10:13:58 +0000

    Initial revision

---
+++ src/typecheck/typecheck.h
@@ -0,0 +1,16 @@
+/*
+ * $Log : $
+ *
+ */
+
+#ifndef _typecheck_h
+
+#define _typecheck_h
+
+extern void Typecheck (node *arg_node);
+
+extern node *TCfundef (node *arg_node, node *arg_info);
+
+extern node *TClet (node *arg_node, node *arg_info);
+
+#endif /* _typecheck_h */
