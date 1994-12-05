71cdddabc7501e3772a7d49ccd918fbdeea5d326
Henning Wolf <info@sac-home.org>
1994-12-05 15:08:48 +0000

    changed output of function Error

--- src/global/Error.c
+++ src/global/Error.c
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1994/11/10 15:44:34  sbs
+ * Revision 1.3  1994/12/05 15:08:48  hw
+ * changed output of function Error
+ *
+ * Revision 1.2  1994/11/10  15:44:34  sbs
  * RCS-header inserted
  *
  *
@@ -12,7 +15,7 @@
 void
 Error (char *string, int status)
 {
-    fprintf (stderr, "%s\n", string);
+    fprintf (stderr, "\n%s\n", string);
     exit (status);
 }
 
