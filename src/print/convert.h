a6ad19aece77cb1d51dd01b7bcb85eb981cab350
Henning Wolf <info@sac-home.org>
1994-12-05 13:26:31 +0000

    *** empty log message ***

---
+++ src/print/convert.h
@@ -0,0 +1,20 @@
+/*
+ *
+ * $Log$
+ * Revision 1.2  1994/12/05 13:26:31  hw
+ * *** empty log message ***
+ *
+ *
+ */
+
+/* NOTE: you must include "tree.h before this !!!
+ */
+
+#ifndef _convert_h
+
+#define _convert_h
+
+extern char *type_string[];
+extern char *Type2String (types *, int);
+
+#endif /* _convert_h */
