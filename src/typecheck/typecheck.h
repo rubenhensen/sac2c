2760f26f39865139d056de269fafc5b4b9a5b0df
Henning Wolf <info@sac-home.org>
1994-12-06 09:53:27 +0000

    added TCreturn

--- src/typecheck/typecheck.h
+++ src/typecheck/typecheck.h
@@ -12,5 +12,6 @@ extern void Typecheck (node *arg_node);
 extern node *TCfundef (node *arg_node, node *arg_info);
 
 extern node *TClet (node *arg_node, node *arg_info);
+extern node *TCreturn (node *arg_node, node *arg_info);
 
 #endif /* _typecheck_h */
