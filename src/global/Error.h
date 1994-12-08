ad59ee70888f7d3820f3ce7702559211b08d86b4
Henning Wolf <info@sac-home.org>
1994-12-08 17:56:38 +0000

    added WARN1 , ERRORS and some external definitions see Error.c

--- src/global/Error.h
+++ src/global/Error.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.3  1994/12/02 12:38:10  sbs
+ * Revision 1.4  1994/12/08 17:56:38  hw
+ * added WARN1 , ERRORS and some external definitions see Error.c
+ *
+ * Revision 1.3  1994/12/02  12:38:10  sbs
  * NOTE macro inserted
  *
  * Revision 1.2  1994/11/10  15:44:34  sbs
@@ -20,7 +23,18 @@
 
 #define NOTE(s)                                                                          \
     if (!silent)                                                                         \
-    fprintf (stderr, s)
+        fprintf (stderr, s);
+#define WARN1(s)                                                                         \
+    if (!silent) {                                                                       \
+        warnings = +1;                                                                   \
+        fprintf (stderr, s);                                                             \
+    }
+#define ERROR(s)                                                                         \
+    fprintf (stderr, s);                                                                 \
+    errors = +1;
+extern int errors;
+extern int warnings;
+extern int silent;
 
 extern void Error (char *string, int status);
 
