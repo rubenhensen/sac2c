d7e1fe271c21caad2dd7e4b899793bb931fd7f40
Henning Wolf <info@sac-home.org>
1994-12-08 17:56:38 +0000

    new declarations: - int errors;                   - int warnings;                   - int silent;

--- src/global/Error.c
+++ src/global/Error.c
@@ -1,7 +1,12 @@
 /*
  *
  * $Log$
- * Revision 1.3  1994/12/05 15:08:48  hw
+ * Revision 1.4  1994/12/08 17:56:38  hw
+ * new declarations: - int errors;
+ *                   - int warnings;
+ *                   - int silent;
+ *
+ * Revision 1.3  1994/12/05  15:08:48  hw
  * changed output of function Error
  *
  * Revision 1.2  1994/11/10  15:44:34  sbs
@@ -11,6 +16,9 @@
  */
 
 #include "Error.h"
+int errors = 0;   /* counter for number of errors */
+int warnings = 0; /* counter for number of warnings */
+int silent = 0;
 
 void
 Error (char *string, int status)
