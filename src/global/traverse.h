1db6ea7ca43a7557670862648392ded44006c7ed
Sven-Bodo Scholz <sbs@sac-home.org>
1994-12-09 10:45:33 +0000

    opt_tab inserted

--- src/global/traverse.h
+++ src/global/traverse.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.3  1994/12/01 17:41:09  hw
+ * Revision 1.4  1994/12/09 10:45:33  sbs
+ * opt_tab inserted
+ *
+ * Revision 1.3  1994/12/01  17:41:09  hw
  * added extern funptr type_tab[];
  *
  * Revision 1.2  1994/11/10  15:44:34  sbs
@@ -27,5 +30,7 @@ extern funptr flat_tab[];
 extern funptr prnt_tab[];
 
 extern funptr type_tab[];
+
+extern funptr opt_tab[];
 
 #endif /* _sac_traverse_h */
