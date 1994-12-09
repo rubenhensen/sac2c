999dbeaaa4f0378c79ffce6f20c618429ab14630
Sven-Bodo Scholz <sbs@sac-home.org>
1994-12-09 10:48:23 +0000

    Initial revision

---
+++ src/optimize/optimize.h
@@ -0,0 +1,18 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  1994/12/09 10:48:23  sbs
+ * Initial revision
+ *
+ *
+ */
+
+#ifndef _optimize_h
+
+#define _optimize_h
+
+extern node *OPTnum (node *, node *);
+
+extern node *Optimize (node *);
+
+#endif /* _optimize_h */
