51b07ddf1c8556cd49d5da044af43a9d644f78ba
Henning Wolf <info@sac-home.org>
1994-12-13 11:26:34 +0000

    changed macros WARN1, NOTE changed macro ERROR to ERROR1 inserted macro ERROR2 added declaration: void DoPrint( char *format, ...)

--- src/global/Error.h
+++ src/global/Error.h
@@ -1,7 +1,13 @@
 /*
  *
  * $Log$
- * Revision 1.4  1994/12/08 17:56:38  hw
+ * Revision 1.5  1994/12/13 11:26:34  hw
+ * changed macros WARN1, NOTE
+ * changed macro ERROR to ERROR1
+ * inserted macro ERROR2
+ * added declaration: void DoPrint( char *format, ...)
+ *
+ * Revision 1.4  1994/12/08  17:56:38  hw
  * added WARN1 , ERRORS and some external definitions see Error.c
  *
  * Revision 1.3  1994/12/02  12:38:10  sbs
@@ -23,19 +29,32 @@
 
 #define NOTE(s)                                                                          \
     if (!silent)                                                                         \
-        fprintf (stderr, s);
+        DoPrint s;
 #define WARN1(s)                                                                         \
     if (!silent) {                                                                       \
         warnings = +1;                                                                   \
-        fprintf (stderr, s);                                                             \
+        DoPrint s;                                                                       \
     }
-#define ERROR(s)                                                                         \
-    fprintf (stderr, s);                                                                 \
-    errors = +1;
+#define ERROR1(s)                                                                        \
+    {                                                                                    \
+        fprintf (stderr, "/n");                                                          \
+        DoPrint s;                                                                       \
+        errors = +1;                                                                     \
+    }
+
+#define ERROR2(n, s)                                                                     \
+    {                                                                                    \
+        fprintf (stderr, "\n");                                                          \
+        DoPrint s;                                                                       \
+        fprintf (stderr, "\n");                                                          \
+        exit (n);                                                                        \
+    }
+
 extern int errors;
 extern int warnings;
 extern int silent;
 
+extern void DoPrint (char *format, ...);
 extern void Error (char *string, int status);
 
 #endif /* _Error_h */
