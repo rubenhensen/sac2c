91d76e76c72b99fd50a82020da02c69726801fe6
Henning Wolf <info@sac-home.org>
1994-12-14 08:47:42 +0000

    added declarations of TCcond & TCassign

--- src/typecheck/typecheck.h
+++ src/typecheck/typecheck.h
@@ -1,6 +1,9 @@
 /*
  * $Log$
- * Revision 1.3  1994/12/06 09:58:19  hw
+ * Revision 1.4  1994/12/14 08:47:42  hw
+ * added declarations of TCcond & TCassign
+ *
+ * Revision 1.3  1994/12/06  09:58:19  hw
  * changed log-header
  *
  *
@@ -16,5 +19,6 @@ extern node *TCfundef (node *arg_node, node *arg_info);
 
 extern node *TClet (node *arg_node, node *arg_info);
 extern node *TCreturn (node *arg_node, node *arg_info);
-
+extern node *TCcond (node *arg_node, node *arg_info);
+extern node *TCassign (node *arg_node, node *arg_info);
 #endif /* _typecheck_h */
