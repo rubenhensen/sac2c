38c1e0ce0d042ef3e2a2e4e7f54b53befee87a1a
Henning Wolf <info@sac-home.org>
1994-12-19 13:01:42 +0000

    inserted TCdo & TCwhile

--- src/typecheck/typecheck.h
+++ src/typecheck/typecheck.h
@@ -1,6 +1,9 @@
 /*
  * $Log$
- * Revision 1.4  1994/12/14 08:47:42  hw
+ * Revision 1.5  1994/12/19 13:01:42  hw
+ * inserted TCdo & TCwhile
+ *
+ * Revision 1.4  1994/12/14  08:47:42  hw
  * added declarations of TCcond & TCassign
  *
  * Revision 1.3  1994/12/06  09:58:19  hw
@@ -21,4 +24,7 @@ extern node *TClet (node *arg_node, node *arg_info);
 extern node *TCreturn (node *arg_node, node *arg_info);
 extern node *TCcond (node *arg_node, node *arg_info);
 extern node *TCassign (node *arg_node, node *arg_info);
+extern node *TCdo (node *arg_node, node *arg_info);
+extern node *TCwhile (node *arg_node, node *arg_info);
+
 #endif /* _typecheck_h */
