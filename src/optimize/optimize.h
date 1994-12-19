caf3d9a9641196a46881dbed390490145f44b95a
Arne Sievers <info@sac-home.org>
1994-12-19 14:43:17 +0000

    Added - OPTfundef OPTarg OPTvardec OPTmodul OPTlet OPTblock preperation for loop invariant removal

--- src/optimize/optimize.h
+++ src/optimize/optimize.h
@@ -1,7 +1,11 @@
 /*
  *
  * $Log$
- * Revision 1.2  1994/12/12 11:04:18  asi
+ * Revision 1.3  1994/12/19 14:43:17  asi
+ * Added - OPTfundef OPTarg OPTvardec OPTmodul OPTlet OPTblock
+ * preperation for loop invariant removal
+ *
+ * Revision 1.2  1994/12/12  11:04:18  asi
  * added OPTdo
  *
  * Revision 1.1  1994/12/09  10:48:23  sbs
@@ -14,7 +18,12 @@
 
 #define _optimize_h
 
-extern node *OPTdo (node *, node *);
+extern node *OPTfundef (node *, node *);
+extern node *OPTarg (node *, node *);
+extern node *OPTvardec (node *, node *);
+extern node *OPTmodul (node *, node *);
+extern node *OPTlet (node *, node *);
+extern node *OPTblock (node *, node *);
 
 extern node *Optimize (node *);
 
