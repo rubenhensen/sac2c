20bba061f2b3aab3e84fb0a3009a1d953df095fd
Sven-Bodo Scholz <sbs@sac-home.org>
1994-12-27 09:28:19 +0000

    Initial revision

---
+++ src/global/free.h
@@ -0,0 +1,18 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  1994/12/27 09:28:19  sbs
+ * Initial revision
+ *
+ *
+ */
+
+#ifndef _sac_free_h
+
+#define _sac_free_h
+
+extern void FreeIds (ids *ids);
+extern void FreeIdsOnly (ids *ids);
+extern void FreeImplist (node *implist);
+
+#endif /* _sac_free_h */
