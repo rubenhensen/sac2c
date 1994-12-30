263b85cdbffcbf642352caa261e7be30654c7487
Sven-Bodo Scholz <sbs@sac-home.org>
1994-12-30 16:59:33 +0000

    added FreeIds

--- src/global/free.c
+++ src/global/free.c
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.3  1994/12/20 17:42:23  hw
+ * Revision 1.4  1994/12/30 16:59:33  sbs
+ * added FreeIds
+ *
+ * Revision 1.3  1994/12/20  17:42:23  hw
  * added includes dbug.h & stdio.h
  *
  * Revision 1.2  1994/12/20  17:34:35  hw
@@ -34,6 +37,35 @@
  */
 
 void
+FreeIds (ids *ids)
+
+{
+
+    DBUG_ENTER ("FreeIds");
+
+    if (ids->next != NULL)
+        FreeIds (ids->next);
+    free (ids->id);
+    free (ids);
+
+    DBUG_VOID_RETURN;
+}
+
+/*
+ *
+ *  functionname  :
+ *  arguments     :
+ *  description   :
+ *  global vars   :
+ *  internal funs :
+ *  external funs :
+ *  macros        :
+ *
+ *  remarks       :
+ *
+ */
+
+void
 FreeIdsOnly (ids *ids)
 
 {
@@ -43,6 +75,38 @@ FreeIdsOnly (ids *ids)
     if (ids->next != NULL)
         FreeIdsOnly (ids->next);
     free (ids);
+
+    DBUG_VOID_RETURN;
+}
+
+/*
+ *
+ *  functionname  :
+ *  arguments     :
+ *  description   :
+ *  global vars   :
+ *  internal funs :
+ *  external funs :
+ *  macros        :
+ *
+ *  remarks       :
+ *
+ */
+
+void
+FreeImplist (node *implist)
+
+{
+    int i;
+
+    DBUG_ENTER ("FreeImplist");
+
+    if (implist->node[0] != NULL)
+        FreeImplist (implist->node[0]);
+    for (i = 1; i < 4; i++)
+        if (implist->node[i] != NULL)
+            FreeIds ((ids *)implist->node[i]);
+    free (implist);
 
     DBUG_VOID_RETURN;
 }
