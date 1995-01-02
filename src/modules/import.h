649067e68f86e03536d57cabf36743f0bf0a930b
Sven-Bodo Scholz <sbs@sac-home.org>
1995-01-02 16:08:59 +0000

    FindSymbolInModul inserted

--- src/modules/import.h
+++ src/modules/import.h
@@ -3,7 +3,10 @@
 /*
  *
  * $Log$
- * Revision 1.1  1994/12/16 14:39:17  sbs
+ * Revision 1.2  1995/01/02 16:08:59  sbs
+ * FindSymbolInModul inserted
+ *
+ * Revision 1.1  1994/12/16  14:39:17  sbs
  * Initial revision
  *
  * Revision 1.1  1994/12/11  17:33:27  sbs
@@ -12,7 +15,31 @@
  *
  */
 
-#define _filemgr_h
+#define _import_h
+
+typedef struct SYMS {
+    char *id; /* symbol */
+    int flag; /* IMPORTED / NOT_IMPORTED */
+    struct SYMS *next;
+} syms;
+
+typedef struct MOD {
+    char *name;    /* modul name */
+    int flag;      /* flag for recursion protection */
+    int allflag;   /* flag for recursion protection */
+    node *moddec;  /* pointer to the respective N_moddec node */
+    syms *syms[3]; /* pointer to implicit type symbols */
+                   /* explicit type symbols */
+                   /* pointer to function symbols */
+    struct MOD *next;
+} mod;
+
+typedef struct MODS {
+    mod *mod;
+    struct MODS *next;
+} mods;
+
+extern mods *FindSymbolInModul (char *modname, char *name, int symbkind);
 
 extern node *IMmodul (node *, node *);
 extern node *Import (node *);
