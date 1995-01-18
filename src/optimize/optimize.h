9bae38d8622d9226cc53149cce53d871ebf1871d
Arne Sievers <info@sac-home.org>
1995-01-18 17:43:14 +0000

    Dead-Code-Removal now frees memory

--- src/optimize/optimize.h
+++ src/optimize/optimize.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.10  1995/01/17 11:05:16  asi
+ * Revision 1.11  1995/01/18 17:43:14  asi
+ * Dead-Code-Removal now frees memory
+ *
+ * Revision 1.10  1995/01/17  11:05:16  asi
  * Added OPT3assign for loop invariant removal
  * OPT2while now named OPT2loop and used for do and while loops
  *
@@ -43,28 +46,34 @@
 
 #define _optimize_h
 
-extern node *Optimize (node *);
-extern char *PrintMask (long *);
+#define MAX_VAR 255
+#define INC_VAR(mask, var) mask[var] += 1
+#define DEC_VAR(mask, var) mask[var] -= 1
 
-extern node *OPT1fundef (node *, node *);
-extern node *OPT1arg (node *, node *);
-extern node *OPT1vardec (node *, node *);
-extern node *OPTmodul (node *, node *);
-extern node *OPT1let (node *, node *);
-extern node *OPT1id (node *, node *);
-extern node *OPT1assign (node *, node *);
-extern node *OPT1pp (node *, node *);
-extern node *OPT1block (node *, node *);
-extern node *OPT1cond (node *, node *);
-extern node *OPT1loop (node *, node *);
+#define VAR_LENGTH 10
 
-extern node *OPT2fundef (node *, node *);
-extern node *OPT2assign (node *, node *);
-extern node *OPT2block (node *, node *);
-extern node *OPT2vardec (node *, node *);
-extern node *OPT2cond (node *, node *);
-extern node *OPT2loop (node *, node *);
+extern node *Optimize (node *arg_node);
+extern char *PrintMask (long *mask);
 
-extern node *OPT3assign (node *, node *);
+extern node *OPT1fundef (node *arg_node, node *arg_info);
+extern node *OPT1arg (node *arg_node, node *arg_info);
+extern node *OPT1vardec (node *arg_node, node *arg_info);
+extern node *OPTmodul (node *arg_node, node *arg_info);
+extern node *OPT1let (node *arg_node, node *arg_info);
+extern node *OPT1id (node *arg_node, node *arg_info);
+extern node *OPT1assign (node *arg_node, node *arg_info);
+extern node *OPT1pp (node *arg_node, node *arg_info);
+extern node *OPT1block (node *arg_node, node *arg_info);
+extern node *OPT1cond (node *arg_node, node *arg_info);
+extern node *OPT1loop (node *arg_node, node *arg_info);
+
+extern node *OPT2fundef (node *arg_node, node *arg_info);
+extern node *OPT2assign (node *arg_node, node *arg_info);
+extern node *OPT2block (node *arg_node, node *arg_info);
+extern node *OPT2vardec (node *arg_node, node *arg_info);
+extern node *OPT2cond (node *arg_node, node *arg_info);
+extern node *OPT2loop (node *arg_node, node *arg_info);
+
+extern node *OPT3assign (node *arg_node, node *arg_info);
 
 #endif /* _optimize_h */
