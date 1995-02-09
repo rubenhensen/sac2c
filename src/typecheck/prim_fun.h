3598f98bdad5bf156e9bb4e96d61ab46853a2d62
Henning Wolf <info@sac-home.org>
1995-02-09 11:07:52 +0000

    - changed function AxA  - renamed function Ixf_F to Axs_f  - added new functions: Shp, Reshp, TakeV, DropV, Psi, TakeDropS, Rot, Cat

--- src/typecheck/prim_fun.h
+++ src/typecheck/prim_fun.h
@@ -1,7 +1,12 @@
 /*
  *  $Log$
- *  Revision 1.2  1995/02/03 16:04:18  hw
- *  added new functions AxA & Ixf_F
+ *  Revision 1.3  1995/02/09 11:07:52  hw
+ *   - changed function AxA
+ *   - renamed function Ixf_F to Axs_f
+ *   - added new functions: Shp, Reshp, TakeV, DropV, Psi, TakeDropS, Rot, Cat
+ *
+ * Revision 1.2  1995/02/03  16:04:18  hw
+ * added new functions AxA & Ixf_F
  *
  * Revision 1.1  1995/02/03  07:45:32  hw
  * Initial revision
@@ -40,7 +45,19 @@ xxxxx /* xxxxx is only a dummy argument */
 
 extern prim_fun_tab_elem *prim_fun_tab;
 extern void InitPrimFunTab ();
-extern types *AxA (types *array1, types *array2);
-extern types *Ixf_F (types *array1);
+extern types *AxA (types *array1, types *array2, simpletype s_type);
+extern types *Axs_F (types *array1);
+extern types *Reshp (node *vec, types *array);
+extern types *Shp (types *array);
+extern types *TakeV (node *vec, types *array);
+extern types *DropV (node *vec, types *array);
+extern types *Psi (types *vec, types *array);
+extern types *TakeDropS (node *vec, types *array, int tag);
+extern types *Cat (node *s_node, types *array1, types *array2);
+extern types *Rot (node *s_node, types *array);
+
+#if 0
+extern types *Psi(types *array1, types *array2);
+#endif
 
 #endif /* _prim_fun_h */
