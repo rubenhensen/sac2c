9d138f80ac8463908711fdb20fd5bb2b089d6b59
Arne Sievers <info@sac-home.org>
1995-03-07 10:17:09 +0000

    added CFcast

--- src/optimize/ConstantFolding.h
+++ src/optimize/ConstantFolding.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.4  1995/02/28 18:34:45  asi
+ * Revision 1.5  1995/03/07 10:17:09  asi
+ * added CFcast
+ *
+ * Revision 1.4  1995/02/28  18:34:45  asi
  * added CFwith
  *
  * Revision 1.3  1995/02/22  18:16:34  asi
@@ -33,5 +36,6 @@ extern node *CFcond (node *arg_node, node *arg_info);
 extern node *CFvar (node *arg_node, node *arg_info);
 extern node *CFassign (node *arg_node, node *arg_info);
 extern node *CFwith (node *arg_node, node *arg_info);
+extern node *CFcast (node *arg_node, node *arg_info);
 
 #endif /* _ConstantFolding_h */
