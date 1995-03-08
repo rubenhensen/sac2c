2c1e88c0ace58e7dbe10aa30516b07e647f06985
Sven-Bodo Scholz <sbs@sac-home.org>
1995-03-08 17:13:08 +0000

    first version with test embedded

--- src/compile/icm2c.c
+++ src/compile/icm2c.c
@@ -1,11 +1,18 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/03/07 18:21:02  sbs
+ * Revision 1.2  1995/03/08 17:13:08  sbs
+ * first version with test embedded
+ *
+ * Revision 1.1  1995/03/07  18:21:02  sbs
  * Initial revision
  *
  *
  */
+
+#include "stdio.h"
+#include "print.h"
+#include "dbug.h"
 
 #define GetNextId(res, ex)                                                               \
     {                                                                                    \
@@ -40,25 +47,24 @@
             GetNextId (v[i], ex);                                                        \
     }
 
-#define ConstVectToOffset(dim, v, a)                                                     \
-    {                                                                                    \
-        int i;                                                                           \
-        for (i = dim - 1; i > 0; i--)                                                    \
-            fprintf (outfile, "ND_KD_A_SHAPE(%s, %d)*(", a, i);                          \
-        fprintf (outfile, "%s ", v[0]);                                                  \
-        for (i = 1; i < dim; i++)                                                        \
-            fprintf (outfile, "+%s) ", v[i]);                                            \
-    }
+#define BEGIN_COMMENT fprintf (outfile, "/*\n *")
+#define END_COMMENT fprintf (outfile, " */\n")
 
-#define VectToOffset(dim, v, a)                                                          \
+#define AccessVect(v, i) fprintf (outfile, "ND_A_FIELD(%s)[%i])", v, i)
+
+#define AccessConst(v, i) fprintf (outfile, "%s", v[i])
+
+#define VectToOffset(dim, v_i_str, a)                                                    \
     {                                                                                    \
         int i;                                                                           \
-        fprintf (outfile, "[");                                                          \
         for (i = dim - 1; i > 0; i--)                                                    \
-            fprintf (outfile, "ND_KD_A_SHAPE(%s, %d)*(", a, i);                          \
-        fprintf (outfile, "%s ", v[0]);                                                  \
-        for (i = 1; i < dim; i++)                                                        \
-            fprintf (outfile, "+ND_A_FIELD(%s)[%i]) ", v, i);                            \
+            fprintf (outfile, "ND_KD_A_SHAPE(%s, %d)* (", a, i);                         \
+        v_i_str;                                                                         \
+        for (i = 1; i < dim; i++) {                                                      \
+            fprintf (outfile, "+");                                                      \
+            v_i_str;                                                                     \
+            fprintf (outfile, ") ");                                                     \
+        }                                                                                \
     }
 
 #define CopyBlock(a, offset, res)                                                        \
@@ -77,196 +83,220 @@
     fprintf (outfile, "ND_A_FIELD(%s)[__idest++]=ND_A_FIELD(%s)[__isrc++];\n", res, a);  \
     indent -= 2;                                                                         \
     INDENT;                                                                              \
-        fprintf(outfile, "})
+    fprintf (outfile, "}")
 
-/*
+#ifdef TEST_BACKEND
 
- ND_KS_DECL_ARRAY( basic_type, name, dim, s0,..., sn)   : declares an array
+int
+main ()
+{
 
-char *type, *name;
-int dim;
-char **s;
+    typedef char *string;
 
-int i;
+    FILE *outfile = stdout;
+    char type[] = "double";
+    char name[] = "array_name";
+    int dim = 3;
+    char *s[] = {"40", "50", "60"};
+    char *vi[] = {"10", "20", "30"};
+    char v[] = "vector";
+    char a[] = "arg";
+    char a1[] = "arg1";
+    char a2[] = "arg2";
+    char res[] = "result";
+    int i;
+    int indent = 1;
 
-  GetNextId( type, exprs);
-  GetNextId( name, exprs);
-  GetNextInt( dim, exprs);
-  GetShape(dim, s, exprs);
+#endif /* TEST_BACKEND */
 
-  INDENT;
-  fprintf( outfile, "%s *%s;\n", type, name);
-  INDENT;
-  fprintf( outfile, "int __%s_rc=1;\n", name);
-  INDENT;
-  fprintf( outfile, "int __%s_sz=", name);
-  fprintf( outfile, "%s", s[0]);
-  for(i=1; i<dim; i++)
-    fprintf( outfile, "*%s", s[i]);
-  fprintf( outfile, ";\n", s[i]);
-  INDENT;
-  fprintf( outfile, "int __%s_d=%d;\n", name, dim);
-  for(i=0; i<dim; i++) {
+    /*
+     * ND_KS_DECL_ARRAY( basic_type, name, dim, s0,..., sn)   : declares an array
+     *
+     * char *type, *name;
+     * int dim;
+     * char **s;
+     */
+
+#ifndef TEST_BACKEND
+    int i;
+
+    GetNextId (type, exprs);
+    GetNextId (name, exprs);
+    GetNextInt (dim, exprs);
+    GetShape (dim, s, exprs);
+#endif /* no TEST_BACKEND */
+
+    BEGIN_COMMENT;
+    fprintf (outfile, " ND_KS_DECL_ARRAY(%s, %s, %d", type, name, dim);
+    for (i = 0; i < dim; i++) {
+        fprintf (outfile, ", ");
+        AccessConst (s, i);
+    }
+    fprintf (outfile, ")\n");
+    END_COMMENT;
+
     INDENT;
-    fprintf( outfile, "int __%s_s%d=%d;\n", name, i, s[i]);
-  }
-
-*/
-
-/*
-
-ND_KD_SET_SHAPE( name, dim, s0,..., sn)        : sets all shape components of an array
-
-char *name;
-int dim;
-char **s;
-
-int i;
-
-  GetNextId( name, exprs);
-  GetNextInt( dim, exprs);
-  GetShape(dim, s, exprs);
-
-  for(i=0; i<dim; i++) {
+    fprintf (outfile, "%s *%s;\n", type, name);
     INDENT;
-    fprintf( outfile, "int __%s_s%d=%d;\n", name, i, s[i]);
-  }
+    fprintf (outfile, "int *__%s_rc;\n", name);
+    INDENT;
+    fprintf (outfile, "int __%s_sz=", name);
+    fprintf (outfile, "%s", s[0]);
+    for (i = 1; i < dim; i++)
+        fprintf (outfile, "*%s", s[i]);
+    fprintf (outfile, ";\n", s[i]);
+    INDENT;
+    fprintf (outfile, "int __%s_d=%d;\n", name, dim);
+    for (i = 0; i < dim; i++) {
+        INDENT;
+        fprintf (outfile, "int __%s_s%d=%s;\n", name, i, s[i]);
+    }
+    fprintf (outfile, "\n");
 
+    /*
+     * ND_KD_SET_SHAPE( name, dim, s0,..., sn)        : sets all shape components of an
+     * array
+     *
+     * char *name;
+     * int dim;
+     * char **s;
+     */
 
-*/
+#ifndef TEST_BACKEND
+    int i;
 
-/*
+    GetNextId (name, exprs);
+    GetNextInt (dim, exprs);
+    GetShape (dim, s, exprs);
+#endif /* no TEST_BACKEND */
 
-ND_KD_PSI_CxA_S( type, a, res, dim, v0,..., vn): selects a single element of the array
+    BEGIN_COMMENT;
+    fprintf (outfile, " ND_KD_SET_SHAPE( %s, %d", name, dim);
+    for (i = 0; i < dim; i++) {
+        fprintf (outfile, ", ");
+        AccessConst (s, i);
+    }
+    fprintf (outfile, ")\n");
+    END_COMMENT;
 
+    for (i = 0; i < dim; i++) {
+        INDENT;
+        fprintf (outfile, "ND_KD_A_SHAPE(%s, %d)=%s;\n", name, i, s[i]);
+    }
+    fprintf (outfile, "\n");
 
-char *type, *a, *res;
-int dim;
-char **v;
+#ifndef TEST_BACKEND
+    free (s);
+#endif /* no TEST_BACKEND */
 
-  INDENT;
-  GetNextId( type, exprs);
-  GetNextId( a, exprs);
-  GetNextId( res, exprs);
-  GetNextInt( dim, exprs);
-  GetShape(dim, v, exprs);
+    /*
+     * ND_KD_PSI_CxA_S( a, res, dim, v0,..., vn): selects a single element of the array
+     *
+     *
+     * char *type, *a, *res;
+     * int dim;
+     * char **vi;
+     */
 
-  fprintf("%s=ND_A_FIELD(%s)[", res, a);
-  ConstVectToOffset(dim, v, a);
-  fprintf("];\n");
+#ifndef TEST_BACKEND
+    fprintf (outfile, "\n--> ND_KD_PSI_CxA_S( %s, %s, %d, %s, %s, %s)\n", a, res, dim,
+             vi[0], vi[1], vi[2]);
+#else  /* TEST_BACKEND */
+GetNextId (type, exprs);
+GetNextId (a, exprs);
+GetNextId (res, exprs);
+GetNextInt (dim, exprs);
+GetShape (dim, vi, exprs);
+#endif /* TEST_BACKEND */
 
-  free(v);
+    INDENT;
+    fprintf (outfile, "%s=ND_A_FIELD(%s)[", res, a);
+    VectToOffset (dim, AccessConst (vi, i), a);
+    fprintf (outfile, "];\n");
 
-*/
-/*
+#ifndef TEST_BACKEND
+    free (v);
+#endif /* no TEST_BACKEND */
 
-ND_KD_PSI_VxA_S( type, a, res, dim, v ) : selects a single element of the array
+    /*
+     * ND_KD_PSI_VxA_S( type, a, res, dim, v ) : selects a single element of the array
+     *
+     * char *type, *a, *res, *v;
+     * int dim;
+     */
 
-char *type, *a, *res, *v;
-int dim;
+#ifdef TEST_BACKEND
+    fprintf (outfile, "\n--> ND_KD_PSI_VxA_S( %s, %s, %s, %d, %s)\n", type, a, res, dim,
+             v);
+#else  /* TEST_BACKEND */
+GetNextId (type, exprs);
+GetNextId (a, exprs);
+GetNextId (res, exprs);
+GetNextInt (dim, exprs);
+GetNextId (v, exprs);
+#endif /* TEST_BACKEND */
 
-  INDENT;
-  GetNextId( type, exprs);
-  GetNextId( a, exprs);
-  GetNextId( res, exprs);
-  GetNextInt( dim, exprs);
-  GetNextId( v, exprs);
+    INDENT;
+    fprintf (outfile, "%s=ND_A_FIELD(%s)[", res, a);
+    VectToOffset (dim, AccessVect (v, i), a);
+    fprintf (outfile, "];\n");
 
-  fprintf("%s=ND_A_FIELD(%s)[", res, a);
-  VectToOffset(dim, v, a);
-  fprintf("];\n");
+#ifndef TEST_BACKEND
+    free (v);
+#endif /* no TEST_BACKEND */
 
-  free(v);
+    /*
+     * ND_KD_PSI_CxA_A( type, a, res, dim, v0,..., vn): selects a sub-array
+     *
+     * char *type, *a, *res;
+     * int dim;
+     * char **vi;
+     */
 
-*/
+#ifdef TEST_BACKEND
+    fprintf (outfile, "\n--> ND_KD_PSI_CxA_A( %s, %s, %s, %d, %s, %s, %s)\n", type, a,
+             res, dim, vi[0], vi[1], vi[2]);
+#else  /* TEST_BACKEND */
+GetNextId (type, exprs);
+GetNextId (a, exprs);
+GetNextId (res, exprs);
+GetNextInt (dim, exprs);
+GetShape (dim, vi, exprs);
+#endif /* TEST_BACKEND */
 
-/*
+    INDENT;
+    CopyBlock (a, VectToOffset (dim, AccessConst (vi, i), a), res);
 
-ND_KD_PSI_CxA_A_NEW( type, a, res, dim, v0,..., vn): selects a sub-array
+#ifndef TEST_BACKEND
+    free (v);
+#endif /* no TEST_BACKEND */
 
+    /*
+     * ND_KD_PSI_VxA_A( type, a, res, dim, v )       : selects a sub-array
+     *
+     * char *type, *a, *res, *v;
+     * int dim;
+     */
 
-char *type, *a, *res;
-int dim;
-char **v;
+#ifdef TEST_BACKEND
+    fprintf (outfile, "\n--> ND_KD_PSI_VxA_A( %s, %s, %s, %d, %s)\n", type, a, res, dim,
+             v);
+#else  /* TEST_BACKEND */
+GetNextId (type, exprs);
+GetNextId (a, exprs);
+GetNextId (res, exprs);
+GetNextInt (dim, exprs);
+GetNextId (v, exprs);
+#endif /* TEST_BACKEND */
 
-  INDENT;
-  GetNextId( type, exprs);
-  GetNextId( a, exprs);
-  GetNextId( res, exprs);
-  GetNextInt( dim, exprs);
-  GetShape(dim, v, exprs);
+    INDENT;
+    CopyBlock (a, VectToOffset (dim, AccessVect (v, i), a), res);
 
-  INDENT;
-  fprintf(outfile, "ND_ALLOC_ARRAY(%s, %s);\n", type, res);
-  INDENT;
-  CopyBlock(a, ConstVectToOffset(dim, v, a), res)
+#ifndef TEST_BACKEND
+    free (v);
+#endif /* no TEST_BACKEND */
 
-  free(v);
-
-*/
-
-/*
-
-ND_KD_PSI_CxA_A_OLD( type, a, res, dim, v0,..., vn): selects a sub-array
-
-
-char *type, *a, *res;
-int dim;
-char **v;
-
-  INDENT;
-  GetNextId( type, exprs);
-  GetNextId( a, exprs);
-  GetNextId( res, exprs);
-  GetNextInt( dim, exprs);
-  GetShape(dim, v, exprs);
-
-  INDENT;
-  CopyBlock(a, ConstVectToOffset(dim, v, a), res)
-
-  free(v);
-
-*/
-/*
-
- ND_KD_PSI_VxA_A_NEW( type, a, res, dim, v )       : selects a sub-array
-
-char *type, *a, *res, *v;
-int dim;
-
-  INDENT;
-  GetNextId( type, exprs);
-  GetNextId( a, exprs);
-  GetNextId( res, exprs);
-  GetNextInt( dim, exprs);
-  GetNextId( v, exprs);
-
-  INDENT;
-  fprintf(outfile, "ND_ALLOC_ARRAY(%s, %s);\n", type, res);
-  INDENT;
-  CopyBlock(a, VectToOffset(dim, v, a), res)
-
-  free(v);
-
-*/
-
-/*
-
- ND_KD_PSI_VxA_A_OLD( type, a, res, dim, v )       : selects a sub-array
-
-char *type, *a, *res, *v;
-int dim;
-
-  INDENT;
-  GetNextId( type, exprs);
-  GetNextId( a, exprs);
-  GetNextId( res, exprs);
-  GetNextInt( dim, exprs);
-  GetNextId( v, exprs);
-
-  INDENT;
-  CopyBlock(a, VectToOffset(dim, v, a), res)
-
-  free(v);
-
-*/
+#ifdef TEST_BACKEND
+    return (0);
+}
+#endif /* TEST_BACKEND */
