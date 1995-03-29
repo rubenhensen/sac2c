90c8193f6d8dbe3a1719a3637422757c176f54d7
Henning Wolf <info@sac-home.org>
1995-03-29 11:52:22 +0000

    macros for access to elements of struct info.ids inserted

--- src/global/access_macros.h
+++ src/global/access_macros.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/03/28 12:01:50  hw
+ * Revision 1.2  1995/03/29 11:52:22  hw
+ * macros for access to elements of struct info.ids inserted
+ *
+ * Revision 1.1  1995/03/28  12:01:50  hw
  * Initial revision
  *
  *
@@ -24,5 +27,10 @@
 /* macros used for N_ap nodes to get the function's name */
 #define FUN_NAME info.fun_name.id
 #define FUN_MOD_NAME info.fun_name.id_mod
+
+/* macros for access to elements of struct info.ids */
+#define IDS info.ids
+#define IDS_ID IDS->id
+#define IDS_NODE IDS->node
 
 #endif /* _access_macros_h */
