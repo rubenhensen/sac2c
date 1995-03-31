038ddad73753c847572047d3a12af3fe22ac8032
Henning Wolf <info@sac-home.org>
1995-03-31 15:47:37 +0000

    CompId, CompArg, CompWith, CompArray, CompAp, CompReturn inserted

--- src/compile/compile.h
+++ src/compile/compile.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/03/29 12:38:10  hw
+ * Revision 1.2  1995/03/31 15:47:37  hw
+ * CompId, CompArg, CompWith, CompArray, CompAp, CompReturn inserted
+ *
+ * Revision 1.1  1995/03/29  12:38:10  hw
  * Initial revision
  *
  *
@@ -16,5 +19,11 @@ extern node *CompVardec (node *arg_node, node *arg_info);
 extern node *CompPrf (node *arg_node, node *arg_info);
 extern node *CompAssign (node *arg_node, node *arg_info);
 extern node *CompLet (node *arg_node, node *arg_info);
+extern node *CompArray (node *arg_node, node *arg_info);
+extern node *CompId (node *arg_node, node *arg_info);
+extern node *CompAp (node *arg_node, node *arg_info);
+extern node *CompReturn (node *arg_node, node *arg_info);
+extern node *CompWith (node *arg_node, node *arg_info);
+extern node *CompArg (node *arg_node, node *arg_info);
 
 #endif /* _compile_h */
