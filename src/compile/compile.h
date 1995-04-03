87f2690a5d011bda8ab0c8cd3b45813f5d406160
Henning Wolf <info@sac-home.org>
1995-04-03 16:34:48 +0000

    CompFundef inserted

--- src/compile/compile.h
+++ src/compile/compile.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1995/03/31 15:47:37  hw
+ * Revision 1.3  1995/04/03 16:34:48  hw
+ * CompFundef inserted
+ *
+ * Revision 1.2  1995/03/31  15:47:37  hw
  * CompId, CompArg, CompWith, CompArray, CompAp, CompReturn inserted
  *
  * Revision 1.1  1995/03/29  12:38:10  hw
@@ -25,5 +28,6 @@ extern node *CompAp (node *arg_node, node *arg_info);
 extern node *CompReturn (node *arg_node, node *arg_info);
 extern node *CompWith (node *arg_node, node *arg_info);
 extern node *CompArg (node *arg_node, node *arg_info);
+extern node *CompFundef (node *arg_node, node *arg_info);
 
 #endif /* _compile_h */
