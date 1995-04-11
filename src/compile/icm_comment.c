9d1aaf2dc04158fe3f9e3a71b2a192f5735b5974
Sven-Bodo Scholz <sbs@sac-home.org>
1995-04-11 15:02:33 +0000

    \b\b eliminated in output

--- src/compile/icm_comment.c
+++ src/compile/icm_comment.c
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1995/04/03 13:58:57  sbs
+ * Revision 1.3  1995/04/11 15:02:33  sbs
+ * \b\b eliminated in output
+ *
+ * Revision 1.2  1995/04/03  13:58:57  sbs
  * first "complete" version
  *
  * Revision 1.1  1995/03/10  17:26:51  sbs
@@ -10,21 +13,37 @@
  *
  */
 
-#define ICM_DEF(prf) fprintf (outfile, "/*\n * %s( ", #prf);
-#define ICM_STR(name) fprintf (outfile, "%s, ", name);
-#define ICM_INT(name) fprintf (outfile, "%d, ", name);
+#define SEP                                                                              \
+    if (sep)                                                                             \
+        fprintf (outfile, ", ");
+
+#define ICM_DEF(prf)                                                                     \
+    {                                                                                    \
+        int sep = 0;                                                                     \
+        fprintf (outfile, "/*\n * %s( ", #prf);
+#define ICM_STR(name)                                                                    \
+    SEP;                                                                                 \
+    fprintf (outfile, "%s", name);                                                       \
+    sep = 1;
+#define ICM_INT(name)                                                                    \
+    SEP;                                                                                 \
+    fprintf (outfile, "%d", name);                                                       \
+    sep = 1;
 #define ICM_VAR(dim, name)                                                               \
     {                                                                                    \
         int i;                                                                           \
         for (i = 0; i < dim; i++) {                                                      \
+            SEP;                                                                         \
             AccessConst (name, i);                                                       \
-            fprintf (outfile, ", ");                                                     \
         }                                                                                \
     }
-#define ICM_END(prf) fprintf (outfile, "\b\b)\n */\n");
+#define ICM_END(prf)                                                                     \
+    fprintf (outfile, ")\n */\n");                                                       \
+    }
 
 #include "icm.data"
 
+#undef SEP
 #undef ICM_DEF
 #undef ICM_STR
 #undef ICM_INT
