5b4520fe6c9597f38661f67772f22c9a50d08e5b
Sven-Bodo Scholz <sbs@sac-home.org>
1995-05-22 12:06:51 +0000

    TRACE_MEM inserted

--- src/compile/trace.h
+++ src/compile/trace.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/05/04 11:48:35  sbs
+ * Revision 1.2  1995/05/22 12:06:51  sbs
+ * TRACE_MEM inserted
+ *
+ * Revision 1.1  1995/05/04  11:48:35  sbs
  * Initial revision
  *
  *
@@ -19,6 +22,7 @@ extern int traceflag; /* imported from main.c */
 #define TRACE_PRF 0x0002
 #define TRACE_WST 0x0004
 #define TRACE_REF 0x0008
+#define TRACE_MEM 0x0010
 #define TRACE_ALL 0xffff
 
 #endif /* _sac_trace_h */
