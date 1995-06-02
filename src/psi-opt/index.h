9cc94ecef308b2134b756575d8988fba7a8262f4
Sven-Bodo Scholz <sbs@sac-home.org>
1995-06-02 17:05:15 +0000

    Idxfuns external declared

--- src/psi-opt/index.h
+++ src/psi-opt/index.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/06/02 10:06:56  sbs
+ * Revision 1.2  1995/06/02 17:05:15  sbs
+ * Idxfuns external declared
+ *
+ * Revision 1.1  1995/06/02  10:06:56  sbs
  * Initial revision
  *
  *
@@ -11,6 +14,13 @@
 
 #define sac_index_h
 
-typedef node *(*chainmodfunptr) (node *);
+typedef node *(*chainmodfunptr) (node *, types *);
+
+extern node *IdxModul (node *, node *);
+extern node *IdxAssign (node *, node *);
+extern node *IdxLet (node *, node *);
+extern node *IdxPrf (node *, node *);
+extern node *IdxAp (node *, node *);
+extern node *IdxId (node *, node *);
 
 #endif /* sac_index_h */
