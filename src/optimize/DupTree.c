679a0cbef80634c321bf77f2a3b11344c31f7b4b
Arne Sievers <info@sac-home.org>
1995-06-02 11:25:48 +0000

    Added functions for all nodes below fundef node

--- src/optimize/DupTree.c
+++ src/optimize/DupTree.c
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1995/05/03 12:41:51  asi
+ * Revision 1.3  1995/06/02 11:25:48  asi
+ * Added functions for all nodes below fundef node
+ *
+ * Revision 1.2  1995/05/03  12:41:51  asi
  * added DupPrf, DupAp and DupIds
  *
  * Revision 1.1  1995/05/01  15:32:27  asi
@@ -16,9 +19,13 @@
 #include "my_debug.h"
 #include "traverse.h"
 #include "typecheck.h"
+#include "internal_lib.h"
 
 #include "DupTree.h"
 #include "optimize.h"
+#include "Inline.h"
+
+#define LEVEL arg_info->lineno
 
 node *
 DupTree (node *arg_node, node *arg_info)
@@ -31,6 +38,7 @@ DupTree (node *arg_node, node *arg_info)
     tmp_tab = act_tab;
     act_tab = dup_tab;
 
+    LEVEL = 0;
     new_node = Trav (arg_node, arg_info);
 
     act_tab = tmp_tab;
@@ -39,23 +47,12 @@ DupTree (node *arg_node, node *arg_info)
 }
 
 node *
-DupNum (node *arg_node, node *arg_info)
+DupInt (node *arg_node, node *arg_info)
 {
     node *new_node;
 
-    DBUG_ENTER ("DupNum");
-    new_node = MakeNode (N_num);
-    new_node->info.cint = arg_node->info.cint;
-    DBUG_RETURN (new_node);
-}
-
-node *
-DupBool (node *arg_node, node *arg_info)
-{
-    node *new_node;
-
-    DBUG_ENTER ("DupBool");
-    new_node = MakeNode (N_bool);
+    DBUG_ENTER ("DupInt");
+    new_node = MakeNode (arg_node->nodetype);
     new_node->info.cint = arg_node->info.cint;
     DBUG_RETURN (new_node);
 }
@@ -66,8 +63,19 @@ DupFloat (node *arg_node, node *arg_info)
     node *new_node;
 
     DBUG_ENTER ("DupFloat");
-    new_node = MakeNode (N_float);
+    new_node = MakeNode (arg_node->nodetype);
     new_node->info.cfloat = arg_node->info.cfloat;
+    DBUG_RETURN (new_node);
+}
+
+node *
+DupStr (node *arg_node, node *arg_info)
+{
+    node *new_node;
+
+    DBUG_ENTER ("DupStr");
+    new_node = MakeNode (arg_node->nodetype);
+    new_node->info.id = StringCopy (arg_node->info.id);
     DBUG_RETURN (new_node);
 }
 
@@ -77,37 +85,30 @@ DupIds (ids *old_ids, node *arg_info)
     ids *new_ids;
 
     DBUG_ENTER ("DupIds");
-    new_ids = MakeIds (strdup (old_ids->id));
-    new_ids->node = old_ids->node;
-    if (NULL != arg_info) {
-        INC_VAR (arg_info->mask[0], old_ids->node->varno);
+    switch (DUPTYPE) {
+    case INLINE:
+        new_ids = MakeIds (RenameInlinedVar (old_ids->id));
+        new_ids = SetDeclPtr (new_ids, arg_info);
+        break;
+    default:
+        new_ids = MakeIds (strdup (old_ids->id));
+        new_ids->node = old_ids->node;
+        break;
     }
-    if (NULL != old_ids->next) {
+    if (NULL != old_ids->next)
         old_ids->next = DupIds (old_ids->next, arg_info);
-    }
     DBUG_RETURN (new_ids);
 }
 
 node *
-DupId (node *arg_node, node *arg_info)
-{
-    node *new_node;
-
-    DBUG_ENTER ("DupId");
-    new_node = MakeNode (N_id);
-    new_node->info.ids = DupIds (arg_node->info.ids, arg_info);
-    new_node->node[0] = arg_node->node[0];
-    DBUG_RETURN (new_node);
-}
-
-node *
-DupArray (node *arg_node, node *arg_info)
+DupIIds (node *arg_node, node *arg_info)
 {
     node *new_node;
     int i;
 
-    DBUG_ENTER ("DupArray");
-    new_node = MakeNode (N_array);
+    DBUG_ENTER ("DupIIds");
+    new_node = MakeNode (arg_node->nodetype);
+    new_node->info.ids = DupIds (arg_node->info.ids, arg_info);
     new_node->nnode = arg_node->nnode;
     for (i = 0; i < arg_node->nnode; i++) {
         new_node->node[i] = Trav (arg_node->node[i], arg_info);
@@ -116,16 +117,43 @@ DupArray (node *arg_node, node *arg_info)
 }
 
 node *
-DupExprs (node *arg_node, node *arg_info)
+DupChain (node *arg_node, node *arg_info)
 {
     node *new_node;
     int i;
 
-    DBUG_ENTER ("DupExprs");
-    new_node = MakeNode (N_exprs);
+    DBUG_ENTER ("DupChain");
+    new_node = MakeNode (arg_node->nodetype);
     new_node->nnode = arg_node->nnode;
     for (i = 0; i < arg_node->nnode; i++) {
+        LEVEL++;
         new_node->node[i] = Trav (arg_node->node[i], arg_info);
+        LEVEL--;
+    }
+    DBUG_RETURN (new_node);
+}
+
+node *
+DupAssign (node *arg_node, node *arg_info)
+{
+    node *new_node = NULL;
+    int i;
+
+    DBUG_ENTER ("DupAssign");
+    switch (DUPTYPE) {
+    case INLINE:
+        if ((0 == LEVEL) && (N_return == arg_node->node[0]->nodetype))
+            break;
+    default:
+        new_node = MakeNode (arg_node->nodetype);
+        new_node->nnode = arg_node->nnode;
+
+        for (i = 0; i < arg_node->nnode; i++) {
+            new_node->node[i] = Trav (arg_node->node[i], arg_info);
+            if (NULL == new_node->node[i])
+                new_node->nnode = i;
+        }
+        break;
     }
     DBUG_RETURN (new_node);
 }
@@ -137,7 +165,7 @@ DupCast (node *arg_node, node *arg_info)
     int i;
 
     DBUG_ENTER ("DupCast");
-    new_node = MakeNode (N_cast);
+    new_node = MakeNode (arg_node->nodetype);
     new_node->info.types = DuplicateTypes (arg_node->info.types);
     new_node->nnode = arg_node->nnode;
     for (i = 0; i < arg_node->nnode; i++) {
@@ -153,7 +181,7 @@ DupPrf (node *arg_node, node *arg_info)
     int i;
 
     DBUG_ENTER ("DupPrf");
-    new_node = MakeNode (N_prf);
+    new_node = MakeNode (arg_node->nodetype);
     new_node->info.prf = arg_node->info.prf;
     new_node->nnode = arg_node->nnode;
     for (i = 0; i < arg_node->nnode; i++) {
@@ -163,13 +191,13 @@ DupPrf (node *arg_node, node *arg_info)
 }
 
 node *
-DupAp (node *arg_node, node *arg_info)
+DupFun (node *arg_node, node *arg_info)
 {
     node *new_node;
     int i;
 
-    DBUG_ENTER ("DupAp");
-    new_node = MakeNode (N_ap);
+    DBUG_ENTER ("DupFun");
+    new_node = MakeNode (arg_node->nodetype);
     new_node->info.fun_name.id = strdup (arg_node->info.fun_name.id);
     new_node->info.fun_name.id_mod = strdup (arg_node->info.fun_name.id_mod);
     new_node->nnode = arg_node->nnode;
--- src/optimize/DupTree.h
+++ src/optimize/DupTree.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1995/05/03 12:41:51  asi
+ * Revision 1.3  1995/06/02 11:25:48  asi
+ * Added functions for all nodes below fundef node
+ *
+ * Revision 1.2  1995/05/03  12:41:51  asi
  * added DupPrf, DupAp and DupIds
  *
  * Revision 1.1  1995/05/01  15:32:32  asi
@@ -13,17 +16,21 @@
 
 #define _DupTree_h
 
+#define DUPTYPE arg_info->flag
+#define NORMAL 0
+#define INLINE 1
+
 extern node *DupTree (node *arg_node, node *arg_info);
 
-extern node *DupNum (node *arg_node, node *arg_info);
-extern node *DupBool (node *arg_node, node *arg_info);
+extern node *DupInt (node *arg_node, node *arg_info);
 extern node *DupFloat (node *arg_node, node *arg_info);
+extern node *DupStr (node *arg_node, node *arg_info);
 extern ids *DupIds (ids *ids, node *arg_info);
-extern node *DupId (node *arg_node, node *arg_info);
-extern node *DupArray (node *arg_node, node *arg_info);
-extern node *DupExprs (node *arg_node, node *arg_info);
+extern node *DupIIds (node *arg_node, node *arg_info);
+extern node *DupChain (node *arg_node, node *arg_info);
+extern node *DupAssign (node *arg_node, node *arg_info);
 extern node *DupCast (node *arg_node, node *arg_info);
 extern node *DupPrf (node *arg_node, node *arg_info);
-extern node *DupAp (node *arg_node, node *arg_info);
+extern node *DupFun (node *arg_node, node *arg_info);
 
 #endif /* _DupTree_h */
