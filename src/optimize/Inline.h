d3a390d867fe2f273eec296b396966f95763c104
Arne Sievers <info@sac-home.org>
1995-06-08 10:03:44 +0000

    added SearchDecl and removed SetDeclPtr

--- src/optimize/Inline.h
+++ src/optimize/Inline.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1995/06/02 11:29:35  asi
+ * Revision 1.3  1995/06/08 10:03:44  asi
+ * added SearchDecl and removed SetDeclPtr
+ *
+ * Revision 1.2  1995/06/02  11:29:35  asi
  * Added Inline, INLfundef, INLblock, INLassign, RenameInlinedVar, SetDeclPtr and INLvar.
  *
  * Revision 1.1  1995/05/26  14:22:18  asi
@@ -14,13 +17,17 @@
 
 #define _Inline_h
 
+#define TYPES arg_info->node[2]
+
 extern node *Inline (node *arg_node, node *arg_info);
 
+extern node *INLmodul (node *arg_node, node *arg_info);
 extern node *INLfundef (node *arg_node, node *arg_info);
 extern node *INLassign (node *arg_node, node *arg_info);
 extern node *INLvar (node *arg_node, node *arg_info);
 extern node *INLblock (node *arg_node, node *arg_info);
 
-extern char *RenameInlinedVar (char *name);
-extern ids *SetDeclPtr (ids *ids_node, node *arg_info);
+extern node *SearchDecl (char *name, node *decl_node);
+extern char *RenameInlinedVar (char *old_name);
+
 #endif /* _Inline_h */
