8bc7aa17c5f638ee1eb0bb460756edd9651f0470
Arne Sievers <info@sac-home.org>
1995-06-26 16:24:38 +0000

    added UNRfundef and enhanced Unroll

--- src/optimize/Unroll.c
+++ src/optimize/Unroll.c
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1995/06/14 13:31:23  asi
+ * Revision 1.3  1995/06/26 16:24:38  asi
+ * added UNRfundef and enhanced Unroll
+ *
+ * Revision 1.2  1995/06/14  13:31:23  asi
  * added Unroll, UNRdo, UNRwhile and UNRassign
  *
  * Revision 1.1  1995/05/26  14:22:26  asi
@@ -14,6 +17,7 @@
 #include <stdlib.h>
 
 #include "tree.h"
+#include "free.h"
 #include "Error.h"
 #include "dbug.h"
 #include "my_debug.h"
@@ -21,11 +25,12 @@
 
 #include "optimize.h"
 #include "ConstantFolding.h"
+#include "DupTree.h"
 #include "Unroll.h"
 
 /*
  *
- *  functionname  :
+ *  functionname  : Unroll
  *  arguments     :
  *  description   :
  *  global vars   :
@@ -37,17 +42,65 @@
  *
  */
 node *
-Unroll (node *arg_node, node *info_node)
+Unroll (node *arg_node, node *arg_info)
 {
     funptr *tmp_tab;
 
     DBUG_ENTER ("Unroll");
     tmp_tab = act_tab;
     act_tab = unroll_tab;
+    arg_info = MakeNode (N_info);
+    def_stack = MAlloc (sizeof (stack));
+    def_stack->tos = -1;
+    def_stack->st_len = MIN_STACK_SIZE;
+    def_stack->stack = (stelm *)MAlloc (sizeof (stelm) * MIN_STACK_SIZE);
 
-    arg_node = Trav (arg_node, info_node);
+    arg_node = Trav (arg_node, arg_info);
 
+    FREE (arg_info);
+    FREE (def_stack->stack);
+    FREE (def_stack);
     act_tab = tmp_tab;
+    DBUG_RETURN (arg_node);
+}
+/*
+ *
+ *  functionname  : UNRfundef
+ *  arguments     : 1) fundef-node
+ *                  2) NULL
+ *                  R) fundef-node with unrolled loops in body of function
+ *  description   : - generates info_node
+ *                  - varno of the info_node will be set to the number of local variables
+ *                      and arguments in this functions
+ *                  - new entry will be pushed on the def_stack
+ *                  - generates two masks and links them to the info_node
+ *                      [0] - variables additional defined in function and
+ *                      [1] - variables additional used in function after optimization
+ *                  - calls Trav to  unrolled loops in body of current function
+ *                  - last entry will be poped from def_stack
+ *                  - updates masks in fundef node.
+ *  global vars   : syntax_tree, def_stack
+ *  internal funs : PushVL, PopVL
+ *  external funs : GenMask, MinusMask, OptTrav
+ *  macros        : DBUG...
+ *
+ *  remarks       : --
+ *
+ */
+node *
+UNRfundef (node *arg_node, node *arg_info)
+{
+    DBUG_ENTER ("UNRfundef");
+
+    DBUG_PRINT ("UNR", ("Unroll in function: %s", arg_node->info.types->id));
+    VARNO = arg_node->varno;
+    PushVL (arg_info->varno);
+
+    arg_node = OptTrav (arg_node, arg_info, 0); /* functionbody */
+
+    PopVL ();
+
+    arg_node = OptTrav (arg_node, arg_info, 1); /* next function */
     DBUG_RETURN (arg_node);
 }
 
@@ -55,8 +108,20 @@ node *
 UNRdo (node *arg_node, node *arg_info)
 {
     DBUG_ENTER ("UNRdo");
-
     DBUG_RETURN (arg_node);
+    /*
+      node *unroll;
+      node *res;
+
+      DBUG_ENTER("UNRdo");
+        res =
+        DBUG_PRINT("UNR",("Unrolling do-loop in line %d",arg_node->lineno));
+        unroll = DupTree(arg_node->node[1]->node[0], NULL);
+        unroll = GenerateMasks(unroll, arg_info);
+        FreeTree(arg_node);
+        unr_expr++;
+      DBUG_RETURN(unroll);
+    */
 }
 
 node *
