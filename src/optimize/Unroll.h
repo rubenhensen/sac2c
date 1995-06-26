ff7b465508eeea5697fa2d7984b20d3798796dd6
Arne Sievers <info@sac-home.org>
1995-06-26 16:24:38 +0000

    added UNRfundef

--- src/optimize/Unroll.h
+++ src/optimize/Unroll.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.3  1995/06/14 13:31:23  asi
+ * Revision 1.4  1995/06/26 16:24:38  asi
+ * added UNRfundef
+ *
+ * Revision 1.3  1995/06/14  13:31:23  asi
  * added Unroll, UNRdo, UNRwhile and UNRassign
  *
  * Revision 1.2  1995/06/02  11:32:51  asi
@@ -19,6 +22,7 @@
 
 extern node *Unroll (node *arg_node, node *arg_info);
 
+extern node *UNRfundef (node *arg_node, node *arg_info);
 extern node *UNRdo (node *arg_node, node *arg_info);
 extern node *UNRwhile (node *arg_node, node *arg_info);
 extern node *UNRassign (node *arg_node, node *arg_info);
