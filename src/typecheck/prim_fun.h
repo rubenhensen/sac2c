43946a2bad48e8a47fcf6e9b0abd907d6c783e6a
Henning Wolf <info@sac-home.org>
1995-06-30 11:35:12 +0000

    - #if 0 .. deleted - functions F2I & I2F inserted

--- src/typecheck/prim_fun.h
+++ src/typecheck/prim_fun.h
@@ -1,9 +1,13 @@
 /*
  *  $Log$
- *  Revision 1.3  1995/02/09 11:07:52  hw
- *   - changed function AxA
- *   - renamed function Ixf_F to Axs_f
- *   - added new functions: Shp, Reshp, TakeV, DropV, Psi, TakeDropS, Rot, Cat
+ *  Revision 1.4  1995/06/30 11:35:12  hw
+ *  - #if 0 .. deleted
+ *  - functions F2I & I2F inserted
+ *
+ * Revision 1.3  1995/02/09  11:07:52  hw
+ *  - changed function AxA
+ *  - renamed function Ixf_F to Axs_f
+ *  - added new functions: Shp, Reshp, TakeV, DropV, Psi, TakeDropS, Rot, Cat
  *
  * Revision 1.2  1995/02/03  16:04:18  hw
  * added new functions AxA & Ixf_F
@@ -24,24 +28,12 @@ typedef struct PRIM_FUN_TAB_ELEM {
                     *  primitive function is defined
                     */
     node *node;    /* pointer to declaration (function header) */
-    int typed_tag; /* tag weather function is typechecked */
-    int user_tag;  /* tag weather function is userdefined */
+    int typed_tag; /* tag whether function is typechecked */
+    int user_tag;  /* tag whether function is userdefined */
     prf new_prf;
     struct PRIM_FUN_TAB_ELEM *next;
 
 } prim_fun_tab_elem;
-
-#if 0
-
-#define TT2(t, t_c, t1, t2, res) t,
-typedef enum 
-{
-#include "prim_fun_tt.mac" 
-xxxxx /* xxxxx is only a dummy argument */
-}type_class_tag;
-#undef TT2
-
-#endif
 
 extern prim_fun_tab_elem *prim_fun_tab;
 extern void InitPrimFunTab ();
@@ -55,8 +47,10 @@ extern types *Psi (types *vec, types *array);
 extern types *TakeDropS (node *vec, types *array, int tag);
 extern types *Cat (node *s_node, types *array1, types *array2);
 extern types *Rot (node *s_node, types *array);
+extern types *F2I (types *array1);
+extern types *I2F (types *array1);
 
-#if 0
+#if 0 
 extern types *Psi(types *array1, types *array2);
 #endif
 
