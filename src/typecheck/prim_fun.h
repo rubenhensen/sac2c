ce793d057c926360daaae0f786fa7ab8e996ad32
Henning Wolf <info@sac-home.org>
1995-07-04 09:06:30 +0000

    - Axs_F, F2I & I2F removed - ConvertType inserted

--- src/typecheck/prim_fun.h
+++ src/typecheck/prim_fun.h
@@ -1,8 +1,12 @@
 /*
  *  $Log$
- *  Revision 1.4  1995/06/30 11:35:12  hw
- *  - #if 0 .. deleted
- *  - functions F2I & I2F inserted
+ *  Revision 1.5  1995/07/04 09:06:30  hw
+ *  - Axs_F, F2I & I2F removed
+ *  - ConvertType inserted
+ *
+ * Revision 1.4  1995/06/30  11:35:12  hw
+ * - #if 0 .. deleted
+ * - functions F2I & I2F inserted
  *
  * Revision 1.3  1995/02/09  11:07:52  hw
  *  - changed function AxA
@@ -38,7 +42,6 @@ typedef struct PRIM_FUN_TAB_ELEM {
 extern prim_fun_tab_elem *prim_fun_tab;
 extern void InitPrimFunTab ();
 extern types *AxA (types *array1, types *array2, simpletype s_type);
-extern types *Axs_F (types *array1);
 extern types *Reshp (node *vec, types *array);
 extern types *Shp (types *array);
 extern types *TakeV (node *vec, types *array);
@@ -47,11 +50,11 @@ extern types *Psi (types *vec, types *array);
 extern types *TakeDropS (node *vec, types *array, int tag);
 extern types *Cat (node *s_node, types *array1, types *array2);
 extern types *Rot (node *s_node, types *array);
-extern types *F2I (types *array1);
-extern types *I2F (types *array1);
+extern types *ConvertType (types *array1, simpletype s_type);
 
 #if 0 
 extern types *Psi(types *array1, types *array2);
+extern types *Axs_F(types *array1);
 #endif
 
 #endif /* _prim_fun_h */
