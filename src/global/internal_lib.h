28c458fb86122f6f6aa2964340e9b2d1458acfca
Arne Sievers <info@sac-home.org>
1995-07-14 17:04:31 +0000

    added macro SWAP, it swaps two pointers

--- src/global/internal_lib.h
+++ src/global/internal_lib.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/03/28 12:01:50  hw
+ * Revision 1.2  1995/07/14 17:04:31  asi
+ * added macro SWAP, it swaps two pointers
+ *
+ * Revision 1.1  1995/03/28  12:01:50  hw
  * Initial revision
  *
  *
@@ -13,5 +16,13 @@
 
 void *Malloc (int size);
 char *StringCopy (char *source);
+
+#define SWAP(ptr1, ptr2)                                                                 \
+    {                                                                                    \
+        void *tmp;                                                                       \
+        tmp = (void *)ptr1;                                                              \
+        ptr1 = (void *)ptr2;                                                             \
+        ptr2 = (void *)tmp;                                                              \
+    }
 
 #endif /* _internal_lib_h */
