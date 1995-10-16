f63960fdb4c229c98a6c299a96265b50f81f296a
Clemens Grelck <cg@sac-home.org>
1995-10-16 12:22:44 +0000

    Initial revision

---
+++ src/objects/objinit.c
@@ -0,0 +1,158 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  1995/10/16 12:22:44  cg
+ * Initial revision
+ *
+ *
+ *
+ */
+
+#include <string.h>
+#include <malloc.h>
+
+#include "types.h"
+#include "tree_basic.h"
+#include "tree_compound.h"
+
+#include "traverse.h"
+#include "dbug.h"
+#include "Error.h"
+
+/*
+ *
+ *  functionname  : OImodul
+ *  arguments     : 1) pointer to N_modul node
+ *                  2) arg_info unused
+ *  description   : traverses all global object definitions
+ *  global vars   : ---
+ *  internal funs : ---
+ *  external funs : Trav
+ *  macros        : ---
+ *
+ *  remarks       :
+ *
+ */
+
+node *
+OImodul (node *arg_node, node *arg_info)
+{
+    DBUG_ENTER ("OImodul");
+
+    if (MODUL_OBJS (arg_node) != NULL) {
+        MODUL_OBJS (arg_node) = Trav (MODUL_OBJS (arg_node), arg_node);
+    }
+
+    DBUG_RETURN (arg_node);
+}
+
+/*
+ *
+ *  functionname  : OIobjdef
+ *  arguments     : 1) pointer to objdef node
+ *                  2) pointer to modul node (head of syntax_tree)
+ *  description   : A new function definition is generated that has no
+ *                  formal parameters, the return type is identical to
+ *                  the type of the global object and the initialization
+ *                  expression is moved to the return-statement of the
+ *                  new function. In the objdef node it's replaced by
+ *                  an application of the new function.
+ *
+ *                  This mechanism allows for arbitrary expression to
+ *                  initialize global objects.
+ *  global vars   : ---
+ *  internal funs : ---
+ *  external funs : malloc, strlen, strcpy, strcat,
+ *                  MakeType, MakeExprs, MakeReturn, MakeAssign, MakeBlock,
+ *                  MakeFundef, MakeAp, Trav
+ *  macros        : ERROR2
+ *
+ *  remarks       :
+ *
+ */
+
+node *
+OIobjdef (node *arg_node, node *arg_info)
+{
+    node *new_node;
+    types *new_fun_type;
+    char *new_fun_name;
+
+    DBUG_ENTER ("OIobjdef");
+
+    new_fun_type = MakeType (OBJDEF_BASETYPE (arg_node), OBJDEF_DIM (arg_node),
+                             OBJDEF_SHPSEG (arg_node), OBJDEF_TNAME (arg_node),
+                             OBJDEF_TMOD (arg_node));
+
+    new_fun_name = (char *)malloc (strlen (OBJDEF_NAME (arg_node)) + 10);
+    if (new_fun_name == NULL) {
+        ERROR2 (1, ("ERROR: out of memory"));
+    }
+
+    new_fun_name = strcpy (new_fun_name, "_CREATE_");
+    new_fun_name = strcat (new_fun_name, OBJDEF_NAME (arg_node));
+
+    new_node = MakeExprs (OBJDEF_EXPR (arg_node), NULL);
+
+    /*------------------------------------------------------------------*/
+    new_node->nnode = 1;
+    /*------------------------------------------------------------------*/
+
+    new_node = MakeReturn (new_node);
+    new_node = MakeAssign (new_node, NULL);
+
+    /*------------------------------------------------------------------*/
+    new_node->nnode = 1;
+    /*------------------------------------------------------------------*/
+
+    new_node = MakeBlock (new_node, NULL);
+
+    /*------------------------------------------------------------------*/
+    new_node->nnode = 1;
+    /*------------------------------------------------------------------*/
+
+    new_node = MakeFundef (new_fun_name, OBJDEF_MOD (arg_node), NULL, new_fun_type, NULL,
+                           new_node, MODUL_FUNS (arg_info));
+
+    /*------------------------------------------------------------------*/
+    new_node->nnode = (MODUL_FUNS (arg_info) == NULL) ? 1 : 2;
+    /*------------------------------------------------------------------*/
+
+    MODUL_FUNS (arg_info) = new_node;
+
+    new_node = MakeAp (new_fun_name, OBJDEF_MOD (arg_node), NULL);
+    OBJDEF_EXPR (arg_node) = new_node;
+
+    if (OBJDEF_NEXT (arg_node) != NULL) {
+        OBJDEF_NEXT (arg_node) = Trav (OBJDEF_NEXT (arg_node), arg_info);
+    }
+
+    DBUG_RETURN (arg_node);
+}
+
+/*
+ *
+ *  functionname  : objinit
+ *  arguments     : 1) syntax_tree (pointer to N_modul node
+ *  description   : starts the traversal mechanism for the objinit
+ *                  compilation phase. Here, all initialization expressions
+ *                  of global objects are moved to new generated functions
+ *                  and replaced by applications of the respective functions.
+ *  global vars   : act_tab, objinit_tab
+ *  internal funs : ---
+ *  external funs : Trav
+ *  macros        :
+ *
+ *  remarks       :
+ *
+ */
+
+node *
+objinit (node *syntax_tree)
+{
+    DBUG_ENTER ("objinit");
+
+    act_tab = objinit_tab;
+
+    DBUG_RETURN (Trav (syntax_tree, NULL));
+}
---
+++ src/objects/objinit.h
@@ -0,0 +1,15 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  1995/10/16 12:22:44  cg
+ * Initial revision
+ *
+ *
+ *
+ */
+
+extern node *OImodul (node *arg_node, node *arg_info);
+
+extern node *OIobjdef (node *arg_node, node *arg_info);
+
+extern node *objinit (node *syntax_tree);
