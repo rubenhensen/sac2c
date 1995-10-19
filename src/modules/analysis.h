8b775f5ea79adbeaa2dc125684f335f8a15b4eab
Clemens Grelck <cg@sac-home.org>
1995-10-19 14:31:06 +0000

    renamed function analysis to Analysis

--- src/modules/analysis.h
+++ src/modules/analysis.h
@@ -1,14 +1,17 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/10/19 11:04:05  cg
+ * Revision 1.2  1995/10/19 14:31:06  cg
+ * renamed function analysis to Analysis
+ *
+ * Revision 1.1  1995/10/19  11:04:05  cg
  * Initial revision
  *
  *
  *
  */
 
-extern node *analysis (node *syntaxtree);
+extern node *Analysis (node *syntaxtree);
 extern node *ANAmodul (node *arg_node, node *arg_info);
 extern node *ANAfundef (node *arg_node, node *arg_info);
 extern node *ANAvardec (node *arg_node, node *arg_info);
