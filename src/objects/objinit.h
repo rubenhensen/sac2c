70188a677548b9a8c59b5402f3ca7602759d7925
Clemens Grelck <cg@sac-home.org>
1995-10-31 17:19:07 +0000

    Now, multiple includes of this header file are avoided.

--- src/objects/objinit.h
+++ src/objects/objinit.h
@@ -1,15 +1,22 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/10/16 12:22:44  cg
+ * Revision 1.2  1995/10/31 17:19:07  cg
+ * Now, multiple includes of this header file are avoided.
+ *
+ * Revision 1.1  1995/10/16  12:22:44  cg
  * Initial revision
  *
  *
  *
  */
 
-extern node *OImodul (node *arg_node, node *arg_info);
+#ifndef _sac_objinit_h
+#define _sac_objinit_h
 
+extern node *OImodul (node *arg_node, node *arg_info);
 extern node *OIobjdef (node *arg_node, node *arg_info);
 
 extern node *objinit (node *syntax_tree);
+
+#endif /* _sac_objinit_h  */
