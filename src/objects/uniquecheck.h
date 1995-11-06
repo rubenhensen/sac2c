f365ef79003c41d79e0f93ab5b6379dfdcaf1c7b
Clemens Grelck <cg@sac-home.org>
1995-11-06 08:14:10 +0000

    Initial revision

---
+++ src/objects/uniquecheck.h
@@ -0,0 +1,28 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  1995/11/06 08:14:10  cg
+ * Initial revision
+ *
+ *
+ *
+ */
+
+#ifndef _sac_uniquecheck_h
+#define _sac_uniquecheck_h
+
+extern node *UniquenessCheck (node *syntax_tree);
+
+extern node *UNQmodul (node *arg_node, node *arg_info);
+extern node *UNQfundef (node *arg_node, node *arg_info);
+extern node *UNQblock (node *arg_node, node *arg_info);
+extern node *UNQvardec (node *arg_node, node *arg_info);
+extern node *UNQarg (node *arg_node, node *arg_info);
+extern node *UNQlet (node *arg_node, node *arg_info);
+extern node *UNQid (node *arg_node, node *arg_info);
+extern node *UNQdo (node *arg_node, node *arg_info);
+extern node *UNQwhile (node *arg_node, node *arg_info);
+extern node *UNQcond (node *arg_node, node *arg_info);
+extern node *UNQwith (node *arg_node, node *arg_info);
+
+#endif /*  _sac_uniquecheck_h  */
