ae5d6220c6cdcb308eb12f8d4a87af2b11f6e4cc
Clemens Grelck <cg@sac-home.org>
1995-11-10 14:59:22 +0000

    new functions ProcessErrorMessage and NumberOfDigit, used by the revised macros in Error.h some further global variables for error handling and layout formatting.

--- src/global/Error.c
+++ src/global/Error.c
@@ -1,7 +1,12 @@
 /*
  *
  * $Log$
- * Revision 1.8  1995/10/19 10:04:45  cg
+ * Revision 1.9  1995/11/10 14:59:22  cg
+ * new functions ProcessErrorMessage and NumberOfDigit,
+ * used by the revised macros in Error.h
+ * some further global variables for error handling and layout formatting.
+ *
+ * Revision 1.8  1995/10/19  10:04:45  cg
  * new function ItemName for convenient output of combined names
  * of types, functions, or global objects.
  *
@@ -41,19 +46,48 @@
 #include "dbug.h"
 #include "internal_lib.h"
 
+#include "Error.h"
+
 #define PUTC_STDERR(c) putc ((c), stderr)
 
-int errors = 0;   /* counter for number of errors */
-int warnings = 0; /* counter for number of warnings */
-int silent = 0;
+int errors = 0;         /* counter for number of errors */
+int warnings = 0;       /* counter for number of warnings */
+int verbose_level = 3;  /* controls compile time output   */
 int compiler_phase = 1; /* counter for compilation phases */
+
+int message_indent = 0;
+int last_indent = 0;
+int current_line_length;
+
+int silent = 0; /* only needed for asi-compatibility */
+
+char error_message_buffer[MAX_ERROR_MESSAGE_LENGTH];
+
+char *compiler_phase_name[] = {"",
+                               "Loading SAC program",
+                               "Resolving global object initializations",
+                               "Resolving imports from modules and classes",
+                               "Flattening source code expressions",
+                               "Running type inference system",
+                               "Checking module/class declaration file",
+                               "Resolving implicit types",
+                               "Analysing functions and global objects",
+                               "Generating SAC-Information-Block",
+                               "Resolving global objects and reference parameters",
+                               "Checking uniqueness property of objects",
+                               "Running SAC optimizations",
+                               "Running PSI optimizations",
+                               "Running reference count inference system",
+                               "Generating C-code",
+                               "Generating C-compiler call",
+                               "Unknown compiler phase"};
 
 /*
  *
  *  functionname  : DoPrint
  *  arguments     : 1) format string like the one of printf
  *                  2) ... arguments of format string
- *  description   : prints a error message on stderr
+ *  description   : prints an error message on stderr
  *
  *  global vars   : ---
  *  internal funs : ---
@@ -71,11 +105,104 @@ DoPrint (char *format, ...)
     va_list arg_p;
 
     DBUG_ENTER ("DoPrint");
-    va_start (arg_p, format);
 
+    va_start (arg_p, format);
     vfprintf (stderr, format, arg_p);
     va_end (arg_p);
+
     DBUG_VOID_RETURN;
+}
+
+/*
+ *
+ *  functionname  : ProcessErrorMessage
+ *  arguments     : 1) format string
+ *                  2) variable number of arguments similar to printf
+ *  description   : evaluates the format string with the given arguments
+ *                  and splits it into single lines using @ as delimiter.
+ *  global vars   : error_message_buffer, current_line_length
+ *  internal funs : ---
+ *  external funs : vsprintf, va_start, va_end
+ *  macros        : DBUG, MAX_ERROR_MESSAGE_LENGTH
+ *
+ *  remarks       :
+ *
+ */
+
+void
+ProcessErrorMessage (char *format, ...)
+{
+    va_list arg_p;
+    int index, line, last_space;
+
+    DBUG_ENTER ("ProcessErrorMessage");
+
+    va_start (arg_p, format);
+    vsprintf (error_message_buffer, format, arg_p);
+    va_end (arg_p);
+
+    index = 0;
+    last_space = 0;
+    line = 0;
+
+    while ((index < MAX_ERROR_MESSAGE_LENGTH) && (error_message_buffer[index] != '\0')) {
+        DBUG_ASSERT (error_message_buffer[index] != '\t',
+                     "TABs not allowed in error messages, use SPACEs !");
+
+        if (error_message_buffer[index] == ' ') {
+            last_space = index;
+        }
+
+        if (error_message_buffer[index] == '\n') {
+            error_message_buffer[index] = '@';
+            line = 0;
+        } else {
+            if (line == current_line_length) {
+                if (error_message_buffer[last_space] == ' ') {
+                    error_message_buffer[last_space] = '@';
+                    line = index - last_space;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        index++;
+        line++;
+    }
+
+    DBUG_PRINT ("ERROR", ("processed message: %s", error_message_buffer));
+
+    DBUG_VOID_RETURN;
+}
+
+/*
+ *
+ *  functionname  : NumberOfDigits
+ *  arguments     : 1) integer
+ *  description   : returns the number of digits, e.g 4 for 1000
+ *  global vars   : ---
+ *  internal funs : ---
+ *  external funs : ---
+ *  macros        : DBUG
+ *
+ *  remarks       :
+ *
+ */
+
+int
+NumberOfDigits (int number)
+{
+    int i = 1;
+
+    DBUG_ENTER ("NumberOfDigits");
+
+    while (number / 10 >= 1) {
+        number = number / 10;
+        i += 1;
+    }
+
+    DBUG_RETURN (i);
 }
 
 /*
