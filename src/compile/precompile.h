c4de6773f673807c6e82d59c35e2d630d08e8fe5
Clemens Grelck <cg@sac-home.org>
1995-12-01 20:29:00 +0000

    added declarations of PRECvardec and PRECtypedef

--- src/compile/precompile.h
+++ src/compile/precompile.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/11/28 12:23:34  cg
+ * Revision 1.2  1995/12/01 20:29:00  cg
+ * added declarations of PRECvardec and PRECtypedef
+ *
+ * Revision 1.1  1995/11/28  12:23:34  cg
  * Initial revision
  *
  *
@@ -23,5 +26,7 @@ extern node *PRECap (node *arg_node, node *arg_info);
 extern node *PRECreturn (node *arg_node, node *arg_info);
 extern node *PRECexprs (node *arg_node, node *arg_info);
 extern node *PRECid (node *arg_node, node *arg_info);
+extern node *PRECvardec (node *arg_node, node *arg_info);
+extern node *PRECtypedef (node *arg_node, node *arg_info);
 
 #endif /* _sac_precompile_h */
