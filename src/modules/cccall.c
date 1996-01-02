90cda49d373f8af51c0d9aec3158ecbc957f0b91
Clemens Grelck <cg@sac-home.org>
1996-01-02 07:57:55 +0000

    first working revision

--- src/modules/cccall.c
+++ src/modules/cccall.c
@@ -1,12 +1,17 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/12/29 17:19:26  cg
+ * Revision 1.2  1996/01/02 07:57:37  cg
+ * first working revision
+ *
+ * Revision 1.1  1995/12/29  17:19:26  cg
  * Initial revision
  *
  *
  *
  */
+
+#include <stdio.h>
 
 #include "types.h"
 #include "tree_basic.h"
@@ -14,13 +19,24 @@
 
 #include "Error.h"
 #include "dbug.h"
+#include "internal_lib.h"
+#include "free.h"
+#include "globals.h"
+#include "scnprs.h"
 
+#include "filemgr.h"
 #include "traverse.h"
 
 static strings *linklist = NULL;
 static strings *ofilelist = NULL;
 static strings *afilelist = NULL;
 
+#define MAKESYSCALL                                                                      \
+    NOTE (("%s", systemcall));                                                           \
+    system (systemcall);                                                                 \
+    DBUG_EXECUTE ("SYSSTEP", printf ("\nHit key to continue\n");                         \
+                  scanf ("%s", systemcall););
+
 /*
  *
  *  functionname  :
@@ -48,23 +64,177 @@ static strings *afilelist = NULL;
  *  remarks       :
  *
  */
-NOTE_COMPILER_PHASE;
-if (MODUL_FILETYPE (syntax_tree) == F_prog) {
-    sprintf (cccallstr,
-             "gcc %s-Wall -Wno-unused -I $RCSROOT/src/compile/"
-             " -o %s %s %s",
-             ccflagsstr, outfilename, cfilename, GenLinkerList ());
-} else {
-    if ((MODUL_FILETYPE (syntax_tree) == F_modimp)
-        || (MODUL_FILETYPE (syntax_tree) == F_classimp)) {
-        sprintf (cccallstr,
-                 "gcc %s-Wall -Wno-unused -I $RCSROOT/src/compile/"
-                 " -o %s.o -c %s",
-                 ccflagsstr, (NULL != module_name) ? module_name : outfilename,
-                 cfilename);
-    } else {
-        DBUG_ASSERT (0, "wrong value of kind_of_file ");
+
+/*
+ *
+ *  functionname  :
+ *  arguments     :
+ *  description   :
+ *  global vars   :
+ *  internal funs :
+ *  external funs :
+ *  macros        :
+ *
+ *  remarks       :
+ *
+ */
+
+/*
+ *
+ *  functionname  : MakeArchive
+ *  arguments     :
+ *  description   :
+ *  global vars   :
+ *  internal funs :
+ *  external funs :
+ *  macros        :
+ *
+ *  remarks       :
+ *
+ */
+
+void
+MakeArchive (char *name)
+{
+    char *systemcall;
+    strings *tmp;
+
+    DBUG_ENTER ("MakeArchive");
+
+    systemcall = (char *)Malloc ((strlen (ccflagsstr) + 2 * strlen (name) + MAX_PATH_LEN)
+                                 * sizeof (char));
+
+    sprintf (systemcall,
+             "gcc %s -Wall -Wno-unused -I $RCSROOT/src/compile/"
+             " -o %s.o -c %s.c",
+             ccflagsstr, name, name);
+
+    NOTE2 (("*** Invoking C-compiler:"));
+
+    MAKESYSCALL;
+
+    NEWLINE (2);
+
+    NOTE2 (("*** Creating archive %s.a", name));
+
+    sprintf (systemcall, "mkdir __tmp_sac");
+    MAKESYSCALL;
+
+    sprintf (systemcall, "mkdir __tmp_store");
+    MAKESYSCALL;
+
+    sprintf (systemcall, "mv %s.o __tmp_sac", name);
+    MAKESYSCALL;
+
+    sprintf (systemcall, "mv *.o __tmp_store");
+    MAKESYSCALL;
+
+    tmp = afilelist;
+
+    while (tmp != NULL) {
+        sprintf (systemcall, "ar x %s", STRINGS_STRING (tmp));
+        MAKESYSCALL;
+
+        tmp = FreeOneStrings (tmp);
     }
+
+    sprintf (systemcall, "mv *.o __tmp_sac");
+    MAKESYSCALL;
+
+    sprintf (systemcall, "rm -f __.SYMDEF");
+    MAKESYSCALL;
+
+    tmp = ofilelist;
+
+    while (tmp != NULL) {
+        sprintf (systemcall, "cp %s __tmp_sac", STRINGS_STRING (tmp));
+        MAKESYSCALL;
+
+        tmp = FreeOneStrings (tmp);
+    }
+
+    sprintf (systemcall, "ar rc %s.a __tmp_sac/*.o", name);
+    MAKESYSCALL;
+
+    sprintf (systemcall, "ranlib %s.a", name);
+    MAKESYSCALL;
+
+    sprintf (systemcall, "mv  __tmp_store/*.o .");
+    MAKESYSCALL;
+
+    sprintf (systemcall, "rm __sac_tmp/*.o");
+    MAKESYSCALL;
+
+    sprintf (systemcall, "rmdir __sac_tmp");
+    MAKESYSCALL;
+
+    sprintf (systemcall, "rmdir __sac_store");
+    MAKESYSCALL;
+
+    NEWLINE (2);
+
+    FREE (systemcall);
+
+    DBUG_VOID_RETURN;
+}
+
+/*
+ *
+ *  functionname  : MakeExecutable
+ *  arguments     :
+ *  description   :
+ *  global vars   :
+ *  internal funs :
+ *  external funs :
+ *  macros        :
+ *
+ *  remarks       :
+ *
+ */
+
+void
+MakeExecutable ()
+{
+    char *systemcall;
+    strings *tmp;
+
+    DBUG_ENTER ("MakeExecutable");
+
+    systemcall = (char *)Malloc ((StringsLength (afilelist) + StringsLength (ofilelist)
+                                  + strlen (ccflagsstr) + strlen (outfilename)
+                                  + strlen (cfilename) + MAX_PATH_LEN)
+                                 * sizeof (char));
+
+    sprintf (systemcall, "gcc %s -Wall -Wno-unused -I $RCSROOT/src/compile/ -o %s %s",
+             ccflagsstr, outfilename, cfilename);
+
+    tmp = ofilelist;
+
+    while (tmp != NULL) {
+        strcat (systemcall, " ");
+        strcat (systemcall, STRINGS_STRING (tmp));
+
+        tmp = FreeOneStrings (tmp);
+    }
+
+    tmp = afilelist;
+
+    while (tmp != NULL) {
+        strcat (systemcall, " ");
+        strcat (systemcall, STRINGS_STRING (tmp));
+
+        tmp = FreeOneStrings (tmp);
+    }
+
+    NOTE2 (("*** Invoking C-compiler:"));
+
+    MAKESYSCALL;
+
+    NEWLINE (2);
+
+    FREE (systemcall);
+
+    DBUG_VOID_RETURN;
 }
 
 /*
@@ -80,35 +250,30 @@ if (MODUL_FILETYPE (syntax_tree) == F_prog) {
  *  remarks       :
  *
  */
-NOTE2 (("*** Invoking C-compiler:"));
-NOTE2 (("%s", cccallstr));
-NEWLINE (2);
 
-system (cccallstr);
+void
+InvokeCC (node *modul)
+{
+    DBUG_ENTER ("InvokeCC");
 
-/*
- *
- *  functionname  :
- *  arguments     :
- *  description   :
- *  global vars   :
- *  internal funs :
- *  external funs :
- *  macros        :
- *
- *  remarks       :
- *
- */
+    if (MODUL_FILETYPE (modul) == F_prog) {
+        MakeExecutable ();
+    } else {
+        MakeArchive (MODUL_NAME (modul));
+    }
+
+    DBUG_VOID_RETURN;
+}
 
 /*
  *
  *  functionname  : SearchLinkFiles
  *  arguments     :
  *  description   :
- *  global vars   :
- *  internal funs :
- *  external funs :
- *  macros        :
+ *  global vars   : linklist, afilelist, ofilelist
+ *  internal funs : ---
+ *  external funs : FindFile, StringCopy, strcpy, strcat, MakeStrings
+ *  macros        : MAX_FILE_NAME
  *
  *  remarks       :
  *
@@ -117,7 +282,7 @@ system (cccallstr);
 void
 SearchLinkFiles ()
 {
-    strings *tmp;
+    strings *tmp, *old;
     char buffer[MAX_FILE_NAME];
     char *pathname;
 
@@ -157,7 +322,7 @@ SearchLinkFiles ()
         /* The strings are not freed because they are shared. */
     }
 
-    DBUG_VOID_RETURN ();
+    DBUG_VOID_RETURN;
 }
 
 /*
@@ -182,6 +347,12 @@ AddToLinklist (char *mod)
     DBUG_ENTER ("AddToLinklist");
 
     if (0 != strcmp (mod, MODUL_NAME (syntax_tree))) {
+        /*
+         *  The global variable syntax_tree is used here by purpose because
+         *  it is the easiest way to access the current module name anywhere
+         *  in the syntax tree.
+         */
+
         tmp = linklist;
 
         while ((tmp != NULL) && (strcmp (STRINGS_STRING (tmp), mod) != 0)) {
--- src/modules/cccall.h
+++ src/modules/cccall.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.1  1995/12/29 17:19:26  cg
+ * Revision 1.2  1996/01/02 07:57:55  cg
+ * first working revision
+ *
+ * Revision 1.1  1995/12/29  17:19:26  cg
  * Initial revision
  *
  *
@@ -11,5 +14,13 @@
 #ifndef _sac_cccall_h
 
 #define _sac_cccall_h
+
+extern void InvokeCC (node *modul);
+
+extern node *PrepareLinking (node *syntax_tree);
+
+extern node *LINKfundef (node *arg_node, node *arg_info);
+extern node *LINKobjdef (node *arg_node, node *arg_info);
+extern node *LINKmodul (node *arg_node, node *arg_info);
 
 #endif /* _sac_cccall_h */
