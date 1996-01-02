14a0dd0d7a74cab55081277a47b6bb1c24022d58
Clemens Grelck <cg@sac-home.org>
1996-01-02 15:57:43 +0000

    added function CheckModuleName The global variables cfilename and outfilename will be set here. The length of module names is limited to 12 characters due to later handling by 'ar'.

--- src/scanparse/scnprs.c
+++ src/scanparse/scnprs.c
@@ -1,7 +1,13 @@
 /*
  *
  * $Log$
- * Revision 1.2  1996/01/02 08:16:29  cg
+ * Revision 1.3  1996/01/02 15:57:43  cg
+ * added function CheckModuleName
+ * The global variables cfilename and outfilename will be set here.
+ * The length of module names is limited to 12 characters due to
+ * later handling by 'ar'.
+ *
+ * Revision 1.2  1996/01/02  08:16:29  cg
  * first compilable revision
  *
  * Revision 1.1  1995/12/29  17:22:05  cg
@@ -14,12 +20,22 @@
 #include <stdio.h>
 
 #include "types.h"
+#include "tree_basic.h"
+
 #include "Error.h"
 #include "dbug.h"
 #include "filemgr.h"
 #include "globals.h"
 
 #include "scnprs.h"
+
+/*
+ *  Since the module/class name is used as basis for generating file names,
+ *  its length must be limited because ar accepts only file names with a
+ *  maximum of 15 characters.
+ */
+
+#define MAX_MODNAME 12
 
 /*
  *
@@ -37,12 +53,53 @@
 
 /*
  *
+ *  functionname  : CheckModuleName
+ *  arguments     : 1) module/class name
+ *  description   : checks the length of module name and sets the global
+ *                  variables outfilename and cfilename accordingly
+ *  global vars   : outfilename, cfilename
+ *  internal funs : ---
+ *  external funs : strcmp, strncpy, strcat, strlen
+ *  macros        : MAX_MODNAME
+ *
+ *  remarks       :
+ *
+ */
+
+void
+CheckModuleName (char *name)
+{
+    DBUG_ENTER ("CheckModuleName");
+
+    if (0 != strcmp (outfilename, "a.out")) {
+        SYSWARN (("Module/Class name overrides command line option '-o`"));
+    }
+
+    if (strlen (name) > MAX_MODNAME) {
+        SYSERROR (
+          ("Module/Class name must be no longer than %d characters", MAX_MODNAME));
+    }
+
+    strncpy (cfilename, name, MAX_MODNAME);
+    strcat (cfilename, ".c");
+
+    strncpy (outfilename, name, MAX_MODNAME);
+    strcat (outfilename, ".o");
+
+    DBUG_VOID_RETURN;
+}
+
+/*
+ *
  *  functionname  : ScanParse
  *  arguments     : ---
  *  description   : searches the file given by the global variable filename
  *                  in the path and parses it.
+ *                  The global variables outfilename and cfilename are
+ *                  set according to a module/class name, which will be
+ *                  abbreviated to a maximum of 13 characters.
  *  global vars   : yyin, filename, syntax_tree
- *  internal funs : ---
+ *  internal funs : CheckModuleName
  *  external funs : FindFile, yyparse, strcpy
  *  macros        : MAX_PATH_LEN, ERROR
  *
@@ -57,7 +114,6 @@ node *
 ScanParse ()
 {
     char *pathname;
-    char cccallstr[MAX_PATH_LEN];
 
     DBUG_ENTER ("ScanParse");
 
@@ -84,6 +140,11 @@ ScanParse ()
     yyparse ();
 
     fclose (yyin);
+
+    if ((MODUL_FILETYPE (syntax_tree) == F_modimp)
+        || (MODUL_FILETYPE (syntax_tree) == F_classimp)) {
+        CheckModuleName (MODUL_NAME (syntax_tree));
+    }
 
     DBUG_RETURN (syntax_tree);
 }
@@ -123,6 +184,11 @@ ScanParse ()
     yyparse ();
 
     pclose (yyin);
+
+    if ((MODUL_FILETYPE (syntax_tree) == F_modimp)
+        || (MODUL_FILETYPE (syntax_tree) == F_classimp)) {
+        CheckModuleName (MODUL_NAME (syntax_tree));
+    }
 
     DBUG_RETURN (syntax_tree);
 }
