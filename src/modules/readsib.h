523f2063fd187de5cb2f23f98cc327962ecd34a9
Clemens Grelck <cg@sac-home.org>
1996-01-05 12:39:04 +0000

    added functions CreateArchive and RSIBobjdef

--- src/modules/readsib.h
+++ src/modules/readsib.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1995/12/29 10:41:52  cg
+ * Revision 1.3  1996/01/05 12:39:04  cg
+ * added functions CreateArchive and RSIBobjdef
+ *
+ * Revision 1.2  1995/12/29  10:41:52  cg
  * first running revision for new SIBs
  *
  * Revision 1.1  1995/12/23  17:28:39  cg
@@ -20,5 +23,8 @@ extern node *ReadSib (node *);
 extern node *RSIBmodul (node *arg_node, node *arg_info);
 extern node *RSIBtypedef (node *arg_node, node *arg_info);
 extern node *RSIBfundef (node *arg_node, node *arg_info);
+extern node *RSIBobjdef (node *arg_node, node *arg_info);
+
+extern void CreateArchive (char *name);
 
 #endif /* _sac_readsib_h  */
