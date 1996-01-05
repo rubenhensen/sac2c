4698f2850f49e9143098b6afc5b6d3bfa5b6f859
Clemens Grelck <cg@sac-home.org>
1996-01-05 12:36:29 +0000

    added global variables tmp_dirname, store_dirname, build_dirname and functions WriteOpen, CreateTmpDirectories, RemoveDirectory

--- src/modules/filemgr.h
+++ src/modules/filemgr.h
@@ -3,7 +3,11 @@
 /*
  *
  * $Log$
- * Revision 1.2  1995/04/05 17:24:16  sbs
+ * Revision 1.3  1996/01/05 12:36:29  cg
+ * added global variables tmp_dirname, store_dirname, build_dirname
+ * and functions WriteOpen, CreateTmpDirectories, RemoveDirectory
+ *
+ * Revision 1.2  1995/04/05  17:24:16  sbs
  * GenLinkList inserted
  *
  * Revision 1.1  1994/12/11  17:33:27  sbs
@@ -21,9 +25,17 @@
 
 typedef enum { PATH, MODDEC_PATH, MODIMP_PATH } pathkind;
 
+extern char *tmp_dirname;
+extern char *store_dirname;
+extern char *build_dirname;
+
 extern char *FindFile (pathkind p, char *name);
 extern void InitPaths ();
 extern int AppendPath (pathkind p, char *path);
 extern int AppendEnvVar (pathkind p, char *var);
+
+extern FILE *WriteOpen (char *format, ...);
+extern void CreateTmpDirectories ();
+extern void RemoveDirectory (char *name);
 
 #endif /* _filemgr_h */
