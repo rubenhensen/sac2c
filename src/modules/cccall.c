7772c2af4a226e05003001fb2f1d80bf2ee8ca1d
Clemens Grelck <cg@sac-home.org>
1996-01-05 12:37:34 +0000

    Now, SAC library files are generated when compiling module/class implementations.

--- src/modules/cccall.c
+++ src/modules/cccall.c
@@ -1,7 +1,11 @@
 /*
  *
  * $Log$
- * Revision 1.3  1996/01/02 16:01:35  cg
+ * Revision 1.4  1996/01/05 12:37:34  cg
+ * Now, SAC library files are generated when compiling module/class
+ * implementations.
+ *
+ * Revision 1.3  1996/01/02  16:01:35  cg
  * first really running revision
  *
  * Revision 1.2  1996/01/02  07:57:37  cg
@@ -34,25 +38,15 @@ static strings *linklist = NULL;
 static strings *ofilelist = NULL;
 static strings *afilelist = NULL;
 
+#if 0
+
 #define MAKESYSCALL                                                                      \
     NOTE (("%s", systemcall));                                                           \
     system (systemcall);                                                                 \
     DBUG_EXECUTE ("SYSSTEP", printf ("\nHit key to continue\n");                         \
                   scanf ("%s", systemcall););
 
-/*
- *
- *  functionname  :
- *  arguments     :
- *  description   :
- *  global vars   :
- *  internal funs :
- *  external funs :
- *  macros        :
- *
- *  remarks       :
- *
- */
+#endif
 
 /*
  *
@@ -84,7 +78,7 @@ static strings *afilelist = NULL;
 
 /*
  *
- *  functionname  : MakeArchive
+ *  functionname  :
  *  arguments     :
  *  description   :
  *  global vars   :
@@ -97,95 +91,31 @@ static strings *afilelist = NULL;
  */
 
 void
-MakeArchive (char *name)
+MakeObjectFile (char *name)
 {
     char *systemcall;
-    strings *tmp;
+    int exit_code;
 
-    DBUG_ENTER ("MakeArchive");
+    DBUG_ENTER ("MakeObjectFile");
 
     systemcall = (char *)Malloc ((strlen (ccflagsstr) + 2 * strlen (name) + MAX_PATH_LEN)
                                  * sizeof (char));
 
     sprintf (systemcall,
              "gcc %s -Wall -Wno-unused -I $RCSROOT/src/compile/"
-             " -o %s.o -c %s.c",
-             ccflagsstr, name, name);
+             " -o %s%s -c %s%s",
+             ccflagsstr, build_dirname, outfilename, targetdir, cfilename);
 
-    NOTE2 (("*** Invoking C-compiler:"));
+    NEWLINE (3);
 
-    MAKESYSCALL;
+    NOTE (("%s", systemcall));
 
-    NEWLINE (2);
+    exit_code = system (systemcall);
 
-    NOTE2 (("*** Creating archive %s.a", name));
-
-    sprintf (systemcall, "mkdir __tmp_sac");
-    MAKESYSCALL;
-
-    sprintf (systemcall, "mkdir __tmp_store_o");
-    MAKESYSCALL;
-
-    sprintf (systemcall, "mv %s.o __tmp_sac", name);
-    MAKESYSCALL;
-
-    sprintf (systemcall, "touch __tmp_store.o");
-    MAKESYSCALL;
-
-    /*
-     *  The last system call assures that at least one .o file exists in
-     *  the current directory, otherwise 'mv *.o ...' causes error messages.
-     */
-
-    sprintf (systemcall, "mv *.o __tmp_store_o");
-    MAKESYSCALL;
-
-    tmp = afilelist;
-
-    while (tmp != NULL) {
-        sprintf (systemcall, "ar x %s", STRINGS_STRING (tmp));
-        MAKESYSCALL;
-
-        sprintf (systemcall, "mv *.o __tmp_sac");
-        MAKESYSCALL;
-
-        sprintf (systemcall, "rm -f __.SYMDEF");
-        MAKESYSCALL;
-
-        tmp = FreeOneStrings (tmp);
+    if (exit_code > 0) {
+        NEWLINE (0);
+        SYSABORT (("Compilation to object file failed (%d)", exit_code / 256));
     }
-
-    tmp = ofilelist;
-
-    while (tmp != NULL) {
-        sprintf (systemcall, "cp %s __tmp_sac", STRINGS_STRING (tmp));
-        MAKESYSCALL;
-
-        tmp = FreeOneStrings (tmp);
-    }
-
-    sprintf (systemcall, "ar rc %s.a __tmp_sac/*.o", name);
-    MAKESYSCALL;
-
-    sprintf (systemcall, "ranlib %s.a", name);
-    MAKESYSCALL;
-
-    sprintf (systemcall, "mv  __tmp_store_o/*.o .");
-    MAKESYSCALL;
-
-    sprintf (systemcall, "rm __tmp_store.o");
-    MAKESYSCALL;
-
-    sprintf (systemcall, "rm __tmp_sac/*.o");
-    MAKESYSCALL;
-
-    sprintf (systemcall, "rmdir __tmp_sac");
-    MAKESYSCALL;
-
-    sprintf (systemcall, "rmdir __tmp_store_o");
-    MAKESYSCALL;
-
-    NEWLINE (2);
 
     FREE (systemcall);
 
@@ -194,13 +124,79 @@ MakeArchive (char *name)
 
 /*
  *
- *  functionname  : MakeExecutable
+ *  functionname  : CreateLibrary
  *  arguments     :
  *  description   :
  *  global vars   :
  *  internal funs :
  *  external funs :
  *  macros        :
+ *
+ *  remarks       :
+ *
+ */
+
+void
+CreateLibrary (node *syntax_tree)
+{
+    strings *tmp;
+    char *name;
+
+    DBUG_ENTER ("CreateLibrary");
+
+    name = MODUL_NAME (syntax_tree);
+
+    /*
+      if (!SystemTest("-d %s", targetdir))
+      {
+          SYSWARN(("Specified target directory \"%s\" does not exist",
+                   targetdir));
+
+          strcpy(targetdir, ".");
+      }
+    */
+
+    NOTE (("Creating SAC library \"%s%s.lib\"", targetdir, name));
+
+    if (SystemTest ("-f *.o")) {
+        SystemCall ("mv *.o %s", tmp_dirname);
+    }
+
+    tmp = afilelist;
+
+    while (tmp != NULL) {
+        SystemCall ("ar x %s", STRINGS_STRING (tmp));
+        SystemCall ("mv *.o %s", build_dirname);
+
+        tmp = FreeOneStrings (tmp);
+    }
+
+    SystemCall ("rm -f __.SYMDEF");
+
+    SystemCall ("ar rc %s%s.a %s*.o", build_dirname, name, build_dirname);
+    SystemCall ("ranlib %s%s.a", build_dirname, name);
+
+    if (SystemTest ("-f %s*.o", tmp_dirname)) {
+        SystemCall ("mv  %s*.o .", tmp_dirname);
+    }
+
+    SystemCall ("cat %s%s.sib %s%s.a > %s%s.lib", store_dirname, name, build_dirname,
+                name, targetdir, name);
+
+    DBUG_VOID_RETURN;
+}
+
+/*
+ *
+ *  functionname  : MakeExecutable
+ *  arguments     : ---
+ *  description   : generates string to call gcc including all object and
+ *                  archive files to link with and executes it
+ *  global vars   : afilelist, ofilelist
+ *  internal funs : ---
+ *  external funs : Malloc, sprintf, strcat, FreeOneStrings, system,
+ *                  StringsLength
+ *  macros        : MAX_PATH_LEN, FREE
  *
  *  remarks       :
  *
@@ -211,6 +207,7 @@ MakeExecutable ()
 {
     char *systemcall;
     strings *tmp;
+    int exit_code;
 
     DBUG_ENTER ("MakeExecutable");
 
@@ -240,11 +237,16 @@ MakeExecutable ()
         tmp = FreeOneStrings (tmp);
     }
 
-    NOTE2 (("*** Invoking C-compiler:"));
+    NEWLINE (3);
 
-    MAKESYSCALL;
+    NOTE (("%s", systemcall));
 
-    NEWLINE (2);
+    exit_code = system (systemcall);
+
+    if (exit_code > 0) {
+        NEWLINE (0);
+        SYSABORT (("Compilation to executable failed (%d)", exit_code / 256));
+    }
 
     FREE (systemcall);
 
@@ -253,12 +255,12 @@ MakeExecutable ()
 
 /*
  *
- *  functionname  :
- *  arguments     :
- *  description   :
- *  global vars   :
- *  internal funs :
- *  external funs :
+ *  functionname  : InvokeCC
+ *  arguments     : 1) syntax tree
+ *  description   : starts the gcc for final compilation
+ *  global vars   : ---
+ *  internal funs : MakeExecutable, MakeObjectFile
+ *  external funs : ---
  *  macros        :
  *
  *  remarks       :
@@ -266,14 +268,14 @@ MakeExecutable ()
  */
 
 void
-InvokeCC (node *modul)
+InvokeCC (node *syntax_tree)
 {
     DBUG_ENTER ("InvokeCC");
 
-    if (MODUL_FILETYPE (modul) == F_prog) {
+    if (MODUL_FILETYPE (syntax_tree) == F_prog) {
         MakeExecutable ();
     } else {
-        MakeArchive (MODUL_NAME (modul));
+        MakeObjectFile (MODUL_NAME (syntax_tree));
     }
 
     DBUG_VOID_RETURN;
@@ -282,12 +284,16 @@ InvokeCC (node *modul)
 /*
  *
  *  functionname  : SearchLinkFiles
- *  arguments     :
- *  description   :
+ *  arguments     : ---
+ *  description   : traverses the linklist and searches for the
+ *                  implementations in store_dirname. The complete pathname
+ *                  of archive files is written to afilelist. Object files
+ *                  are moved to build_dirname and that new complete
+ *                  pathname is written to ofilelist.
  *  global vars   : linklist, afilelist, ofilelist
  *  internal funs : ---
- *  external funs : FindFile, StringCopy, strcpy, strcat, MakeStrings
- *  macros        : MAX_FILE_NAME
+ *  external funs : StringCopy, sprintf, MakeStrings, SystemCall, SystemTest
+ *  macros        : MAX_PATH_LEN
  *
  *  remarks       :
  *
@@ -297,37 +303,35 @@ void
 SearchLinkFiles ()
 {
     strings *tmp, *old;
-    char buffer[MAX_FILE_NAME];
-    char *pathname;
+    char pathname[MAX_PATH_LEN];
 
     DBUG_ENTER ("SearchLinkFiles");
 
     tmp = linklist;
 
     while (tmp != NULL) {
-        strcpy (buffer, STRINGS_STRING (tmp));
-        strcat (buffer, ".a");
+        sprintf (pathname, "%s%s.a", store_dirname, STRINGS_STRING (tmp));
 
-        pathname = FindFile (MODIMP_PATH, buffer);
-
-        if (pathname == NULL) {
-            strcpy (buffer, STRINGS_STRING (tmp));
-            strcat (buffer, ".o");
-
-            pathname = FindFile (MODIMP_PATH, buffer);
-
-            if (pathname == NULL) {
-                SYSERROR (("Unable to find implementation of module/class '%s`",
-                           STRINGS_STRING (tmp)));
-            } else {
-                ofilelist = MakeStrings (StringCopy (pathname), ofilelist);
-
-                DBUG_PRINT ("LINK", ("Added %s to ofilelist", pathname));
-            }
-        } else {
+        if (SystemTest ("-f %s", pathname)) {
             afilelist = MakeStrings (StringCopy (pathname), afilelist);
 
             DBUG_PRINT ("LINK", ("Added %s to afilelist", pathname));
+        } else {
+            sprintf (pathname, "%s%s.o", store_dirname, STRINGS_STRING (tmp));
+
+            if (SystemTest ("-f %s%s.o", store_dirname, STRINGS_STRING (tmp))) {
+                SystemCall ("mv %s%s.o %s", store_dirname, STRINGS_STRING (tmp),
+                            build_dirname);
+
+                sprintf (pathname, "%s%s.o", build_dirname, STRINGS_STRING (tmp));
+                ofilelist = MakeStrings (StringCopy (pathname), ofilelist);
+
+                DBUG_PRINT ("LINK", ("Added %s to ofilelist", pathname));
+            } else {
+                SYSERROR (("No implementation of module/class '%s` available "
+                           "in directory \"%s\"",
+                           STRINGS_STRING (tmp), store_dirname));
+            }
         }
 
         old = tmp;
