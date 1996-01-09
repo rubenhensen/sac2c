e26f5cf524d7e1b297a25215f32a83bce476271f
Clemens Grelck <cg@sac-home.org>
1996-01-09 08:54:00 +0000

    __SAC__Runtime_malloc(int size) now returns void*

--- src/compile/libsac.h
+++ src/compile/libsac.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.1  1996/01/09 08:31:41  cg
+ * Revision 1.2  1996/01/09 08:54:00  cg
+ * __SAC__Runtime_malloc(int size) now returns void*
+ *
+ * Revision 1.1  1996/01/09  08:31:41  cg
  * Initial revision
  *
  *
@@ -17,6 +20,6 @@ extern void __SAC__RuntimeError (char *format, ...);
 extern void __SAC__Runtime_Print (char *format, ...);
 extern void __SAC__Runtime_PrintTraceHeader (char *format, ...);
 extern void __SAC__Runtime_PrintTraceInfo (char *format, ...);
-extern void __SAC__Runtime_malloc (int size);
+extern void *__SAC__Runtime_malloc (int size);
 
 #endif /* _sac_libsac_h */
