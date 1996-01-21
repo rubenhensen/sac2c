fdcbaa10cd590a8e7d9c7ab8b770e5bbaeecfef0
Clemens Grelck <cg@sac-home.org>
1996-01-21 14:15:39 +0000

    Now, routines from the SAC runtime library are used instead of those from stdio

--- src/compile/icm_trace.c
+++ src/compile/icm_trace.c
@@ -1,7 +1,11 @@
 /*
  *
  * $Log$
- * Revision 1.2  1995/05/23 09:59:39  sbs
+ * Revision 1.3  1996/01/21 14:15:39  cg
+ * Now, routines from the SAC runtime library are used instead
+ * of those from stdio
+ *
+ * Revision 1.2  1995/05/23  09:59:39  sbs
  * fprintf's of arguments to stderr inserted.
  *
  * Revision 1.1  1995/05/04  11:48:35  sbs
@@ -14,23 +18,23 @@
 #define SEP                                                                              \
     if (sep) {                                                                           \
         INDENT;                                                                          \
-        fprintf (outfile, "fprintf( stderr, \", \");\n");                                \
+        fprintf (outfile, "__SAC__Runtime_Print( \", \");\n");                           \
     }
 
 #define ICM_DEF(prf, trf)                                                                \
     if (trf & traceflag) {                                                               \
         int sep = 0;                                                                     \
         INDENT;                                                                          \
-        fprintf (outfile, "fprintf( stderr, \"%s( \");\n", #prf);
+        fprintf (outfile, "__SAC__Runtime_Print( \"%s( \");\n", #prf);
 #define ICM_STR(name)                                                                    \
     SEP;                                                                                 \
     INDENT;                                                                              \
-    fprintf (outfile, "fprintf( stderr, \"%s\");\n", name);                              \
+    fprintf (outfile, "__SAC__Runtime_Print( \"%s \");\n", name);                        \
     sep = 1;
 #define ICM_INT(name)                                                                    \
     SEP;                                                                                 \
     INDENT;                                                                              \
-    fprintf (outfile, "fprintf( stderr, \"%d\");\n", name);                              \
+    fprintf (outfile, "__SAC__Runtime_Print( \"%d \");\n", name);                        \
     sep = 1;
 #define ICM_VAR(dim, name)                                                               \
     {                                                                                    \
@@ -38,13 +42,13 @@
         for (i = 0; i < dim; i++) {                                                      \
             SEP;                                                                         \
             INDENT;                                                                      \
-            fprintf (outfile, "fprintf( stderr, \"%s\");\n", name[i]);                   \
+            fprintf (outfile, "__SAC__Runtime_Print( \"%s \");\n", name[i]);             \
             sep = 1;                                                                     \
         }                                                                                \
     }
 #define ICM_END(prf)                                                                     \
     INDENT;                                                                              \
-    fprintf (outfile, "fprintf( stderr, \")\\n\");\n");                                  \
+    fprintf (outfile, "__SAC__Runtime_Print( \")\\n\");\n");                             \
     }                                                                                    \
     ;
 
