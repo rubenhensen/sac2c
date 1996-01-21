e1902a4abc9620d62809d582bf37202fa92b3027
Clemens Grelck <cg@sac-home.org>
1996-01-21 18:07:34 +0000

    added declaration of __SAC__Runtime_trace_memcnt

--- src/compile/libsac.h
+++ src/compile/libsac.h
@@ -1,7 +1,10 @@
 /*
  *
  * $Log$
- * Revision 1.2  1996/01/09 08:54:00  cg
+ * Revision 1.3  1996/01/21 18:07:34  cg
+ * added declaration of __SAC__Runtime_trace_memcnt
+ *
+ * Revision 1.2  1996/01/09  08:54:00  cg
  * __SAC__Runtime_malloc(int size) now returns void*
  *
  * Revision 1.1  1996/01/09  08:31:41  cg
@@ -15,6 +18,8 @@
 #ifndef _sac_libsac_h
 
 #define _sac_libsac_h
+
+extern int __SAC__Runtime_trace_memcnt;
 
 extern void __SAC__RuntimeError (char *format, ...);
 extern void __SAC__Runtime_Print (char *format, ...);
