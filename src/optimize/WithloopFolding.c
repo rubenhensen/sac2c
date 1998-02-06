10f5ede61ae2e31e1f70aee814a70d5916e286ab
Sören Schwartz <info@sac-home.org>
1998-02-06 10:14:25 +0000

    Initial revision

---
+++ src/optimize/WithloopFolding.c
@@ -0,0 +1,25 @@
+
+/*******************************************************************************
+ * This file realizes the WL-folding for the new SAC-WLs.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *****************************************************************************/
+*
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tree.h"
+#include "internal_lib.h"
+#include "free.h"
+#include "globals.h"
+#include "Error.h"
+#include "dbug.h"
+#include "my_debug.h"
+#include "traverse.h"
+#include "WithloopFolding.h"
---
+++ src/optimize/WithloopFolding.h
@@ -0,0 +1,5 @@
+#ifndef _WithloopFolding_h
+
+#define _WithloopFolding_h
+
+#endif /* _WithloopFolding_h */
