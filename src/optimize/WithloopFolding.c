e2a06545eddb0f6ea340df9f27f1be0a6fa65ef8
Sören Schwartz <info@sac-home.org>
1998-02-06 14:30:23 +0000

    *** empty log message ***

--- src/optimize/WithloopFolding.c
+++ src/optimize/WithloopFolding.c
@@ -8,13 +8,13 @@
  *
  *
  *
- *****************************************************************************/
-*
+ ******************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "tree.h"
+#include "types.h"
 #include "internal_lib.h"
 #include "free.h"
 #include "globals.h"
@@ -23,3 +23,26 @@
 #include "my_debug.h"
 #include "traverse.h"
 #include "WithloopFolding.h"
+
+/******************************************************************************
+ *
+ * function:
+ *   node *WLFWithloopFolding(node *arg_node, node* arg_info)
+ *
+ * description:
+ *
+ *
+ *
+ ******************************************************************************/
+
+node *
+WLFWithloopFolding (node *arg_node, node *arg_info)
+{
+    DBUG_ENTER ("WLFWithloopFolding");
+
+    act_tab = wlf_tab;
+
+    Trav (arg_node, arg_info);
+
+    DBUG_RETURN (arg_node);
+}
--- src/optimize/WithloopFolding.h
+++ src/optimize/WithloopFolding.h
@@ -1,5 +1,6 @@
 #ifndef _WithloopFolding_h
-
 #define _WithloopFolding_h
 
-#endif /* _WithloopFolding_h */
+extern node *WLFWithloopFolding (node *, node *);
+
+#endif
