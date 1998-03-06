5eb78f8a7aa1ddd0050f247d13455a817557d8f5
Sören Schwartz <info@sac-home.org>
1998-03-06 13:29:05 +0000

    added new WLI functions

--- src/optimize/WithloopFolding.h
+++ src/optimize/WithloopFolding.h
@@ -1,6 +1,9 @@
 /* 	$Id$
  *
  * $Log$
+ * Revision 1.7  1998/03/06 13:29:05  srs
+ * added new WLI functions
+ *
  * Revision 1.6  1998/02/24 14:19:26  srs
  * *** empty log message ***
  *
@@ -19,12 +22,20 @@
 #define _WithloopFolding_h
 
 extern node *WLFWithloopFolding (node *, node *);
-extern node *WLFfundef (node *, node *);
-extern node *WLFassign (node *, node *);
-extern node *WLFcond (node *, node *);
-extern node *WLFdo (node *, node *);
-extern node *WLFwhile (node *arg_node, node *);
-extern node *WLFwith (node *, node *);
+
+extern node *WLIfundef (node *, node *);
+extern node *WLIassign (node *, node *);
+extern node *WLIcond (node *, node *);
+extern node *WLIdo (node *, node *);
+extern node *WLIwhile (node *, node *);
+extern node *WLIwith (node *, node *);
+extern node *WLINwith (node *, node *);
+extern node *WLIlet (node *, node *);
+extern node *WLIid (node *arg_node, node *arg_info);
+
+extern node *WLINpart (node *, node *);
+extern node *WLINcode (node *, node *);
+
 extern node *WLFNwith (node *, node *);
 
 #endif
