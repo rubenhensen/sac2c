af95a46f7a62ce69b35139ce67be18ecf993d17f
Dietmar Kreye <dietmar@computer-scientist.org>
1998-03-18 09:57:39 +0000

    added O2Nnwith

--- src/compile/Old2NewWith.h
+++ src/compile/Old2NewWith.h
@@ -7,6 +7,7 @@
 extern node *Old2NewWith (node *arg_node);
 
 extern node *O2Nwith (node *arg_node, node *arg_info);
-extern node *O2NNpart (node *arg_node, node *arg_info);
+extern node *O2Nnwith (node *arg_node, node *arg_info);
+extern node *O2Nnpart (node *arg_node, node *arg_info);
 
 #endif
