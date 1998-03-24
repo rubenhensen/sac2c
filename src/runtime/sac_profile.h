aa289034d1d44612c76ec2624df8f8e1a8217cd5
Clemens Grelck <cg@sac-home.org>
1998-03-24 13:53:24 +0000

    The presentation of profiling information is now entirely implemented using macros. This is necessary because neither the number of functions nor the maximum number of applications of a single function should be statically fixed. These numbers are inherently application dependent, so access to the timer arrays cannot be specified as part of the runtime library.

--- src/runtime/sac_profile.h
+++ src/runtime/sac_profile.h
@@ -1,6 +1,14 @@
 /*
  *
  * $Log$
+ * Revision 1.2  1998/03/24 13:53:24  cg
+ * The presentation of profiling information is now entirely implemented
+ * using macros. This is necessary because neither the number of functions
+ * nor the maximum number of applications of a single function should be
+ * statically fixed. These numbers are inherently application dependent,
+ * so access to the timer arrays cannot be specified as part of the
+ * runtime library.
+ *
  * Revision 1.1  1998/03/19 16:54:53  cg
  * Initial revision
  *
@@ -35,6 +43,10 @@
 
 #if PROFILE
 
+/*
+ * Type definitions
+ */
+
 #include <sys/time.h>
 /*
  * for struct timeval
@@ -45,37 +57,32 @@
  * for struct rusage
  */
 
-extern int getrusage (int who, struct rusage *rusage);
-
 typedef struct timeval __PF_TIMER;
 
-extern struct rusage _SAC_start_timer; /* defined in profile.c */
-extern struct rusage _SAC_stop_timer;  /* defined in profile.c */
+/*
+ * External declarations of C library functions needed
+ */
 
-extern __PF_TIMER *_SAC_PrintProfileOverall (
-  int display_with, int __PF_maxfun, int *__PF_maxfunap, void *__PF_fun_timer,
-  void *__PF_with_modarray_timer, void *__PF_with_genarray_timer,
-  void *__PF_with_fold_timer, void *__PF_fw_fun_timer, void *__PF_fw_with_modarray_timer,
-  void *__PF_fw_with_genarray_timer, void *__PF_fw_with_fold_timer);
+extern int getrusage (int who, struct rusage *rusage);
 
-extern void _SAC_PrintProfileFuns (__PF_TIMER *total_time, int display_fun,
-                                   int display_with, int __PF_maxfun, int *__PF_maxfunap,
-                                   void *__PF_fun_timer, void *__PF_with_modarray_timer,
-                                   void *__PF_with_genarray_timer,
-                                   void *__PF_with_fold_timer, void *__PF_fw_fun_timer,
-                                   void *__PF_fw_with_modarray_timer,
-                                   void *__PF_fw_with_genarray_timer,
-                                   void *__PF_fw_with_fold_timer, void *__PF_fun_name);
+/*
+ * External declarations of library functions defined in libsac
+ */
 
-#define PROFILE_SETUP(maxfun)                                                            \
-    __PF_act_timer = &__PF_fun_timer[0][0];                                              \
-    __PF_act_funno = 0;                                                                  \
-    __PF_act_funapno = 0;                                                                \
-    __PF_maxfun = maxfun;                                                                \
+extern void _SAC_PrintHeader (char *title);
+extern void _SAC_PrintTime (char *title, char *space, __PF_TIMER *time);
+extern void _SAC_PrintTimePercentage (char *title, char *space, __PF_TIMER *time1,
+                                      __PF_TIMER *time2);
+
+/*
+ *  Macro definitions
+ */
+
+#define __PROFILE_SETUP()                                                                \
     __PF_INIT_CLOCK ();                                                                  \
     {                                                                                    \
         int i, j;                                                                        \
-        for (i = 0; i < maxfun; i++) {                                                   \
+        for (i = 0; i < __PROFILE_MAXFUN; i++) {                                         \
             for (j = 0; j < __PF_maxfunap[i]; j++) {                                     \
                 __PF_INIT_TIMER (__PF_fun_timer[i][j]);                                  \
                 __PF_INIT_TIMER (__PF_with_modarray_timer[i][j]);                        \
@@ -90,50 +97,143 @@ extern void _SAC_PrintProfileFuns (__PF_TIMER *total_time, int display_fun,
     }                                                                                    \
     __PF_START_CLOCK ()
 
-#define PROFILE_PRINT()                                                                  \
+#define __PROFILE_PRINT()                                                                \
     __PF_STOP_CLOCK ();                                                                  \
     __PF_ADD_TO_TIMER (*__PF_act_timer);                                                 \
     {                                                                                    \
         __PF_TIMER *tmp;                                                                 \
                                                                                          \
-        tmp = _SAC_PrintProfileOverall (DISPLAY_WITH, __PF_maxfun, __PF_maxfunap,        \
-                                        __PF_fun_timer, __PF_with_modarray_timer,        \
-                                        __PF_with_genarray_timer, __PF_with_fold_timer,  \
-                                        __PF_fw_fun_timer, __PF_fw_with_modarray_timer,  \
-                                        __PF_fw_with_genarray_timer,                     \
-                                        __PF_fw_with_fold_timer);                        \
-        _SAC_PrintProfileFuns (tmp, DISPLAY_FUN, DISPLAY_WITH, __PF_maxfun,              \
-                               __PF_maxfunap, __PF_fun_timer, __PF_with_modarray_timer,  \
-                               __PF_with_genarray_timer, __PF_with_fold_timer,           \
-                               __PF_fw_fun_timer, __PF_fw_with_modarray_timer,           \
-                               __PF_fw_with_genarray_timer, __PF_fw_with_fold_timer,     \
-                               __PF_fun_name);                                           \
+        __PROFILE_PRINT_OVERALL (tmp, __PF_DISPLAY_WITH, __PROFILE_MAXFUN,               \
+                                 __PF_maxfunap, __PF_fun_timer,                          \
+                                 __PF_with_modarray_timer, __PF_with_genarray_timer,     \
+                                 __PF_with_fold_timer, __PF_fw_fun_timer,                \
+                                 __PF_fw_with_modarray_timer,                            \
+                                 __PF_fw_with_genarray_timer, __PF_fw_with_fold_timer);  \
+                                                                                         \
+        __PROFILE_PRINT_FUNS (tmp, __PF_DISPLAY_FUN, __PF_DISPLAY_WITH,                  \
+                              __PROFILE_MAXFUN, __PF_maxfunap, __PF_fun_timer,           \
+                              __PF_with_modarray_timer, __PF_with_genarray_timer,        \
+                              __PF_with_fold_timer, __PF_fw_fun_timer,                   \
+                              __PF_fw_with_modarray_timer, __PF_fw_with_genarray_timer,  \
+                              __PF_fw_with_fold_timer, __PF_fun_name);                   \
+    }
+
+#define __PROFILE_PRINT_OVERALL(tmp, display, maxfun, maxfunap, fun_timer,               \
+                                with_modarray_timer, with_genarray_timer,                \
+                                with_fold_timer, fw_fun_timer, fw_with_modarray_timer,   \
+                                fw_with_genarray_timer, fw_with_fold_timer)              \
+    {                                                                                    \
+        int i, j;                                                                        \
+        __PF_TIMER with_total, non_with_total;                                           \
+        __PF_TIMER total;                                                                \
+                                                                                         \
+        __PF_INIT_TIMER (with_total);                                                    \
+        __PF_INIT_TIMER (non_with_total);                                                \
+        __PF_INIT_TIMER (total);                                                         \
+                                                                                         \
+        for (i = 0; i < maxfun; i++) {                                                   \
+            for (j = 0; j < maxfunap[i]; j++) {                                          \
+                __PF_ADD_TIMERS (with_total, with_total, with_modarray_timer[i][j]);     \
+                __PF_ADD_TIMERS (with_total, with_total, with_genarray_timer[i][j]);     \
+                __PF_ADD_TIMERS (with_total, with_total, with_fold_timer[i][j]);         \
+                __PF_ADD_TIMERS (with_total, with_total, fw_fun_timer[i][j]);            \
+                __PF_ADD_TIMERS (with_total, with_total, fw_with_modarray_timer[i][j]);  \
+                __PF_ADD_TIMERS (with_total, with_total, fw_with_genarray_timer[i][j]);  \
+                __PF_ADD_TIMERS (with_total, with_total, fw_with_fold_timer[i][j]);      \
+                __PF_ADD_TIMERS (non_with_total, non_with_total, fun_timer[i][j]);       \
+            }                                                                            \
+        }                                                                                \
+        __PF_ADD_TIMERS (total, with_total, non_with_total);                             \
+                                                                                         \
+        _SAC_PrintHeader ("Overall Profile");                                            \
+        _SAC_PrintTime ("time used", __PF_TIMER_SPACE, &total);                          \
+        if (display) {                                                                   \
+            _SAC_PrintTimePercentage ("   with-loop", "", &with_total, &total);          \
+            _SAC_PrintTimePercentage ("   non-with-loop", "", &non_with_total, &total);  \
+        }                                                                                \
+                                                                                         \
+        tmp = &total;                                                                    \
+    }
+
+#define __PROFILE_PRINT_FUNS(total_time, display_fun, display_with, maxfun, maxfunap,    \
+                             fun_timer, with_modarray_timer, with_genarray_timer,        \
+                             with_fold_timer, fw_fun_timer, fw_with_modarray_timer,      \
+                             fw_with_genarray_timer, fw_with_fold_timer, fun_name)       \
+    {                                                                                    \
+        int i, j;                                                                        \
+        __PF_TIMER with_total, parent_with_total, non_with_total, total;                 \
+                                                                                         \
+        _SAC_PrintHeader ("Function Profiles");                                          \
+                                                                                         \
+        for (i = 0; i < (display_fun ? maxfun : 1); i++) {                               \
+            __PF_INIT_TIMER (with_total);                                                \
+            __PF_INIT_TIMER (parent_with_total);                                         \
+            __PF_INIT_TIMER (non_with_total);                                            \
+            __PF_INIT_TIMER (total);                                                     \
+                                                                                         \
+            for (j = 0; j < maxfunap[i]; j++) {                                          \
+                __PF_ADD_TIMERS (with_total, with_total, with_modarray_timer[i][j]);     \
+                __PF_ADD_TIMERS (with_total, with_total, with_genarray_timer[i][j]);     \
+                __PF_ADD_TIMERS (with_total, with_total, with_fold_timer[i][j]);         \
+                __PF_ADD_TIMERS (with_total, with_total, fw_with_modarray_timer[i][j]);  \
+                __PF_ADD_TIMERS (with_total, with_total, fw_with_genarray_timer[i][j]);  \
+                __PF_ADD_TIMERS (with_total, with_total, fw_with_fold_timer[i][j]);      \
+                __PF_ADD_TIMERS (parent_with_total, parent_with_total,                   \
+                                 fw_fun_timer[i][j]);                                    \
+                __PF_ADD_TIMERS (non_with_total, non_with_total, fun_timer[i][j]);       \
+            }                                                                            \
+            __PF_ADD_TIMERS (total, with_total, non_with_total);                         \
+            __PF_ADD_TIMERS (total, total, parent_with_total);                           \
+                                                                                         \
+            if ((total.tv_sec != 0) || (total.tv_usec != 0)) {                           \
+                _SAC_PrintHeader (fun_name[i]);                                          \
+                _SAC_PrintTimePercentage ("time used", __PF_TIMER_SPACE, &total,         \
+                                          total_time);                                   \
+                                                                                         \
+                if (display_with) {                                                      \
+                    _SAC_PrintTimePercentage ("   with-loop(own)", "", &with_total,      \
+                                              &total);                                   \
+                                                                                         \
+                    _SAC_PrintTimePercentage ("   with-loop(parent)", "",                \
+                                              &parent_with_total, &total);               \
+                                                                                         \
+                    _SAC_PrintTimePercentage ("   non-with-loop", "", &non_with_total,   \
+                                              &total);                                   \
+                }                                                                        \
+            }                                                                            \
+        }                                                                                \
     }
 
 #define __PF_INIT_CLOCK()
 
 #define __PF_INIT_TIMER(timer) timer##.tv_sec = timer##.tv_usec = 0
 
-#define __PF_START_CLOCK() getrusage (RUSAGE_SELF, &_SAC_start_timer)
+#define __PF_START_CLOCK() getrusage (RUSAGE_SELF, &__PF_start_timer)
 
-#define __PF_STOP_CLOCK() getrusage (RUSAGE_SELF, &_SAC_stop_timer)
+#define __PF_STOP_CLOCK() getrusage (RUSAGE_SELF, &__PF_stop_timer)
 
 #define __PF_ADD_TO_TIMER(timer)                                                         \
     if (((timer).tv_usec                                                                 \
-         += _SAC_stop_timer.ru_utime.tv_usec - _SAC_start_timer.ru_utime.tv_usec)        \
+         += __PF_stop_timer.ru_utime.tv_usec - __PF_start_timer.ru_utime.tv_usec)        \
         < 0) {                                                                           \
         (timer).tv_usec += 1000000;                                                      \
         (timer).tv_sec                                                                   \
-          += _SAC_stop_timer.ru_utime.tv_sec - _SAC_start_timer.ru_utime.tv_sec - 1;     \
+          += __PF_stop_timer.ru_utime.tv_sec - __PF_start_timer.ru_utime.tv_sec - 1;     \
     } else {                                                                             \
         (timer).tv_sec                                                                   \
-          += _SAC_stop_timer.ru_utime.tv_sec - _SAC_start_timer.ru_utime.tv_sec;         \
+          += __PF_stop_timer.ru_utime.tv_sec - __PF_start_timer.ru_utime.tv_sec;         \
     }
+
+#define __PF_ADD_TIMERS(timer, timer1, timer2)                                           \
+    timer##.tv_sec = timer1##.tv_sec + timer2##.tv_sec;                                  \
+    timer##.tv_usec = timer1##.tv_usec + timer2##.tv_usec
+
+#define __PF_TIMER_SPACE "              "
 
 #else /* PROFILE */
 
-#define PROFILE_SETUP(maxfun)
-#define PROFILE_PRINT()
+#define __PROFILE_SETUP()
+#define __PROFILE_PRINT()
 
 #endif /* PROFILE */
 
@@ -141,7 +241,7 @@ extern void _SAC_PrintProfileFuns (__PF_TIMER *total_time, int display_fun,
  *  Macros for profiling with-loops
  */
 
-#if PROFILE_WITH
+#if (PROFILE_WITH && PROFILE)
 
 #define PROFILE_BEGIN_WITH(str)                                                          \
     __PF_TIMER *__PF_mem_act;                                                            \
@@ -162,13 +262,13 @@ extern void _SAC_PrintProfileFuns (__PF_TIMER *total_time, int display_fun,
     __PF_act_timer = __PF_mem_act;                                                       \
     __PF_START_CLOCK ()
 
-#define DISPLAY_WITH 1
+#define __PF_DISPLAY_WITH 1
 
 #else /* PROFILE_WITH */
 
 #define PROFILE_BEGIN_WITH(str)
 #define PROFILE_END_WITH(str)
-#define DISPLAY_WITH 0
+#define __PF_DISPLAY_WITH 0
 
 #endif /* PROFILE_WITH */
 
@@ -176,7 +276,7 @@ extern void _SAC_PrintProfileFuns (__PF_TIMER *total_time, int display_fun,
  *  Macros for profiling function applications
  */
 
-#if PROFILE_FUN
+#if (PROFILE_FUN && PROFILE)
 
 #define PROFILE_BEGIN_UDF(funno, funapno)                                                \
     {                                                                                    \
@@ -203,13 +303,13 @@ extern void _SAC_PrintProfileFuns (__PF_TIMER *total_time, int display_fun,
     __PF_START_CLOCK ();                                                                 \
     }
 
-#define DISPLAY_FUN 1
+#define __PF_DISPLAY_FUN 1
 
 #else /* PROFILE_FUN */
 
 #define PROFILE_BEGIN_UDF(funno, funapno)
 #define PROFILE_END_UDF(funno, funapno)
-#define DISPLAY_FUN 0
+#define __PF_DISPLAY_FUN 0
 
 #endif /* PROFILE_FUN */
 
@@ -217,7 +317,7 @@ extern void _SAC_PrintProfileFuns (__PF_TIMER *total_time, int display_fun,
  *  Macros for profiling even inlined function applications
  */
 
-#if PROFILE_INL
+#if (PROFILE_INL && PROFILE)
 
 #define PROFILE_INLINE(x) x
 
@@ -231,7 +331,7 @@ extern void _SAC_PrintProfileFuns (__PF_TIMER *total_time, int display_fun,
  *  Macros for profiling even library function applications
  */
 
-#if PROFILE_LIB
+#if (PROFILE_LIB && PROFILE)
 
 #define PROFILE_LIBRARY(x) x
 
