5d6a76d0d8be9a85a32303e40b3e0da4bc13d70f
Dietmar Kreye <dietmar@computer-scientist.org>
1998-04-07 13:06:01 +0000

    fixed a bug in O2Nnwith:   remove all original parts but one

--- src/compile/Old2NewWith.c
+++ src/compile/Old2NewWith.c
@@ -315,6 +315,8 @@ O2Nnwith (node *arg_node, node *arg_info)
 
     DBUG_ENTER ("O2Nnwith");
 
+    NPART_NEXT (NWITH_PART (arg_node)) = FreeTree (NPART_NEXT (NWITH_PART (arg_node)));
+
     NWITH_CODE (arg_node) = FreeTree (NWITH_CODE (arg_node));
 
     /* generate a new code table */
