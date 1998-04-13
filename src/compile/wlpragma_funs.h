b32733409ba465ea0d18a941db910f21f7ca0bf2
Dietmar Kreye <dietmar@computer-scientist.org>
1998-04-13 19:59:14 +0000

    added ConstSegs

--- src/compile/wlpragma_funs.h
+++ src/compile/wlpragma_funs.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.3  1998/04/13 19:59:06  dkr
+ * added ConstSegs
+ *
  * Revision 1.2  1998/04/13 18:11:13  dkr
  * rcs-header added
  *
@@ -14,6 +17,7 @@
 
 extern node *All (node *segs, node *parms, node *cubes, int dims);
 extern node *Cubes (node *segs, node *parms, node *cubes, int dims);
+extern node *ConstSegs (node *segs, node *parms, node *cubes, int dims);
 extern node *NoBlocking (node *segs, node *parms, node *cubes, int dims);
 extern node *Bv (node *segs, node *parms, node *cubes, int dims);
 extern node *BvL0 (node *segs, node *parms, node *cubes, int dims);
