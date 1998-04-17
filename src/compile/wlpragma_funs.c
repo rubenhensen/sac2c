75506cb6b243796fd3a203a317549487d98d583c
Dietmar Kreye <dietmar@computer-scientist.org>
1998-04-17 02:15:15 +0000

    ConstSegs() is now implemented

--- src/compile/wlpragma_funs.c
+++ src/compile/wlpragma_funs.c
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.5  1998/04/17 02:15:15  dkr
+ * ConstSegs() is now implemented
+ *
  * Revision 1.4  1998/04/13 19:58:40  dkr
  * added ConstSegs (dummy)
  *
@@ -15,10 +18,145 @@
 #include "tree_basic.h"
 #include "free.h"
 #include "DupTree.h"
+#include "precompile.h"
 
 #include "wlpragma_funs.h"
 
 #include "dbug.h"
+
+/******************************************************************************
+ *
+ * function:
+ *   node *IntersectStridesArray(node *strides,
+ *                               node *aelems1, node *aelems2)
+ *
+ * description:
+ *   returns the intersection of the N_WLstride-chain 'strides' with
+ *    the index vector space ['alemes1', 'aelems2'].
+ *    (-> 'aelems1' is the upper, 'aelems2' the lower bound).
+ *
+ ******************************************************************************/
+
+node *
+IntersectStridesArray (node *strides, node *aelems1, node *aelems2)
+{
+    node *stride, *last_stride, *grid, *grid2, *last_grid;
+    int width, old_bound1, old_step, offset;
+    int empty = 0;
+
+    DBUG_ENTER ("IntersectStridesArray");
+
+    last_stride = NULL;
+    stride = strides;
+    while (stride != NULL) {
+
+        DBUG_ASSERT (((aelems1 != NULL) && (aelems2 != NULL)),
+                     "ConstSegs(): arg has wrong dim");
+        DBUG_ASSERT (((NODE_TYPE (EXPRS_EXPR (aelems1)) == N_num)
+                      && (NODE_TYPE (EXPRS_EXPR (aelems2)) == N_num)),
+                     "ConstSegs(): array element not an int");
+
+        /* intersect outline */
+        old_bound1 = WLSTRIDE_BOUND1 (stride);
+        WLSTRIDE_BOUND1 (stride) = MAX (old_bound1, NUM_VAL (EXPRS_EXPR (aelems1)));
+        WLSTRIDE_BOUND2 (stride)
+          = MIN (WLSTRIDE_BOUND2 (stride), NUM_VAL (EXPRS_EXPR (aelems2)));
+
+        old_step = WLSTRIDE_STEP (stride);
+        WLSTRIDE_STEP (stride) = MIN (old_step, width);
+
+        /* adjust grids */
+        width = WLSTRIDE_BOUND2 (stride) - WLSTRIDE_BOUND1 (stride);
+        if (width > 0) {
+            last_grid = NULL;
+            grid = WLSTRIDE_CONTENTS (stride);
+            do {
+                /* compute offset */
+                offset = GridOffset (WLSTRIDE_BOUND1 (stride), old_bound1, old_step,
+                                     WLGRID_BOUND2 (grid));
+
+                if (offset <= WLGRID_BOUND1 (grid)) {
+                    /* grid is still in one pice :) */
+                    grid2 = NULL;
+                    WLGRID_BOUND1 (grid) -= offset;
+                    WLGRID_BOUND2 (grid) -= offset;
+                } else {
+                    /* the grid is split into two parts :( */
+                    grid2 = DupNode (grid);
+                    /* first part: */
+                    WLGRID_BOUND1 (grid) -= offset - old_step;
+                    WLGRID_BOUND2 (grid) = old_step;
+                    /* second part: duplicate old grid first */
+                    WLGRID_BOUND1 (grid2) = 0;
+                    WLGRID_BOUND2 (grid2) -= offset;
+                }
+
+                if (WLGRID_BOUND1 (grid) >= width) {
+                    if (last_grid == NULL) {
+                        grid = WLSTRIDE_CONTENTS (stride)
+                          = FreeNode (WLSTRIDE_CONTENTS (stride));
+                    } else {
+                        grid = WLGRID_NEXT (last_grid)
+                          = FreeNode (WLGRID_NEXT (last_grid));
+                    }
+                } else {
+                    WLGRID_BOUND2 (grid) = MIN (WLGRID_BOUND2 (grid), width);
+
+                    if (WLGRID_NEXTDIM (grid) != NULL) {
+                        WLGRID_NEXTDIM (grid)
+                          = IntersectStridesArray (WLGRID_NEXTDIM (grid),
+                                                   EXPRS_NEXT (aelems1),
+                                                   EXPRS_NEXT (aelems2));
+                        if (WLGRID_NEXTDIM (grid) == NULL) {
+                            empty = 1;
+                        }
+                    }
+
+                    last_grid = grid;
+                    grid = WLGRID_NEXT (grid);
+                }
+
+                if (grid2 != NULL) {
+                    WLGRID_BOUND2 (grid2) = MIN (WLGRID_BOUND2 (grid2), width);
+
+                    if (WLGRID_NEXTDIM (grid2) != NULL) {
+                        WLGRID_NEXTDIM (grid2)
+                          = IntersectStridesArray (WLGRID_NEXTDIM (grid2),
+                                                   EXPRS_NEXT (aelems1),
+                                                   EXPRS_NEXT (aelems2));
+                        if (WLGRID_NEXTDIM (grid2) == NULL) {
+                            empty = 1;
+                        }
+                    }
+
+                    if (last_grid == NULL) {
+                        WLGRID_NEXT (grid2) = WLSTRIDE_CONTENTS (stride);
+                        WLSTRIDE_CONTENTS (stride) = grid2;
+                    } else {
+                        WLGRID_NEXT (grid2) = WLGRID_NEXT (last_grid);
+                        WLGRID_NEXT (last_grid) = grid2;
+                    }
+                    last_grid = grid2;
+                }
+            } while ((empty == 0) && (grid != NULL));
+        }
+
+        if ((width < 1) || (empty == 1)) {
+            /* stride is empty -> remove it */
+            if (last_stride == NULL) {
+                stride = strides = FreeNode (strides);
+            } else {
+                stride = WLSTRIDE_NEXT (last_stride)
+                  = FreeNode (WLSTRIDE_NEXT (last_stride));
+            }
+        } else {
+            last_stride = stride;
+            stride = WLSTRIDE_NEXT (stride);
+        }
+    }
+
+    DBUG_RETURN (strides);
+}
 
 /******************************************************************************
  *
@@ -33,20 +171,19 @@
 long *
 Array2Bv (node *array, long *bv, int dims)
 {
-    node *array1;
     int d;
 
     DBUG_ENTER ("Array2Bv");
 
-    array1 = ARRAY_AELEMS (EXPRS_EXPR (array));
+    array = ARRAY_AELEMS (array);
     for (d = 0; d < dims; d++) {
-        DBUG_ASSERT ((array1 != NULL), "Bv(): bv-arg has wrong dim");
-        DBUG_ASSERT ((NODE_TYPE (EXPRS_EXPR (array1)) == N_num),
+        DBUG_ASSERT ((array != NULL), "Bv(): bv-arg has wrong dim");
+        DBUG_ASSERT ((NODE_TYPE (EXPRS_EXPR (array)) == N_num),
                      "Bv(): bv-arg not an int-array");
-        bv[d] = NUM_VAL (EXPRS_EXPR (array1));
-        array1 = EXPRS_NEXT (array1);
+        bv[d] = NUM_VAL (EXPRS_EXPR (array));
+        array = EXPRS_NEXT (array);
     }
-    DBUG_ASSERT ((array1 == NULL), "Bv(): bv-arg has wrong dim");
+    DBUG_ASSERT ((array == NULL), "Bv(): bv-arg has wrong dim");
 
     DBUG_RETURN (bv);
 }
@@ -111,6 +248,8 @@ All (node *segs, node *parms, node *cubes, int dims)
 {
     DBUG_ENTER ("All");
 
+    DBUG_ASSERT ((parms == NULL), "All(): too many parms found");
+
     if (segs != NULL) {
         segs = FreeTree (segs);
     }
@@ -140,6 +279,8 @@ Cubes (node *segs, node *parms, node *cubes, int dims)
     node *new_seg, *last_seg;
 
     DBUG_ENTER ("Cubes");
+
+    DBUG_ASSERT ((parms == NULL), "Cubes(): too many parms found");
 
     if (segs != NULL) {
         segs = FreeTree (segs);
@@ -182,20 +323,26 @@ Cubes (node *segs, node *parms, node *cubes, int dims)
 node *
 ConstSegs (node *segs, node *parms, node *cubes, int dims)
 {
-    node *new_seg, *last_seg, *new_cubes;
+    node *new_cubes, *new_seg, *last_seg;
 
     DBUG_ENTER ("ConstSegs");
 
-    DBUG_ASSERT ((0), "ConstSegs(): not yet implemented :(");
+    if (segs != NULL) {
+        segs = FreeTree (segs);
+    }
 
-    segs = NULL;
     while (parms != NULL) {
-
-        DBUG_ASSERT ((NODE_TYPE (parms) == N_array),
+        DBUG_ASSERT ((EXPRS_NEXT (parms) != NULL), "ConstSegs(): upper bound not found");
+        DBUG_ASSERT (((NODE_TYPE (EXPRS_EXPR (parms)) == N_array)
+                      && (NODE_TYPE (EXPRS_EXPR (EXPRS_NEXT (parms))) == N_array)),
                      "ConstSegs(): argument is not an array");
 
-        new_cubes = NULL;
-        new_seg = MakeWLseg (dims, DupNode (new_cubes), NULL);
+        new_cubes
+          = IntersectStridesArray (DupTree (cubes, NULL),
+                                   ARRAY_AELEMS (EXPRS_EXPR (parms)),
+                                   ARRAY_AELEMS (EXPRS_EXPR (EXPRS_NEXT (parms))));
+
+        new_seg = MakeWLseg (dims, new_cubes, NULL);
 
         if (segs == NULL) {
             segs = new_seg;
@@ -203,7 +350,11 @@ ConstSegs (node *segs, node *parms, node *cubes, int dims)
             WLSEG_NEXT (last_seg) = new_seg;
         }
         last_seg = new_seg;
+
+        parms = EXPRS_NEXT (EXPRS_NEXT (parms));
     }
+
+    segs = NoBlocking (segs, parms, cubes, dims);
 
     DBUG_RETURN (segs);
 }
@@ -226,6 +377,8 @@ NoBlocking (node *segs, node *parms, node *cubes, int dims)
     node *seg = segs;
 
     DBUG_ENTER ("NoBlocking");
+
+    DBUG_ASSERT ((parms == NULL), "NoBlocking(): too many parms found");
 
     while (seg != NULL) {
 
@@ -277,22 +430,24 @@ Bv (node *segs, node *parms, node *cubes, int dims)
 
     DBUG_ENTER ("Bv");
 
+    DBUG_ASSERT ((parms != NULL), "Bv(): first parm not found");
     DBUG_ASSERT ((NODE_TYPE (EXPRS_EXPR (parms)) == N_num),
                  "Bv(): first argument not an int");
 
     level = NUM_VAL (EXPRS_EXPR (parms));
     parms = EXPRS_NEXT (parms);
 
-    if ((parms != NULL) && (level <= WLSEG_BLOCKS (seg))) {
+    if ((parms != NULL) && (seg != NULL) && (level <= WLSEG_BLOCKS (seg))) {
 
         while ((seg != NULL) && (EXPRS_NEXT (parms) != NULL)) {
             DBUG_ASSERT ((NODE_TYPE (EXPRS_EXPR (parms)) == N_array),
                          "Bv(): bv-arg not an array");
 
             if (level < WLSEG_BLOCKS (seg)) {
-                WLSEG_BV (seg, level) = Array2Bv (parms, WLSEG_BV (seg, level), dims);
+                WLSEG_BV (seg, level)
+                  = Array2Bv (EXPRS_EXPR (parms), WLSEG_BV (seg, level), dims);
             } else {
-                WLSEG_UBV (seg) = Array2Bv (parms, WLSEG_UBV (seg), dims);
+                WLSEG_UBV (seg) = Array2Bv (EXPRS_EXPR (parms), WLSEG_UBV (seg), dims);
             }
 
             seg = WLSTRIDE_NEXT (seg);
@@ -302,9 +457,10 @@ Bv (node *segs, node *parms, node *cubes, int dims)
         while (seg != NULL) {
 
             if (level < WLSEG_BLOCKS (seg)) {
-                WLSEG_BV (seg, level) = Array2Bv (parms, WLSEG_BV (seg, level), dims);
+                WLSEG_BV (seg, level)
+                  = Array2Bv (EXPRS_EXPR (parms), WLSEG_BV (seg, level), dims);
             } else {
-                WLSEG_UBV (seg) = Array2Bv (parms, WLSEG_UBV (seg), dims);
+                WLSEG_UBV (seg) = Array2Bv (EXPRS_EXPR (parms), WLSEG_UBV (seg), dims);
             }
 
             seg = WLSTRIDE_NEXT (seg);
@@ -395,9 +551,11 @@ Ubv (node *segs, node *parms, node *cubes, int dims)
 {
     DBUG_ENTER ("Ubv");
 
-    parms = MakeExprs (MakeNum (WLSEG_BLOCKS (segs)), parms);
-    segs = Bv (segs, parms, cubes, dims);
-    parms = FreeNode (parms);
+    if (segs != NULL) {
+        parms = MakeExprs (MakeNum (WLSEG_BLOCKS (segs)), parms);
+        segs = Bv (segs, parms, cubes, dims);
+        parms = FreeNode (parms);
+    }
 
     DBUG_RETURN (segs);
 }
