f45de1bb8f3ec70a42e71096bb1fa590f63ac7c7
Dietmar Kreye <dietmar@computer-scientist.org>
1998-04-17 19:16:20 +0000

    lifting of spmd-fun is performed in precompile now

--- src/concurrent/spmdregions.c
+++ src/concurrent/spmdregions.c
@@ -1,29 +1,11 @@
 /*
  *
  * $Log$
+ * Revision 1.2  1998/04/17 19:16:20  dkr
+ * lifting of spmd-fun is performed in precompile now
+ *
  * Revision 1.1  1998/04/17 17:22:07  dkr
  * Initial revision
- *
- * Revision 1.6  1998/04/16 21:45:54  dkr
- * fixed a few bugs in ConcAssert:
- *   AP_FUNDEF of concregion-ap is set correctly
- *   refcounters of lifted concregion are now (more ;) correct
- *
- * Revision 1.5  1998/04/09 21:33:33  dkr
- * fixed a bug in ConcAssign
- *
- * Revision 1.4  1998/04/09 14:01:37  dkr
- * new funs ConcFundef, ConcNcode, ConcNpart, ...
- *
- * Revision 1.3  1998/04/03 21:07:33  dkr
- * changed ConcAssign
- *
- * Revision 1.2  1998/04/03 11:59:40  dkr
- * include order is now correct :)
- *
- * Revision 1.1  1998/04/03 11:37:21  dkr
- * Initial revision
- *
  *
  *
  */
@@ -36,87 +18,10 @@
 #include "internal_lib.h"
 #include "traverse.h"
 
-#include "typecheck.h"
 #include "optimize.h"
-#include "DupTree.h"
 #include "dbug.h"
 
 #include "spmdregions.h"
-
-/* returns 0 for refcounting-objects and -1 otherwise */
-#define GET_ZERO_REFCNT(vardec)                                                          \
-    (NODE_TYPE (vardec) == N_arg) ? ((ARG_REFCNT (vardec) >= 0) ? 0 : -1)                \
-                                  : ((VARDEC_REFCNT (vardec) >= 0) ? 0 : -1)
-
-/******************************************************************************
- *
- * function:
- *   char *SpmdFunName(char *name)
- *
- * description:
- *   create a name for a spmd-fun.
- *   this name is build from the name of the current scope ('name')
- *    and an unambiguous number.
- *
- ******************************************************************************/
-
-char *
-SpmdFunName (char *name)
-{
-    static no;
-    char *funname;
-
-    DBUG_ENTER ("SpmdFunName");
-
-    funname = (char *)Malloc ((strlen (name) + 10) * sizeof (char));
-    sprintf (funname, "SPMD_%s_%d", name, no);
-
-    DBUG_RETURN (funname);
-}
-
-/******************************************************************************
- *
- * function:
- *   node *FindVardec(int varno, node *fundef)
- *
- * description:
- *   returns the vardec of var number 'varno'
- *
- ******************************************************************************/
-
-node *
-FindVardec (int varno, node *fundef)
-{
-    node *tmp, *result = NULL;
-
-    DBUG_ENTER ("FindVardec");
-
-    if (result == NULL) {
-        tmp = FUNDEF_ARGS (fundef);
-        while (tmp != NULL) {
-            if (ARG_VARNO (tmp) == varno) {
-                result = tmp;
-                break;
-            } else {
-                tmp = ARG_NEXT (tmp);
-            }
-        }
-    }
-
-    if (result == NULL) {
-        tmp = BLOCK_VARDEC (FUNDEF_BODY (fundef));
-        while (tmp != NULL) {
-            if (VARDEC_VARNO (tmp) == varno) {
-                result = tmp;
-                break;
-            } else {
-                tmp = VARDEC_NEXT (tmp);
-            }
-        }
-    }
-
-    DBUG_RETURN (result);
-}
 
 /******************************************************************************
  *
@@ -169,20 +74,13 @@ SpmdFundef (node *arg_node, node *arg_info)
 node *
 SpmdAssign (node *arg_node, node *arg_info)
 {
-    node *spmdlet, *fundefs, *old_vardec;
-    node *args, *arg, *last_arg;
-    node *fargs, *farg, *last_farg;
-    ids *lets, *let, *last_let;
-    node *localvars, *localvar, *last_localvar;
-    node *retexprs, *retexpr, *last_retexpr;
-    types *rettypes, *rettype, *last_rettype;
-    node *ret, *fundec, *ap, *spmd;
-    char *name;
-    int varno, i;
+    node *spmd, *spmdlet;
+    int varno;
 
     DBUG_ENTER ("SpmdAssign");
 
     spmdlet = ASSIGN_INSTR (arg_node);
+    varno = FUNDEF_VARNO (INFO_SPMD_FUNDEF (arg_info));
 
     if ((NODE_TYPE (spmdlet) == N_let) && (NODE_TYPE (LET_EXPR (spmdlet)) == N_Nwith)) {
 
@@ -195,15 +93,9 @@ SpmdAssign (node *arg_node, node *arg_info)
         ASSIGN_INSTR (arg_node) = spmd;
 
         /*
-         * we will often need the following values
-         */
-        fundefs = INFO_SPMD_FUNDEF (arg_info);
-        varno = FUNDEF_VARNO (INFO_SPMD_FUNDEF (arg_info));
-
-        /*
          * generate masks (for SPMD_USEDVARS, SPMD_DEFVARS, ...)
          *
-         * we must start at the fundef node, to get the right value for VARNO
+         * we must start at the fundef node to get the right value for VARNO
          *  in GenerateMasks().
          */
         INFO_SPMD_FUNDEF (arg_info) = GenerateMasks (INFO_SPMD_FUNDEF (arg_info), NULL);
@@ -224,175 +116,6 @@ SpmdAssign (node *arg_node, node *arg_info)
          */
         MinusMask (SPMD_USEDVARS (spmd), INFO_SPMD_LOCALUSED (arg_info), varno);
         MinusMask (SPMD_DEFVARS (spmd), INFO_SPMD_LOCALDEF (arg_info), varno);
-
-        /*
-         * generate SPMD_FUNDEC, SPMD_VARDEC, SPMD_AP_LET
-         *  for this spmd region
-         */
-        name = SpmdFunName (FUNDEF_NAME (INFO_SPMD_FUNDEF (arg_info)));
-
-        args = NULL;
-        fargs = NULL;
-        lets = NULL;
-        localvars = NULL;
-        retexprs = NULL;
-        rettypes = NULL;
-        for (i = 0; i < varno; i++) {
-            DBUG_ASSERT ((((SPMD_USEDVARS (spmd))[i] >= 0)
-                          && ((SPMD_DEFVARS (spmd))[i] >= 0)),
-                         "wrong mask entry found");
-
-            if ((SPMD_USEDVARS (spmd))[i] > 0) {
-
-                old_vardec = FindVardec (i, fundefs);
-
-                if (NODE_TYPE (old_vardec) == N_vardec) {
-                    arg = MakeId (StringCopy (VARDEC_NAME (old_vardec)), NULL,
-                                  VARDEC_STATUS (old_vardec));
-                    ID_ATTRIB (arg) = VARDEC_ATTRIB (old_vardec);
-
-                    farg = MakeArg (StringCopy (VARDEC_NAME (old_vardec)),
-                                    DuplicateTypes (VARDEC_TYPE (old_vardec), 1),
-                                    VARDEC_STATUS (old_vardec),
-                                    VARDEC_ATTRIB (old_vardec), NULL);
-                } else {
-                    arg = MakeId (StringCopy (ARG_NAME (old_vardec)), NULL,
-                                  ARG_STATUS (old_vardec));
-                    ID_ATTRIB (arg) = ARG_ATTRIB (old_vardec);
-
-                    farg
-                      = MakeArg (StringCopy (ARG_NAME (old_vardec)),
-                                 DuplicateTypes (ARG_TYPE (old_vardec), 1),
-                                 ARG_STATUS (old_vardec), ARG_ATTRIB (old_vardec), NULL);
-                }
-
-                ID_VARDEC (arg) = old_vardec;
-                ID_MAKEUNIQUE (arg) = 0;
-                ID_REFCNT (arg) = GET_ZERO_REFCNT (old_vardec); /* dummy value */
-                arg = MakeExprs (arg, NULL);
-
-                ARG_REFCNT (farg) = GET_ZERO_REFCNT (old_vardec); /* dummy value */
-                ARG_VARNO (farg) = -1; /* dummy value --- not needed anymore! */
-
-                if (args == NULL) {
-                    args = arg;
-                    fargs = farg;
-                } else {
-                    EXPRS_NEXT (last_arg) = arg;
-                    ARG_NEXT (last_farg) = farg;
-                }
-                last_arg = arg;
-                last_farg = farg;
-            }
-
-            if ((SPMD_DEFVARS (spmd))[i] > 0) {
-
-                old_vardec = FindVardec (i, fundefs);
-
-                if (NODE_TYPE (old_vardec) == N_vardec) {
-                    let = MakeIds (StringCopy (VARDEC_NAME (old_vardec)), NULL,
-                                   VARDEC_STATUS (old_vardec));
-                    IDS_ATTRIB (let) = VARDEC_ATTRIB (old_vardec);
-
-                    if ((SPMD_USEDVARS (spmd))[i] > 0) {
-                        /* the current var is an arg of the conregion-fun */
-                        localvar = NULL;
-                    } else {
-                        /* the current var is a local var of the conregion-fun */
-                        localvar = DupNode (old_vardec);
-                    }
-
-                    retexpr = MakeId (StringCopy (VARDEC_NAME (old_vardec)), NULL,
-                                      VARDEC_STATUS (old_vardec));
-                    ID_ATTRIB (retexpr) = VARDEC_ATTRIB (old_vardec);
-
-                    rettype = DuplicateTypes (VARDEC_TYPE (old_vardec), 1);
-                } else {
-                    let = MakeIds (StringCopy (ARG_NAME (old_vardec)), NULL,
-                                   ARG_STATUS (old_vardec));
-                    IDS_ATTRIB (let) = ARG_ATTRIB (old_vardec);
-
-                    if ((SPMD_USEDVARS (spmd))[i] > 0) {
-                        /* the current var is an arg of the conregion-fun */
-                        localvar = NULL;
-                    } else {
-                        /* the current var is a local var of the conregion-fun */
-                        localvar
-                          = MakeVardec (StringCopy (ARG_NAME (old_vardec)),
-                                        DuplicateTypes (ARG_TYPE (old_vardec), 1), NULL);
-                        VARDEC_STATUS (localvar) = ARG_STATUS (old_vardec);
-                        VARDEC_ATTRIB (localvar) = ARG_ATTRIB (old_vardec);
-                    }
-
-                    retexpr = MakeId (StringCopy (ARG_NAME (old_vardec)), NULL,
-                                      ARG_STATUS (old_vardec));
-                    ID_ATTRIB (retexpr) = ARG_ATTRIB (old_vardec);
-
-                    rettype = DuplicateTypes (ARG_TYPE (old_vardec), 1);
-                }
-
-                IDS_VARDEC (let) = old_vardec;
-                /* dummy value --- do not change rc after aplication: */
-                IDS_REFCNT (let) = GET_ZERO_REFCNT (old_vardec) + 1;
-
-                if (localvar != NULL) {
-                    VARDEC_TYPEDEF (localvar) = NULL;
-                    VARDEC_REFCNT (localvar)
-                      = GET_ZERO_REFCNT (old_vardec); /* dummy value */
-                    VARDEC_VARNO (localvar) = -1; /* dummy value --- not needed anymore */
-                }
-
-                if ((SPMD_USEDVARS (spmd))[i] > 0) {
-                    /* the current var is an arg of the conregion-fun */
-                    ID_VARDEC (retexpr) = farg;
-                } else {
-                    /* the current var is a local var of the conregion-fun */
-                    ID_VARDEC (retexpr) = localvar;
-                }
-                ID_MAKEUNIQUE (retexpr) = 0;
-                ID_REFCNT (retexpr) = GET_ZERO_REFCNT (old_vardec); /* dummy value */
-                retexpr = MakeExprs (retexpr, NULL);
-
-                if (lets == NULL) {
-                    lets = let;
-                    retexprs = retexpr;
-                    rettypes = rettype;
-                } else {
-                    IDS_NEXT (last_let) = let;
-                    EXPRS_NEXT (last_retexpr) = retexpr;
-                    TYPES_NEXT (last_rettype) = rettype;
-                }
-                last_let = let;
-                last_retexpr = retexpr;
-                last_rettype = rettype;
-
-                if (localvar != NULL) {
-                    if (localvars == NULL) {
-                        localvars = localvar;
-                    } else {
-                        VARDEC_NEXT (last_localvar) = localvar;
-                    }
-                    last_localvar = localvar;
-                }
-            }
-        }
-
-        ret = MakeReturn (retexprs);
-        RETURN_INWITH (ret) = 0;
-
-        fundec = MakeFundef (name, NULL, rettypes, fargs, NULL, NULL);
-        FUNDEF_STATUS (fundec) = ST_spmdfun;
-        FUNDEF_ATTRIB (fundec) = ST_regular;
-        FUNDEF_INLINE (fundec) = 0;
-        FUNDEF_RETURN (fundec) = ret;
-
-        ap = MakeAp (name, NULL, args);
-        AP_FUNDEF (ap) = fundec;
-        AP_ATFLAG (ap) = 1;
-
-        SPMD_FUNDEC (spmd) = fundec;
-        SPMD_VARDEC (spmd) = localvars;
-        SPMD_AP_LET (spmd) = MakeLet (ap, lets);
 
         /*
          * we only traverse the following assignments to prevent nested
