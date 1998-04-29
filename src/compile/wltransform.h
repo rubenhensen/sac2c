8918e2369c38769b28b3283956eeb42d0816ca2f
Dietmar Kreye <dietmar@computer-scientist.org>
1998-04-29 17:17:17 +0000

    Initial revision

---
+++ src/compile/wltransform.h
@@ -0,0 +1,25 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  1998/04/29 17:17:17  dkr
+ * Initial revision
+ *
+ *
+ *
+ */
+
+#ifndef _sac_wltransform_h
+
+#define _sac_wltransform_h
+
+extern node *WlTransform (node *syntax_tree);
+
+extern node *WLTRANwith (node *arg_node, node *arg_info);
+extern node *WLTRANcode (node *arg_node, node *arg_info);
+extern node *WLTRAFundef (node *arg_node, node *arg_info);
+extern node *WLTRAAssign (node *arg_node, node *arg_info);
+
+extern int GridOffset (int new_bound1, int bound1, int step, int grid_b2);
+extern node *InsertWLnodes (node *nodes, node *insert_nodes);
+
+#endif /* _sac_wltransform_h */
