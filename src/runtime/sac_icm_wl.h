e8c2691eeeb1998358fe17d1074bfe4b4ff62307
Dietmar Kreye <dietmar@computer-scientist.org>
1998-05-02 17:49:52 +0000

    Initial revision

---
+++ src/runtime/sac_icm_wl.h
@@ -0,0 +1,32 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  1998/05/02 17:49:52  dkr
+ * Initial revision
+ *
+ *
+ *
+ */
+
+/*****************************************************************************
+ *
+ * file:   sac_icm_wl.h
+ *
+ * description:
+ *
+ *   This file is part of the SAC standard header file sac.h
+ *
+ *   It provides definitions of Intermediate Code Macros (ICM)
+ *   implemented as real macros.
+ *
+ *****************************************************************************/
+
+#ifndef SAC_ICM_WL_H
+
+#define SAC_ICM_WL_H
+
+/*
+ * Macros used for compilation of with-loop:
+ */
+
+#endif /* SAC_ICM_WL_H */
