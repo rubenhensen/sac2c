e2782b67bbc9fe8cc962c2d86702f2265886f17c
Dietmar Kreye <dietmar@computer-scientist.org>
1998-05-06 21:42:50 +0000

    added WLTRALet

--- src/compile/wltransform.h
+++ src/compile/wltransform.h
@@ -1,8 +1,8 @@
 /*
  *
  * $Log$
- * Revision 1.3  1998/05/06 21:39:40  dkr
- * added WLTRAAssign
+ * Revision 1.4  1998/05/06 21:42:50  dkr
+ * added WLTRALet
  *
  * Revision 1.2  1998/05/06 18:09:04  dkr
  * removed WLTRAAssign
@@ -22,7 +22,7 @@ extern node *WlTransform (node *syntax_tree);
 extern node *WLTRANwith (node *arg_node, node *arg_info);
 extern node *WLTRANcode (node *arg_node, node *arg_info);
 extern node *WLTRAFundef (node *arg_node, node *arg_info);
-extern node *WLTRAAssign (node *arg_node, node *arg_info);
+extern node *WLTRALet (node *arg_node, node *arg_info);
 
 extern int GridOffset (int new_bound1, int bound1, int step, int grid_b2);
 extern node *InsertWLnodes (node *nodes, node *insert_nodes);
