1112cc18749bc6a5fe336e5d02e3e1caca316d66
Dietmar Kreye <dietmar@computer-scientist.org>
1998-05-06 21:39:40 +0000

    added WLTRAAssign

--- src/compile/wltransform.h
+++ src/compile/wltransform.h
@@ -1,12 +1,14 @@
 /*
  *
  * $Log$
+ * Revision 1.3  1998/05/06 21:39:40  dkr
+ * added WLTRAAssign
+ *
  * Revision 1.2  1998/05/06 18:09:04  dkr
  * removed WLTRAAssign
  *
  * Revision 1.1  1998/04/29 17:17:17  dkr
  * Initial revision
- *
  *
  *
  */
@@ -20,6 +22,7 @@ extern node *WlTransform (node *syntax_tree);
 extern node *WLTRANwith (node *arg_node, node *arg_info);
 extern node *WLTRANcode (node *arg_node, node *arg_info);
 extern node *WLTRAFundef (node *arg_node, node *arg_info);
+extern node *WLTRAAssign (node *arg_node, node *arg_info);
 
 extern int GridOffset (int new_bound1, int bound1, int step, int grid_b2);
 extern node *InsertWLnodes (node *nodes, node *insert_nodes);
