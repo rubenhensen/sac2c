68c5f65a12285cf51ec484cd2eef04252ad2ee99
Clemens Grelck <cg@sac-home.org>
1998-05-07 11:14:59 +0000

    converted to new naming conventions

--- src/runtime/sac_idx.h
+++ src/runtime/sac_idx.h
@@ -1,12 +1,11 @@
 /*
  *
  * $Log$
+ * Revision 1.2  1998/05/07 11:14:59  cg
+ * converted to new naming conventions
+ *
  * Revision 1.1  1998/05/07 08:38:05  cg
  * Initial revision
- *
- * Revision 1.1  1998/03/19 16:39:02  cg
- * Initial revision
- *
  *
  */
 
@@ -32,16 +31,16 @@
  * ===========================================
  */
 
-#define ND_IDX_PSI_S(s, a, res)                                                          \
+#define SAC_ND_IDX_PSI_S(s, a, res)                                                      \
     PRINT_PRF (("ND_IDX_PSI_S( %s, %s, %s)\n", #s, #a, #res));                           \
-    res = ND_A_FIELD (a)[s];
+    res = SAC_ND_A_FIELD (a)[s];
 
-#define ND_IDX_PSI_A(s, a, res)                                                          \
+#define SAC_ND_IDX_PSI_A(s, a, res)                                                      \
     PRINT_PRF (("ND_IDX_PSI_A( %s, %s, %s)\n", #s, #a, #res));                           \
     {                                                                                    \
         int __i, __s = s;                                                                \
-        for (__i = 0; __i < ND_A_SIZE (res); __i++)                                      \
-            ND_A_FIELD (res)[__i] = ND_A_FIELD (a)[__s++];                               \
+        for (__i = 0; __i < SAC_ND_A_SIZE (res); __i++)                                  \
+            SAC_ND_A_FIELD (res)[__i] = SAC_ND_A_FIELD (a)[__s++];                       \
     };
 
 /*
@@ -49,61 +48,61 @@
  * ================================================
  */
 
-#define ND_IDX_MODARRAY_AxVxA_CHECK_REUSE(line, basetype, res, a, s, val)                \
+#define SAC_ND_IDX_MODARRAY_AxVxA_CHECK_REUSE(line, basetype, res, a, s, val)            \
     PRINT_PRF (("ND_IDX_MODARRAY_AxVxA_CHECK_REUSE( %s, %s, %s, %s, %s, %s)\n", #line,   \
                 #basetype, #res, #a, #s, #val));                                         \
-    ND_CHECK_REUSE_ARRAY (a, res)                                                        \
+    SAC_ND_CHECK_REUSE_ARRAY (a, res)                                                    \
     {                                                                                    \
         int __i;                                                                         \
-        ND_ALLOC_ARRAY (basetype, res, 0);                                               \
+        SAC_ND_ALLOC_ARRAY (basetype, res, 0);                                           \
         for (__i = 0; __i < s; __i++)                                                    \
-            ND_A_FIELD (res)[__i] = ND_A_FIELD (a)[__i];                                 \
-        for (i = i + ND_A_SIZE (val); __i < ND_A_SIZE (res); __i++)                      \
-            ND_A_FIELD (res)[__i] = ND_A_FIELD (a)[__i];                                 \
+            SAC_ND_A_FIELD (res)[__i] = SAC_ND_A_FIELD (a)[__i];                         \
+        for (i = i + SAC_ND_A_SIZE (val); __i < SAC_ND_A_SIZE (res); __i++)              \
+            SAC_ND_A_FIELD (res)[__i] = SAC_ND_A_FIELD (a)[__i];                         \
     }                                                                                    \
     {                                                                                    \
         int __i, __s;                                                                    \
-        for (__s = 0, __i = s; __s < ND_A_SIZE (val); __i++, __s++)                      \
-            ND_A_FIELD (res)[__i] = ND_A_FIELD (val)[__s];                               \
+        for (__s = 0, __i = s; __s < SAC_ND_A_SIZE (val); __i++, __s++)                  \
+            SAC_ND_A_FIELD (res)[__i] = SAC_ND_A_FIELD (val)[__s];                       \
     }
 
-#define ND_IDX_MODARRAY_AxVxA(line, basetype, res, a, s, val)                            \
+#define SAC_ND_IDX_MODARRAY_AxVxA(line, basetype, res, a, s, val)                        \
     PRINT_PRF (("ND_IDX_MODARRAY_AxVxA( %s, %s, %s, %s, %s, %s)\n", #line, #basetype,    \
                 #res, #a, #s, #val));                                                    \
     {                                                                                    \
         int __i, __s;                                                                    \
-        ND_ALLOC_ARRAY (basetype, res, 0);                                               \
+        SAC_ND_ALLOC_ARRAY (basetype, res, 0);                                           \
         for (__i = 0; __i < s; __i++)                                                    \
-            ND_A_FIELD (res)[__i] = ND_A_FIELD (a)[__i];                                 \
-        for (__s = 0; __s < ND_A_SIZE (val); __i++, __s++)                               \
-            ND_A_FIELD (res)[__i] = ND_A_FIELD (val)[__s];                               \
-        for (; __i < ND_A_SIZE (res); __i++)                                             \
-            ND_A_FIELD (res)[__i] = ND_A_FIELD (a)[__i];                                 \
+            SAC_ND_A_FIELD (res)[__i] = SAC_ND_A_FIELD (a)[__i];                         \
+        for (__s = 0; __s < SAC_ND_A_SIZE (val); __i++, __s++)                           \
+            SAC_ND_A_FIELD (res)[__i] = SAC_ND_A_FIELD (val)[__s];                       \
+        for (; __i < SAC_ND_A_SIZE (res); __i++)                                         \
+            SAC_ND_A_FIELD (res)[__i] = SAC_ND_A_FIELD (a)[__i];                         \
     }
 
-#define ND_IDX_MODARRAY_AxVxS_CHECK_REUSE(line, basetype, res, a, s, val)                \
+#define SAC_ND_IDX_MODARRAY_AxVxS_CHECK_REUSE(line, basetype, res, a, s, val)            \
     PRINT_PRF (("ND_IDX_MODARRAY_AxVxS_CHECK_REUSE( %s, %s, %s, %s, %s, %s)\n", #line,   \
                 #basetype, #res, #a, #s, #val));                                         \
-    ND_CHECK_REUSE_ARRAY (a, res)                                                        \
+    SAC_ND_CHECK_REUSE_ARRAY (a, res)                                                    \
     {                                                                                    \
         int __i;                                                                         \
-        ND_ALLOC_ARRAY (basetype, res, 0);                                               \
-        for (__i = 0; __i < ND_A_SIZE (res); __i++)                                      \
-            ND_A_FIELD (res)[__i] = ND_A_FIELD (a)[__i];                                 \
+        SAC_ND_ALLOC_ARRAY (basetype, res, 0);                                           \
+        for (__i = 0; __i < SAC_ND_A_SIZE (res); __i++)                                  \
+            SAC_ND_A_FIELD (res)[__i] = SAC_ND_A_FIELD (a)[__i];                         \
     }                                                                                    \
-    ND_A_FIELD (res)[s] = val;
+    SAC_ND_A_FIELD (res)[s] = val;
 
-#define ND_IDX_MODARRAY_AxVxS(line, basetype, res, a, s, val)                            \
+#define SAC_ND_IDX_MODARRAY_AxVxS(line, basetype, res, a, s, val)                        \
     PRINT_PRF (("ND_IDX_MODARRAY_AxVxS( %s, %s, %s, %s, %s, %s)\n", #line, #basetype,    \
                 #res, #a, #s, #val));                                                    \
     {                                                                                    \
         int __i;                                                                         \
-        ND_ALLOC_ARRAY (basetype, res, 0);                                               \
-        for (__i = 0; __i < ND_A_SIZE (res); __i++)                                      \
-            ND_A_FIELD (res)[__i] = ND_A_FIELD (a)[__i];                                 \
+        SAC_ND_ALLOC_ARRAY (basetype, res, 0);                                           \
+        for (__i = 0; __i < SAC_ND_A_SIZE (res); __i++)                                  \
+            SAC_ND_A_FIELD (res)[__i] = SAC_ND_A_FIELD (a)[__i];                         \
     }                                                                                    \
-    ND_A_FIELD (res)[s] = val;
+    SAC_ND_A_FIELD (res)[s] = val;
 
-#define ND_KS_USE_GENVAR_OFFSET(offsetvar, res) offsetvar = res##__destptr;
+#define SAC_ND_KS_USE_GENVAR_OFFSET(offsetvar, res) offsetvar = res##__destptr;
 
 #endif /* SAC_IDX_H */
