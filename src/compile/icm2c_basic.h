7e005d3f642fb2c02cd87b5f59c5a8be0369c27e
Dietmar Kreye <dietmar@computer-scientist.org>
1998-05-07 11:27:08 +0000

    ICM names converted to new naming conventions.

--- src/compile/icm2c_basic.h
+++ src/compile/icm2c_basic.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.2  1998/05/07 11:27:08  dkr
+ * ICM names converted to new naming conventions.
+ *
  * Revision 1.1  1998/05/03 13:07:29  dkr
  * Initial revision
  *
@@ -11,11 +14,11 @@
 #ifndef _icm2c_basic_h
 #define _icm2c_basic_h
 
-#define AccessVect(v, i) fprintf (outfile, "ND_A_FIELD(%s)[%i]", v, i)
+#define AccessVect(v, i) fprintf (outfile, "SAC_ND_A_FIELD(%s)[%i]", v, i)
 
 #define AccessConst(v, i) fprintf (outfile, "%s", v[i])
 
-#define AccessShape(v, i) fprintf (outfile, "ND_KD_A_SHAPE(%s, %d)", v, i)
+#define AccessShape(v, i) fprintf (outfile, "SAC_ND_KD_A_SHAPE(%s, %d)", v, i)
 
 #define VectToOffset2(dim, v_i_str, dima, a_i_str)                                       \
     {                                                                                    \
