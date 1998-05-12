48e28c61b8c73735c1166370712d23849e0ed3b9
Sören Schwartz <info@sac-home.org>
1998-05-12 15:10:48 +0000

    added more functions

--- src/psi-opt/ArrayElimination.h
+++ src/psi-opt/ArrayElimination.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.2  1998/05/12 15:10:48  srs
+ * added more functions
+ *
  * Revision 1.1  1995/07/24 10:00:19  asi
  * Initial revision
  *
@@ -13,8 +16,13 @@
 
 extern node *ArrayElimination (node *arg_node, node *arg_info);
 
+extern node *AEprf (node *arg_node, node *arg_info);
 extern node *AEfundef (node *arg_node, node *arg_info);
 extern node *AEassign (node *arg_node, node *arg_info);
-extern node *AEprf (node *arg_node, node *arg_info);
+extern node *AEcond (node *arg_node, node *arg_info);
+extern node *AEdo (node *arg_node, node *arg_info);
+extern node *AEwhile (node *arg_node, node *arg_info);
+extern node *AEwith (node *arg_node, node *arg_info);
+extern node *AENwith (node *arg_node, node *arg_info);
 
 #endif /* _ArrayElimination_h */
