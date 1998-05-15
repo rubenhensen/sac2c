55e151b963116b756c2cf06d152cab6922b397ae
Clemens Grelck <cg@sac-home.org>
1998-05-15 09:19:29 +0000

    added ICMs MT_SYNC_NONFOLD and MT_SYNC_ONEFOLD_NONFOLD

--- src/compile/icm2c_mt.h
+++ src/compile/icm2c_mt.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.3  1998/05/15 09:19:29  cg
+ * added ICMs MT_SYNC_NONFOLD and MT_SYNC_ONEFOLD_NONFOLD
+ *
  * Revision 1.2  1998/05/13 14:48:03  cg
  * added ICM MT_SYNC_ONEFOLD
  *
@@ -33,8 +36,11 @@ extern void ICMCompileMT_SPMD_FUN_DEC (char *name, char *from, int narg, char **
 extern void ICMCompileMT_SPMD_FUN_RET (int narg, char **vararg);
 extern void ICMCompileMT_START_SYNCBLOCK (int narg, char **vararg);
 extern void ICMCompileMT_SYNC_FOLD (int narg, char **vararg);
+extern void ICMCompileMT_SYNC_NONFOLD ();
 extern void ICMCompileMT_SYNC_ONEFOLD (char *foldtype, char *accu_var, char *tmp_var,
                                        char *foldop);
+extern void ICMCompileMT_SYNC_ONEFOLD_NONFOLD (char *foldtype, char *accu_var,
+                                               char *tmp_var, char *foldop);
 extern void ICMCompileMT_SYNC_FOLD_NONFOLD (int narg, char **vararg);
 extern void ICMCompileMT_CONTINUE (int narg, char **vararg);
 extern void ICMCompileMT_SPMD_BLOCK (char *name, int narg, char **vararg);
