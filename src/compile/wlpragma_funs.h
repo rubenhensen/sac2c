d3918c99eab23ea1ad97131752908ab53a6508d4
Dietmar Kreye <dietmar@computer-scientist.org>
1998-05-25 13:15:42 +0000

    signature of wlpragma-funs changed:   lineno added for error-messages

--- src/compile/wlpragma_funs.h
+++ src/compile/wlpragma_funs.h
@@ -1,6 +1,10 @@
 /*
  *
  * $Log$
+ * Revision 1.4  1998/05/25 13:15:42  dkr
+ * signature of wlpragma-funs changed:
+ *   lineno added for error-messages
+ *
  * Revision 1.3  1998/04/13 19:59:06  dkr
  * added ConstSegs
  *
@@ -15,14 +19,14 @@
 
 #define _sac_wlpragma_funs_h
 
-extern node *All (node *segs, node *parms, node *cubes, int dims);
-extern node *Cubes (node *segs, node *parms, node *cubes, int dims);
-extern node *ConstSegs (node *segs, node *parms, node *cubes, int dims);
-extern node *NoBlocking (node *segs, node *parms, node *cubes, int dims);
-extern node *Bv (node *segs, node *parms, node *cubes, int dims);
-extern node *BvL0 (node *segs, node *parms, node *cubes, int dims);
-extern node *BvL1 (node *segs, node *parms, node *cubes, int dims);
-extern node *BvL2 (node *segs, node *parms, node *cubes, int dims);
-extern node *Ubv (node *segs, node *parms, node *cubes, int dims);
+extern node *All (node *segs, node *parms, node *cubes, int dims, int line);
+extern node *Cubes (node *segs, node *parms, node *cubes, int dims, int line);
+extern node *ConstSegs (node *segs, node *parms, node *cubes, int dims, int line);
+extern node *NoBlocking (node *segs, node *parms, node *cubes, int dims, int line);
+extern node *Bv (node *segs, node *parms, node *cubes, int dims, int line);
+extern node *BvL0 (node *segs, node *parms, node *cubes, int dims, int line);
+extern node *BvL1 (node *segs, node *parms, node *cubes, int dims, int line);
+extern node *BvL2 (node *segs, node *parms, node *cubes, int dims, int line);
+extern node *Ubv (node *segs, node *parms, node *cubes, int dims, int line);
 
 #endif /* _sac_wlpragma_funs_h */
