b10f719d8aec0dab70dd53a390aca1e9397ab49f
Jan-Hendrik Schöler <info@sac-home.org>
1999-05-05 09:14:47 +0000

    VectToOffset2 changed, it prints "0" for empty arrays now. So it is able to handle empty arays now.

--- src/compile/icm2c_basic.h
+++ src/compile/icm2c_basic.h
@@ -1,6 +1,10 @@
 /*
  *
  * $Log$
+ * Revision 2.3  1999/05/05 09:14:47  jhs
+ * VectToOffset2 changed, it prints "0" for empty arrays now.
+ * So it is able to handle empty arays now.
+ *
  * Revision 2.2  1999/04/12 09:37:48  cg
  * All accesses to C arrays are now performed through the new ICMs
  * ND_WRITE_ARRAY and ND_READ_ARRAY. This allows for an integration
@@ -31,22 +35,26 @@
 #define VectToOffset2(dim, v_i_str, dima, a_i_str)                                       \
     {                                                                                    \
         int i;                                                                           \
-        for (i = dim - 1; i > 0; i--) {                                                  \
-            fprintf (outfile, "( ");                                                     \
-            a_i_str;                                                                     \
-            fprintf (outfile, "* ");                                                     \
-        }                                                                                \
-        v_i_str;                                                                         \
-        for (i = 1; i < dim; i++) {                                                      \
-            fprintf (outfile, "+");                                                      \
+        if (dim > 0) {                                                                   \
+            for (i = dim - 1; i > 0; i--) {                                              \
+                fprintf (outfile, "( ");                                                 \
+                a_i_str;                                                                 \
+                fprintf (outfile, "* ");                                                 \
+            }                                                                            \
             v_i_str;                                                                     \
-            fprintf (outfile, ") ");                                                     \
-        }                                                                                \
-        while (i < dima) {                                                               \
-            fprintf (outfile, "*");                                                      \
-            a_i_str;                                                                     \
-            fprintf (outfile, " ");                                                      \
-            i++;                                                                         \
+            for (i = 1; i < dim; i++) {                                                  \
+                fprintf (outfile, "+");                                                  \
+                v_i_str;                                                                 \
+                fprintf (outfile, ") ");                                                 \
+            }                                                                            \
+            while (i < dima) {                                                           \
+                fprintf (outfile, "*");                                                  \
+                a_i_str;                                                                 \
+                fprintf (outfile, " ");                                                  \
+                i++;                                                                     \
+            }                                                                            \
+        } else {                                                                         \
+            fprintf (outfile, "0");                                                      \
         }                                                                                \
     }
 
