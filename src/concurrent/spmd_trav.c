3d59f5fa96c928cccbf99195460b04165c329633
Jan-Hendrik Schöler <info@sac-home.org>
1999-06-15 14:15:23 +0000

    Initial revision generated.

---
+++ src/concurrent/spmd_trav.c
@@ -0,0 +1,10 @@
+/*
+ *
+ * $Log$
+ * Revision 2.1  1999/06/15 14:15:23  jhs
+ * Initial revision generated.
+ *
+ *
+ */
+
+/* spmd_trav traversal of blocks for checking out refcount */
---
+++ src/concurrent/spmd_trav.h
@@ -0,0 +1,14 @@
+/*
+ *
+ * $Log$
+ * Revision 2.1  1999/06/15 14:12:32  jhs
+ * Initial revision generated.
+ *
+ *
+ */
+
+#ifndef _spmd_trav_h
+
+#define _spmd_trav_h
+
+#endif /* _spmd_trav_h */
