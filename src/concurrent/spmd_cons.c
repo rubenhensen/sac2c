66c726c550057f850d46d1ec2035db7e1640ccdf
Jan-Hendrik Schöler <info@sac-home.org>
1999-07-26 11:35:24 +0000

    Initial revision

---
+++ src/concurrent/spmd_cons.c
@@ -0,0 +1,32 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  1999/07/26 11:35:24  jhs
+ * Initial revision
+ *
+ *
+ *
+ */
+
+/*****************************************************************************
+ *
+ * file:   spmd_cons.c
+ *
+ * prefix: SPMDC
+ *
+ * description:
+ *
+ *  ####
+ *
+ *****************************************************************************/
+
+#include "dbug.h"
+
+#include "types.h"
+#include "tree_basic.h"
+#include "tree_compound.h"
+#include "traverse.h"
+#include "DupTree.h"
+#include "DataFlowMask.h"
+#include "globals.h"
+#include "internal_lib.h"
---
+++ src/concurrent/spmd_cons.h
@@ -0,0 +1,28 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  1999/07/26 11:35:24  jhs
+ * Initial revision
+ *
+ *
+ *
+ *
+ */
+
+/*****************************************************************************
+ *
+ * file:   spmd_cons.h
+ *
+ * prefix: SPMDC
+ *
+ * description:
+ *
+ *   header file for spmd_cons.c
+ *
+ *****************************************************************************/
+
+#ifndef SPMD_CONS_H
+
+#define SPMD_CONS_H
+
+#endif /* SPMD_CONS_H */
