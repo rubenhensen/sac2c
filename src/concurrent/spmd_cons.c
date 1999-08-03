a55475ff06a69a14c30b21a2bc6fb588c762363b
Jan-Hendrik Schöler <info@sac-home.org>
1999-08-03 11:43:37 +0000

    Expanded comments.

--- src/concurrent/spmd_cons.c
+++ src/concurrent/spmd_cons.c
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.5  1999/08/03 11:43:37  jhs
+ * Expanded comments.
+ *
  * Revision 1.4  1999/07/30 14:12:39  jhs
  * Bug removed in DBUG_PRINT arguments.
  *
@@ -75,6 +78,9 @@ SPMDCspmd (node *arg_node, node *arg_info)
 
     DBUG_ASSERT (NODE_TYPE (arg_node) == N_spmd, "Wrong NODE_TYPE");
 
+    /*
+     *  build nwe arg_info or secure values
+     */
     if (arg_info == NULL) {
         own_arg_info = TRUE;
         arg_info = MakeInfo ();
@@ -91,39 +97,51 @@ SPMDCspmd (node *arg_node, node *arg_info)
     SPMD_FUNDEF (arg_node) = Trav (SPMD_FUNDEF (arg_node), arg_info);
     first_sync = INFO_SPMDC_FIRSTSYNC (arg_info);
 
-    DBUG_ASSERT (first_sync != NULL, "first sync not found");
+    DBUG_ASSERT (first_sync != NULL, "no first sync-block found");
 
     CONLDisplayMask ("SPMDC", "sync-inout", SYNC_INOUT (first_sync));
     CONLDisplayMask ("SPMDC", "spmd-in", SPMD_IN (arg_node));
 
     /*
-     *  fetching inouts of first sync-block and adding them to
-     *  in-mask of spmd-block
-     *  (needed to pass values produced by extracted prolog-icms)
+     *  To pass values produced by extracted prolog-icms, one needs to
+     *  add the inout-variables of the first sync-block of a spmd-block
+     *  to the in-variables of this spmd-block.
+     *
+     *  This includes to move the vardecs (in the spmd-function), blonging
+     *  to these inout-variables to the args (of the spmd-function).
      */
     fundef = SPMD_FUNDEF (arg_node);
     old_vardec = BLOCK_VARDEC (FUNDEF_BODY (fundef));
     new_vardec = NULL;
     new_args = FUNDEF_ARGS (fundef);
     while (old_vardec != NULL) {
+        /*
+         *  node is modified, so that next will be not available after
+         *  these changes, so get catch it here.
+         */
         next_vardec = VARDEC_NEXT (old_vardec);
         if (DFMTestMaskEntry (SYNC_INOUT (first_sync), NULL, old_vardec)) {
+            /* move to args */
             name = VARDEC_NAME (old_vardec);
+            /* adding to in-mask of spmd */
             DFMSetMaskEntrySet (SPMD_IN (arg_node), name, NULL);
             /*
-             *  Now happens something very ugly:
-             *  Due to back-references, one wants to keep the pointer to the
-             *  vardec which has to be transformed into an arg!!!
+             *  **** Now happens something very ugly: ****
+             *  Because of back-references, one wants to keep the pointer to the
+             *  vardecs, but these have to be args now ...
+             *  (otherwise one would have to traverse the whole thing again).
+             *  So the NODE_TYPE is changed here from N_vardec to N_arg!!!
              */
             NODE_TYPE (old_vardec) = N_arg;
             ARG_NEXT (old_vardec) = new_args;
-            ARG_FUNDEF (old_vardec) = fundef; /* #### really needed??? */
+            ARG_FUNDEF (old_vardec) = fundef;
             ARG_STATUS (old_vardec) = ST_regular;
             ARG_ATTRIB (old_vardec) = ST_regular;
             ARG_REFCNT (old_vardec) = GET_STD_REFCNT (VARDEC, old_vardec);
             ARG_NAIVE_REFCNT (old_vardec) = GET_STD_REFCNT (VARDEC, old_vardec);
             new_args = old_vardec;
         } else {
+            /* no mask is changed, keep as vardec */
             VARDEC_NEXT (old_vardec) = new_vardec;
             new_vardec = old_vardec;
         }
@@ -135,6 +153,9 @@ SPMDCspmd (node *arg_node, node *arg_info)
     CONLDisplayMask ("SPMDC", "sync-inout", SYNC_INOUT (first_sync));
     CONLDisplayMask ("SPMDC", "spmd-in", SPMD_IN (arg_node));
 
+    /*
+     *  delete or reset arg_info
+     */
     if (own_arg_info) {
         FreeTree (arg_info);
     } else {
@@ -152,7 +173,8 @@ SPMDCspmd (node *arg_node, node *arg_info)
  * description:
  *   Traversal of N_sync, during SPMDC.
  *   First sync-block reached from a spmd-block annotates itself at the
- *   arg_info-node.
+ *   arg_info-node. So one knews which sync-block is the first of an
+ *   spmd-block.
  *
  ******************************************************************************/
 node *
