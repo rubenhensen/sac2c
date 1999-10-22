1cefa6423bf5095120dd12257e9de5e51c6631de
Sven-Bodo Scholz <sbs@sac-home.org>
1999-10-22 14:15:19 +0000

    commented and added versions for almost all simpletypes...

--- src/typecheck/cv2cv.c
+++ src/typecheck/cv2cv.c
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.2  1999/10/22 14:15:19  sbs
+ * commented and added versions for almost all simpletypes...
+ *
  * Revision 1.1  1999/10/19 13:02:59  sacbase
  * Initial revision
  *
@@ -7,10 +10,17 @@
  *
  */
 
-#include "tree_basic.h"
+/*
+ * This module provides a set of functions for copying elements of constant vectors
+ * into other constant vectors. The problem with this task is that such a function
+ * has to be overloaded on all potential types. Since C does not support such
+ * specifications, a function table is created which is parameterized over simpletype.
+ * This is done by using the    .mac-mechanism     on type_info.mac in order to
+ * make sure that all simpletypes are dealt with.
+ */
+
 #include "dbug.h"
 
-#include "free.h"
 #include "cv2cv.h"
 
 #define TYP_IFcv2cv(fun) fun
@@ -18,29 +28,50 @@ cv2cvfunptr cv2cv[] = {
 #include "type_info.mac"
 };
 
-/*
- * Functions for copying a tile from a Constant-Vector (void *)
+/******************************************************************************
  *
- */
+ * function:
+ *    void COCv2CvXXX( void *src, int off, int len, void *res, int res_off)
+ *
+ * description:
+ *    Functions for copying elements from one cv to another one!
+ *    Since all these functions are identical apart from the type casts,
+ *    they are implemented using the C macro mechanism.
+ *
+ ******************************************************************************/
 
 #define COCv2CvTEMPLATE(type, ext)                                                       \
-    void *COCv2Cv##ext (void *elems, int offset, int length)                             \
+    void COCv2Cv##ext (void *src, int off, int len, void *res, int res_off)              \
     {                                                                                    \
-        type *res;                                                                       \
         int i;                                                                           \
                                                                                          \
         DBUG_ENTER ("COCv2Cv##ext");                                                     \
-        res = (type *)MALLOC (sizeof (type) * length);                                   \
-        for (i = 0; i < length; i++) {                                                   \
-            res[i] = ((type *)elems)[i + offset];                                        \
+        for (i = 0; i < len; i++) {                                                      \
+            ((type *)res)[i + res_off] = ((type *)src)[i + off];                         \
         }                                                                                \
-        DBUG_RETURN (res);                                                               \
+        DBUG_VOID_RETURN;                                                                \
     }
 
-COCv2CvTEMPLATE (int, Int) COCv2CvTEMPLATE (double, Double) COCv2CvTEMPLATE (float, Float)
+/*
+ * The actual function definitions are defined by the following macro usages:
+ */
 
-  void *COCv2CvDummy (void *elems, int offset, int length)
+COCv2CvTEMPLATE (unsigned short, UShort) COCv2CvTEMPLATE (unsigned int, UInt)
+  COCv2CvTEMPLATE (unsigned long, ULong) COCv2CvTEMPLATE (short, Short)
+    COCv2CvTEMPLATE (int, Int) COCv2CvTEMPLATE (long, Long)
+
+      COCv2CvTEMPLATE (float, Float) COCv2CvTEMPLATE (double, Double)
+        COCv2CvTEMPLATE (long double, LongDouble)
+
+  /*
+   * Finally, we provide a dummy function which should never be called!
+   * It is defined for being able to make entries for all simpletypes in
+   * type_info.mac!
+   */
+
+  void COCv2CvDummy (void *src, int off, int len, void *res, int res_off)
 {
+    DBUG_ENTER ("COCv2CvDummy");
     DBUG_ASSERT ((1 == 0), "COCv2CvDummy called!");
-    return (NULL);
+    DBUG_VOID_RETURN;
 }
--- src/typecheck/cv2cv.h
+++ src/typecheck/cv2cv.h
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.2  1999/10/22 14:15:19  sbs
+ * commented and added versions for almost all simpletypes...
+ *
  * Revision 1.1  1999/10/19 13:02:59  sacbase
  * Initial revision
  *
@@ -10,14 +13,21 @@
 #ifndef _cv2cv_h
 #define _cv2cv_h
 
-typedef void *(*cv2cvfunptr) (void *, int, int);
+typedef void (*cv2cvfunptr) (void *, int, int, void *, int);
 
 extern cv2cvfunptr cv2cv[];
 
-extern void *COCv2CvInt (void *elems, int offset, int length);
-extern void *COCv2CvDouble (void *elems, int offset, int length);
-extern void *COCv2CvFloat (void *elems, int offset, int length);
+extern void COCv2CvUShort (void *src, int off, int len, void *res, int res_off);
+extern void COCv2CvUInt (void *src, int off, int len, void *res, int res_off);
+extern void COCv2CvULong (void *src, int off, int len, void *res, int res_off);
+extern void COCv2CvShort (void *src, int off, int len, void *res, int res_off);
+extern void COCv2CvInt (void *src, int off, int len, void *res, int res_off);
+extern void COCv2CvLong (void *src, int off, int len, void *res, int res_off);
 
-extern void *COCv2CvDummy (void *elems, int offset, int length);
+extern void COCv2CvFloat (void *src, int off, int len, void *res, int res_off);
+extern void COCv2CvDouble (void *src, int off, int len, void *res, int res_off);
+extern void COCv2CvLongDouble (void *src, int off, int len, void *res, int res_off);
+
+extern void COCv2CvDummy (void *src, int off, int len, void *res, int res_off);
 
 #endif /* _cv2cv_h */
