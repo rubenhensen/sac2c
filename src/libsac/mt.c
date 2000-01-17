470d071c9822f15adaeb8e51b5c29c28138feb8e
Clemens Grelck <cg@sac-home.org>
2000-01-17 16:25:58 +0000

    Reorganized implementation of the runtime system for multi-threaded execution.

--- src/libsac/mt.c
+++ src/libsac/mt.c
@@ -1,6 +1,10 @@
 /*
  *
  * $Log$
+ * Revision 2.3  2000/01/17 16:25:58  cg
+ * Reorganized implementation of the runtime system for
+ * multi-threaded execution.
+ *
  * Revision 2.2  1999/07/08 12:30:02  cg
  * File moved to new directory src/libsac.
  *
@@ -61,7 +65,7 @@
  *   By means of the make tool, this source file is used to produce two
  *   different object files:
  *   1. mt.o        which contains the normal routines
- *   2. trace_mt.o  which contains variants of the normal routines along
+ *   2. mt_trace.o  which contains variants of the normal routines along
  *                     with some extra routines and identifiers for tracing
  *                     program execution.
  *
@@ -70,80 +74,60 @@
  *
  *****************************************************************************/
 
-#define _REENTRANT
-#define _POSIX_C_SOURCE 199506L
-
 #include <pthread.h>
 #include <stdio.h>
 
 #include "sac_message.h"
 
-/*
- * Definition of trace macros.
- */
-
 #ifdef TRACE
-
-#include <stdarg.h>
-
-#define TRACE_PRINT(msg) SAC_TRMT_Print msg
-
+#define SAC_DO_TRACE 1
 #else
+#define SAC_DO_TRACE 0
+#endif /* TRACE */
 
-#define TRACE_PRINT(msg)
+#include "sac_trace.h"
 
-#endif
+#define SAC_DO_MULTITHREAD 1
+#define SAC_DO_THREADS_STATIC 1
+#include "sac_mt.h"
+#undef SAC_DO_MULTITHREAD
+#undef SAC_DO_THREADS_STATIC
 
 /*
- * Definition of global variables.
+ * External declarations of global variables defined in the
+ * compiled SAC program.
  */
 
-#ifdef TRACE
+extern SAC_MT_barrier_t SAC_MT_barrier_space[];
 
 /*
- * Warning:
+ *  Definition of global variables.
  *
- * At least on Solaris 2.5.x the static initialization of mutex locks causes
- * warnings upon compilation. The reason is that in the corresponding file
- * pthread.h, the definition of PTHREAD_MUTEX_INITIALIZER does NOT fit the
- * type definition of pthread_mutex_t.
- *
- * However, so far it works  :-)))
+ *    These variables define the multi-threaded runtime system.
  */
 
-pthread_mutex_t SAC_TRMT_array_memcnt_lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t SAC_TRMT_hidden_memcnt_lock = PTHREAD_MUTEX_INITIALIZER;
+#ifndef TRACE
 
-static pthread_mutex_t output_lock = PTHREAD_MUTEX_INITIALIZER;
+/*
+ * If we compile for mt_trace.o, we don't need the global variables since
+ * these always remain in mt.o.
+ */
 
-pthread_key_t SAC_TRMT_threadid_key;
+pthread_key_t SAC_MT_threadid_key;
 
-const unsigned int SAC_TRMT_master_id = 0;
+SAC_MT_DEFINE_LOCK (SAC_MT_output_lock);
 
-extern pthread_attr_t SAC_MT_thread_attribs;
+SAC_MT_DEFINE_LOCK (SAC_MT_init_lock);
 
-extern void *SAC_MT_barrier;
-
-extern volatile unsigned int SAC_MT_master_flag;
-
-extern unsigned int SAC_MT_not_yet_parallel;
-
-extern unsigned int SAC_MT_masterclass;
-
-extern unsigned int SAC_MT_threads;
-
-extern volatile unsigned int (*SAC_MT_spmd_function) (const unsigned int,
-                                                      const unsigned int, unsigned int);
-
-#else
+unsigned int SAC_MT_master_id = 0;
 
 pthread_attr_t SAC_MT_thread_attribs;
 
-void *SAC_MT_barrier;
+volatile SAC_MT_barrier_t *SAC_MT_barrier;
 
 volatile unsigned int SAC_MT_master_flag = 0;
 
-unsigned int SAC_MT_not_yet_parallel = 1;
+volatile unsigned int SAC_MT_not_yet_parallel = 1;
 
 unsigned int SAC_MT_masterclass;
 
@@ -154,38 +138,21 @@ volatile unsigned int (*SAC_MT_spmd_function) (const unsigned int, const unsigne
 
 #endif /* TRACE */
 
-/*
- * Definition of trace output function
- */
-
-#ifdef TRACE
-
-void
-SAC_TRMT_Print (char *format, ...)
-{
-    va_list arg_p;
-    unsigned int threadid;
-
-    threadid = *((unsigned int *)pthread_getspecific (SAC_TRMT_threadid_key));
-
-    pthread_mutex_lock (&output_lock);
-
-    fprintf (stderr, "TR:%2u:-> ", threadid);
-
-    va_start (arg_p, format);
-    vfprintf (stderr, format, arg_p);
-    va_end (arg_p);
-
-    fprintf (stderr, "\n");
-
-    pthread_mutex_unlock (&output_lock);
-}
-
-#endif
-
-/*
- * Definition of thread control functions.
- */
+/******************************************************************************
+ *
+ * function:
+ *   void ThreadControl(void *arg)
+ *
+ * description:
+ *
+ *   This thread control function is executed by worker threads immediately
+ *   after creation. After some thread specific initalizations, further worker
+ *   threads are created depending on the worker class.
+ *
+ *   Afterwards, the worker thread enters a non-terminating loop where it waits
+ *   at a barrier for work to be assigned and returns after finishing its task.
+ *
+ ******************************************************************************/
 
 static void
 ThreadControl (void *arg)
@@ -196,20 +163,20 @@ ThreadControl (void *arg)
     const unsigned int my_thread_id = ((unsigned int)arg) & 0xFFFF;
     pthread_t tmp;
 
-#ifdef TRACE
-    pthread_setspecific (SAC_TRMT_threadid_key, &my_thread_id);
-#endif
+    SAC_MT_ACQUIRE_LOCK (SAC_MT_init_lock);
+
+    pthread_setspecific (SAC_MT_threadid_key, &my_thread_id);
 
     while (my_thread_id + my_worker_class >= SAC_MT_threads) {
         my_worker_class >>= 1;
     }
 
-    TRACE_PRINT (
+    SAC_TR_PRINT (
       ("This is worker thread #%u with class %u.", my_thread_id, my_worker_class));
 
     for (i = my_worker_class; i; i >>= 1) {
 
-        TRACE_PRINT (
+        SAC_TR_PRINT (
           ("Creating thread #%u with maximum class %u.", my_thread_id + i, i >> 1));
 
         if (0
@@ -223,11 +190,13 @@ ThreadControl (void *arg)
         }
     }
 
+    SAC_MT_RELEASE_LOCK (SAC_MT_init_lock);
+
     for (;;) {
-        TRACE_PRINT (("Worker thread #%u ready.", my_thread_id));
+        SAC_TR_PRINT (("Worker thread #%u ready.", my_thread_id));
 
         while (worker_flag == SAC_MT_master_flag)
-            ;
+            ; /* wait here */
         worker_flag = SAC_MT_master_flag;
 
         worker_flag
@@ -235,29 +204,39 @@ ThreadControl (void *arg)
     }
 }
 
-#ifdef TRACE
-void
-SAC_TRMT_ThreadControl (void *arg)
-#else
-void
-SAC_MT_ThreadControl (void *arg)
-#endif
+/******************************************************************************
+ *
+ * function:
+ *   void ThreadControlInitialWorker(void *arg)
+ *
+ * description:
+ *
+ *   This function is a special thread control function for the first worker
+ *   thread. This thread creates additional threads on behalf of the master
+ *   thread. As an optimization technique, the master thread does not create
+ *   worker threads according to its thread class. Instead, the master thread
+ *   only creates the first worker thread which in turn creates additional
+ *   worker threads.
+ *
+ ******************************************************************************/
+
+static void
+ThreadControlInitialWorker (void *arg)
 {
     unsigned int worker_flag = 0;
     unsigned int i;
     pthread_t tmp;
-
-#ifdef TRACE
     const unsigned int my_thread_id = 1;
 
-    pthread_setspecific (SAC_TRMT_threadid_key, &my_thread_id);
-#endif
+    SAC_MT_ACQUIRE_LOCK (SAC_MT_init_lock);
 
-    TRACE_PRINT (("This is worker thread #1 with class 0."));
+    pthread_setspecific (SAC_MT_threadid_key, &my_thread_id);
+
+    SAC_TR_PRINT (("This is worker thread #1 with class 0."));
 
     for (i = SAC_MT_masterclass; i > 1; i >>= 1) {
 
-        TRACE_PRINT (("Creating thread #%u with maximum class %u.", i, i >> 1));
+        SAC_TR_PRINT (("Creating thread #%u with maximum class %u.", i, i >> 1));
 
         if (0
             != pthread_create (&tmp, &SAC_MT_thread_attribs,
@@ -270,13 +249,162 @@ SAC_MT_ThreadControl (void *arg)
         }
     }
 
+    SAC_MT_RELEASE_LOCK (SAC_MT_init_lock);
+
     for (;;) {
-        TRACE_PRINT (("Worker thread #1 ready."));
+        SAC_TR_PRINT (("Worker thread #1 ready."));
 
         while (worker_flag == SAC_MT_master_flag)
-            ;
+            ; /* wait here */
         worker_flag = SAC_MT_master_flag;
 
         worker_flag = (*SAC_MT_spmd_function) (1, 0, worker_flag);
+    }
+}
+
+/******************************************************************************
+ *
+ * function:
+ *   void SAC_MT_TR_SetupInitial( int argc, char *argv[],
+ *                                unsigned int num_threads,
+ *                                unsigned int max_threads)
+ *   void SAC_MT_SetupInitial( int argc, char *argv[],
+ *                             unsigned int num_threads,
+ *                             unsigned int max_threads)
+ *
+ * description:
+ *
+ *   This function implements an initial setup of the runtime system for
+ *   multi-threaded program execution. Here initializations are made which
+ *   may not wait till worker thread creation. Basically, these are
+ *   - the creation of the thread speicific data key which holds the thread ID,
+ *   - the initialization of the  thread speicific data for the master thread,
+ *   - the evaluation of the -mt command line option.
+ *
+ ******************************************************************************/
+
+#ifdef TRACE
+void
+SAC_MT_TR_SetupInitial (int argc, char *argv[], unsigned int num_threads,
+                        unsigned int max_threads)
+#else
+void
+SAC_MT_SetupInitial (int argc, char *argv[], unsigned int num_threads,
+                     unsigned int max_threads)
+#endif
+{
+    int i;
+
+    if (0 != pthread_key_create (&SAC_MT_threadid_key, NULL)) {
+        SAC_RuntimeError ("Unable to create thread specific data key.");
+    }
+
+    if (0 != pthread_setspecific (SAC_MT_threadid_key, &SAC_MT_master_id)) {
+        SAC_RuntimeError ("Unable to initialize thread specific data.");
+    }
+
+    SAC_TR_PRINT (("Creating thread specific data key to hold thread ID."));
+    SAC_TR_PRINT (("Initializing thread specific data for master thread."));
+
+    if (num_threads == 0) {
+        for (i = 1; i < argc - 1; i++) {
+            if ((argv[i][0] == '-') && (argv[i][1] == 'm') && (argv[i][2] == 't')
+                && (argv[i][3] == '\0')) {
+                SAC_MT_threads = atoi (argv[i + 1]);
+                break;
+            }
+        }
+
+        if ((SAC_MT_threads <= 0) || (SAC_MT_threads > max_threads)) {
+            SAC_RuntimeError (
+              "Number of threads is unspecified or exceeds legal range (1 to %d).\n"
+              "    Use option '-mt <num>'.",
+              max_threads);
+        }
+    } else {
+        SAC_MT_threads = num_threads;
+    }
+
+    SAC_TR_PRINT (("Number of threads determined as %u.", SAC_MT_threads));
+}
+
+/******************************************************************************
+ *
+ * function:
+ *   void SAC_MT_Setup( int cache_line_max, int barrier_offset)
+ *   void SAC_MT_TR_Setup( int cache_line_max, int barrier_offset)
+ *
+ * description:
+ *
+ *   This function initializes the runtime system for multi-threaded
+ *   program execution. The basic steps performed are
+ *   - aligning the synchronization barrier data structure so that no two
+ *     threads write to the same cache line,
+ *   - determining the thread class of the master thread,
+ *   - creation and initialization of POSIX thread attributes,
+ *   - creation of the initial worker thread.
+ *
+ ******************************************************************************/
+
+#ifdef TRACE
+void
+SAC_MT_TR_Setup (int cache_line_max, int barrier_offset)
+#else
+void
+SAC_MT_Setup (int cache_line_max, int barrier_offset)
+#endif
+{
+    SAC_TR_PRINT (("Aligning synchronization barrier data structure "
+                   "to data cache specification."));
+
+    if (cache_line_max > 0) {
+        SAC_MT_barrier = (SAC_MT_barrier_t *)((char *)(SAC_MT_barrier_space + 1)
+                                              - ((unsigned long int)SAC_MT_barrier_space
+                                                 % barrier_offset));
+    } else {
+        SAC_MT_barrier = SAC_MT_barrier_space;
+    }
+
+    SAC_TR_PRINT (("Barrier base address is %p", SAC_MT_barrier));
+
+    SAC_TR_PRINT (("Computing thread class of master thread."));
+
+    for (SAC_MT_masterclass = 1; SAC_MT_masterclass < SAC_MT_threads;
+         SAC_MT_masterclass <<= 1) {
+        SAC_MT_CLEAR_BARRIER (SAC_MT_masterclass);
+    }
+
+    SAC_MT_masterclass >>= 1;
+
+    SAC_TR_PRINT (("Thread class of master thread is %d.", (int)SAC_MT_masterclass));
+
+    if (SAC_MT_threads > 1) {
+        pthread_t tmp;
+
+        SAC_TR_PRINT (("Setting up POSIX thread attributes"));
+
+        if (0 != pthread_attr_init (&SAC_MT_thread_attribs)) {
+            SAC_RuntimeError ("Unable to initialize POSIX thread attributes");
+        }
+
+        if (0 != pthread_attr_setscope (&SAC_MT_thread_attribs, PTHREAD_SCOPE_SYSTEM)) {
+            SAC_RuntimeError ("Unable to set POSIX thread attributes to "
+                              "PTHREAD_SCOPE_SYSTEM");
+        }
+
+        if (0
+            != pthread_attr_setdetachstate (&SAC_MT_thread_attribs,
+                                            PTHREAD_CREATE_DETACHED)) {
+            SAC_RuntimeError ("Unable to set POSIX thread attributes to "
+                              "PTHREAD_CREATE_DETACHED");
+        }
+
+        SAC_TR_PRINT (("Creating worker thread #1 of class 0"));
+
+        if (0
+            != pthread_create (&tmp, &SAC_MT_thread_attribs,
+                               (void *(*)(void *))ThreadControlInitialWorker, NULL)) {
+            SAC_RuntimeError ("Unable to create (initial) worker thread #1");
+        }
     }
 }
--- src/libsac/trace.c
+++ src/libsac/trace.c
@@ -1,6 +1,10 @@
 /*
  *
  * $Log$
+ * Revision 2.3  2000/01/17 16:25:58  cg
+ * Reorganized implementation of the runtime system for
+ * multi-threaded execution.
+ *
  * Revision 2.2  1999/07/08 12:30:35  cg
  * File moved to new directory src/libsac.
  *
@@ -35,11 +39,18 @@
  *
  * file:   trace.c
  *
- * prefix: SAC_
+ * prefix: SAC_TR
  *
  * description:
  *
  *   This file is part of the implementation of the SAC runtime library.
+ *
+ *   It provides global variables and function definitions for tracing
+ *   program execution.
+ *
+ *   Upon multi-threaded execution access to global variables is protected
+ *   by locks and trace information is preceded by the corresponding
+ *   thread ID.
  *
  *****************************************************************************/
 
@@ -47,8 +58,68 @@
 #include <stdarg.h>
 #include <string.h>
 
+#ifdef MT
+#define SAC_DO_MULTITHREAD 1
+#define SAC_DO_THREADS_STATIC 1
+#else
+#define SAC_DO_MULTITHREAD 0
+#endif /*  MT  */
+
+#include "sac_mt.h"
+
+#undef SAC_DO_MULTITHREAD
+#undef SAC_DO_THREADS_STATIC
+
 int SAC_TR_array_memcnt = 0;
 int SAC_TR_hidden_memcnt = 0;
+
+SAC_MT_DEFINE_LOCK (SAC_TR_array_memcnt_lock);
+SAC_MT_DEFINE_LOCK (SAC_TR_hidden_memcnt_lock);
+
+/******************************************************************************
+ *
+ * function:
+ *   void SAC_TR_Print(char *format, ...)
+ *
+ * description:
+ *
+ *   This function is used to print trace output to stderr.
+ *
+ *   The multi-threaded version of the trace print function assures single
+ *   threaded execution through a mutex lock;
+ *   the usual layout of the trace message is extended by the thread ID
+ *   which is determined via thread specific global data ('SAC_MT_threadid_key').
+ *
+ ******************************************************************************/
+
+#ifdef MT
+
+void
+SAC_TR_Print (char *format, ...)
+{
+    va_list arg_p;
+    unsigned int *thread_id_ptr;
+
+    thread_id_ptr = (unsigned int *)pthread_getspecific (SAC_MT_threadid_key);
+
+    SAC_MT_ACQUIRE_LOCK (SAC_MT_output_lock);
+
+    if (thread_id_ptr == NULL) {
+        fprintf (stderr, "TR:??:-> ");
+    } else {
+        fprintf (stderr, "TR:%2u:-> ", *thread_id_ptr);
+    }
+
+    va_start (arg_p, format);
+    vfprintf (stderr, format, arg_p);
+    va_end (arg_p);
+
+    fprintf (stderr, "\n");
+
+    SAC_MT_RELEASE_LOCK (SAC_MT_output_lock);
+}
+
+#else /* MT */
 
 void
 SAC_TR_Print (char *format, ...)
@@ -64,56 +135,76 @@ SAC_TR_Print (char *format, ...)
     fprintf (stderr, "\n");
 }
 
-#if 0
+#endif /* MT */
 
-#define TRACE_BUFFER_SIZE 256
+/******************************************************************************
+ *
+ * function:
+ *   void SAC_TR_IncArrayMemcnt(int size)
+ *   void SAC_TR_DecArrayMemcnt(int size)
+ *
+ * description:
+ *
+ *   These functions are used to increment and decrement the allocated array
+ *   element counter and print the corresponding counter status information.
+ *
+ *   Under a multi-threaded execution scheme the counter is protected
+ *   through a dedicated lock.
+ *
+ ******************************************************************************/
 
-static int trace_layout_flag=0;
-
-
-
-void SAC_TR_PrintTraceHeader(char *format, ...)
+void
+SAC_TR_IncArrayMemcnt (int size)
 {
-  va_list arg_p;
-  static char buffer[TRACE_BUFFER_SIZE];
-
-  va_start(arg_p, format);
-  vsprintf(buffer, format, arg_p);
-  va_end(arg_p);
-
-  if (strlen(buffer)>40)
-  {
-    fprintf(stderr, "%s\n", buffer);
-    fprintf(stderr, "%-40s -> ", " ");
-  }
-  else
-  {
-    fprintf(stderr, "%-40s -> ", buffer);
-  }
-  
-  trace_layout_flag=0;
+    SAC_MT_ACQUIRE_LOCK (SAC_TR_array_memcnt_lock);
+    SAC_TR_array_memcnt += size;
+    SAC_TR_Print ("%d array elements allocated, total now: %d.", size,
+                  SAC_TR_array_memcnt);
+    SAC_MT_RELEASE_LOCK (SAC_TR_array_memcnt_lock);
 }
 
-
-
-void SAC_TR_PrintTraceInfo(char *format, ...)
+void
+SAC_TR_DecArrayMemcnt (int size)
 {
-  va_list arg_p;
-
-  if (trace_layout_flag==0)
-  {
-    trace_layout_flag=1;
-  }
-  else
-  {
-    fprintf(stderr, "%-40s    ", " ");
-  }
-
-  va_start(arg_p, format);
-  vfprintf(stderr, format, arg_p);
-  va_end(arg_p);
-
-  fprintf(stderr, "\n");
+    SAC_MT_ACQUIRE_LOCK (SAC_TR_array_memcnt_lock);
+    SAC_TR_array_memcnt -= size;
+    SAC_TR_Print ("%d array elements allocated, total now: %d.", size,
+                  SAC_TR_array_memcnt);
+    SAC_MT_RELEASE_LOCK (SAC_TR_array_memcnt_lock);
 }
 
-#endif
+/******************************************************************************
+ *
+ * function:
+ *   void SAC_TR_IncHiddenMemcnt(int size)
+ *   void SAC_TR_DecHiddenMemcnt(int size)
+ *
+ * description:
+ *
+ *   These functions are used to increment and decrement the allocated hidden
+ *   objects counter and print the corresponding counter status information.
+ *
+ *   Under a multi-threaded execution scheme the counter is protected
+ *   through a dedicated lock.
+ *
+ ******************************************************************************/
+
+void
+SAC_TR_IncHiddenMemcnt (int size)
+{
+    SAC_MT_ACQUIRE_LOCK (SAC_TR_hidden_memcnt_lock);
+    SAC_TR_hidden_memcnt += size;
+    SAC_TR_Print ("%d hidden objects allocated, total now: %d.", size,
+                  SAC_TR_hidden_memcnt);
+    SAC_MT_RELEASE_LOCK (SAC_TR_hidden_memcnt_lock);
+}
+
+void
+SAC_TR_DecHiddenMemcnt (int size)
+{
+    SAC_MT_ACQUIRE_LOCK (SAC_TR_hidden_memcnt_lock);
+    SAC_TR_hidden_memcnt -= size;
+    SAC_TR_Print ("%d hidden objects allocated, total now: %d.", size,
+                  SAC_TR_hidden_memcnt);
+    SAC_MT_RELEASE_LOCK (SAC_TR_hidden_memcnt_lock);
+}
