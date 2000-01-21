f22c2d425276d627c6dcda8cea4e751f7749f4bd
Jan-Hendrik Schöler <info@sac-home.org>
2000-01-21 13:11:16 +0000

    Initial revision

---
+++ src/multithread/multithread.h
@@ -0,0 +1,28 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  2000/01/21 13:11:16  jhs
+ * Initial revision
+ *
+ *
+ */
+
+/*****************************************************************************
+ *
+ * file:        multithread.h
+ *
+ * prefix:      MUTH
+ *
+ * description:
+ *
+ *   header file for multithread.c
+ *
+ *****************************************************************************/
+
+#ifndef MULTITHREAD_H
+
+#define MULTITHREAD_H
+
+extern node *BuildMultiThread (node *syntax_tree);
+
+#endif /* MULTITHREAD_H */
