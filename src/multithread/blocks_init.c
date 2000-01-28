497804736f8cc956d2c541159351290108348cbf
Jan-Hendrik Schöler <info@sac-home.org>
2000-01-28 13:21:35 +0000

    Initial revision

---
+++ src/multithread/blocks_init.c
@@ -0,0 +1,44 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  2000/01/28 13:21:35  jhs
+ * Initial revision
+ *
+ *
+ */
+
+/******************************************************************************
+ *
+ * file:   blocks_init.c
+ *
+ * prefix: BLKIN
+ *
+ * description:
+ *   ####
+ *
+ ******************************************************************************/
+
+#include "dbug.h"
+
+#include "types.h"
+#include "tree_basic.h"
+#include "tree_compound.h"
+#include "traverse.h"
+#include "scheduling.h"
+
+/******************************************************************************
+ *
+ * function:
+ *   node *BlocksInit(node *arg_node, node *arg_info)
+ *
+ * description:
+ *   ####
+ *
+ ******************************************************************************/
+node *
+BlocksInit (node *arg_node, node *arg_info)
+{
+    DBUG_ENTER ("BlocksInit");
+
+    DBUG_RETURN (arg_node);
+}
---
+++ src/multithread/blocks_init.h
@@ -0,0 +1,25 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  2000/01/28 13:21:35  jhs
+ * Initial revision
+ *
+ *
+ */
+
+/*****************************************************************************
+ *
+ * file:   blocks_init.h
+ *
+ * description:
+ *   header file for blocks_init.c
+ *
+ *****************************************************************************/
+
+#ifndef BLOCKS_INIT_H
+
+#define BLOCKS_INIT_H
+
+extern node *ScheduleInit (node *arg_node, node *arg_info);
+
+#endif /* BLOCKS_INIT_H */
