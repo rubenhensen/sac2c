1289122949526778273d9667f742b13e52a37516
Jan-Hendrik Schöler <info@sac-home.org>
2000-03-09 18:35:59 +0000

    new copy routine

--- src/tree/DataFlowMaskUtils.h
+++ src/tree/DataFlowMaskUtils.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.3  2000/03/09 18:35:59  jhs
+ *  new copy routine
+ *
  * Revision 1.2  2000/02/03 17:30:06  dkr
  * LUTs added
  *
@@ -17,5 +20,7 @@ extern node *DFM2Vardecs (DFMmask_t mask, lut_t *lut);
 extern node *DFM2Args (DFMmask_t mask, lut_t *lut);
 extern node *DFM2Exprs (DFMmask_t mask, lut_t *lut);
 extern ids *DFM2Ids (DFMmask_t mask, lut_t *lut);
+
+extern DFMmask_t DFMDuplicateMask (DFMmask_t mask, DFMmask_base_t base);
 
 #endif /* _sac_DataFlowMaskUtils_h */
