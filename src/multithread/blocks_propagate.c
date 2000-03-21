9b3bbdfce0f6ab8607232ee271c0ea17daa7d53f
Jan-Hendrik Schöler <info@sac-home.org>
2000-03-21 13:07:38 +0000

    Finished this traversal.

--- src/multithread/blocks_propagate.c
+++ src/multithread/blocks_propagate.c
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.2  2000/03/21 13:07:38  jhs
+ * Finished this traversal.
+ *
  * Revision 1.1  2000/03/09 19:50:36  jhs
  * Initial revision
  *
@@ -56,8 +59,7 @@ BlocksPropagate (node *arg_node, node *arg_info)
     if ((FUNDEF_BODY (arg_node) != NULL) && (FUNDEF_STATUS (arg_node) != ST_foldfun)
         && (FUNDEF_ATTRIB (arg_node) != ST_call_rep)) {
         old_tab = act_tab;
-        DBUG_ASSERT (0, ("no trav table"));
-        /*    act_tab = blkpp_tab; */
+        act_tab = blkpp_tab;
 
         /* push info */
 
@@ -109,31 +111,56 @@ BLKPPassign (node *arg_node, node *arg_info)
     node *ap;
     node *fundef;
     statustype state;
-    int embedding;
+    int embedding; /* bool */
 
     DBUG_ENTER ("BLKPPassign");
 
-    embedding = 1;
+    DBUG_PRINT ("BLKPP", ("begin, instr is a %s", NODE_TEXT (ASSIGN_INSTR (arg_node))));
 
     let = ASSIGN_INSTR (arg_node);
     if (NODE_TYPE (let) == N_let) {
-        ap = LET_EXPR (arg_node);
+        DBUG_PRINT ("BLKIN", ("instr is %s", NODE_TEXT (LET_EXPR (let))));
+        ap = LET_EXPR (let);
         if (NODE_TYPE (ap) == N_ap) {
             fundef = AP_FUNDEF (ap);
-            state = FUNDEF_STATUS (fundef);
-            if (state == ST_dofun) {
-                if (embedding) {
-                    arg_node = InsertMT (arg_node, arg_info);
+            if (fundef != NULL) {
+                DBUG_PRINT ("BLKPP", ("hit"));
+                state = FUNDEF_STATUS (fundef);
+                DBUG_PRINT ("BLKPP", ("hit"));
+
+                if (FUNDEF_COMPANION (fundef) != NULL) {
+                    embedding = (NODE_TYPE (FUNDEF_COMPANION (fundef)) == N_mt);
+                } else {
+                    embedding = FALSE;
                 }
-            } else if (state == ST_whilefun) {
-                if (embedding) {
-                    arg_node = InsertMT (arg_node, arg_info);
+                DBUG_PRINT ("BLKIN", ("embedding is %i", embedding));
+                DBUG_PRINT ("BLKPP", ("found %s", mdb_statustype[state]));
+
+                if (state == ST_dofun) {
+                    if (embedding) {
+                        arg_node = InsertMT (arg_node, arg_info);
+                    }
+                } else if (state == ST_whilefun) {
+                    if (embedding) {
+                        arg_node = InsertMT (arg_node, arg_info);
+                    }
+                } else {
+                    DBUG_PRINT ("BLKPP", ("ignoring %s", mdb_statustype[state]));
                 }
-            } else {
-                DBUG_PRINT ("BLKPP", ("ignoring %s", mdb_statustype[state]));
             }
         }
+    } else if (NODE_TYPE (let) == N_cond) {
+        DBUG_PRINT ("BLKPP", ("trav into cond"));
+        ASSIGN_INSTR (arg_node) = Trav (ASSIGN_INSTR (arg_node), arg_info);
+        DBUG_PRINT ("BLKPP", ("trav from cond"));
     }
 
+    if (ASSIGN_NEXT (arg_node) != NULL) {
+        DBUG_PRINT ("BLKPP", ("trav info next"));
+        ASSIGN_NEXT (arg_node) = Trav (ASSIGN_NEXT (arg_node), arg_info);
+        DBUG_PRINT ("BLKPP", ("trav from next"));
+    }
+
+    DBUG_PRINT ("BLKPP", ("end"));
     DBUG_RETURN (arg_node);
 }
