36a54bde9e4f888c603727ed1eb40be91c97c734
Jan-Hendrik Schöler <info@sac-home.org>
2000-03-21 13:07:26 +0000

    Added BLKPPassign, BLKPPfundef.

--- src/multithread/blocks_propagate.h
+++ src/multithread/blocks_propagate.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.2  2000/03/21 13:07:26  jhs
+ * Added BLKPPassign, BLKPPfundef.
+ *
  * Revision 1.1  2000/03/09 19:50:36  jhs
  * Initial revision
  *
@@ -23,5 +26,8 @@
 #define BLOCKS_PROPAGATE_H
 
 extern node *BlocksPropagate (node *arg_node, node *arg_info);
+
+extern node *BLKPPassign (node *arg_node, node *arg_info);
+extern node *BLKPPfundef (node *arg_node, node *arg_info);
 
 #endif /* BLOCKS_PROPAGATE_H */
