bc88ec7a55ce91341021458cca465b8a1bcf36a3
Jan-Hendrik Schöler <info@sac-home.org>
2000-03-29 09:46:37 +0000

    Initial revision

---
+++ src/multithread/blocks_lift.h
@@ -0,0 +1,23 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  2000/03/29 09:46:37  jhs
+ * Initial revision
+ *
+ *
+ */
+
+/*****************************************************************************
+ *
+ * file:   blocks_lift.h
+ *
+ * description:
+ *   header file for blocks_lift.c
+ *
+ *****************************************************************************/
+
+#ifndef BLOCKS_LIFT_H
+
+#define BLOCKS_LIFT_H
+
+#endif /* BLOCKS_CONS_H */
