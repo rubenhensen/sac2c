c4a526f02e7d8a27a900b7c678aaa78c6da2df2a
Sven-Bodo Scholz <sbs@sac-home.org>
2000-05-26 14:24:29 +0000

    dummy function ArrayPadding added.

--- src/psi-opt/pad.c
+++ src/psi-opt/pad.c
@@ -1,9 +1,24 @@
 /*
  * $Log$
+ * Revision 1.2  2000/05/26 14:24:29  sbs
+ * dummy function ArrayPadding added.
+ *
  * Revision 1.1  2000/05/26 13:41:35  sbs
  * Initial revision
  *
  *
  */
 
+#include "dbug.h"
+#include "tree_basic.h"
 #include "pad.h"
+
+node *
+ArrayPadding (node *arg_node)
+{
+    DBUG_ENTER ("ArrayPadding");
+
+    DBUG_PRINT ("AP", ("Entering Array Padding"));
+
+    DBUG_RETURN (arg_node);
+}
--- src/psi-opt/pad.h
+++ src/psi-opt/pad.h
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.2  2000/05/26 14:24:29  sbs
+ * dummy function ArrayPadding added.
+ *
  * Revision 1.1  2000/05/26 13:41:40  sbs
  * Initial revision
  *
@@ -9,5 +12,7 @@
 #ifndef sac_pad_h
 
 #define sac_pad_h
+
+extern node *ArrayPadding (node *arg_node);
 
 #endif /* sac_pad_h */
