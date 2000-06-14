8b0dc692bada665dda00585c3e6153b14430a47a
User mab <info@sac-home.org>
2000-06-14 10:45:04 +0000

    added methods for accessing data structure

--- src/psi-opt/pad_info.c
+++ src/psi-opt/pad_info.c
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.2  2000/06/14 10:45:04  mab
+ * added methods for accessing data structure
+ *
  * Revision 1.1  2000/06/08 11:20:16  mab
  * Initial revision
  *
@@ -20,40 +23,64 @@
 #define PI_TYPE(p) p->type
 #define PI_OLD_SHAPE(p) p->old_shape
 #define PI_NEW_SHAPE(p) p->new_shape
+#define PI_FUNDEF_PAD(p) p->fundef_pad
+#define PI_FUNDEF_UNPAD(p) p->fundef_unpad
 #define PI_NEXT(p) p->next
 
-/* to do:
+/*****************************************************************************
+ *
+ * file:   pad_info.c
+ *
+ * prefix: PI
+ *
+ * description:
+ *
+ *   This is an abstract data structure for storing information needed by
+ *   the array padding.
+ *
+ *
+ *****************************************************************************/
 
-*/
+/*****************************************************************************
+ *
+ * function:
+ *   int PIequalShapes(int dim, shpseg* shape2, shpseg* shape1)
+ *
+ * description:
+ *   compares two shapes, result is TRUE, if shapes are equal
+ *   !internal function only!
+ *
+ *****************************************************************************/
 
-/* private */
 int
-PIequalShapes (pad_info_t *pi, nums *old_shape)
+PIequalShapes (int dim, shpseg *shape2, shpseg *shape1)
 {
-    nums *tmp1 = old_shape;
-    nums *tmp2 = PI_OLD_SHAPE (pi);
     int equalShapes = TRUE;
+    int i;
 
     DBUG_ENTER ("PIequalShapes");
 
-    while (tmp1 != NULL && tmp2 != NULL) {
-        if (NUMS_NUM (tmp1) != NUMS_NUM (tmp2)) {
+    i = 0;
+    while (i < dim && equalShapes == TRUE) {
+        if (SHPSEG_SHAPE (shape1, i) != SHPSEG_SHAPE (shape2, i)) {
             equalShapes = FALSE;
-            tmp1 = NULL;
-            tmp2 = NULL;
-        } else {
-            tmp1 = NUMS_NEXT (tmp1);
-            tmp2 = NUMS_NEXT (tmp2);
-            if (tmp1 == NULL || tmp2 == NULL) {
-                equalShapes = FALSE;
-            }
         }
+        i++;
     }
 
     DBUG_RETURN (equalShapes);
 }
 
-/* public */
+/*****************************************************************************
+ *
+ * function:
+ *   int PIgetDim(pad_info_t* pi)
+ *
+ * description:
+ *   return dimension of a pad_info_t*
+ *
+ *****************************************************************************/
+
 int
 PIgetDim (pad_info_t *pi)
 {
@@ -62,7 +89,16 @@ PIgetDim (pad_info_t *pi)
     DBUG_RETURN (PI_DIM (pi));
 }
 
-/* public */
+/*****************************************************************************
+ *
+ * function:
+ *   simpletype PIgetType(pad_info_t* pi)
+ *
+ * description:
+ *   return simpletype of a pad_info_t*
+ *
+ *****************************************************************************/
+
 simpletype
 PIgetType (pad_info_t *pi)
 {
@@ -71,8 +107,17 @@ PIgetType (pad_info_t *pi)
     DBUG_RETURN (PI_TYPE (pi));
 }
 
-/* public */
-nums *
+/*****************************************************************************
+ *
+ * function:
+ *   shpseg* PIgetOldShape(pad_info_t* pi)
+ *
+ * description:
+ *   return pointer to original shape of a pad_info_t*
+ *
+ *****************************************************************************/
+
+shpseg *
 PIgetOldShape (pad_info_t *pi)
 {
     DBUG_ENTER ("PIgetOldShape");
@@ -80,8 +125,17 @@ PIgetOldShape (pad_info_t *pi)
     DBUG_RETURN (PI_OLD_SHAPE (pi));
 }
 
-/* public */
-nums *
+/*****************************************************************************
+ *
+ * function:
+ *   shpseg* PIgetNewShape(pad_info_t* pi)
+ *
+ * description:
+ *   return pointer to newly inferred shape of a pad_info_t*
+ *
+ *****************************************************************************/
+
+shpseg *
 PIgetNewShape (pad_info_t *pi)
 {
     DBUG_ENTER ("PIgetNewShape");
@@ -89,9 +143,135 @@ PIgetNewShape (pad_info_t *pi)
     DBUG_RETURN (PI_NEW_SHAPE (pi));
 }
 
-/* public */
+/*****************************************************************************
+ *
+ * function:
+ *   shpseg* PIcopyNewShape(pad_info_t* pi)
+ *
+ * description:
+ *   return copy of new shape contained in pad_info_t
+ *
+ *****************************************************************************/
+
+shpseg *
+PIcopyNewShape (pad_info_t *pi)
+{
+    shpseg *res;
+    int i;
+
+    DBUG_ENTER ("PIcopyNewShape");
+
+    res = (shpseg *)malloc (sizeof (shpseg *));
+    SHPSEG_NEXT (res) = NULL;
+    for (i = 0; i < SHP_SEG_SIZE; i++) {
+        SHPSEG_SHAPE (res, i) = SHPSEG_SHAPE (PI_NEW_SHAPE (pi), i);
+    }
+
+    DBUG_RETURN (res);
+}
+
+/*****************************************************************************
+ *
+ * function:
+ *   node* PIgetFUndefPad(pad_info_t* pi)
+ *
+ * description:
+ *   return pointer to fundef-node of padding-function
+ *
+ *****************************************************************************/
+
+node *
+PIgetFundefPad (pad_info_t *pi)
+{
+    DBUG_ENTER ("PIgetFundefPad");
+
+    DBUG_RETURN (PI_FUNDEF_PAD (pi));
+}
+
+/*****************************************************************************
+ *
+ * function:
+ *   node* PIgetFUndefUnpad(pad_info_t* pi)
+ *
+ * description:
+ *   return pointer to fundef-node of unpadding-function
+ *
+ *****************************************************************************/
+
+node *
+PIgetFUndefUnpad (pad_info_t *pi)
+{
+    DBUG_ENTER ("PIgetFundefUnpad");
+
+    DBUG_RETURN (PI_FUNDEF_UNPAD (pi));
+}
+
+/*****************************************************************************
+ *
+ * function:
+ *   void PIinit()
+ *
+ * description:
+ *   initialize abstract data structure
+ *   !!! need to call this before use of any other functions !!!
+ *
+ *****************************************************************************/
+
 void
-PIadd (simpletype type, int dim, nums *old_shape, nums *new_shape)
+PIinit ()
+{
+    DBUG_ENTER ("PIinit");
+
+    pad_info = NULL;
+
+    DBUG_VOID_RETURN;
+}
+
+/*****************************************************************************
+ *
+ * function:
+ *   void PIfree()
+ *
+ * description:
+ *   free abstract data structure
+ *   call this after finishing work mith pad_info
+ *
+ *****************************************************************************/
+
+void
+PIfree ()
+{
+    pad_info_t *next;
+    pad_info_t *current;
+
+    DBUG_ENTER ("PIfree");
+
+    current = pad_info;
+
+    while (current != NULL) {
+        next = PI_NEXT (current);
+
+        PI_OLD_SHAPE (current) = NULL;
+        PI_NEW_SHAPE (current) = NULL;
+        free (current);
+
+        current = next;
+    }
+    DBUG_VOID_RETURN;
+}
+
+/*****************************************************************************
+ *
+ * function:
+ *   void PIadd(types* old_type, shpseg* new_shape)
+ *
+ * description:
+ *   add a new entry to the data structure for a newly inferred type
+ *
+ *****************************************************************************/
+
+void
+PIadd (types *old_type, shpseg *new_shape)
 {
 
     pad_info_t *tmp;
@@ -99,17 +279,29 @@ PIadd (simpletype type, int dim, nums *old_shape, nums *new_shape)
     DBUG_ENTER ("PIadd");
 
     tmp = (pad_info_t *)malloc (sizeof (pad_info_t));
-    PI_DIM (tmp) = dim;
-    PI_TYPE (tmp) = type;
-    PI_OLD_SHAPE (tmp) = old_shape;
+    PI_DIM (tmp) = TYPES_DIM (old_type);
+    PI_TYPE (tmp) = TYPES_BASETYPE (old_type);
+    PI_OLD_SHAPE (tmp) = TYPES_SHPSEG (old_type);
     PI_NEW_SHAPE (tmp) = new_shape;
+    PI_FUNDEF_PAD (tmp) = NULL;
+    PI_FUNDEF_UNPAD (tmp) = NULL;
     PI_NEXT (tmp) = pad_info;
     pad_info = tmp;
 
     DBUG_VOID_RETURN;
 }
 
-/* public */
+/*****************************************************************************
+ *
+ * function:
+ *   int PIvalid(pad_info_t* pi)
+ *
+ * description:
+ *   check, if a pad_info_t* points to a valid structure
+ *   you may want to call this after use of PInewShape
+ *
+ *****************************************************************************/
+
 int
 PIvalid (pad_info_t *pi)
 {
@@ -125,9 +317,18 @@ PIvalid (pad_info_t *pi)
     DBUG_RETURN (result);
 }
 
-/* public */
+/*****************************************************************************
+ *
+ * function:
+ *   pad_info_t* PInewShape(types* old_type)
+ *
+ * description:
+ *   return newly inferred shape for a given old shape
+ *
+ *****************************************************************************/
+
 pad_info_t *
-PInewShape (int dim, simpletype type, nums *old_shape)
+PInewShape (types *old_type)
 {
     pad_info_t *tmp = pad_info;
     pad_info_t *res = NULL;
@@ -135,12 +336,15 @@ PInewShape (int dim, simpletype type, nums *old_shape)
     DBUG_ENTER ("PInewShape");
 
     while (tmp != NULL) {
-        if ((PI_DIM (tmp) == dim) && (PI_TYPE (tmp) == type)
-            && PIequalShapes (tmp, old_shape) == TRUE) {
+        if ((PI_TYPE (tmp) == TYPES_BASETYPE (old_type))
+            && (PI_DIM (tmp) == TYPES_DIM (old_type))
+            && PIequalShapes (PI_DIM (tmp), PI_OLD_SHAPE (tmp), TYPES_SHPSEG (old_type))
+                 == TRUE) {
             res = tmp;
             tmp = NULL;
+        } else {
+            tmp = PI_NEXT (tmp);
         }
-        tmp = PI_NEXT (tmp);
     }
 
     DBUG_RETURN (res);
--- src/psi-opt/pad_info.h
+++ src/psi-opt/pad_info.h
@@ -1,31 +1,57 @@
 /*
  * $Log$
+ * Revision 1.2  2000/06/14 10:45:04  mab
+ * added methods for accessing data structure
+ *
  * Revision 1.1  2000/06/08 11:20:16  mab
  * Initial revision
  *
  */
 
+/*****************************************************************************
+ *
+ * file:   pad_info.h
+ *
+ * prefix: PI
+ *
+ * description:
+ *
+ *   This is an abstract data structure for storing information needed by
+ *   the array padding.
+ *
+ *
+ *****************************************************************************/
+
 #ifndef sac_pad_info_h
 
-#define sac_pad__info_h
+#define sac_pad_info_h
 
 /* structure containing old and infered array shape */
 typedef struct pad_i {
     int dim;
     simpletype type;
-    nums *old_shape;
-    nums *new_shape;
+    shpseg *old_shape;
+    shpseg *new_shape;
+    node *fundef_pad;
+    node *fundef_unpad;
     struct pad_i *next;
 } pad_info_t;
 
 pad_info_t *pad_info;
-
+/* BE CARFULL! These are only references to a global structure.
+               Do not link them to syntax tree! */
 extern int PIgetDim (pad_info_t *pi);
 extern simpletype PIgetType (pad_info_t *pi);
-extern nums *PIgetOldShape (pad_info_t *pi);
-extern nums *PIgetNewShape (pad_info_t *pi);
+extern shpseg *PIgetOldShape (pad_info_t *pi);
+extern shpseg *PIgetNewShape (pad_info_t *pi);
+extern pad_info_t *PInewShape (types *old_type);
+extern node *PIgetFundefPad (pad_info_t *pi);
+extern node *PIgetFundefUnpad (pad_info_t *pi);
+
+extern shpseg *PIcopyNewShape (pad_info_t *pi);
 extern bool PIvalid (pad_info_t *pi);
-extern pad_info_t *PInewShape (int dim, simpletype type, nums *old_shape);
-extern void PIadd (simpletype type, int dim, nums *old_shape, nums *new_shape);
+extern void PIadd (types *old_type, shpseg *new_shape);
+extern void PIinit ();
+extern void PIfree ();
 
 #endif /* sac_pad_info_h */
