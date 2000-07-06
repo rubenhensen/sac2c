b4a539d07551ce2db4720f35d27b327b2418fd0e
Nico Marcussen-Wulff <info@sac-home.org>
2000-07-06 15:54:08 +0000

    macros debugged

--- src/runtime/sac_cwrapper.h
+++ src/runtime/sac_cwrapper.h
@@ -13,17 +13,30 @@
         return (2);                                                                      \
     }
 
+/* argument is simple type */
 #define SAC_ARGCALL_SIMPLE(var, type) (*((type *)(SAC_ARG_ELEMS (var))))
+
+/* argument is array type - expand with additional refcounter */
 #define SAC_ARGCALL_REFCNT(var, type) (type *)(SAC_ARG_ELEMS (var)), (SAC_ARG_RC (var))
-#define SAC_RESULT_SIMPLE(var, type) (*((type **)(SAC_ARG_ELEMS ((*var)))))
+
+/* result is simple type */
+#define SAC_RESULT_SIMPLE(var, type) ((type *)(SAC_ARG_ELEMS ((*var))))
+
+/* result is array type - expand with additional refcounter */
 #define SAC_RESULT_REFCNT(var, type)                                                     \
-    (type **)(SAC_ARG_ELEMS ((*var))), (int **)(SAC_ARG_RC ((*var)))
+    (type **)(&SAC_ARG_ELEMS ((*var))), (int **)(&SAC_ARG_RC ((*var)))
+
+/* alloc data memory for simple results */
+#define SAC_CI_INIT_SIMPLE_RESULT(var, type)                                             \
+    SAC_ARG_ELEMS ((*var)) = SAC_MALLOC (sizeof (type));
+
+/* simple type used as direct result */
 #define SAC_ASSIGN_RESULT(var, type) (*((type *)SAC_ARG_ELEMS ((*var))))
-#define SAC_SETREFCOUNT(var, value)                                                      \
-    {                                                                                    \
-        *(SAC_ARG_RC ((*var))) = value;                                                  \
-        SAC_ARG_LRC ((*var)) = value;                                                    \
-    }
+
+/* set local refcount of var to value */
+#define SAC_SETLOCALRC(var, value) SAC_ARG_LRC ((*var)) = value;
+
+/* */
 #define SAC_DECANDFREERC(var)                                                            \
     {                                                                                    \
         if (SAC_ARG_LRC (var) == 0) {                                                    \
