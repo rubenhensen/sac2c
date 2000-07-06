a901916ddf2d8407f5886bedf8c163de77a58c76
Nico Marcussen-Wulff <info@sac-home.org>
2000-07-06 15:54:33 +0000

    convert functions changed to car_Args parameters

--- src/libsac/cinterface.c
+++ src/libsac/cinterface.c
@@ -12,7 +12,7 @@
 typedef enum {
 #define TYP_IFname(name) name
 #include "../tree/type_info.mac"
-};
+} SAC_ARG_simpletype_names;
 
 /* constants */
 #define SAC_CI_SIMPLETYPE 1
@@ -69,73 +69,123 @@ SAC_FreeRuntimeSystem ()
  *
  */
 
+/* macros used for all types */
+#define SAC_ARRAY2SAC(c_type, SAC_type)                                                  \
+    va_list Argp;                                                                        \
+    SAC_arg result;                                                                      \
+    int elemscount;                                                                      \
+    int i;                                                                               \
+    int *shpvec;                                                                         \
+    c_type *elems;                                                                       \
+                                                                                         \
+    /* create shapevector */                                                             \
+    if (dim > 0) {                                                                       \
+        shpvec = (int *)SAC_MALLOC (dim * sizeof (int));                                 \
+        va_start (Argp, dim);                                                            \
+        for (i = 0; i < dim; i++) {                                                      \
+            shpvec[i] = va_arg (Argp, int);                                              \
+        }                                                                                \
+    } else {                                                                             \
+        shpvec = NULL;                                                                   \
+    }                                                                                    \
+                                                                                         \
+    if (reuseflag == SAC_COPY_ARGS) {                                                    \
+        /*                                                                               \
+         * create copy of args for internal usage                                        \
+         * calculate number of data elements                                             \
+         */                                                                              \
+        elemscount = 1;                                                                  \
+        for (i = 0; i < dim; i++)                                                        \
+            elemscount *= shpvec[i];                                                     \
+                                                                                         \
+        if (elemscount <= 0) {                                                           \
+            SAC_RuntimeError ("Illegal shape vector!");                                  \
+        }                                                                                \
+                                                                                         \
+        elems = (c_type *)SAC_MALLOC (elemscount * sizeof (c_type));                     \
+        elems = memcpy (elems, array, elemscount * sizeof (c_type));                     \
+    } else {                                                                             \
+        /* args cans be reused internally */                                             \
+        elems = array;                                                                   \
+    }                                                                                    \
+                                                                                         \
+    result = SAC_CI_NewSACArg (SAC_type, dim, shpvec);                                   \
+                                                                                         \
+    SAC_ARG_ELEMS (result) = elems;                                                      \
+                                                                                         \
+    result = SAC_CI_InitRefcounter (result, 1);                                          \
+    return (result)
+
+#define SAC_SIMPLE2SAC(c_type, SAC_type)                                                 \
+    SAC_arg result;                                                                      \
+    result = SAC_CI_NewSACArg (SAC_type, 0, NULL);                                       \
+    SAC_ARG_ELEMS (result) = (c_type *)SAC_MALLOC (sizeof (c_type));                     \
+    *((c_type *)SAC_ARG_ELEMS (result)) = value;                                         \
+                                                                                         \
+    result = SAC_CI_InitRefcounter (result, 1);                                          \
+    return (result)
+
+#define SAC_SAC2ARRAY(c_type, SAC_type)                                                  \
+    /* check for valid data */                                                           \
+    SAC_CI_ExitOnInvalidArg (sa, SAC_type, SAC_CI_ARRAYTYPE);                            \
+    /* return value */                                                                   \
+    return ((c_type *)SAC_ARG_ELEMS (sa))
+
+#define SAC_SAC2SIMPLE(c_type, SAC_type)                                                 \
+    /* check for valid data */                                                           \
+    SAC_CI_ExitOnInvalidArg (sa, SAC_type, SAC_CI_SIMPLETYPE);                           \
+    /* return value */                                                                   \
+    return (*((c_type *)SAC_ARG_ELEMS (sa)))
+
+/******************************************************************************
+ *
+ * functions: here for type ???
+ *   SAC_arg  SAC_???Array2Sac(SAC_reusetype reuseflag, ??? *array, int dim, ...)
+ *   SAC_arg  SAC_Int2Sac(??? value)
+ *   ???     *SAC_Sac2IntArray(SAC_arg sa)
+ *   ???      SAC_Sac2Int(SAC_arg sa)
+ *
+ * description:
+ *   converts c datatype to SAC_arg datatype and vice versa.
+ *   for every basic type there are 4 functions:
+ *         simpletype -> SAC_arg
+ *         arraytype  -> SAC_arg
+ *         SAC_arg    -> simpletype
+ *         SAC_arg    -> arraytype
+ *
+ *   for the arraytype to SAC_arg you have to specify the dimension and its
+ *   shape (as varargs after the dimension).
+ *   When reuseflag is set to SAC_CONSUME_ARGS, the array is reused internally,
+ *   and you are not allowed to access this array any more!!! Do not free this
+ *   array!!!
+ *   When reuseflag is set to SAC_COPY_ARGS, then the argument array is not
+ *   touched and you might use it in the future.
+ *
+ ******************************************************************************/
+
+/* functions for the usual basetypes */
 SAC_arg
-SAC_IntArray2Sac (int *array, int dim, int *shape, SAC_reusetype reuseflag)
+SAC_IntArray2Sac (SAC_reusetype reuseflag, int *array, int dim, ...)
 {
-    SAC_arg result;
-    int elemscount;
-    int i;
-    int *shpvec;
-    int *elems;
-
-    if (reuseflag == SAC_COPY_ARGS) {
-        /* create copy of args for internal usage */
-        shpvec = (int *)SAC_MALLOC (dim * sizeof (int));
-        shpvec = memcpy (shpvec, shape, dim * sizeof (int));
-
-        /* calculate number of dataelements */
-        elemscount = 1;
-        for (i = 0; i < dim; i++)
-            elemscount *= shape[i];
-
-        if (elemscount <= 0) {
-            SAC_RuntimeError ("Illegal shape vector!");
-        }
-
-        elems = (int *)SAC_MALLOC (elemscount * sizeof (int));
-        elems = memcpy (elems, array, elemscount * sizeof (int));
-    } else {
-        /* args cans be reused internally */
-        shpvec = shape;
-        elems = array;
-    }
-
-    result = SAC_CI_NewSACArg (T_int, dim, shpvec);
-    SAC_ARG_ELEMS (result) = elems;
-    SAC_ARG_RC (result) = (int *)SAC_MALLOC (sizeof (int));
-    *SAC_ARG_RC (result) = 1; /* init refcounter */
-    SAC_ARG_LRC (result) = 1; /* init local refcounter */
-    return (result);
+    SAC_ARRAY2SAC (int, T_int);
 }
 
 SAC_arg
 SAC_Int2Sac (int value)
 {
-    SAC_arg result;
-    result = SAC_CI_NewSACArg (T_int, 0, NULL);
-    SAC_ARG_ELEMS (result) = (int *)SAC_MALLOC (sizeof (int));
-    SAC_ARG_RC (result) = (int *)SAC_MALLOC (sizeof (int));
-    *SAC_ARG_RC (result) = 1; /* init refcounter */
-    SAC_ARG_LRC (result) = 1; /* init local refcounter */
-    return (result);
+    SAC_SIMPLE2SAC (int, T_int);
 }
 
 int *
 SAC_Sac2IntArray (SAC_arg sa)
 {
-    /* check for valid data */
-    SAC_CI_ExitOnInvalidArg (sa, T_int, SAC_CI_ARRAYTYPE);
-    /* return value */
-    return ((int *)SAC_ARG_ELEMS (sa));
+    SAC_SAC2ARRAY (int, T_int);
 }
 
 int
 SAC_Sac2Int (SAC_arg sa)
 {
-    /* check for valid data */
-    SAC_CI_ExitOnInvalidArg (sa, T_int, SAC_CI_SIMPLETYPE);
-    /* return value */
-    return (*((int *)SAC_ARG_ELEMS (sa)));
+    SAC_SAC2SIMPLE (int, T_int);
 }
 
 /******************************************************************************
