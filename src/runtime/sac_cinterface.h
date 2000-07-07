3b9f91f926b0f6c3ccafddfe4138c4af7f3e9b47
Nico Marcussen-Wulff <info@sac-home.org>
2000-07-07 15:32:34 +0000

    utility functions added

--- src/runtime/sac_cinterface.h
+++ src/runtime/sac_cinterface.h
@@ -8,7 +8,7 @@
 /* functions and datatype for c <-> SAC interface */
 
 /* constants for mode */
-typedef enum { SAC_CONSUME_ARGS, SAC_COPY_ARGS } SAC_reusetype;
+typedef enum { SAC_CONSUME_ARG, SAC_COPY_ARG } SAC_reusetype;
 
 /* data type */
 typedef struct SAC_ARG_STRUCT *SAC_arg;
@@ -27,17 +27,24 @@ extern void SAC_InitRuntimeSystem ();
 /* do some cleanup after running the SAC Module         */
 extern void SAC_FreeRuntimeSystem ();
 
+/* utilities for SAC_arg data type */
+extern int SAC_isValid (SAC_arg sa);
+extern int SAC_GetDim (SAC_arg sa);
+extern int SAC_GetShape (SAC_arg sa, int pos);
+extern int SAC_GetRefcounter (SAC_arg sa);
+extern int SAC_SetRefcounter (SAC_arg sa, int newrc);
+
 /* functions to convert SAC_arg from and to c datatypes */
-/* int */
+/* for int */
 extern SAC_arg SAC_IntArray2Sac (SAC_reusetype reuseflag, int *array, int dim, ...);
 extern SAC_arg SAC_Int2Sac (int value);
-extern int *SAC_Sac2IntArray (SAC_arg sa);
+extern int *SAC_Sac2IntArray (SAC_reusetype reuseflag, SAC_arg sa);
 extern int SAC_Sac2Int (SAC_arg sa);
 
-/* double */
+/* for double */
 extern SAC_arg SAC_DoubleArray2Sac (SAC_reusetype reuseflag, double *array, int dim, ...);
 extern SAC_arg SAC_Double2Sac (double value);
-extern double *SAC_Sac2DoubleArray (SAC_arg sa);
+extern double *SAC_Sac2DoubleArray (SAC_reusetype reuseflag, SAC_arg sa);
 extern double SAC_Sac2Double (SAC_arg sa);
 
 /* other types -> to be implemented */
