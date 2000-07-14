86016339848d37a1b432b6c76f6ac21cd8c7f500
Dietmar Kreye <dietmar@computer-scientist.org>
2000-07-14 11:51:18 +0000

    DFRblock added

--- src/optimize/DeadFunctionRemoval.h
+++ src/optimize/DeadFunctionRemoval.h
@@ -1,24 +1,25 @@
 /*
  *
  * $Log$
+ * Revision 2.2  2000/07/14 11:51:18  dkr
+ * DFRblock added
+ *
  * Revision 2.1  1999/02/23 12:41:15  sacbase
  * new release made
  *
  * Revision 1.1  1999/01/07 17:37:03  sbs
  * Initial revision
  *
- *
- *
  */
 
-#ifndef _DeadFunctionRemoval_h
-
-#define _DeadFunctionRemoval_h
+#ifndef _sac_DeadFunctionRemoval_h
+#define _sac_DeadFunctionRemoval_h
 
 extern node *DeadFunctionRemoval (node *arg_node, node *info_node);
 
 extern node *DFRmodul (node *arg_node, node *arg_info);
 extern node *DFRfundef (node *arg_node, node *arg_info);
+extern node *DFRblock (node *arg_node, node *arg_info);
 extern node *DFRap (node *arg_node, node *arg_info);
 
 #endif /* _DeadFunctionRemoval_h */
