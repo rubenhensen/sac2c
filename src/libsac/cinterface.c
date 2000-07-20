3a5d499f15cf51e6514bc02e4d76d97ce4d3ae71
Nico Marcussen-Wulff <info@sac-home.org>
2000-07-20 11:38:43 +0000

    SAC_FreeRuntimesystem now frees all used sac-modules

--- src/libsac/cinterface.c
+++ src/libsac/cinterface.c
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.9  2000/07/20 11:38:43  nmw
+ * SAC_FreeRuntimesystem now frees all used sac-modules
+ *
  * Revision 1.8  2000/07/13 09:46:07  nmw
  * convert functions for double, float, long, char added
  *
@@ -45,6 +48,11 @@ typedef enum {
 /* global vars */
 static bool SAC_CI_runtime_system_active = 0;
 
+#define CHECK_FOR_ACTIVE_RUNTIMESYSTEM()                                                 \
+    if (!SAC_CI_runtime_system_active) {                                                 \
+        SAC_InitRuntimeSystem ();                                                        \
+    }
+
 /******************************************************************************
  *
  * function:
@@ -54,6 +62,7 @@ static bool SAC_CI_runtime_system_active = 0;
  *   do some init procedures for the SAC runtime system
  *
  ******************************************************************************/
+
 void
 SAC_InitRuntimeSystem ()
 {
@@ -84,6 +93,7 @@ SAC_FreeRuntimeSystem ()
     if (SAC_CI_runtime_system_active) {
         /* free all used SAC_args */
         SAC_CI_FreeSACArgDirectory ();
+        SAC_FIH_FreeAllModules ();
         printf ("SAC-Runtime-System cleaned up!\n");
     }
 }
@@ -101,6 +111,7 @@ SAC_FreeRuntimeSystem ()
 int
 SAC_isValid (SAC_arg sa)
 {
+    CHECK_FOR_ACTIVE_RUNTIMESYSTEM ();
     return (SAC_ARG_LRC (sa) > 0 ? 1 : 0);
 }
 
@@ -117,6 +128,7 @@ SAC_isValid (SAC_arg sa)
 int
 SAC_GetDim (SAC_arg sa)
 {
+    CHECK_FOR_ACTIVE_RUNTIMESYSTEM ();
     return (SAC_ARG_DIM (sa));
 }
 
@@ -133,6 +145,7 @@ SAC_GetDim (SAC_arg sa)
 int
 SAC_GetShapeElement (SAC_arg sa, int pos)
 {
+    CHECK_FOR_ACTIVE_RUNTIMESYSTEM ();
     if (pos >= SAC_ARG_DIM (sa)) {
         SAC_RuntimeError ("Access to illegal position in shape vector!\n");
     }
@@ -152,6 +165,7 @@ SAC_GetShapeElement (SAC_arg sa, int pos)
 int
 SAC_GetRefcounter (SAC_arg sa)
 {
+    CHECK_FOR_ACTIVE_RUNTIMESYSTEM ();
     return (SAC_ARG_LRC (sa));
 }
 
@@ -170,6 +184,7 @@ SAC_GetRefcounter (SAC_arg sa)
 int
 SAC_SetRefcounter (SAC_arg sa, int newrc)
 {
+    CHECK_FOR_ACTIVE_RUNTIMESYSTEM ();
     if (newrc < 0)
         SAC_RuntimeError ("Illegal refcounter value specified!\n");
     if (SAC_ARG_LRC (sa) > 0) {
@@ -195,6 +210,7 @@ SAC_SetRefcounter (SAC_arg sa, int newrc)
     int *shpvec;                                                                         \
     c_type *elems;                                                                       \
                                                                                          \
+    CHECK_FOR_ACTIVE_RUNTIMESYSTEM ();                                                   \
     /* create shapevector */                                                             \
     if (dim > 0) {                                                                       \
         shpvec = (int *)SAC_MALLOC (dim * sizeof (int));                                 \
@@ -237,6 +253,8 @@ SAC_SetRefcounter (SAC_arg sa, int newrc)
 
 #define SAC_SIMPLE2SAC(c_type, SAC_type)                                                 \
     SAC_arg result;                                                                      \
+                                                                                         \
+    CHECK_FOR_ACTIVE_RUNTIMESYSTEM ();                                                   \
     result = SAC_CI_NewSACArg (SAC_type, 0, NULL);                                       \
     SAC_ARG_ELEMS (result) = (c_type *)SAC_MALLOC (sizeof (c_type));                     \
     *((c_type *)SAC_ARG_ELEMS (result)) = value;                                         \
@@ -250,6 +268,8 @@ SAC_SetRefcounter (SAC_arg sa, int newrc)
     c_type *result;                                                                      \
     int i;                                                                               \
     int elemscount;                                                                      \
+                                                                                         \
+    CHECK_FOR_ACTIVE_RUNTIMESYSTEM ();                                                   \
     /* check for valid data */                                                           \
     SAC_CI_ExitOnInvalidArg (sa, SAC_type, SAC_CI_ARRAYTYPE);                            \
     if (reuseflag == SAC_COPY_ARG) {                                                     \
@@ -275,6 +295,8 @@ SAC_SetRefcounter (SAC_arg sa, int newrc)
 /* SAC_arg -> c-simpletype */
 
 #define SAC_SAC2SIMPLE(c_type, SAC_type)                                                 \
+                                                                                         \
+    CHECK_FOR_ACTIVE_RUNTIMESYSTEM ();                                                   \
     /* check for valid data */                                                           \
     SAC_CI_ExitOnInvalidArg (sa, SAC_type, SAC_CI_SIMPLETYPE);                           \
     /* return value */                                                                   \
