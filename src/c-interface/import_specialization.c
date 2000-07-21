032034513b036618a2149bbbb4e59018cc4500cc
Nico Marcussen-Wulff <info@sac-home.org>
2000-07-21 15:13:06 +0000

    specfile parsing integrated

--- src/c-interface/import_specialization.c
+++ src/c-interface/import_specialization.c
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.2  2000/07/21 15:13:06  nmw
+ * specfile parsing integrated
+ *
  * Revision 1.1  2000/07/21 08:18:37  nmw
  * Initial revision
  *
@@ -20,6 +23,7 @@
 #include "globals.h"
 #include "free.h"
 #include "resource.h"
+#include "scnprs.h"
 #include "import_specialization.h"
 
 node *
@@ -49,7 +53,63 @@ IMPSPECarg (node *arg_node, node *arg_info)
 node *
 ImportSpecialization (node *syntax_tree)
 {
+    node *arg_info;
+    funtab *old_tab;
+
+    char buffer[MAX_FILE_NAME];
+    node *spec = NULL;
+    char *pathname, *abspathname, *old_filename;
+
     DBUG_ENTER ("ImportSpecialization");
+
+    strcpy (buffer, MODUL_NAME (syntax_tree));
+    strcat (buffer, ".spec");
+
+    pathname = FindFile (PATH, buffer);
+    yyin = fopen (pathname, "r");
+
+    if (yyin == NULL) {
+        NOTE (("No additional specialization-file found !"));
+    } else {
+        abspathname = AbsolutePathname (pathname);
+
+        NOTE (("Loading own specializations !"));
+        NOTE (("  Parsing file \"%s\" ...", abspathname));
+
+        linenum = 1;
+        old_filename = filename; /* required for restauration */
+        filename = buffer;
+        start_token = PARSE_SPEC;
+        My_yyparse ();
+        fclose (yyin);
+
+        if ((strcmp (MODSPEC_NAME (spec_tree), MODUL_NAME (syntax_tree)) != 0)
+            || (NODE_TYPE (spec_tree) != N_modspec)) {
+            SYSERROR (("File \"%s\" provides wrong specialization data", filename));
+            ABORT_ON_ERROR;
+        }
+        spec = spec_tree;
+
+        /* analyse specializations of fundefs
+         * start traversal of spec fundefs
+         */
+
+        arg_info = MakeInfo ();
+        INFO_IMPSPEC_SPECS (arg_info) = spec;
+        INFO_IMPSPEC_MODUL (arg_info) = syntax_tree;
+
+        old_tab = act_tab;
+        act_tab = impspec_tab;
+
+        spec = Trav (spec, arg_info);
+
+        syntax_tree = INFO_IMPSPEC_MODUL (arg_info);
+        act_tab = old_tab;
+
+        FREE (arg_info);
+
+        FreeNode (spec);
+    }
 
     DBUG_RETURN (syntax_tree);
 }
