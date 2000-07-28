d2458bb2b04ed3d58351823224efd44a2547bfcd
Nico Marcussen-Wulff <info@sac-home.org>
2000-07-28 14:42:15 +0000

    Prototypes changed to handle T_user types

--- src/runtime/sac_arg.h
+++ src/runtime/sac_arg.h
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.6  2000/07/28 14:42:15  nmw
+ * Prototypes changed to handle T_user types
+ *
  * Revision 1.5  2000/07/07 15:32:53  nmw
  * InUseDirectory added
  *
@@ -33,12 +36,13 @@ typedef int SAC_ARG_simpletype;
 
 /* SAC integer array */
 typedef struct SAC_ARG_STRUCT {
-    int type;    /* type of argument */
-    int *shpvec; /* shapevector of argument */
-    int dim;     /* dim of shape */
-    void *elems; /* pointer to data elements */
-    int *rc;     /* pointer to revcounter */
-    int lrc;     /* local refcounter copy */
+    int type;       /* type of argument */
+    int *shpvec;    /* shapevector of argument */
+    int dim;        /* dim of shape */
+    void *elems;    /* pointer to data elements */
+    int *rc;        /* pointer to revcounter */
+    int lrc;        /* local refcounter copy */
+    char *typename; /* name of usertype or NULL */
 } SAC_ARG_STRUCT;
 
 #define SAC_ARG_LRC(a) (a->lrc)
@@ -47,12 +51,17 @@ typedef struct SAC_ARG_STRUCT {
 #define SAC_ARG_TYPE(a) (a->type)
 #define SAC_ARG_SHPVEC(a) (a->shpvec)
 #define SAC_ARG_DIM(a) (a->dim)
+#define SAC_ARG_TNAME(a) (a->typename)
 
-extern SAC_arg SAC_CI_NewSACArg (SAC_ARG_simpletype basetype, int dim, int *shpvec);
-extern SAC_arg SAC_CI_CreateSACArg (SAC_ARG_simpletype basetype, int dim, ...);
-extern bool SAC_CI_CmpSACArgType (SAC_arg sa, SAC_ARG_simpletype basetype, int dim, ...);
+extern SAC_arg SAC_CI_NewSACArg (SAC_ARG_simpletype basetype, char *tname, int dim,
+                                 int *shpvec);
+extern SAC_arg SAC_CI_CreateSACArg (SAC_ARG_simpletype basetype, char *tname, int dim,
+                                    ...);
+extern bool SAC_CI_CmpSACArgType (SAC_arg sa, SAC_ARG_simpletype basetype, char *tname,
+                                  int dim, ...);
 extern SAC_arg SAC_CI_InitRefcounter (SAC_arg sa, int initvalue);
-extern void SAC_CI_ExitOnInvalidArg (SAC_arg sa, SAC_ARG_simpletype basetype, int flag);
+extern void SAC_CI_ExitOnInvalidArg (SAC_arg sa, SAC_ARG_simpletype basetype, char *tname,
+                                     int flag);
 
 extern void SAC_CI_InitSACArgDirectory ();
 extern void SAC_CI_FreeSACArgDirectory ();
