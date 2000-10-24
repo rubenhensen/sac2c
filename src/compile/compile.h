d9e081fa7f1d394e89b2d64ffa51b49a744a72f2
Dietmar Kreye <dietmar@computer-scientist.org>
2000-10-24 11:08:35 +0000

    all deprecated macros have been moved to compile.c

--- src/compile/compile.h
+++ src/compile/compile.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 2.15  2000/10/24 11:08:35  dkr
+ * all deprecated macros have been moved to compile.c
+ *
  * Revision 2.14  2000/10/17 13:03:30  dkr
  * macro COUNT_ELEMS renamed into EXPRS_LENGTH and moved to tree_compound.h
  * macro FUN_DOES_REFCOUNT moved from compile.c to compile.h
@@ -95,87 +98,5 @@ extern node *GetFoldVardecs (node *fundef);
      || ((FUNDEF_PRAGMA (fundef) != NULL) && (FUNDEF_REFCOUNTING (fundef) != NULL)       \
          && (PRAGMA_NUMPARAMS (FUNDEF_PRAGMA (fundef)) > i)                              \
          && FUNDEF_REFCOUNTING (fundef)[i]))
-
-/*
- * some macros for creation of N_icms
- *
- * PLEASE DO NOT USE THE FOLLOWING MACROS FOR NEW CODE!!!!!
- * Use the functions MakeIcm? and MakeAssignIcm? instead!!
- */
-
-#define MAKE_NEXT_ICM_ARG(prev, new_node)                                                \
-    {                                                                                    \
-        node *_tmp;                                                                      \
-        _tmp = MakeExprs (new_node, NULL);                                               \
-        EXPRS_NEXT (prev) = _tmp;                                                        \
-        prev = _tmp;                                                                     \
-    }
-
-#define APPEND_ICM_ARG(prev, new)                                                        \
-    EXPRS_NEXT (prev) = new;                                                             \
-    prev = new;
-
-/*
- * The following macros use a variabel 'node *icm_arg', it has to be defined
- * before usage.
- *
- * PLEASE DO NOT USE THE FOLLOWING MACROS FOR NEW CODE!!!!!
- * Use the functions MakeIcm? and MakeAssignIcm? instead!!
- */
-
-#define CREATE_1_ARY_ICM(assign, str, arg)                                               \
-    assign = MakeAssign (MakeIcm (str, NULL), NULL);                                     \
-    ICM_ARGS (ASSIGN_INSTR (assign)) = MakeExprs (arg, NULL);                            \
-    icm_arg = ICM_ARGS (ASSIGN_INSTR (assign));
-
-#define CREATE_2_ARY_ICM(assign, str, arg1, arg2)                                        \
-    CREATE_1_ARY_ICM (assign, str, arg1);                                                \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg2)
-
-#define CREATE_3_ARY_ICM(assign, str, arg1, arg2, arg3)                                  \
-    CREATE_1_ARY_ICM (assign, str, arg1);                                                \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg2);                                                   \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg3)
-
-#define CREATE_4_ARY_ICM(assign, str, arg1, arg2, arg3, arg4)                            \
-    CREATE_1_ARY_ICM (assign, str, arg1);                                                \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg2);                                                   \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg3);                                                   \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg4)
-
-#define CREATE_5_ARY_ICM(assign, str, arg1, arg2, arg3, arg4, arg5)                      \
-    CREATE_4_ARY_ICM (assign, str, arg1, arg2, arg3, arg4);                              \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg5)
-
-#define CREATE_6_ARY_ICM(assign, str, arg1, arg2, arg3, arg4, arg5, arg6)                \
-    CREATE_5_ARY_ICM (assign, str, arg1, arg2, arg3, arg4, arg5);                        \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg6)
-
-#define CREATE_7_ARY_ICM(assign, str, arg1, arg2, arg3, arg4, arg5, arg6, arg7)          \
-    CREATE_6_ARY_ICM (assign, str, arg1, arg2, arg3, arg4, arg5, arg6);                  \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg7)
-
-#define BIN_ICM_REUSE(reuse, str, arg1, arg2)                                            \
-    NODE_TYPE (reuse) = N_icm;                                                           \
-    ICM_NAME (reuse) = str;                                                              \
-    ICM_ARGS (reuse) = MakeExprs (arg1, NULL);                                           \
-    ICM_INDENT_BEFORE (reuse) = 0;                                                       \
-    ICM_INDENT_AFTER (reuse) = 0;                                                        \
-    icm_arg = ICM_ARGS (reuse);                                                          \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg2)
-
-#define TRI_ICM_REUSE(reuse, str, arg1, arg2, arg3)                                      \
-    NODE_TYPE (reuse) = N_icm;                                                           \
-    ICM_NAME (reuse) = str;                                                              \
-    ICM_ARGS (reuse) = MakeExprs (arg1, NULL);                                           \
-    ICM_INDENT_BEFORE (reuse) = 0;                                                       \
-    ICM_INDENT_AFTER (reuse) = 0;                                                        \
-    icm_arg = ICM_ARGS (reuse);                                                          \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg2);                                                   \
-    MAKE_NEXT_ICM_ARG (icm_arg, arg3)
-
-#define APPEND_ASSIGNS(first, next)                                                      \
-    ASSIGN_NEXT (first) = next;                                                          \
-    first = next
 
 #endif /* _sac_compile_h */
