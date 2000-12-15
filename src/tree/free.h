77748f2bc5c9f951da9b1c351197ecb218e4e648
Sven-Bodo Scholz <sbs@sac-home.org>
2000-12-15 17:08:11 +0000

    include "malloc.h" changed into include <malloc.h> for the alpha to find it!

--- src/tree/free.h
+++ src/tree/free.h
@@ -1,126 +1,17 @@
 /*
  *
  * $Log$
+ * Revision 3.3  2000/12/15 17:08:11  sbs
+ * include "malloc.h" changed into include <malloc.h> for
+ * the alpha to find it!
+ *
  * Revision 3.2  2000/12/12 12:13:59  dkr
  * nodes N_pre, N_post, N_inc, N_dec added
  *
  * Revision 3.1  2000/11/20 18:03:24  sacbase
  * new release made
  *
- * Revision 1.5  2000/07/21 08:20:15  nmw
- * FreeModspec added
- *
- * Revision 1.4  2000/06/23 15:32:44  nmw
- * FreeCWrapper added
- *
- * Revision 1.3  2000/06/13 12:25:28  dkr
- * functions for old with-loop removed
- *
- * Revision 1.2  2000/03/29 16:09:54  jhs
- * FreeST and FreeMT added.
- *
- * Revision 1.1  2000/01/21 15:38:23  dkr
- * Initial revision
- *
- * Revision 2.3  1999/10/04 12:33:53  sbs
- * globals.h included
- * (needed by FREE
- *
- * Revision 2.2  1999/02/28 21:07:40  srs
- * added macro FREE_INDEX_INFO
- *
- * Revision 2.1  1999/02/23 12:39:19  sacbase
- * new release made
- *
- * Revision 1.27  1999/02/11 13:37:23  cg
- * Improved debugging opportunities by hiding calls to free() behind
- * wrapper function Free().
- *
- * Revision 1.26  1999/02/06 12:53:23  srs
- * added FreeNodelistNode()
- *
- * Revision 1.25  1999/01/15 15:13:19  cg
- * added functions FreeOneAccess() and FreeAllAccess().
- *
- * Revision 1.24  1998/08/07 14:36:48  dkr
- * FreeWLsegVar added
- *
- * Revision 1.23  1998/04/26 21:51:28  dkr
- * FreeSPMD renamed to FreeSpmd
- *
- * Revision 1.22  1998/04/23 17:32:19  dkr
- * added FreeSync
- *
- * Revision 1.21  1998/04/17 17:26:00  dkr
- * 'concurrent regions' are now called 'SPMD regions'
- *
- * Revision 1.20  1998/04/02 17:40:54  dkr
- * added FreeConc
- *
- * Revision 1.19  1998/04/01 23:55:39  dkr
- * added FreeWLstriVar, FreeWLgridVar
- *
- * Revision 1.18  1998/03/27 18:38:09  dkr
- * WLproj renamed in WLstride:
- *   WLPROJ... -> WLSTRIDE...
- *   FreeWLproj -> FreeWLstride
- *
- * Revision 1.17  1998/03/16 00:07:33  dkr
- * added FreeWLseg, FreeWLblock, FreeWLublock, FreeWLproj, FreeWLgrid, FreeNwith2
- *
- * Revision 1.16  1997/12/08 19:19:01  dkr
- * no arithmetic on void-pointers anymore (FREE)
- *
- * Revision 1.15  1997/12/06 17:14:55  srs
- * changed FREE
- *
- * Revision 1.14  1997/11/23 15:17:52  dkr
- * CC-flag: show_malloc -> SHOW_MALLOC
- *
- * Revision 1.13  1997/11/13 16:11:31  srs
- * free functions for the new WL-syntaxtree
- *
- * Revision 1.12  1997/10/29 14:55:44  srs
- * changed FREE and removed HAVE_MALLOC_O
- *
- * Revision 1.11  1997/10/29 13:01:00  srs
- * new macro FREE (show_malloc).
- * removed HAVE_MALLOC_O
- *
- * Revision 1.10  1997/04/24 14:05:37  sbs
- * HAVE_MALLOC_O inserted
- *
- * Revision 1.9  1997/03/19  13:34:57  cg
- * added functions FreeAllDeps() and FreeOneDeps()
- *
- * Revision 1.8  1995/12/29  10:24:18  cg
- * inserted call to malloc_verify into macro FREE, added FreeInfo
- *
- * Revision 1.7  1995/12/20  08:14:25  cg
- * new function FreeChar for new N_char node
- *
- * Revision 1.6  1995/12/18  16:14:08  cg
- * macro FREE modified: now you can compile with -DNOFREE to disable
- * all memory deallocation caused by this macro.
- *
- * Revision 1.5  1995/12/01  16:16:12  cg
- * new macro FREE is designed to replace macros with the same name
- * in typecheck, compile, optimize, etc.
- *
- * Revision 1.4  1995/11/16  19:33:24  cg
- * The free module was entirely rewritten.
- * Each node type now has its own free function.
- * Functions FreeTree and FreeNode for deleting single nodes and
- * whole sub trees.
- * Various free functions for non-node structures.
- * FreeNodelist moved from tree_compound.c
- *
- * Revision 1.3  1995/06/16  13:10:46  asi
- * added FreePrf2, which will free memory occupied by a N_prf-subtree,
- *                 but it will not free one given argument.
- *
- * Revision 1.2  1995/01/18  17:28:37  asi
- * Added FreeTree, FreeNoInfo, FreeInfoId, FreeInfoIds, FreeInfoType, FreeModul
+ * ... [eliminated]
  *
  * Revision 1.1  1994/12/27  09:28:19  sbs
  * Initial revision
@@ -132,9 +23,9 @@
 
 #define _sac_free_h
 
+#include <malloc.h>
 #include "dbug.h"
 #include "my_debug.h"
-#include "malloc.h"
 #include "globals.h"
 #include "internal_lib.h"
 
