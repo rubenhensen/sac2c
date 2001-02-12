8e7628e6e481e1955ca41796cc33ffce3fb444e9
Nico Marcussen-Wulff <info@sac-home.org>
2001-02-12 16:59:27 +0000

    Initial revision

---
+++ src/tree/CheckAvis.c
@@ -0,0 +1,55 @@
+/*
+ * $Log$
+ * Revision 1.1  2001/02/12 16:59:27  nmw
+ * Initial revision
+ *
+ *
+ */
+
+/*****************************************************************************
+ *
+ * file:   CheckAvis.c
+ *
+ * prefix: CAV
+ *
+ * description:
+ *
+ *   This module restores the AVIS attribute in N_id, N_vardec/N_arg
+ *   when old code did not updates all references correctly.
+ *
+ *
+ *****************************************************************************/
+
+#include "dbug.h"
+#include "tree.h"
+#include "CheckAvis.h"
+
+node *
+CAVarg (node *arg_node, node *arg_info)
+{
+    return (NULL);
+}
+
+node *
+CAVvardec (node *arg_node, node *arg_info)
+{
+    return (NULL);
+}
+
+node *
+CAVid (node *arg_node, node *arg_info)
+{
+    return (NULL);
+}
+
+node *
+CAVlet (node *arg_node, node *arg_info)
+{
+    return (NULL);
+}
+
+node *
+CheckAvis (node *syntax_tree)
+{
+    return (NULL);
+}
---
+++ src/tree/CheckAvis.h
@@ -0,0 +1,34 @@
+/*
+ * $Log$
+ * Revision 1.1  2001/02/12 16:58:52  nmw
+ * Initial revision
+ *
+ *
+ */
+
+/*****************************************************************************
+ *
+ * file:   CheckAvis.h
+ *
+ * prefix: CAV
+ *
+ * description:
+ *
+ *   This module restores the AVIS attribute in N_id, N_vardec/N_arg
+ *   when old code did not updates all references correctly.
+ *
+ *
+ *****************************************************************************/
+
+#ifndef SAC_CHECKAVIS_H
+
+#define SAC_CHECKAVIS_H
+
+extern node *CheckAvis (node *syntax_tree);
+
+extern node *CAVarg (node *arg_node, node *arg_info);
+extern node *CAVvardec (node *arg_node, node *arg_info);
+extern node *CAVid (node *arg_node, node *arg_info);
+extern node *CAVlet (node *arg_node, node *arg_info);
+
+#endif /* SAC_CHECKAVIS_H */
