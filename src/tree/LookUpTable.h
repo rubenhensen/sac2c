9fd376b9f802a486370cc615c666f0d8cb23b652
Dietmar Kreye <dietmar@computer-scientist.org>
2001-03-22 13:30:39 +0000

    Support for strings added: InsertIntoLUT renamed into InsertIntoLUT_P, SearchInLUT renamed into SearchInLUT_P, InsertIntoLUT_S, SearchInLUT_S added.

--- src/tree/LookUpTable.h
+++ src/tree/LookUpTable.h
@@ -1,6 +1,12 @@
 /*
  *
  * $Log$
+ * Revision 3.2  2001/03/22 13:30:39  dkr
+ * Support for strings added:
+ * InsertIntoLUT renamed into InsertIntoLUT_P,
+ * SearchInLUT renamed into SearchInLUT_P,
+ * InsertIntoLUT_S, SearchInLUT_S added.
+ *
  * Revision 3.1  2000/11/20 18:03:21  sacbase
  * new release made
  *
@@ -18,15 +24,43 @@
  *
  */
 
-#ifndef _sac_LookUpTable_h
-#define _sac_LookUpTable_h
+/*
+ *  Look-Up-Table (LUT) for Pointers and Strings
+ *  --------------------------------------------
+ *
+ *  Each entry of a LUT can hold a pair [old, new] of pointers (void*) or
+ *  strings (char*).
+ *
+ *  To insert a pair of pointers or strings into the LUT use the function
+ *  InsertIntoLUT_P()  or  InsertIntoLUT_S()  respectively.
+ *
+ *  The function  SearchInLUT_?( lut, old)  searches the LUT for an entry
+ *  [old, new]. If the LUT contains such an entry the associated data
+ *  'new' is returned. Otherwise the return value equals 'old'.
+ *  SearchInLUT_P()  searches for a pointer (pointer compare),
+ *  SearchInLUT_S()  searches for a string (string compare).
+ *
+ *  *** CAUTION ***
+ *  - InsertIntoLUT_S() copies the strings before inserting them into the LUT.
+ *  - RemoveLUT() removes all the stored strings from heap memory.
+ *  - SearchInLUT_S() returns a pointer to the argument string (if no string
+ *    entry of the same name is found in the LUT) or a *pointer* to the found
+ *    look-up string. In the latter case the returned pointer will be undefined
+ *    if RemoveLUT() has been called.
+ *    Therefore you should not forget to use StringCopy() here ... :-/
+ */
+
+#ifndef _sac_LookUpTable_h_
+#define _sac_LookUpTable_h_
 
 typedef void *LUT_t;
 
 extern LUT_t GenerateLUT (void);
 extern LUT_t RemoveLUT (LUT_t lut);
-extern LUT_t InsertIntoLUT (LUT_t lut, void *old_entry, void *new_entry);
-extern void *SearchInLUT (LUT_t *lut, void *old_entry);
 extern void PrintLUT (FILE *handle, LUT_t lut);
+extern LUT_t InsertIntoLUT_P (LUT_t lut, void *old_item, void *new_item);
+extern LUT_t InsertIntoLUT_S (LUT_t lut, char *old_item, char *new_item);
+extern void *SearchInLUT_P (LUT_t *lut, void *old_item);
+extern void *SearchInLUT_S (LUT_t *lut, char *old_item);
 
-#endif /* _sac_LookUpTable_h */
+#endif /* _sac_LookUpTable_h_ */
