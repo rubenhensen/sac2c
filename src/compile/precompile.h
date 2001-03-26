b5478bc7680271aa4469992f25b2e79f75da5c29
Dietmar Kreye <dietmar@computer-scientist.org>
2001-03-26 15:05:25 +0000

    header abbrevated

--- src/compile/precompile.h
+++ src/compile/precompile.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 3.6  2001/03/26 15:05:25  dkr
+ * header abbrevated
+ *
  * Revision 3.5  2001/03/05 13:54:59  dkr
  * PREC1code added
  *
@@ -47,61 +50,7 @@
  * Revision 2.2  1999/06/25 14:52:25  rob
  * Introduce definitions and utility infrastructure for tagged array support.
  *
- * Revision 2.1  1999/02/23 12:42:54  sacbase
- * new release made
- *
- * Revision 1.23  1998/08/07 14:39:45  dkr
- * PRECWLsegVar added
- *
- * Revision 1.22  1998/07/03 10:14:49  cg
- * function PRECspmd removed because attribute INOUT_IDS is no longer needed.
- *
- * Revision 1.21  1998/06/23 12:53:19  cg
- * added traversal function PRECspmd in order to correctly rename
- * the identifiers stored in SPMD_INOUT_IDS.
- *
- * Revision 1.20  1998/06/18 13:44:04  cg
- * file is now able to deal correctly with data objects of
- * the abstract data type for the representation of schedulings.
- *
- * Revision 1.19  1998/06/12 14:06:37  cg
- * core renaming of local identifiers moved to new function
- * PRECRenameLocalIdentifier() which is also exported for
- * usage in other compiler modules.
- *
- * Revision 1.18  1998/06/04 17:00:54  cg
- * information about refcounted variables in the context of loops,
- * conditionals and the old with-loop are now stored in ids-chains
- * instead of N_exprs lists.
- *
- * Revision 1.17  1998/06/03 14:53:41  cg
- * Now, all identifiers including local ones are systematically renamed.
- *
- * Revision 1.16  1998/04/29 17:20:09  dkr
- * with-loop transformation moved to wltransform.[ch]
- *
- * Revision 1.15  1998/04/26 21:49:52  dkr
- * PRECSPMD renamed to PRECSpmd
- *
- * Revision 1.14  1998/04/24 17:14:51  dkr
- * renamed Prec...() to PREC...()
- *
- * Revision 1.13  1998/04/23 17:33:16  dkr
- * added PrecSync
- *
- * Revision 1.12  1998/04/17 17:27:06  dkr
- * 'concurrent regions' are now called 'SPMD regions'
- *
- * Revision 1.11  1998/04/17 15:28:57  dkr
- * InsertWLnodes is now external
- *
- * Revision 1.10  1998/04/17 02:14:24  dkr
- * exported GridOffset()
- *
  * [...]
- *
- * Revision 1.1  1995/11/28  12:23:34  cg
- * Initial revision
  *
  */
 
