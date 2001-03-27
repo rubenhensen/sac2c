3c01a2adc594afa91e00930cdcc7dbced033ac0d
Dietmar Kreye <dietmar@computer-scientist.org>
2001-03-27 11:29:56 +0000

    OptCmp() removed

--- src/global/internal_lib.c
+++ src/global/internal_lib.c
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 3.4  2001/03/27 11:29:56  dkr
+ * OptCmp() removed
+ *
  * Revision 3.3  2001/03/12 10:29:39  nmw
  * TmpVar for undossa_tab added
  *
@@ -67,142 +70,7 @@
  * Revision 2.7  1999/05/17 11:24:26  jhs
  * CopyConstVec will be called only if ID/ARRAY_ISCONST.
  *
- * Revision 2.6  1999/05/14 09:25:13  jhs
- * Dbugged constvec annotations and their housekeeping in various compilation stages.
- *
- * Revision 2.5  1999/05/12 08:41:11  sbs
- * CopyIntVector and friends eliminated ; instead,
- * CopyConstVec, AllocConstVec, and ModConstVec have been added.
- * /.
- *
- * Revision 2.4  1999/04/14 16:29:49  jhs
- * Adjustment for empty arrays.
- *
- * Revision 2.3  1999/03/15 13:51:15  bs
- * CopyIntArray renamed into CopyIntVector, CopyFloatVector and CopyDoubleVector added.
- *
- * Revision 2.2  1999/02/24 20:21:59  bs
- * New function added: CopyIntArray
- *
- * Revision 2.1  1999/02/23 12:39:23  sacbase
- * new release made
- *
- * Revision 1.34  1998/08/20 12:09:00  srs
- * removed upper allocation bound in Malloc (maybe this was inserted
- * for debugging).
- *
- * Revision 1.33  1998/06/23 15:04:35  cg
- * The printing of system calls is now triggered via the global variable
- * show_syscall and the command line option -dshow_syscall.
- *
- * Revision 1.32  1998/06/19 12:51:44  srs
- * compute_malloc_align_step() => ComputeMallocAlignStep()
- *
- * Revision 1.31  1998/06/19 09:10:37  sbs
- * added some DBUG_PRINTs
- *
- * Revision 1.30  1998/06/03 14:24:00  cg
- * added some new fun_tabs for TmpVar.
- * now, sufficient memory is allocated even for unknown fun_tabs in TmpVar
- *
- * Revision 1.29  1998/03/25 19:22:53  srs
- * added new WL phases to TmpVar()
- *
- * Revision 1.28  1998/03/17 11:54:31  dkr
- * added fun lcm()
- *
- * Revision 1.27  1998/03/03 13:50:21  srs
- * removed 'tmp' infix from name generation in TmpVar()
- *
- * Revision 1.26  1998/03/02 13:57:21  cg
- * added function OptCmp() to compare two strings regardless of lower
- * or upper case letters (used for scanning optimization command line options.
- *
- * Revision 1.25  1998/02/24 16:10:32  srs
- * new function TmpVarName()
- *
- * Revision 1.24  1998/02/05 17:08:30  srs
- * TmpVar(): wr_tab and changed fusion_tab into wlf_tab
- *
- * Revision 1.23  1998/01/29 13:17:49  srs
- * modified TmpVar(). Now the compiler phase is inserted
- * in the variable name.
- *
- * Revision 1.22  1997/12/08 19:19:34  dkr
- * no arithmetic on void-pointers anymore (Malloc())
- *
- * Revision 1.21  1997/12/06 17:15:13  srs
- * changed Malloc (SHOW_MALLOC)
- *
- * Revision 1.20  1997/12/05 16:36:40  srs
- * StringCopy: allocate one byte less now
- *
- * Revision 1.19  1997/11/23 15:18:45  dkr
- * CC-flag: show_malloc -> SHOW_MALLOC
- *
- * Revision 1.18  1997/10/29 14:56:07  srs
- * changed Malloc() and removed HAVE_MALLOC_O
- *
- * Revision 1.17  1997/10/09 13:53:49  srs
- * counter for memory allocation
- *
- * Revision 1.16  1997/08/04 15:11:18  dkr
- * changed DBUG_PRINT-strings in Malloc
- *
- * Revision 1.15  1997/04/24  14:59:22  sbs
- * HAVE_MALLOC_O inserted
- *
- * Revision 1.14  1996/09/11  06:13:14  cg
- * Function SystemCall2 added that executes a system call and returns the
- * exit code rather than terminating with an error message upon failure.
- *
- * Revision 1.13  1996/05/24  16:18:26  sbs
- *   * if -dnocleanup flag is set print all syscalls !
- *    * This allows for easy C-code patches.
- *    *
- *   if (!cleanup)
- *     NOTE(("%s", syscall));
- *
- * inserted in SystemCall since EVERYBODY who uses -dnocleanup
- * ALLWAYS wants to know how to invoce the compiler!!
- *
- * Revision 1.12  1996/01/16  16:44:42  cg
- * added function TmpVar for generation of variable names
- *
- * Revision 1.11  1996/01/07  16:53:11  cg
- * added DBUG_PRINT
- *
- * Revision 1.10  1996/01/05  12:26:54  cg
- * added functions SystemTest and SystemCall
- *
- * Revision 1.9  1995/12/28  14:24:12  cg
- * bug in StringCopy fixed, StringCopy extremely simplified.
- *
- * Revision 1.8  1995/12/28  10:31:55  cg
- * malloc_verify is used in Malloc with Fred Fish Tag MEMVERIFY
- *
- * Revision 1.7  1995/12/15  13:40:59  cg
- * DBUG_PRINT in Malloc reactivated, ( Malloc no longer used by ItemName
- * and ModName)
- *
- * Revision 1.6  1995/10/20  11:29:04  cg
- * DBUG_PRINT removed
- *
- * Revision 1.5  1995/10/18  12:51:58  cg
- * converted to new error macros
- *
- * Revision 1.4  1995/07/24  15:43:52  asi
- * itoa will now work correctly ;-)
- *
- * Revision 1.3  1995/07/24  09:01:48  asi
- * added function itoa
- *
- * Revision 1.2  1995/05/12  13:14:10  hw
- * changed tag of DBUG_PRINT in function Malloc to MEM
- *
- * Revision 1.1  1995/03/28  12:01:50  hw
- * Initial revision
- *
+ * [...]
  *
  */
 
@@ -246,11 +114,6 @@ typedef struct {
  *  arguments     :  1) size of memory to allocate
  *  description   : allocates memory, if there is enough
  *                  iff size=0 NULL is returned!
- *  global vars   : ---
- *  internal funs : ---
- *  external funs : malloc, Error
- *  macros        : DBUG...,NULL
- *
  *  remarks       : exit if there is not enough memory
  *
  ******************************************************************************/
@@ -261,32 +124,37 @@ Malloc (int size)
     void *tmp;
 
     DBUG_ENTER ("Malloc");
+
     DBUG_PRINT ("MEMALLOC_TRY", ("trying to allocate %d bytes", size));
     DBUG_ASSERT ((size >= 0), "Malloc called with negative size!");
 
 #ifdef SHOW_MALLOC
     tmp = malloc (size + malloc_align_step);
-    if (NULL == tmp)
+    if (NULL == tmp) {
         SYSABORT (("Out of memory: %d Bytes allocated!", current_allocated_mem));
+    }
     *(int *)tmp = size;
     tmp = (char *)tmp + malloc_align_step;
 
     total_allocated_mem += size;
     current_allocated_mem += size;
     DBUG_PRINT ("MEM_OBSERVE", ("mem currently allocated: %d", current_allocated_mem));
-    if (max_allocated_mem < current_allocated_mem)
+    if (max_allocated_mem < current_allocated_mem) {
         max_allocated_mem = current_allocated_mem;
+    }
 #else /* not SHOW_MALLOC */
     tmp = malloc (size);
     /*
      * Since some UNIX system (e.g. ALPHA) do return NULL for size 0 as well
      * we do complain for ((NULL == tmp) && (size > 0)) only!!
      */
-    if ((NULL == tmp) && (size > 0))
+    if ((NULL == tmp) && (size > 0)) {
         SYSABORT (("Out of memory"));
+    }
 #endif
 
     DBUG_PRINT ("MEMALLOC", ("new memory: " P_FORMAT, tmp));
+
     DBUG_RETURN (tmp);
 }
 
@@ -295,12 +163,6 @@ Malloc (int size)
  *  functionname  : StringCopy
  *  arguments     : 1) source string
  *  description   : allocates memory and returns a pointer to the copy of 1)
- *  global vars   : ---
- *  internal funs : Malloc
- *  external funs : strlen, strcpy
- *  macros        : DBUG..., NULL
- *
- *  remarks       :
  *
  ******************************************************************************/
 
@@ -314,10 +176,11 @@ StringCopy (char *source)
     if (source) {
         DBUG_PRINT ("STRINGCOPY", ("copying string \"%s\"", source));
 
-        ret = (char *)Malloc (sizeof (char) * (strlen (source) + 1));
+        ret = (char *)MALLOC (sizeof (char) * (strlen (source) + 1));
         strcpy (ret, source);
-    } else
+    } else {
         ret = NULL;
+    }
 
     DBUG_RETURN (ret);
 }
@@ -341,7 +204,7 @@ StringConcat (char *first, char *second)
 
     DBUG_ENTER ("StringConcat");
 
-    result = (char *)Malloc (strlen (first) + strlen (second) + 1);
+    result = (char *)MALLOC (strlen (first) + strlen (second) + 1);
 
     strcpy (result, first);
     strcat (result, second);
@@ -355,14 +218,9 @@ StringConcat (char *first, char *second)
  *  arguments     : 1) number
  *		    R) result string
  *  description   : converts long to string
- *  global vars   :
- *  internal funs : Malloc
- *  external funs : sizeof
- *  macros        : DBUG..., NULL
- *
- *  remarks       :
  *
  ******************************************************************************/
+
 char *
 itoa (long number)
 {
@@ -371,18 +229,20 @@ itoa (long number)
     int length, i;
 
     DBUG_ENTER ("itoa");
+
     tmp = number;
     length = 1;
     while (9 < tmp) {
         tmp /= 10;
         length++;
     }
-    str = (char *)Malloc (sizeof (char) * length + 1);
+    str = (char *)MALLOC (sizeof (char) * length + 1);
     str[length] = atoi ("\0");
     for (i = 0; (i < length); i++) {
         str[i] = ((int)'0') + (number / pow (10, (length - 1)));
         number = number % ((int)pow (10, (length - 1)));
     }
+
     DBUG_RETURN (str);
 }
 
@@ -426,12 +286,6 @@ lcm (int x, int y)
  *  description   : evaluates the given string and executes the respective
  *                  system call. If the system call fails, an error message
  *                  occurs and compilation is aborted.
- *  global vars   : ---
- *  internal funs : ---
- *  external funs : system, vsprintf, va_start, va_end
- *  macros        : vararg macros
- *
- *  remarks       :
  *
  ******************************************************************************/
 
@@ -453,8 +307,9 @@ SystemCall (char *format, ...)
     /* if -dnocleanup flag is set print all syscalls !
      * This allows for easy C-code patches.
      */
-    if (show_syscall)
+    if (show_syscall) {
         NOTE (("%s", syscall));
+    }
 
     exit_code = system (syscall);
 
@@ -475,12 +330,6 @@ SystemCall (char *format, ...)
  *  description   : evaluates the given string and executes the respective
  *                  system call. In contrast to SystemCall no error message
  *                  is printed upon failure but the exit code is returned.
- *  global vars   : ---
- *  internal funs : ---
- *  external funs : system, vsprintf, va_start, va_end
- *  macros        : vararg macros
- *
- *  remarks       :
  *
  ******************************************************************************/
 
@@ -501,8 +350,9 @@ SystemCall2 (char *format, ...)
     /* if -dnocleanup flag is set print all syscalls !
      * This allows for easy C-code patches.
      */
-    if (show_syscall)
+    if (show_syscall) {
         NOTE (("%s", syscall));
+    }
 
     DBUG_RETURN (system (syscall));
 }
@@ -515,12 +365,6 @@ SystemCall2 (char *format, ...)
  *  description   : evaluates the given string and executes the respective
  *                  system call. If the system call fails, an error message
  *                  occurs and compilation is aborted.
- *  global vars   : ---
- *  internal funs : ---
- *  external funs : system, vsprintf, va_start, va_end
- *  macros        : vararg macros
- *
- *  remarks       :
  *
  ******************************************************************************/
 
@@ -559,11 +403,6 @@ SystemTest (char *format, ...)
  *  functionname  : TmpVar
  *  arguments     : ---
  *  description   : generates string to be used as artificial variable
- *  global vars   : ---
- *  internal funs : Malloc
- *  external funs : sprintf
- *  macros        : DBUG...
- *
  *  remarks       : The variable name is different in each call of TmpVar.
  *                  The string has the form "__tmp_" plus compiler phase
  *                  plus consecutive number.
@@ -574,11 +413,12 @@ char *
 TmpVar ()
 {
     static int counter = 0;
-    char *result, *s = NULL;
+    char *result;
+    char *s = NULL;
 
     DBUG_ENTER ("TmpVar");
 
-    result = (char *)Malloc (sizeof (char) * 16);
+    result = (char *)MALLOC (sizeof (char) * 16);
 
     if (act_tab == imp_tab) {
         s = "imp";
@@ -599,7 +439,7 @@ TmpVar ()
     } else if (act_tab == free_tab) {
         s = "free";
     } else if (act_tab == refcnt_tab) {
-        s = "refcnt";
+        s = "rc";
     } else if (act_tab == comp_tab) {
         s = "comp";
     } else if (act_tab == lir_tab) {
@@ -665,7 +505,7 @@ TmpVar ()
     } else if (act_tab == syncopt_tab) {
         s = "synco";
     } else if (act_tab == fun2lac_tab) {
-        s = "fun2lac";
+        s = "f2l";
     } else if (act_tab == ai_tab) {
         s = "ai";
     } else if (act_tab == apc_tab) {
@@ -705,37 +545,11 @@ TmpVarName (char *postfix)
     DBUG_ENTER ("TmpVarName");
 
     tmp = TmpVar ();
-    result = (char *)Malloc ((strlen (tmp) + strlen (postfix) + 2) * sizeof (char));
+    result = (char *)MALLOC ((strlen (tmp) + strlen (postfix) + 2) * sizeof (char));
     sprintf (result, "%s_%s", tmp, postfix);
     FREE (tmp);
 
     DBUG_RETURN (result);
-}
-
-/******************************************************************************
- *
- * function:
- *   int OptCmp(char *first, char *second)
- *
- * description:
- *   compares two strings and returns 1 (true) if the strings are equal.
- *   All characters are converted to lower case before the comparison applies.
- *
- ******************************************************************************/
-
-int
-OptCmp (char *first, char *second)
-{
-    int i = 0;
-
-    while ((first[i] != '\0') && (second[i] != '\0')
-           && (tolower ((int)first[i]) == tolower ((int)second[i])))
-        i++;
-
-    if (first[i] == second[i])
-        return (1);
-    else
-        return (0);
 }
 
 #ifdef SHOW_MALLOC
@@ -756,7 +570,4 @@ ComputeMallocAlignStep (void)
 
     DBUG_VOID_RETURN;
 }
-
-/* ========================================================================== */
-
 #endif
--- src/global/internal_lib.h
+++ src/global/internal_lib.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 3.3  2001/03/27 11:29:43  dkr
+ * OptCmp() removed
+ *
  * Revision 3.2  2000/12/15 10:43:20  dkr
  * macros for bit fields added
  *
@@ -56,62 +59,11 @@
  * Revision 2.1  1999/02/23 12:39:24  sacbase
  * new release made
  *
- * Revision 1.16  1998/06/19 12:51:53  srs
- * compute_malloc_align_step() => ComputeMallocAlignStep()
- *
- * Revision 1.15  1998/03/17 11:54:43  dkr
- * added fun lcm()
- *
- * Revision 1.14  1998/03/13 13:13:47  dkr
- * removed a bug in macros CHECK_DBUG_START, CHECK_DBUG_STOP
- *
- * Revision 1.13  1998/03/02 13:57:21  cg
- * added function OptCmp() to compare two strings regardless of lower
- * or upper case letters (used for scanning optimization command line options.
- *
- * Revision 1.12  1998/02/24 16:10:43  srs
- * new function TmpVarName()
- *
- * Revision 1.11  1997/12/06 17:16:01  srs
- * new global var malloc_align_step
- * new function compute_malloc_align_step()
- *
- * Revision 1.10  1997/10/28 12:30:18  srs
- * inserted macro MALLOC
- *
- * Revision 1.9  1997/08/07 15:24:33  dkr
- * added DBUG_OFF at CHECK_DBUG_START, CHECK_DBUG_STOP
- *
- * Revision 1.8  1997/08/07 11:12:33  dkr
- * added macros CHECK_DBUG_START, CHECK_DBUG_STOP for new compiler option -_DBUG (main.c)
- *
- * Revision 1.7  1996/09/11 06:13:14  cg
- * Function SystemCall2 added that executes a system call and returns the
- * exit code rather than terminating with an error message upon failure.
- *
- * Revision 1.6  1996/01/16  16:44:42  cg
- * added function TmpVar for generation of variable names
- *
- * Revision 1.5  1996/01/05  12:26:54  cg
- * added functions SystemTest and SystemCall
- *
- * Revision 1.4  1995/07/24  09:01:48  asi
- * added function itoa
- *
- * Revision 1.3  1995/07/19  18:41:23  asi
- * added macros MIN and MAX
- *
- * Revision 1.2  1995/07/14  17:04:31  asi
- * added macro SWAP, it swaps two pointers
- *
- * Revision 1.1  1995/03/28  12:01:50  hw
- * Initial revision
- *
+ * [...]
  *
  */
 
 #ifndef _internal_lib_h
-
 #define _internal_lib_h
 
 #include "types.h"
@@ -134,8 +86,6 @@ extern int SystemTest (char *format, ...);
 
 extern char *TmpVar ();
 extern char *TmpVarName (char *postfix);
-
-extern int OptCmp (char *first, char *second);
 
 #ifdef SHOW_MALLOC
 extern void ComputeMallocAlignStep (void);
