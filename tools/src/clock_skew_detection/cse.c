1974708f4b6e6dd0f0e79661d60f2863de1c6a46
Sac Historical <info@sac-home.org>
2001-11-15 14:28:51 +0000

    Initial revision

---
+++ tools/src/clock_skew_detection/cse.c
@@ -0,0 +1,57 @@
+#include <sys/time.h>
+#include <utime.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "main_args.h"
+
+/*****************************************************************************
+ *
+ *  function:
+ *    void Usage()
+ *
+ *  description:
+ *    prints usage info to stdout.
+ */
+
+#define HEADER "   "
+void
+Usage ()
+{
+    printf ("cse\n");
+    printf (HEADER "-h              print this help mesage.\n");
+    printf (HEADER "file [file]*    files whose time is to be updated.\n");
+}
+
+/*****************************************************************************
+ *
+ *  function:
+ *    void fool( char* file)
+ *
+ *  description:
+ *    updates the file's access time to the actual time minus 2secs.
+ */
+
+void
+fool (char *file)
+{
+    time_t sti;
+    struct utimbuf uti;
+
+    sti = time (NULL);
+    uti.actime = sti - 2;
+    uti.modtime = sti - 2;
+    utime (file, &uti);
+}
+
+int
+main (int argc, char *argv[])
+{
+    ARGS_BEGIN (argc, argv);
+    ARGS_FLAG ("h", Usage (); exit (0));
+    ARGS_ARGUMENT ({ fool (ARG); });
+    ARGS_END ();
+
+    return (0);
+}
