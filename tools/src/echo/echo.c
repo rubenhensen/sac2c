4117573af7d48b055ba9cfb9486682048128df0a
Dietmar Kreye <dietmar@computer-scientist.org>
2001-11-19 18:24:26 +0000

    strings.h included

--- tools/src/echo/echo.c
+++ tools/src/echo/echo.c
@@ -15,8 +15,9 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-#include <stdio.h>
 #include <sys/types.h>
+#include <strings.h>
+#include <stdio.h>
 
 #define _(text) text
 
