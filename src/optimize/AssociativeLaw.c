f078ae696c3634b4cfe39463fb1f4ce39f1f9733
Michael Werner <info@sac-home.org>
2002-06-07 17:38:35 +0000

    Initial revision

---
+++ src/optimize/AssociativeLaw.c
@@ -0,0 +1,24 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "types.h"
+#include "tree_basic.h"
+#include "tree_compound.h"
+#include "traverse.h"
+#include "dbug.h"
+#include "internal_lib.h"
+#include "optimize.h"
+#include "free.h"
+#include "DataFlowMask.h"
+#include "DupTree.h"
+#include "SSATransform.h"
+
+#include "AssociativeLaw.h"
+
+node *
+AssociativeLaw (node *arg_node, node *arg_info)
+{
+    return (arg_node);
+}
---
+++ src/optimize/AssociativeLaw.h
@@ -0,0 +1,6 @@
+#ifndef _AssociativeLaw_h_
+#define _AssociativeLaw_h_
+
+extern node *AssociativeLaw (node *arg_node, node *arg_info);
+
+#endif
