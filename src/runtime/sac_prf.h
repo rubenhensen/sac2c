ff9f975f91cac2e946e442e242b36796ea9b541e
Dietmar Kreye <dietmar@computer-scientist.org>
2002-07-15 14:46:57 +0000

    macros SAC_ND_PRF_... moved from sac_std.tagged.h to sac_prf.h

--- src/runtime/sac_prf.h
+++ src/runtime/sac_prf.h
@@ -1,8 +1,8 @@
 /*
  *
  * $Log$
- * Revision 3.2  2002/04/30 09:10:19  dkr
- * no changes done
+ * Revision 3.3  2002/07/15 14:46:57  dkr
+ * macros SAC_ND_PRF_... moved from sac_std.tagged.h to sac_prf.h
  *
  * Revision 3.1  2000/11/20 18:02:18  sacbase
  * new release made
@@ -38,8 +38,139 @@
  *
  *****************************************************************************/
 
-#ifndef _SAC_PRF_H
-#define _SAC_PRF_H
+#ifndef _SAC_PRF_H_
+#define _SAC_PRF_H_
+
+#ifdef TAGGED_ARRAYS
+
+/******************************************************************************
+ *
+ * ICMs for primitive functions on scalars
+ * =======================================
+ *
+ * ND_MIN( a1, a2)
+ * ND_MAX( a1, a2)
+ * ND_ABS( a)
+ *
+ ******************************************************************************/
+
+#define SAC_ND_MIN(a1, a2) ((a1) < (a2) ? (a1) : (a2))
+#define SAC_ND_MAX(a1, a2) ((a1) > (a2) ? (a1) : (a2))
+#define SAC_ND_ABS(a) ((a) < 0 ? (-(a)) : (a))
+
+/******************************************************************************
+ *
+ * ICMs for primitive functions on arrays
+ * ======================================
+ *
+ * ND_PRF_DIM__DATA( to_nt, to_sdim, from_nt, from_sdim)
+ *
+ * ND_PRF_SHAPE__DATA( to_nt, to_sdim, from_nt, from_sdim)
+ *
+ * ND_PRF_RESHAPE__SHAPE_id( to_nt, to_sdim, shp_nt)
+ * ND_PRF_RESHAPE__SHAPE_arr( to_nt, to_sdim, shp_size, ...shpa_any...)
+ *
+ * ND_PRF_SEL__SHAPE_id( to_nt, to_sdim, from_nt, from_sdim,
+ *                       idx_size, idx_nt)
+ * ND_PRF_SEL__DATA_id( to_nt, to_sdim, from_nt, from_sdim,
+ *                      idx_size, idx_nt)
+ * ND_PRF_SEL__SHAPE_arr( to_nt, to_sdim, from_nt, from_sdim,
+ *                        idx_size, idxa_any)
+ * ND_PRF_SEL__DATA_arr( to_nt, to_sdim, from_nt, from_sdim,
+ *                       idx_size, idxa_any)
+ *
+ * ND_PRF_MODARRAY__DATA_id( to_nt, to_sdim, from_nt, from_sdim,
+ *                           idx_size, idx_nt, val_any)
+ * ND_PRF_MODARRAY__DATA_arr( to_nt, to_sdim, from_nt, from_sdim,
+ *                            idx_size, ...idxa_any..., val_any)
+ *
+ * ND_PRF_CONV_A__DATA( to_nt, from_nt)
+ *
+ * ND_PRF_SxA__DATA( to_nt, op, scl,  a2_nt)
+ * ND_PRF_AxS__DATA( to_nt, op, a2_nt, scl)
+ * ND_PRF_AxA__DATA( to_nt, op, a1_nt, a2_nt)
+ *
+ ******************************************************************************/
+
+#define SAC_ND_PRF_DIM__DATA(to_nt, to_sdim, from_nt, from_sdim)                         \
+    SAC_TR_PRF_PRINT (                                                                   \
+      ("ND_PRF_DIM__...( %s, %s, %s, %s)\n", #to_nt, #to_sdim, #from_nt, #from_sdim))    \
+    SAC_ND_CREATE__SCALAR__DATA (to_nt, SAC_ND_A_DIM (from_nt))
+
+/* ND_PRF_SHAPE__DATA( ...) is a C-ICM */
+
+/* ND_PRF_RESHAPE__SHAPE_id( ...) is a C-ICM */
+/* ND_PRF_RESHAPE__SHAPE_arr( ...) is a C-ICM */
+
+/* ND_PRF_SEL__SHAPE_id( ...) is a C-ICM */
+/* ND_PRF_SEL__DATA_id( ...) is a C-ICM */
+/* ND_PRF_SEL__SHAPE_arr( ...) is a C-ICM */
+/* ND_PRF_SEL__DATA_arr( ...) is a C-ICM */
+
+/* ND_PRF_MODARRAY__DATA( ...) is a C-ICM */
+
+#define SAC_ND_PRF_CONV_A__DATA(to_nt, from_nt)                                          \
+    SAC_TR_PRF_PRINT (("ND_PRF_CONV_A__DATA( %s, %s)\n", #to_nt, #from_nt));             \
+    {                                                                                    \
+        int SAC_i;                                                                       \
+        for (SAC_i = 0; SAC_i < SAC_ND_A_SIZE (to_nt); SAC_i++) {                        \
+            SAC_ND_WRITE (to_nt, SAC_i) = SAC_ND_READ (from_nt, SAC_i);                  \
+        }                                                                                \
+    }
+
+#define SAC_ND_PRF_AxA__DATA(to_nt, op, a1_nt, a2_nt)                                    \
+    SAC_TR_PRF_PRINT (                                                                   \
+      ("ND_PRF_AxA__DATA( %s, %s, %s, %s)\n", #to_nt, #op, #a1_nt, #a2_nt));             \
+    {                                                                                    \
+        int SAC_i;                                                                       \
+        for (SAC_i = 0; SAC_i < SAC_ND_A_SIZE (to_nt); SAC_i++) {                        \
+            SAC_ND_WRITE (to_nt, SAC_i)                                                  \
+              = SAC_ND_READ (a1_nt, SAC_i) op SAC_ND_READ (a2_nt, SAC_i);                \
+        }                                                                                \
+    }
+
+#define SAC_ND_PRF_AxS__DATA(to_nt, op, a1_nt, scl)                                      \
+    SAC_TR_PRF_PRINT (                                                                   \
+      ("ND_PRF_AxS__DATA( %s, %s, %s, %s)\n", #to_nt, #op, #a1_nt, #scl));               \
+    {                                                                                    \
+        int SAC_i;                                                                       \
+        for (SAC_i = 0; SAC_i < SAC_ND_A_SIZE (to_nt); SAC_i++) {                        \
+            SAC_ND_WRITE (to_nt, SAC_i) = SAC_ND_READ (a1_nt, SAC_i) op scl;             \
+        }                                                                                \
+    }
+
+#define SAC_ND_PRF_SxA__DATA(to_nt, op, scl, a2_nt)                                      \
+    SAC_TR_PRF_PRINT (                                                                   \
+      ("ND_PRF_SxA__DATA( %s, %s, %s, %s)\n", #to_nt, #op, #scl, #a2_nt));               \
+    {                                                                                    \
+        int SAC_i;                                                                       \
+        for (SAC_i = 0; SAC_i < SAC_ND_A_SIZE (to_nt); SAC_i++) {                        \
+            SAC_ND_WRITE (to_nt, SAC_i) = scl op SAC_ND_READ (a2_nt, SAC_i);             \
+        }                                                                                \
+    }
+
+/******************************************************************************
+ *
+ * ICMs for primitive functions after IVE
+ * ======================================
+ *
+ * ND_PRF_IDX_SEL__SHAPE( to_nt, to_sdim, from_nt, from_sdim, idx_any)
+ * ND_PRF_IDX_SEL__DATA( to_nt, to_sdim, from_nt, from_sdim, idx_any)
+ *
+ * ND_PRF_IDX_MODARRAY__DATA( to_nt, to_sdim, from_nt, from_sdim, idx, val)
+ *
+ * ND_USE_GENVAR_OFFSET( offset, wl)
+ *
+ ******************************************************************************/
+
+/* ND_PRF_IDX_SEL__SHAPE( ...) is a C-ICM */
+/* ND_PRF_IDX_SEL__DATA( ...) is a C-ICM */
+
+/* ND_PRF_IDX_MODARRAY__DATA( ...) is a C-ICM */
+
+#define SAC_ND_USE_GENVAR_OFFSET(offset, wl) offset = SAC_WL_OFFSET (wl);
+
+#else /* TAGGED_ARRAYS */
 
 /*
  * Macros for primitve arithmetic operations:
@@ -94,12 +225,6 @@
 #define SAC_ND_2F_A(a1, res) SAC_ND_CONV_A (a1, res)
 #define SAC_ND_2D_A(a1, res) SAC_ND_CONV_A (a1, res)
 
-/*
- * Macros for primitive functions min, max, and abs
- */
+#endif /* TAGGED_ARRAYS */
 
-#define SAC_ND_MIN(a1, a2) ((a1) < (a2) ? (a1) : (a2))
-#define SAC_ND_MAX(a1, a2) ((a1) > (a2) ? (a1) : (a2))
-#define SAC_ND_ABS(a) ((a) < 0 ? (-(a)) : (a))
-
-#endif /* _SAC_PRF_H */
+#endif /* _SAC_PRF_H_ */
