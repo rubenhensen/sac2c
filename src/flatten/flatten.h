171f70eee73d5ad931fc27fbe3c26c385a68e876
Dietmar Kreye <dietmar@computer-scientist.org>
2002-08-07 12:32:39 +0000

    FltnNwithid added

--- src/flatten/flatten.h
+++ src/flatten/flatten.h
@@ -1,5 +1,9 @@
 /*
+ *
  * $Log$
+ * Revision 3.2  2002/08/07 12:32:39  dkr
+ * FltnNwithid added
+ *
  * Revision 3.1  2000/11/20 17:59:20  sacbase
  * new release made
  *
@@ -37,7 +41,7 @@
  * - removed FltnPrf
  *
  * Revision 1.10  1995/04/07  13:41:35  hw
- * >> FltnAp, FltnReturn inserted
+ * FltnAp, FltnReturn inserted
  *
  * Revision 1.9  1995/03/07  11:00:03  hw
  * added function FltnGen (flatten N_generator)
@@ -45,27 +49,11 @@
  * Revision 1.8  1995/01/06  16:45:15  hw
  * added FltnFundef
  *
- * Revision 1.7  1994/12/15  11:47:06  hw
- * inserted FltnModul
+ * [...]
  *
- * Revision 1.6  1994/11/22  16:39:55  hw
- * added FltnDo
- *
- * Revision 1.5  1994/11/17  16:50:16  hw
- * added FltnWhile
- *
- * Revision 1.4  1994/11/15  14:45:31  hw
- * deleted FltnFor
- *
- * Revision 1.3  1994/11/14  17:50:53  hw
- * added FltnCond FltnFor
- *
- * Revision 1.2  1994/11/10  15:39:42  sbs
- * RCS-header inserted
  */
 
 #ifndef _flatten_h
-
 #define _flatten_h
 
 extern node *Flatten (node *);
@@ -85,6 +73,7 @@ extern node *FltnLet (node *arg_node, node *arg_info);
 extern node *FltnArgs (node *arg_node, node *arg_info);
 extern node *FltnPrf (node *arg_node, node *arg_info);
 extern node *FltnNwith (node *arg_node, node *arg_info);
+extern node *FltnNwithid (node *arg_node, node *arg_info);
 extern node *FltnNpart (node *arg_node, node *arg_info);
 extern node *FltnNgenerator (node *arg_node, node *arg_info);
 extern node *FltnNwithop (node *arg_node, node *arg_info);
