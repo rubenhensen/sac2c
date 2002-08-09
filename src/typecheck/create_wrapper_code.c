f3a6b40e74db1aabfbb126e005db9ee74a585dbe
Dietmar Kreye <dietmar@computer-scientist.org>
2002-08-09 13:15:29 +0000

    CWCmodul, CWCfundef added

--- src/typecheck/create_wrapper_code.c
+++ src/typecheck/create_wrapper_code.c
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.2  2002/08/09 13:15:20  dkr
+ * CWCmodul, CWCfundef added
+ *
  * Revision 1.1  2002/08/09 13:00:02  dkr
  * Initial revision
  *
@@ -14,6 +17,40 @@
 #include "free.h"
 #include "DupTree.h"
 #include "new_types.h"
+
+/******************************************************************************
+ *
+ * Function:
+ *   node *CWCmodul( node *arg_node, node *arg_info);
+ *
+ * Description:
+ *
+ *
+ ******************************************************************************/
+
+node *CWCmodul (node *arg_node, node *arg_info);
+{
+    DBUG_ENTER ("CWCmodul");
+
+    DBUG_RETURN (arg_node);
+}
+
+/******************************************************************************
+ *
+ * Function:
+ *   node *CWCfundef( node *arg_node, node *arg_info);
+ *
+ * Description:
+ *
+ *
+ ******************************************************************************/
+
+node *CWCfundef (node *arg_node, node *arg_info);
+{
+    DBUG_ENTER ("CWCfundef");
+
+    DBUG_RETURN (arg_node);
+}
 
 /******************************************************************************
  *
--- src/typecheck/create_wrapper_code.h
+++ src/typecheck/create_wrapper_code.h
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.2  2002/08/09 13:15:29  dkr
+ * CWCmodul, CWCfundef added
+ *
  * Revision 1.1  2002/08/09 13:00:03  dkr
  * Initial revision
  *
@@ -9,5 +12,8 @@
 #define _create_wrapper_code_h_
 
 node *CreateWrapperCode (node *ast);
+
+node *CWCmodul (node *arg_node, node *arg_info);
+node *CWCfundef (node *arg_node, node *arg_info);
 
 #endif /* _create_wrapper_code_h_ */
