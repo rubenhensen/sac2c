ba32ef3ea5ed5cd1a936be627955e05e16cad316
Dietmar Kreye <dietmar@computer-scientist.org>
2002-08-13 15:59:09 +0000

    some more cwc stuff added (not finished yet)

--- src/typecheck/create_wrapper_code.c
+++ src/typecheck/create_wrapper_code.c
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.4  2002/08/13 15:59:09  dkr
+ * some more cwc stuff added (not finished yet)
+ *
  * Revision 1.3  2002/08/09 14:50:53  dkr
  * CWCap added
  *
@@ -22,6 +25,8 @@
 #include "DupTree.h"
 #include "new_types.h"
 
+#define INFO_CWC_TRAVNO(n) n->flag
+
 /******************************************************************************
  *
  * Function:
@@ -37,11 +42,135 @@ CWCmodul (node *arg_node, node *arg_info)
 {
     DBUG_ENTER ("CWCmodul");
 
+    /*
+     * create separate wrapper function for all base type constellations
+     */
+    INFO_CWC_TRAVNO (arg_info) = 1;
+    if (MODUL_FUNS (arg_node) != NULL) {
+        MODUL_FUNS (arg_node) = Trav (MODUL_FUNS (arg_node), arg_info);
+    }
+
+    /*
+     * adjust AP_FUNDEF pointers
+     */
+    INFO_CWC_TRAVNO (arg_info) = 2;
+    if (MODUL_FUNS (arg_node) != NULL) {
+        MODUL_FUNS (arg_node) = Trav (MODUL_FUNS (arg_node), arg_info);
+    }
+
+    /*
+     * create separate wrapper function for all base type constellations
+     */
+    INFO_CWC_TRAVNO (arg_info) = 3;
     if (MODUL_FUNS (arg_node) != NULL) {
         MODUL_FUNS (arg_node) = Trav (MODUL_FUNS (arg_node), arg_info);
     }
 
     DBUG_RETURN (arg_node);
+}
+
+/******************************************************************************
+ *
+ * Function:
+ *   node *SplitWrapper( node *fundef)
+ *
+ * Description:
+ *
+ *
+ ******************************************************************************/
+
+static node *
+SplitWrapper (node *fundef)
+{
+    ntype *old_type, *tmp_type;
+    ntype *new_type;
+    bool finished;
+    node *new_fundef;
+    node *new_fundefs = NULL;
+
+    DBUG_ENTER ("SplitWrapper");
+
+    old_type = FUNDEF_TYPE (fundef);
+    tmp_type = TYCopyType (old_type);
+    FUNDEF_TYPE (fundef) = NULL;
+    do {
+        new_fundef = DupNode (fundef);
+        new_type = TYSplitWrapperType (tmp_type, &finished);
+        FUNDEF_TYPE (new_fundef) = new_type;
+        FUNDEF_RET_TYPE (new_fundef) = TYGetWrapperRetType (new_type);
+        FUNDEF_ARGS (new_fundef)
+          = TYCorrectWrapperArgTypes (FUNDEF_ARGS (new_fundef), new_type);
+        FUNDEF_NEXT (new_fundef) = new_fundefs;
+        new_fundefs = new_fundef;
+    } while (!finished);
+    FUNDEF_TYPE (fundef) = old_type;
+    tmp_type = TYFreeType (tmp_type);
+
+    DBUG_RETURN (new_fundefs);
+}
+
+/******************************************************************************
+ *
+ * Function:
+ *   node *InsertWrapperCode( node *fundef)
+ *
+ * Description:
+ *
+ *
+ ******************************************************************************/
+
+static node *
+InsertWrapperCode (node *fundef)
+{
+    node *ret_exprs;
+    node *assigns;
+    node *vardec;
+    node *vardecs;
+
+    DBUG_ENTER ("InsertWrapperCode");
+
+    /*
+     * generate wrapper code together with the needed vardecs
+     */
+    vardecs = TYCreateWrapperVardecs (fundef);
+    assigns = TYCreateWrapperCode (fundef, vardecs);
+
+    /*
+     * vardecs -> return exprs
+     */
+    ret_exprs = NULL;
+    vardec = vardecs;
+    while (vardec != NULL) {
+        node *ret_expr;
+        node *id_node = MakeId_Copy (VARDEC_NAME (vardec));
+        ID_VARDEC (id_node) = vardec;
+        ID_AVIS (id_node) = VARDEC_AVIS (vardec);
+
+        if (ret_exprs == NULL) {
+            ret_expr = ret_exprs = MakeExprs (id_node, NULL);
+        } else {
+            ret_expr = EXPRS_NEXT (ret_expr) = MakeExprs (id_node, NULL);
+        }
+
+        vardec = VARDEC_NEXT (vardec);
+    }
+
+    /*
+     * append return statement to assignments
+     */
+    assigns = AppendAssign (assigns, MakeAssign (MakeReturn (ret_exprs), NULL));
+
+    /*
+     * insert function body
+     */
+    FUNDEF_BODY (fundef) = MakeBlock (assigns, vardecs);
+
+    /*
+     * mark wrapper function as a inline function
+     */
+    FUNDEF_INLINE (fundef) = TRUE;
+
+    DBUG_RETURN (fundef);
 }
 
 /******************************************************************************
@@ -57,78 +186,78 @@ CWCmodul (node *arg_node, node *arg_info)
 node *
 CWCfundef (node *arg_node, node *arg_info)
 {
-    node *ret_exprs;
-    node *assigns;
-    node *vardec;
-    node *vardecs = NULL;
+    node *new_fundef;
+    node *new_fundefs;
 
     DBUG_ENTER ("CWCfundef");
 
-    if (FUNDEF_STATUS (arg_node) == ST_wrapperfun) {
+    if (INFO_CWC_TRAVNO (arg_info) == 1) {
         /*
-         * wrapper function -> generate wrapper code
+         * first traversal -> build wrapper functions and there bodies
          */
-        DBUG_ASSERT ((FUNDEF_BODY (arg_node) == NULL),
-                     "wrapper function has already a body!");
 
-        /*
-         * generate wrapper code together with the needed vardecs
-         */
-        assigns = TYType2WrapperCode (FUNDEF_TYPE (arg_node), &vardecs);
+        if (FUNDEF_STATUS (arg_node) == ST_wrapperfun) {
+            DBUG_ASSERT ((FUNDEF_BODY (arg_node) == NULL),
+                         "wrapper function has already a body!");
 
-        /*
-         * vardecs -> return exprs
-         */
-        ret_exprs = NULL;
-        vardec = vardecs;
-        while (vardec != NULL) {
-            node *ret_expr;
-            node *id_node = MakeId_Copy (VARDEC_NAME (vardec));
-            ID_VARDEC (id_node) = vardec;
-            ID_AVIS (id_node) = VARDEC_AVIS (vardec);
+            /*
+             * build a separate fundef for each base type constellation
+             */
+            new_fundefs = SplitWrapper (arg_node);
 
-            if (ret_exprs == NULL) {
-                ret_expr = ret_exprs = MakeExprs (id_node, NULL);
-            } else {
-                ret_expr = EXPRS_NEXT (ret_expr) = MakeExprs (id_node, NULL);
+            /*
+             * build code for all wrapper functions
+             */
+            new_fundef = new_fundefs;
+            DBUG_ASSERT ((new_fundef != NULL), "no wrapper functions found!");
+            do {
+                new_fundef = InsertWrapperCode (new_fundef);
+                new_fundef = FUNDEF_NEXT (new_fundef);
+            } while (new_fundef != NULL);
+
+            if (FUNDEF_NEXT (arg_node) != NULL) {
+                FUNDEF_NEXT (arg_node) = Trav (FUNDEF_NEXT (arg_node), arg_info);
             }
 
-            vardec = VARDEC_NEXT (vardec);
+            /*
+             * insert new wrapper functions behind the original one and
+             * free original wrapper function (-> zombie function)
+             */
+            new_fundefs = AppendFundef (new_fundefs, FUNDEF_NEXT (arg_node));
+            arg_node = FreeNode (arg_node);
+            DBUG_ASSERT (((arg_node != NULL)
+                          && (FUNDEF_STATUS (arg_node) == ST_zombiefun)),
+                         "zombie fundef not found!");
+            FUNDEF_NEXT (arg_node) = new_fundefs;
         }
-
+    } else if (INFO_CWC_TRAVNO (arg_info) == 2) {
         /*
-         * append return statement to assignments
-         */
-        assigns = AppendAssign (assigns, MakeAssign (MakeReturn (ret_exprs), NULL));
-
-        /*
-         * insert function body
-         */
-        FUNDEF_BODY (arg_node) = MakeBlock (assigns, vardecs);
-
-        /*
-         * mark wrapper function as a inline function
-         */
-        FUNDEF_INLINE (arg_node) = TRUE;
-
-        /*
-         * create a separate wrapper function for each base type
-         */
-        /* not implemented yet */
-    } else {
-        /*
-         * no wrapper function -> adjust all AP_FUNDEFs
+         * second traversal -> adjust all AP_FUNDEF pointers
          *
          * This is needed if the original wrapper function was valid for more than
          * a single base type.
          */
-        if (FUNDEF_BODY (arg_node) != NULL) {
+        if ((FUNDEF_STATUS (arg_node) != ST_wrapperfun)
+            && (FUNDEF_BODY (arg_node) != NULL)) {
             FUNDEF_BODY (arg_node) = Trav (FUNDEF_BODY (arg_node), arg_info);
         }
-    }
 
-    if (FUNDEF_NEXT (arg_node) != NULL) {
-        FUNDEF_NEXT (arg_node) = Trav (FUNDEF_NEXT (arg_node), arg_info);
+        if (FUNDEF_NEXT (arg_node) != NULL) {
+            FUNDEF_NEXT (arg_node) = Trav (FUNDEF_NEXT (arg_node), arg_info);
+        }
+    } else {
+        DBUG_ASSERT ((INFO_CWC_TRAVNO (arg_info) == 3), "illegal INFO_CWC_TRAVNO found!");
+        /*
+         * third traversal -> remove zombie functions
+         */
+
+        if (FUNDEF_NEXT (arg_node) != NULL) {
+            FUNDEF_NEXT (arg_node) = Trav (FUNDEF_NEXT (arg_node), arg_info);
+        }
+
+        if (FUNDEF_STATUS (arg_node) == ST_zombiefun) {
+            arg_node = FreeZombie (arg_node);
+        }
     }
 
     DBUG_RETURN (arg_node);
@@ -147,10 +276,24 @@ CWCfundef (node *arg_node, node *arg_info)
 node *
 CWCap (node *arg_node, node *arg_info)
 {
+    node *fundef;
+
     DBUG_ENTER ("CWCap");
 
     if (AP_ARGS (arg_node) != NULL) {
         AP_ARGS (arg_node) = Trav (AP_ARGS (arg_node), arg_info);
+    }
+
+    fundef = AP_FUNDEF (arg_node);
+    DBUG_ASSERT ((fundef != NULL), "AP_FUNDEF not found!");
+    if (FUNDEF_STATUS (fundef) == ST_zombiefun) {
+        do {
+            fundef = FUNDEF_NEXT (fundef);
+            DBUG_ASSERT ((fundef != NULL), "no appropriate wrapper function found!");
+        } while (0); /* !!! not implemented yet !!! */
+        DBUG_ASSERT ((!strcmp (AP_NAME (arg_node), FUNDEF_NAME (fundef))),
+                     "no appropriate wrapper function found!");
+        AP_FUNDEF (arg_node) = fundef;
     }
 
     DBUG_RETURN (arg_node);
