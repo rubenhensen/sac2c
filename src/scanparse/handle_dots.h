bdd4b1130a6bf8329170858ea86533a00b9e99c3
Stephan Herhut <sah@sac-home.org>
2002-08-13 09:54:52 +0000

    added several traversalfunctions.

--- src/scanparse/handle_dots.h
+++ src/scanparse/handle_dots.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.3  2002/08/13 09:54:52  sah
+ * added several traversalfunctions.
+ *
  * Revision 1.2  2002/07/19 13:24:49  sah
  * added functions for traversal.
  *
@@ -19,7 +22,11 @@
 #include "internal_lib.h"
 
 extern node *EliminateSelDots (node *arg_node);
+extern node *HDwith (node *arg_node, node *arg_info);
+extern node *HDwithop (node *arg_node, node *arg_info);
+extern node *HDgenerator (node *arg_node, node *arg_info);
 extern node *HDdot (node *arg_node, node *arg_info);
+extern node *HDap (node *arg_node, node *arg_info);
 extern node *HDprf (node *arg_node, node *arg_info);
 
 #endif /* _handle_dots_h */
