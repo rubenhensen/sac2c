d5d9a1b0b604c09c35d23a36a4884c7fa09c64f5
Stephan Herhut <sah@sac-home.org>
2002-09-05 13:25:50 +0000

    added first support for WL shortcut and fixed some minor bugs

--- src/scanparse/handle_dots.c
+++ src/scanparse/handle_dots.c
@@ -1,6 +1,10 @@
 /*
  *
  * $Log$
+ * Revision 1.10  2002/09/05 13:25:50  sah
+ * added first support for WL shortcut and fixed
+ * some minor bugs
+ *
  * Revision 1.9  2002/08/26 13:46:00  sah
  * first full implementation of multi-
  * dimensional select.
@@ -49,6 +53,8 @@
  * Structures to store all information about dots occuring in select state-
  * ments that are needed to perform the transformation. These structures are
  * built by BuildDotList.
+ * All int values are counted beginning with 1. The 0 value is used as
+ * flag for non-existent (false).
  */
 
 typedef struct DOTLIST {
@@ -69,13 +75,41 @@ typedef struct DOTINFO {
 } dotinfo;
 
 /*
+ * Structures to store ids and shapes during shape-scan. Filled during
+ * traversal in HD_sacn mode.
+ */
+
+typedef enum TRAVSTATE { HD_sel, HD_scan } travstate;
+
+typedef struct SHPCHAIN {
+    node *shape;
+    struct SHPCHAIN *next;
+} shpchain;
+
+typedef struct IDTABLE {
+    char *id;
+    shpchain *shapes;
+    struct IDTABLE *next;
+} idtable;
+
+/*
  * arg_info in this file:
  * DOTSHAPE:    this field is used in order to transport the generic shape
  * (node[0])    from Nwithid (via Nwith) to Ngenerator, where it may be used
  *              in order to replace . generator boundaries.
+ * TRAVSTATE:   this field is used to determine the current traversalmode
+ * (flag)       HD_sel in normal mode (eliminate dots)
+ *              HD_scan in shape scanning mode
+ * IDTABLE:     used to reference the current idtable.
+ * (info2)
+ * ASSIGNS:     stores any assigns that have to be inserted prior to
+ * (node[1])    the current one. Used to build shape for WLs.
  */
 
 #define INFO_HD_DOTSHAPE(n) n->node[0]
+#define INFO_HD_TRAVSTATE(n) ((travstate)n->flag)
+#define INFO_HD_IDTABLE(n) ((idtable *)n->info2)
+#define INFO_HD_ASSIGNS(n) n->node[1]
 
 void
 BuildDotList (node *tree, dotinfo *info)
@@ -450,7 +484,7 @@ MakeAssignLetNV (char *var_name, node *let_expr)
 }
 
 node *
-BuildShape (node *args, node *array, node *block, dotinfo *info)
+BuildShape (node *args, node *array, node **assigns, dotinfo *info)
 {
     node *leftshape = NULL;
     node *leftid = NULL;
@@ -464,24 +498,23 @@ BuildShape (node *args, node *array, node *block, dotinfo *info)
     if (info->triplepos != 1) {
         leftshape = BuildLeftShape (args, array, info);
         leftid = MakeTmpId ();
-        BLOCK_INSTR (block)
-          = AppendAssign (BLOCK_INSTR (block),
-                          MakeAssignLetNV (StringCopy (ID_NAME (leftid)), leftshape));
+        *assigns = AppendAssign (*assigns, MakeAssignLetNV (StringCopy (ID_NAME (leftid)),
+                                                            leftshape));
     }
 
     if (info->triplepos != 0) {
         middleshape = BuildMiddleShape (args, array, info);
         middleid = MakeTmpId ();
-        BLOCK_INSTR (block)
-          = AppendAssign (BLOCK_INSTR (block),
+        *assigns
+          = AppendAssign (*assigns,
                           MakeAssignLetNV (StringCopy (ID_NAME (middleid)), middleshape));
     }
 
     if ((info->triplepos != 0) && (info->triplepos != info->selcnt)) {
         rightshape = BuildRightShape (args, array, info);
         rightid = MakeTmpId ();
-        BLOCK_INSTR (block)
-          = AppendAssign (BLOCK_INSTR (block),
+        *assigns
+          = AppendAssign (*assigns,
                           MakeAssignLetNV (StringCopy (ID_NAME (rightid)), rightshape));
     }
 
@@ -490,10 +523,9 @@ BuildShape (node *args, node *array, node *block, dotinfo *info)
 
         tmpid = MakeTmpId ();
 
-        BLOCK_INSTR (block)
-          = AppendAssign (BLOCK_INSTR (block),
-                          MakeAssignLetNV (StringCopy (ID_NAME (tmpid)),
-                                           BuildConcat (middleid, rightid)));
+        *assigns
+          = AppendAssign (*assigns, MakeAssignLetNV (StringCopy (ID_NAME (tmpid)),
+                                                     BuildConcat (middleid, rightid)));
 
         middleid = tmpid;
         rightid = NULL;
@@ -508,10 +540,9 @@ BuildShape (node *args, node *array, node *block, dotinfo *info)
 
             tmpid = MakeTmpId ();
 
-            BLOCK_INSTR (block)
-              = AppendAssign (BLOCK_INSTR (block),
-                              MakeAssignLetNV (StringCopy (ID_NAME (tmpid)),
-                                               BuildConcat (leftid, middleid)));
+            *assigns
+              = AppendAssign (*assigns, MakeAssignLetNV (StringCopy (ID_NAME (tmpid)),
+                                                         BuildConcat (leftid, middleid)));
 
             leftid = tmpid;
             middleid = NULL;
@@ -706,6 +737,112 @@ BuildWithLoop (node *shape, node *iv, node *array, node *index, node *block)
     DBUG_RETURN (result);
 }
 
+idtable *
+BuildIdTable (ids *ids)
+{
+    idtable *result = NULL;
+
+    DBUG_ENTER ("BuildIdTable");
+
+    while (ids != NULL) {
+        idtable *newtab = Malloc (sizeof (idtable));
+
+        newtab->id = StringCopy (ids->id);
+        newtab->shapes = NULL;
+        newtab->next = result;
+        result = newtab;
+        ids = ids->next;
+    }
+
+    DBUG_RETURN (result);
+}
+
+void
+FreeIdTable (idtable *table)
+{
+    DBUG_ENTER ("FreeIdTable");
+
+    while (table != NULL) {
+        idtable *next = table->next;
+        Free (table->id);
+        Free (table);
+        table = next;
+    }
+
+    DBUG_VOID_RETURN;
+}
+
+void
+ScanVector (node *vector, node *array, idtable *ids)
+{
+    int poscnt = 0;
+
+    DBUG_ENTER ("ScanVector");
+
+    while (vector != NULL) {
+        if (NODE_TYPE (EXPRS_EXPR (vector)) == N_id) {
+            idtable *handle = ids;
+
+            while (handle != NULL) {
+                if (strcmp (handle->id, ID_NAME (EXPRS_EXPR (vector))) == 0) {
+                    node *shape
+                      = MAKE_BIN_PRF (F_sel,
+                                      MakeArray (MakeExprs (MakeNum (poscnt), NULL)),
+                                      MakePrf (F_shape,
+                                               MakeExprs (DupTree (array), NULL)));
+                    shpchain *chain = Malloc (sizeof (shpchain));
+
+                    chain->shape = shape;
+                    chain->next = handle->shapes;
+                    handle->shapes = chain;
+
+                    break;
+                }
+
+                handle = handle->next;
+            }
+        }
+
+        poscnt++;
+        vector = EXPRS_NEXT (vector);
+    }
+
+    DBUG_VOID_RETURN;
+}
+
+node *
+BuildWLShape (idtable *table)
+{
+    node *result = NULL;
+
+    DBUG_ENTER ("BuildWLShape");
+
+    while (table != NULL) {
+        node *shape = NULL;
+        shpchain *handle = table->shapes;
+
+        if (handle == NULL) {
+            ERROR (linenum, ("no shape information found for %s", table->id));
+            shape = MakeNum (0);
+        } else {
+            shape = handle->shape;
+            handle = handle->next;
+
+            while (handle != NULL) {
+                shape = MAKE_BIN_PRF (F_min, shape, handle->shape);
+                handle = handle->next;
+            }
+        }
+
+        result = MakeExprs (shape, result);
+        table = table->next;
+    }
+
+    result = MakeArray (result);
+
+    DBUG_RETURN (result);
+}
+
 /******************************************************************************
  *
  * function:
@@ -728,10 +865,11 @@ EliminateSelDots (node *arg_node)
     act_tab = hd_tab;
 
     info_node = MakeInfo ();
+    INFO_HD_TRAVSTATE (info_node) = HD_sel;
 
     arg_node = Trav (arg_node, info_node);
 
-    FreeNode (info_node);
+    info_node = FreeNode (info_node);
 
     act_tab = tmp_tab;
 
@@ -755,8 +893,33 @@ HDwith (node *arg_node, node *arg_info)
 {
     DBUG_ENTER ("HDwith");
 
+    /* checking for a '.' as shape */
+
+    if (NODE_TYPE (NWITHOP_SHAPE (NWITH_WITHOP (arg_node))) == N_dot) {
+        if (NWITHOP_TYPE (NWITH_WITHOP (arg_node)) == WO_genarray) {
+            travstate oldstate = INFO_HD_TRAVSTATE (arg_info);
+            idtable *oldtable = INFO_HD_IDTABLE (arg_info);
+            node *shape = NULL;
+
+            INFO_HD_TRAVSTATE (arg_info) = HD_scan;
+            INFO_HD_IDTABLE (arg_info)
+              = BuildIdTable (NWITHID_IDS (NPART_WITHID (NWITH_PART (arg_node))));
+
+            TravSons (arg_node, arg_info);
+
+            shape = BuildWLShape (INFO_HD_IDTABLE (arg_info));
+
+            FreeNode (NWITHOP_SHAPE (NWITH_WITHOP (arg_node)));
+            NWITHOP_SHAPE (NWITH_WITHOP (arg_node)) = shape;
+
+            FreeIdTable (INFO_HD_IDTABLE (arg_info));
+            INFO_HD_IDTABLE (arg_info) = oldtable;
+            INFO_HD_TRAVSTATE (arg_info) = oldstate;
+        }
+    }
+
     /*
-     * by default (TravSons), withop would be traversed last, but some
+     * by default (TravSons), withop would be traversed last, but
      * some information from withop is needed in order to traverse
      * the rest, so the withop is handeled first.
      */
@@ -784,36 +947,41 @@ HDwithop (node *arg_node, node *arg_info)
 {
     DBUG_ENTER ("HDWithop");
 
-    switch (NWITHOP_TYPE (arg_node)) {
+    if (INFO_HD_TRAVSTATE (arg_info) == HD_sel) {
+        switch (NWITHOP_TYPE (arg_node)) {
 
-    case WO_modarray:
-        if (sbs == 1) {
-            INFO_HD_DOTSHAPE (arg_info)
-              = MakeAp1 (StringCopy ("shape"), NULL, DupTree (NWITHOP_ARRAY (arg_node)));
-        } else {
+        case WO_modarray:
+            if (sbs == 1) {
+                INFO_HD_DOTSHAPE (arg_info)
+                  = MakeAp1 (StringCopy ("shape"), NULL,
+                             DupTree (NWITHOP_ARRAY (arg_node)));
+            } else {
+                INFO_HD_DOTSHAPE (arg_info) = NULL;
+            }
+            break;
+
+        case WO_genarray:
+            if (sbs == 1) {
+                INFO_HD_DOTSHAPE (arg_info) = DupTree (NWITHOP_SHAPE (arg_node));
+            } else {
+                INFO_HD_DOTSHAPE (arg_info) = NULL;
+            }
+
+            break;
+
+        case WO_foldfun:
+            /* here is no break missing! */
+        case WO_foldprf:
             INFO_HD_DOTSHAPE (arg_info) = NULL;
+
+            break;
+
+        default:
+            DBUG_ASSERT (0, "wrong withop tag in N_Nwithop node!");
         }
-        break;
-
-    case WO_genarray:
-        if (sbs == 1) {
-            INFO_HD_DOTSHAPE (arg_info) = DupTree (NWITHOP_SHAPE (arg_node));
-        } else {
-            INFO_HD_DOTSHAPE (arg_info) = NULL;
-        }
-
-        break;
-
-    case WO_foldfun:
-        /* here is no break missing! */
-    case WO_foldprf:
-        INFO_HD_DOTSHAPE (arg_info) = NULL;
-
-        break;
-
-    default:
-        DBUG_ASSERT (0, "wrong withop tag in N_Nwithop node!");
     }
+
+    TravSons (arg_node, arg_info);
 
     DBUG_RETURN (arg_node);
 }
@@ -833,7 +1001,7 @@ HDgenerator (node *arg_node, node *arg_info)
 {
     DBUG_ENTER ("HDGenerator");
 
-    if (sbs == 1) {
+    if ((sbs == 1) && (INFO_HD_TRAVSTATE (arg_info) == HD_sel)) {
         /*
          * Dots are replaced by the "shape" expressions, that are imported via
          * INFO_HD_DOTSHAPE( arg_info)    (cf. HDWithop),
@@ -887,6 +1055,8 @@ HDgenerator (node *arg_node, node *arg_info)
         }
     }
 
+    arg_node = TravSons (arg_node, arg_info);
+
     DBUG_RETURN (arg_node);
 }
 
@@ -905,7 +1075,8 @@ HDdot (node *arg_node, node *arg_info)
 {
     DBUG_ENTER ("HDdot");
 
-    ERROR (linenum, ("'.' or '...' not allowed here."));
+    if ((INFO_HD_TRAVSTATE (arg_info) == HD_sel) && sbs)
+        ERROR (linenum, ("'.' or '...' not allowed here."));
 
     DBUG_RETURN (arg_node);
 }
@@ -932,28 +1103,32 @@ HDap (node *arg_node, node *arg_info)
     /* besides ARG1 must be an array. because otherwise there   */
     /* is no possibility to find any dot...                     */
 
-    if ((strcmp (AP_NAME (arg_node), "sel") == 0)
-        && (NODE_TYPE (AP_ARG1 (arg_node)) == N_array)) {
-        dotinfo *info = MakeDotInfo (ARRAY_AELEMS (AP_ARG1 (arg_node)));
+    if ((INFO_HD_TRAVSTATE (arg_info) == HD_sel)
+        && (strcmp (AP_NAME (arg_node), "sel") == 0)
+        && (NODE_TYPE (AP_ARG1 (arg_node)) == N_array))
 
-        /* for now, ... is not supported anyway, so check for that*/
-        /*
-        if (info->tripledot != 0)
-        {
-          ERROR(linenum, ("'...' is not supported yet."));
-        }
-        */
+    {
+        dotinfo *info = MakeDotInfo (ARRAY_AELEMS (AP_ARG1 (arg_node)));
 
         if (info->dotcnt != 0) {
             node *shape;
             node *iv;
             node *index;
             node *block;
+            node *assigns = MakeEmpty ();
 
             iv = MakeTmpId ();
             block = MAKE_EMPTY_BLOCK ();
             shape = BuildShape (ARRAY_AELEMS (AP_ARG1 (arg_node)), AP_ARG2 (arg_node),
-                                block, info);
+                                &assigns, info);
+
+            if (INFO_HD_ASSIGNS (arg_info) == NULL) {
+                INFO_HD_ASSIGNS (arg_info) = assigns;
+            } else {
+                INFO_HD_ASSIGNS (arg_info)
+                  = AppendAssign (INFO_HD_ASSIGNS (arg_info), assigns);
+            }
+
             index = BuildIndex (ARRAY_AELEMS (AP_ARG1 (arg_node)), iv, block, info);
 
             result = BuildWithLoop (shape, iv, AP_ARG2 (arg_node), index, block);
@@ -963,6 +1138,15 @@ HDap (node *arg_node, node *arg_info)
         }
 
         FreeDotInfo (info);
+    }
+
+    /* if in HD_scan mode, scan for shapes */
+
+    if ((INFO_HD_TRAVSTATE (arg_info) == HD_scan)
+        && (strcmp (AP_NAME (arg_node), "sel") == 0)
+        && (NODE_TYPE (AP_ARG1 (arg_node)) == N_array)) {
+        ScanVector (ARRAY_AELEMS (AP_ARG1 (arg_node)), AP_ARG2 (arg_node),
+                    INFO_HD_IDTABLE (arg_info));
     }
 
     /* Now we traverse our result in order to handle any */
@@ -990,62 +1174,54 @@ HDap (node *arg_node, node *arg_info)
 node *
 HDprf (node *arg_node, node *arg_info)
 {
-    node *result = arg_node;
-
     DBUG_ENTER ("HDprf");
 
-#if 0
-  
-  /* only sel statements are of interest here, so just return */
-  /* on anything else                                         */
-  /* besides ARG1 must be an array. because otherwise there   */
-  /* is no possibility to find any dot...                     */
-  
-  if ((PRF_PRF(arg_node) == F_sel) && 
-      (NODE_TYPE(PRF_ARG1(arg_node)) == N_array))
-  {
-    dotinfo* info = MakeDotInfo(ARRAY_AELEMS(PRF_ARG1(arg_node)));
+    /* if in HD_scan mode, scan for shapes */
 
-    /* for now, ... is not supported anyway, so check for that*/
-    
-    if (info->tripledot != 0)
-    {
-      ERROR(linenum, ("'...' is not supported yet."));
+    if ((INFO_HD_TRAVSTATE (arg_info) == HD_scan)
+        && (NODE_TYPE (PRF_ARG1 (arg_node)) == N_array)) {
+        ScanVector (ARRAY_AELEMS (PRF_ARG1 (arg_node)), PRF_ARG2 (arg_node),
+                    INFO_HD_IDTABLE (arg_info));
     }
 
-    if (info->dotcnt != 0)
-    {
-      node* shape;
-      node* iv;
-      node* index;
-    
-      iv = MakeTmpId();
-      shape = BuildShape(ARRAY_AELEMS(PRF_ARG1(arg_node)),
-                          PRF_ARG2(arg_node),
-                          info);
-      index = BuildIndex(ARRAY_AELEMS(PRF_ARG1(arg_node)),
-                          iv,
-                          info);
-                          
-      result = BuildWithLoop(shape, iv, PRF_ARG2(arg_node), index);
-      
-      FreeTree(arg_node);
-      FreeNode(iv);
-    } 
-  
-    FreeDotInfo(info);  
-  }
+    arg_node = TravSons (arg_node, arg_info);
 
-#endif
+    DBUG_RETURN (arg_node);
+}
 
-    /* Now we traverse our result in order to handle any */
-    /* dots inside.                                      */
+/******************************************************************************
+ *
+ * function:
+ *    node * HDassign( node *arg_node, node *arg_info);
+ *
+ * description:
+ *    ...
+ *
+ ******************************************************************************/
 
-    if (NODE_TYPE (result) == N_prf) {
-        if (PRF_ARGS (result) != NULL)
-            PRF_ARGS (result) = Trav (PRF_ARGS (result), arg_info);
-    } else
-        result = Trav (result, arg_info);
+node *
+HDassign (node *arg_node, node *arg_info)
+{
+    node *result = arg_node;
+    node *oldassigns = INFO_HD_ASSIGNS (arg_info);
+
+    DBUG_ENTER ("HDassign");
+
+    /* first traverse sons with a fresg assigns-chain */
+
+    INFO_HD_ASSIGNS (arg_info) = NULL;
+    result = TravSons (arg_node, arg_info);
+
+    /* check for assigns that are to be inserted */
+
+    if (INFO_HD_ASSIGNS (arg_info) != NULL) {
+        result = INFO_HD_ASSIGNS (arg_info);
+        AppendAssign (result, arg_node);
+    }
+
+    /* reinstall old assigns-chain */
+
+    INFO_HD_ASSIGNS (arg_info) = oldassigns;
 
     DBUG_RETURN (result);
 }
