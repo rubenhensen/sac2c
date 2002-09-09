b2e616d7799a52d599a8a8aa5351345f27eaa8c6
Dietmar Kreye <dietmar@computer-scientist.org>
2002-09-09 17:40:59 +0000

    categories removed (too error prone!)

--- src/tree/prf.h
+++ src/tree/prf.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 3.2  2002/09/09 17:40:59  dkr
+ * categories removed (too error prone!)
+ *
  * Revision 3.1  2000/11/20 18:03:27  sacbase
  * new release made
  *
@@ -18,36 +21,14 @@
 #ifndef _sac_prf_h_
 #define _sac_prf_h_
 
-/*
- * macros for categorising prfs.
- */
-
-/* the bounds of each category */
+/* the bounds of the prf list */
 
 #define FIRST_LEGAL_PRF F_toi
 
 #define LAST_LEGAL_PRF F_rotate
 
-/* ... */
-
 /* all legal prfs */
 
 #define LEGAL_PRF(prf) ((prf >= FIRST_LEGAL_PRF) && (prf <= LAST_LEGAL_PRF))
-
-/* the four categories */
-
-#define SINGLE_SCALAR_ARG(prf) ((prf >= FIRST_LEGAL_PRF) && (prf <= F_not))
-
-#define MULTI_SCALAR_ARGS(prf) ((prf > F_not) && (prf <= F_neq))
-
-#define ARRAY_ARGS_INTRINSIC(prf) ((prf > F_neq) && (prf <= F_modarray))
-
-#define ARRAY_ARGS_NON_INTRINSIC(prf) ((prf > F_modarray) && (prf <= LAST_LEGAL_PRF))
-
-/* combinations of categories */
-
-#define SCALAR_ARGS(prf) (SINGLE_SCALAR_ARG (prf) || MULTI_SCALAR_ARGS (prf))
-
-#define ARRAY_ARGS(prf) (ARRAY_ARGS_INTRINSIC (prf) || ARRAY_ARGS_NON_INTRINSIC (prf))
 
 #endif
