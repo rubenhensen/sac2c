619b68baf45d5af38c4e24f7c4ecd3ad8ee389e0
Dietmar Kreye <dietmar@computer-scientist.org>
2002-10-09 02:06:25 +0000

    no changes done

--- src/optimize/SSAWLUnroll.h
+++ src/optimize/SSAWLUnroll.h
@@ -1,17 +1,19 @@
 /*
  *
  * $Log$
+ * Revision 1.2  2002/10/09 02:06:25  dkr
+ * no changes done
+ *
  * Revision 1.1  2002/10/08 22:10:05  dkr
  * Initial revision
- *
  *
  *
  * created from WLUnroll.h, Revision 3.1 on 2002/10/10 by dkr
  *
  */
 
-#ifndef _SSAWLUnroll_h
-#define _SSAWLUnroll_h
+#ifndef _SSAWLUnroll_h_
+#define _SSAWLUnroll_h_
 
 extern int SSACheckUnrollModarray (node *wln);
 extern node *SSADoUnrollModarray (node *wln, node *arg_info);
@@ -22,4 +24,4 @@ extern node *SSADoUnrollGenarray (node *wln, node *arg_info);
 extern int SSACheckUnrollFold (node *wln);
 extern node *SSADoUnrollFold (node *wln, node *arg_info);
 
-#endif
+#endif /* _SSAWLUnroll_h_ */
