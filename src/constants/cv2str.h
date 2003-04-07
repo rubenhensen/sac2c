1e97fad0aef1e4b3be99e7a587e3ceb133f10ae1
Sven-Bodo Scholz <sbs@sac-home.org>
2003-04-07 14:25:44 +0000

    max_char parameter added for abbreviation.

--- src/constants/cv2str.h
+++ src/constants/cv2str.h
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.3  2003/04/07 14:25:44  sbs
+ * max_char parameter added for abbreviation.
+ *
  * Revision 1.2  2001/04/04 10:00:02  nmw
  *  missing convert functions for basetype char added
  *
@@ -15,23 +18,23 @@
 #ifndef _cv2str_h
 #define _cv2str_h
 
-typedef char *(*cv2strfunptr) (void *, int, int);
+typedef char *(*cv2strfunptr) (void *, int, int, int);
 
 extern cv2strfunptr cv2str[];
 
-extern char *COCv2StrUShort (void *src, int off, int len);
-extern char *COCv2StrUInt (void *src, int off, int len);
-extern char *COCv2StrULong (void *src, int off, int len);
-extern char *COCv2StrShort (void *src, int off, int len);
-extern char *COCv2StrInt (void *src, int off, int len);
-extern char *COCv2StrLong (void *src, int off, int len);
+extern char *COCv2StrUShort (void *src, int off, int len, int max_char);
+extern char *COCv2StrUInt (void *src, int off, int len, int max_char);
+extern char *COCv2StrULong (void *src, int off, int len, int max_char);
+extern char *COCv2StrShort (void *src, int off, int len, int max_char);
+extern char *COCv2StrInt (void *src, int off, int len, int max_char);
+extern char *COCv2StrLong (void *src, int off, int len, int max_char);
 
-extern char *COCv2StrFloat (void *src, int off, int len);
-extern char *COCv2StrDouble (void *src, int off, int len);
-extern char *COCv2StrLongDouble (void *src, int off, int len);
+extern char *COCv2StrFloat (void *src, int off, int len, int max_char);
+extern char *COCv2StrDouble (void *src, int off, int len, int max_char);
+extern char *COCv2StrLongDouble (void *src, int off, int len, int max_char);
 
-extern char *COCv2StrChar (void *src, int off, int len);
+extern char *COCv2StrChar (void *src, int off, int len, int max_char);
 
-extern char *COCv2StrDummy (void *src, int off, int len);
+extern char *COCv2StrDummy (void *src, int off, int len, int max_char);
 
 #endif /* _cv2str_h */
