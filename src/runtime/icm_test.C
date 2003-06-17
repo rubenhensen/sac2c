3c17e05fbf197f6fa6626535c30e572545fbac56
Dietmar Kreye <dietmar@computer-scientist.org>
2003-06-17 18:47:54 +0000

    Initial revision

---
+++ src/runtime/icm_test.C
@@ -0,0 +1,108 @@
+/*
+ * This file can be used to test the expansion of H-ICMs
+ *
+ * Apply the CC preprocessor to this file and verify the generated output:
+ *   gcc -I$SACHOME/runtime -E icm_test.C
+ *
+ * It exists a Makefile target for this purpose (icm_test) !!!
+ */
+
+#ifndef _SHP_
+#define _SHP_ SCL /* SCL, AKS, AKD, AUD */
+#endif
+
+#ifndef _HID_
+#define _HID_ HID /* NHD, HID */
+#endif
+
+#ifndef _UNQ_
+#define _UNQ_ NUQ /* NUQ, UNQ */
+#endif
+
+#define TAGGED_ARRAYS
+#include "sac.h"
+
+SAC_ND_A_DESC ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+SAC_ND_A_DESC_DIM ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+SAC_ND_A_DESC_SIZE ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+SAC_ND_A_DESC_SHAPE ((nt, (_SHP_, (_HID_, (_UNQ_, )))), 0)
+SAC_ND_A_FIELD ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+SAC_ND_A_RC ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+SAC_ND_A_DIM ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+SAC_ND_A_SIZE ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+SAC_ND_A_SHAPE ((nt, (_SHP_, (_HID_, (_UNQ_, )))), 0)
+
+SAC_ND_READ ((nt, (_SHP_, (_HID_, (_UNQ_, )))), 0)
+SAC_ND_WRITE ((nt, (_SHP_, (_HID_, (_UNQ_, )))), 0)
+
+SAC_ND_TYPEDEF ((nt, (_SHP_, (_HID_, (_UNQ_, )))), int)
+
+SAC_ND_DECL__DATA ((nt, (_SHP_, (_HID_, (_UNQ_, )))), int, )
+SAC_ND_DECL__DESC ((nt, (_SHP_, (_HID_, (_UNQ_, )))), )
+
+SAC_ND_PARAM_ (dummy, int)
+SAC_ND_PARAM_in ((nt, (_SHP_, (_HID_, (_UNQ_, )))), int) SAC_ND_PARAM_in_nodesc (
+  (nt, (_SHP_, (_HID_, (_UNQ_, )))),
+  int) SAC_ND_PARAM_out ((nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                         int) SAC_ND_PARAM_out_nodesc ((nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                                                       int)
+  SAC_ND_PARAM_inout ((nt, (_SHP_, (_HID_, (_UNQ_, )))), int) SAC_ND_PARAM_inout_nodesc (
+    (nt, (_SHP_, (_HID_, (_UNQ_, )))),
+    int) SAC_ND_PARAM_inout_nodesc_bx ((nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                                       int) SAC_ND_ARG_in ((nt,
+                                                            (_SHP_, (_HID_, (_UNQ_, )))))
+    SAC_ND_ARG_in_nodesc ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+      SAC_ND_ARG_out ((nt, (_SHP_, (_HID_, (_UNQ_, ))))) SAC_ND_ARG_out_nodesc ((
+        nt,
+        (_SHP_, (_HID_, (_UNQ_, ))))) SAC_ND_ARG_inout ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+        SAC_ND_ARG_inout_nodesc ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+          SAC_ND_ARG_inout_nodesc_bx ((nt, (_SHP_, (_HID_, (_UNQ_, ))))) SAC_ND_RET_out (
+            (nt, (_SHP_, (_HID_, (_UNQ_, )))),
+            (ntp,
+             (_SHP_,
+              (_HID_, (_UNQ_, ))))) SAC_ND_RET_inout ((nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                                                      (ntp, (_SHP_, (_HID_, (_UNQ_, )))))
+            SAC_ND_DECL_PARAM_inout ((nt, (_SHP_, (_HID_, (_UNQ_, )))), int)
+
+              SAC_ND_ALLOC ((nt, (_SHP_, (_HID_, (_UNQ_, )))), rc, get_dim, set_shape_icm)
+                SAC_ND_ALLOC_BEGIN ((nt, (_SHP_, (_HID_, (_UNQ_, )))), rc, dim)
+                  SAC_ND_ALLOC_END ((nt, (_SHP_, (_HID_, (_UNQ_, )))), rc, dim)
+                    SAC_ND_FREE ((nt, (_SHP_, (_HID_, (_UNQ_, )))), freefun)
+                      SAC_ND_ALLOC__DATA ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+                        SAC_ND_FREE__DATA ((nt, (_SHP_, (_HID_, (_UNQ_, )))), freefun)
+                          SAC_ND_ALLOC__DESC ((nt, (_SHP_, (_HID_, (_UNQ_, )))), dim)
+                            SAC_ND_FREE__DESC ((nt, (_SHP_, (_HID_, (_UNQ_, )))))
+
+                              SAC_ND_ASSIGN__DATA ((to_nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                                                   (from_nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                                                   copyfun)
+                                SAC_ND_COPY__DATA ((to_nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                                                   (from_nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                                                   copyfun)
+
+                                  SAC_ND_SET__RC ((nt, (_SHP_, (_HID_, (_UNQ_, )))), rc)
+                                    SAC_ND_INC_RC ((nt, (_SHP_, (_HID_, (_UNQ_, )))), rc)
+                                      SAC_ND_DEC_RC ((nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                                                     rc)
+                                        SAC_ND_DEC_RC_FREE ((nt,
+                                                             (_SHP_, (_HID_, (_UNQ_, )))),
+                                                            rc, freefun)
+
+                                          SAC_ND_CREATE__SCALAR__DATA (
+                                            (nt, (_SHP_, (_HID_, (_UNQ_, )))), 17)
+                                            SAC_ND_CREATE__STRING__DATA (
+                                              (nt, (_SHP_, (_HID_, (_UNQ_, )))), "hallo")
+
+                                              SAC_IS_LASTREF__BLOCK_BEGIN (
+                                                (to_nt, (_SHP_, (_HID_, (_UNQ_, )))))
+                                                SAC_IS_LASTREF__BLOCK_ELSE (
+                                                  (to_nt, (_SHP_, (_HID_, (_UNQ_, )))))
+SAC_IS_LASTREF__BLOCK_END
+((to_nt, (_SHP_, (_HID_, (_UNQ_, )))))
+
+  SAC_IS_REUSED__BLOCK_BEGIN ((to_nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                              (from_nt, (_SHP_, (_HID_, (_UNQ_, )))))
+    SAC_IS_REUSED__BLOCK_ELSE ((to_nt, (_SHP_, (_HID_, (_UNQ_, )))),
+                               (from_nt, (_SHP_, (_HID_, (_UNQ_, )))))
+SAC_IS_REUSED__BLOCK_END
+((to_nt, (_SHP_, (_HID_, (_UNQ_, )))), (from_nt, (_SHP_, (_HID_, (_UNQ_, )))))
