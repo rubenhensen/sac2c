70ab8399458208e686fb8e147e82c6893fb02ca4
Kai Trojahner <ktr@sac-home.org>
2004-04-26 07:23:06 +0000

    Just checking in in order to have the file available at ISP.

--- src/compile/blir.c
+++ src/compile/blir.c
@@ -9,6 +9,11 @@
 #include "DupTree.h"
 #include "print.h"
 #include "blir.h"
+#include "lac2fun.h"
+#include "fun2lac.h"
+#include "SSATransform.h"
+#include "UndoSSATransform.h"
+#include "CheckAvis.h"
 
 node *
 Blir (node *syntax_tree)
@@ -19,10 +24,24 @@ Blir (node *syntax_tree)
 
     info = MakeInfo ();
 
+    /* Set traversal table */
     act_tab = blir_tab;
+
+    /* syntax tree must be in SSA form */
+    syntax_tree = Lac2Fun (syntax_tree);
+    syntax_tree = CheckAvis (syntax_tree);
+    syntax_tree = SSATransform (syntax_tree);
+
+    /* perform BLIR */
     syntax_tree = Trav (syntax_tree, info);
 
+    /* free info node */
     info = FreeTree (info);
 
+    /* revert syntax tree into LaC form */
+    syntax_tree = UndoSSATransform (syntax_tree);
+    syntax_tree = Fun2Lac (syntax_tree);
+
+    /* return the result */
     DBUG_RETURN (syntax_tree);
 }
