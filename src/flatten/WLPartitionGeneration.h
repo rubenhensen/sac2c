6a2661dd4d43a0bcd381680885d947087c7fdd24
Karsten Hinckfuß <hinckfuss@gmx.de>
2004-07-22 17:26:23 +0000

    added WLPGap

--- src/flatten/WLPartitionGeneration.h
+++ src/flatten/WLPartitionGeneration.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.6  2004/07/22 17:26:23  khf
+ * added WLPGap
+ *
  * Revision 1.5  2004/07/21 12:48:33  khf
  * switch to new INFO structure
  * take changes of sbs in SSAWLT.c over
@@ -31,6 +34,7 @@ extern node *WLPGmodul (node *arg_node, info *arg_info);
 extern node *WLPGfundef (node *arg_node, info *arg_info);
 extern node *WLPGassign (node *arg_node, info *arg_info);
 extern node *WLPGlet (node *arg_node, info *arg_info);
+extern node *WLPGap (node *arg_node, info *arg_info);
 
 extern node *WLPGNwith (node *arg_node, info *arg_info);
 extern node *WLPGNwithop (node *arg_node, info *arg_info);
