ff62f61b19c901137daf927cafab5e6e9c375e1f
Stephan Herhut <sah@sac-home.org>
2004-09-30 20:02:59 +0000

    added some DBUG_PRINTs

--- src/modules/libmanager.c
+++ src/modules/libmanager.c
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.3  2004/09/30 20:02:59  sah
+ * added some DBUG_PRINTs
+ *
  * Revision 1.2  2004/09/23 21:15:39  sah
  * interface complete and
  * working implementation for Solaris
@@ -40,11 +43,15 @@ LoadLibrary (const char *name)
 
     DBUG_ENTER ("LoadLibrary");
 
+    DBUG_PRINT ("LIB", ("Loading library `%s'", name));
+
     result = dlopen (name, RTLD_WORLD | RTLD_LAZY);
 
     if (result == NULL) {
         SYSABORT (("Cannot open library `%s': %s", name, LibManagerError ()));
     }
+
+    DBUG_PRINT ("LIB", ("Done loading library"));
 
     DBUG_RETURN (result);
 }
@@ -56,10 +63,14 @@ UnLoadLibrary (dynlib_t lib)
 
     DBUG_ENTER ("UnLoadLibrary");
 
+    DBUG_PRINT ("LIB", ("Unoading library"));
+
     result = dlclose (lib);
 
     if (result != 0)
         SYSABORT (("Cannot close library: %s", LibManagerError ()));
+
+    DBUG_PRINT ("LIB", ("Done unloading library"));
 
     DBUG_RETURN ((dynlib_t)NULL);
 }
@@ -69,13 +80,17 @@ GetLibraryFunction (const char *name, dynlib_t lib)
 {
     dynfun_t result;
 
-    DBUG_ENTER ("GetDeserializeFun");
+    DBUG_ENTER ("GetLibraryFunction");
+
+    DBUG_PRINT ("LIB", ("Getting library function `%s'", name));
 
     result = dlsym (lib, name);
 
     if (result == NULL) {
         SYSABORT (("Cannot open library function `%s': %s", name, LibManagerError ()));
     }
+
+    DBUG_PRINT ("LIB", ("Done getting library function"));
 
     DBUG_RETURN (result);
 }
