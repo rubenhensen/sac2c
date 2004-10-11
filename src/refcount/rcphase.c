52bab1d71ed5e3b1f7adde513caecc68a1860aa0
Kai Trojahner <ktr@sac-home.org>
2004-10-11 14:45:13 +0000

    Initial revision

---
+++ src/refcount/rcphase.c
@@ -0,0 +1,72 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  2004/10/11 14:45:07  ktr
+ * Initial revision
+ *
+ */
+
+/**
+ *
+ * @defgroup rcp Reference Counting
+ *
+ * <pre>
+ * </pre>
+ * @{
+ */
+
+/**
+ *
+ * @file rcphase.c
+ *
+ *
+ */
+#include "globals.h"
+#include "tree_basic.h"
+#include "tree_compound.h"
+#include "traverse.h"
+#include "dbug.h"
+#include "print.h"
+#include "refcounting.h"
+#include "rcopt.h"
+
+/** <!--*******************************************************************-->
+ *
+ * @fn node *RCphase( node *syntax_tree)
+ *
+ * @brief
+ *
+ * @param syntax_tree The whole syntax tree.
+ *
+ * @return The transformed syntax tree.
+ *
+ ****************************************************************************/
+node *
+RCphase (node *syntax_tree)
+{
+    DBUG_ENTER ("RCphase");
+
+    DBUG_ASSERT ((NODE_TYPE (syntax_tree) == N_modul),
+                 "RCphase not started with N_modul node");
+
+    /*
+     * Reference counting
+     */
+    syntax_tree = EMRefCount (syntax_tree);
+    if ((break_after == PH_refcnt) && (0 == strcmp (break_specifier, "rc"))) {
+        goto DONE;
+    }
+
+    /*
+     * Reference counting optimizations
+     */
+    syntax_tree = EMRCORefCountOpt (syntax_tree);
+    if ((break_after == PH_refcnt) && (0 == strcmp (break_specifier, "rco"))) {
+        goto DONE;
+    }
+
+DONE:
+    DBUG_RETURN (syntax_tree);
+}
+
+/*@}*/ /* defgroup rcp */
---
+++ src/refcount/rcphase.h
@@ -0,0 +1,14 @@
+/*
+ *
+ * $Log$
+ * Revision 1.1  2004/10/11 14:45:13  ktr
+ * Initial revision
+ *
+ */
+
+#ifndef _rcphase_h
+#define _rcphase_h
+
+extern node *RCphase (node *syntax_tree);
+
+#endif
