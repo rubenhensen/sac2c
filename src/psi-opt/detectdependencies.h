89ec863984d26be5b1989fe5bcbfe3b31379de16
Karsten Hinckfuß <hinckfuss@gmx.de>
2004-10-20 08:10:29 +0000

    added DDEPENDprf, DDEPENDcode and changed signature of startfunction

--- src/psi-opt/detectdependencies.h
+++ src/psi-opt/detectdependencies.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.3  2004/10/20 08:10:29  khf
+ * added DDEPENDprf, DDEPENDcode and changed signature of startfunction
+ *
  * Revision 1.2  2004/09/02 15:27:01  khf
  * DDEPENDwithop removed
  *
@@ -14,11 +17,14 @@
 #ifndef _detectdependencies_h
 #define _detectdependencies_h
 
-extern node *DetectDependencies (node *arg_node);
+extern node *DetectDependencies (node *with, node *fusionable_wl,
+                                 nodelist *references_fwl);
 
 extern node *DDEPENDassign (node *arg_node, info *arg_info);
+extern node *DDEPENDprf (node *arg_node, info *arg_info);
 extern node *DDEPENDid (node *arg_node, info *arg_info);
 
 extern node *DDEPENDwith (node *arg_node, info *arg_info);
+extern node *DDEPENDcode (node *arg_node, info *arg_info);
 
 #endif
