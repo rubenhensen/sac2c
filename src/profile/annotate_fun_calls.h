f0e3f994db44eec6347de347e470d9ffbcb38a88
Sven-Bodo Scholz <sbs@sac-home.org>
2004-11-22 16:13:54 +0000

    SACDecCamp04

--- src/profile/annotate_fun_calls.h
+++ src/profile/annotate_fun_calls.h
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.3  2004/11/22 16:13:54  sbs
+ * SACDecCamp04
+ *
  * Revision 1.2  2004/07/17 14:52:03  sah
  * switch to INFO structure
  * PHASE I
@@ -10,13 +13,16 @@
  *
  */
 
-#ifndef _annotate_fun_calls_h
-#define _annotate_fun_calls_h
+#ifndef _SAC_ANNOTATE_FUN_CALLS_H_
+#define _SAC_ANNOTATE_FUN_CALLS_H_
 
-extern node *ProfileFunCalls (node *fundef);
+include "types.h"
+
+  extern node *
+  PFdoProfileFunCalls (node *fundef);
 
 extern node *PFfundef (node *arg_node, info *arg_info);
 extern node *PFassign (node *arg_node, info *arg_info);
 extern node *PFap (node *arg_node, info *arg_info);
 
-#endif /* _annotate_fun_calls_h */
+#endif /* _SAC_ANNOTATE_FUN_CALLS_H_ */
