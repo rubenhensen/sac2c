52d6263b3a510655651216681bc5a6d77dea03e0
Sven-Bodo Scholz <sbs@sac-home.org>
2004-11-24 18:57:29 +0000

    compiles

--- src/typecheck/ct_basic.c
+++ src/typecheck/ct_basic.c
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.5  2004/11/24 18:57:29  sbs
+ * compiles
+ *
  * Revision 1.4  2004/08/08 16:05:08  sah
  * fixed some includes.
  *
@@ -21,11 +24,13 @@
 
 #include <stdio.h>
 #include <string.h>
+
+#include "ct_basic.h"
 #include "dbug.h"
+#include "internal_lib.h"
 
 #include "new_types.h"
 #include "sig_deps.h"
-#include "ct_basic.h"
 #include "traverse.h"
 #include "tree_basic.h"
 
@@ -43,7 +48,7 @@
 /******************************************************************************
  *
  * function:
- *    ntype *NTCCTComputeType( ct_funptr CtFun, te_info *info, ntype *args)
+ *    ntype *NTCCTcomputeType( ct_funptr CtFun, te_info *info, ntype *args)
  *
  * description:
  *   Either computes a return type or establishes a signature dependency.
@@ -51,25 +56,25 @@
  ******************************************************************************/
 
 ntype *
-NTCCTComputeType (ct_funptr CtFun, te_info *info, ntype *args)
+NTCCTcomputeType (ct_funptr CtFun, te_info *info, ntype *args)
 {
     ntype *res;
 #ifndef DBUG_OFF
     char *tmp_str;
 #endif
 
-    DBUG_ENTER ("NTCCTComputeType");
+    DBUG_ENTER ("NTCCTcomputeType");
 
-    DBUG_EXECUTE ("NTC", tmp_str = TYType2String (args, 0, 0););
-    DBUG_PRINT ("NTC", ("computing type of %s \"%s\" applied to %s", TEGetKindStr (info),
-                        TEGetNameStr (info), tmp_str));
-    DBUG_EXECUTE ("NTC", Free (tmp_str););
+    DBUG_EXECUTE ("NTC", tmp_str = TYtype2String (args, 0, 0););
+    DBUG_PRINT ("NTC", ("computing type of %s \"%s\" applied to %s", TEgetKindStr (info),
+                        TEgetNameStr (info), tmp_str));
+    DBUG_EXECUTE ("NTC", ILIBfree (tmp_str););
 
     /*
      * as all CtFun s  operate on array types only, we try to fix as many argument
      * types as possible:
      */
-    args = TYEliminateAlpha (args);
+    args = TYeliminateAlpha (args);
 
     /*
      * At this point, we have to distinguish two fundamentally different
@@ -83,15 +88,15 @@ NTCCTComputeType (ct_funptr CtFun, te_info *info, ntype *args)
      *    all result types!
      */
 
-    if (TYIsProdOfArray (args)) {
+    if (TYisProdOfArray (args)) {
         res = CtFun (info, args);
     } else {
-        res = SDCreateSignatureDependency (CtFun, info, args);
+        res = SDcreateSignatureDependency (CtFun, info, args);
     }
 
-    DBUG_EXECUTE ("NTC", tmp_str = TYType2String (res, FALSE, 0););
+    DBUG_EXECUTE ("NTC", tmp_str = TYtype2String (res, FALSE, 0););
     DBUG_PRINT ("NTC", ("yields %s", tmp_str));
-    DBUG_EXECUTE ("NTC", Free (tmp_str););
+    DBUG_EXECUTE ("NTC", ILIBfree (tmp_str););
 
     DBUG_RETURN (res);
 }
@@ -122,41 +127,41 @@ NTCCond (te_info *err_info, ntype *args)
     info *arg_info;
 
     DBUG_ENTER ("NTCCond");
-    DBUG_ASSERT ((TYIsProdOfArray (args)), "NTCCond called with non-fixed predicate!");
+    DBUG_ASSERT ((TYisProdOfArray (args)), "NTCCond called with non-fixed predicate!");
 
-    pred = TYGetProductMember (args, 0);
-    TEAssureBoolS ("predicate", pred);
+    pred = TYgetProductMember (args, 0);
+    TEassureBoolS ("predicate", pred);
 
-    cond = TEGetWrapper (err_info);
-    arg_info = (info *)TEGetAssign (err_info);
+    cond = TEgetWrapper (err_info);
+    arg_info = (info *)TEgetAssign (err_info);
 
 #if 0
   /**
    * I'm not sure whether the sequel will work. Most liekely, 
    * we will have to make sure, that NO code is traversed more than once!!
    */
-  if( TYIsAKV( pred) ) {
-    if( COIsTrue( TYGetValue( pred), TRUE) ) {
+  if( TYisAKV( pred) ) {
+    if( COIsTrue( TYgetValue( pred), TRUE) ) {
       DBUG_PRINT( "NTC", ("traversing then branch only..."));
-      COND_THEN( cond) = Trav( COND_THEN( cond), arg_info);
+      COND_THEN( cond) = TRAVdo( COND_THEN( cond), arg_info);
     } else {
       DBUG_PRINT( "NTC", ("traversing else branch only..."));
-      COND_ELSE( cond) = Trav( COND_ELSE( cond), arg_info);
+      COND_ELSE( cond) = TRAVdo( COND_ELSE( cond), arg_info);
     }
   } else {
     DBUG_PRINT( "NTC", ("traversing then branch..."));
-    COND_THEN( cond) = Trav( COND_THEN( cond), arg_info);
+    COND_THEN( cond) = TRAVdo( COND_THEN( cond), arg_info);
     DBUG_PRINT( "NTC", ("traversing else branch..."));
-    COND_ELSE( cond) = Trav( COND_ELSE( cond), arg_info);
+    COND_ELSE( cond) = TRAVdo( COND_ELSE( cond), arg_info);
   }
 #else
     DBUG_PRINT ("NTC", ("traversing then branch..."));
-    COND_THEN (cond) = Trav (COND_THEN (cond), arg_info);
+    COND_THEN (cond) = TRAVdo (COND_THEN (cond), arg_info);
     DBUG_PRINT ("NTC", ("traversing else branch..."));
-    COND_ELSE (cond) = Trav (COND_ELSE (cond), arg_info);
+    COND_ELSE (cond) = TRAVdo (COND_ELSE (cond), arg_info);
 #endif
 
-    res = TYMakeProductType (0);
+    res = TYmakeProductType (0);
 
     DBUG_RETURN (res);
 }
