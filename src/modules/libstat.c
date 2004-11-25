9d414fdd81d08d297b1d7c4168b1a4629227e2fe
Stephan Herhut <sah@sac-home.org>
2004-11-25 20:22:11 +0000

    COMPILES

--- src/modules/libstat.c
+++ src/modules/libstat.c
@@ -1,33 +1,9 @@
 /*
  *
  * $Log$
- * Revision 1.10  2004/11/21 11:22:03  sah
- * removed some old ast infos
+ * Revision 1.11  2004/11/25 20:22:11  sah
+ * COMPILES
  *
- * Revision 1.9  2004/11/17 19:48:01  sah
- * interface changes
- *
- * Revision 1.8  2004/11/14 15:22:42  sah
- * switched to AddSymbolByName
- *
- * Revision 1.7  2004/11/01 21:49:10  sah
- * dependencises are printed now as well
- *
- * Revision 1.6  2004/10/28 17:18:33  sah
- * now deserialisation has an internal state
- *
- * Revision 1.5  2004/10/26 09:33:09  sah
- * uses deserialize.h now
- *
- * Revision 1.4  2004/10/25 11:58:47  sah
- * major code cleanup
- *
- * Revision 1.3  2004/10/22 13:24:09  sah
- * added a default case
- *
- * Revision 1.2  2004/10/15 10:03:58  sah
- * removed old module system nodes in
- * new ast mode
  *
  * Revision 1.1  2004/10/04 11:35:10  sah
  * Initial revision
@@ -44,6 +20,8 @@
 #include "tree_basic.h"
 #include "print.h"
 #include "new2old.h"
+#include "stringset.h"
+#include "internal_lib.h"
 #include "free.h"
 
 static void
@@ -51,8 +29,35 @@ PrintLibStatHeader (module_t *module)
 {
     DBUG_ENTER ("PrintLibStatHeader");
 
-    printf ("\nLibrary Information for Module `%s':\n\n", GetModuleName (module));
+#ifndef PRODUCTION
+    printf ("\nThe SaC DevCamp DK Team presents the ");
+#endif
+    printf ("\nLibrary Information for Module `%s':\n\n", MODMgetModuleName (module));
 
+    DBUG_VOID_RETURN;
+}
+
+static void
+PrintLibStatDependencies (module_t *module)
+{
+    DBUG_ENTER ("PrintLibStatDependencies");
+
+    printf ("\n\nModule Dependencies:\n\n");
+
+    STRSprint (MODMgetDependencyTable (module));
+
+    DBUG_VOID_RETURN;
+}
+
+static void
+PrintLibStatTable (sttable_t *table)
+{
+    DBUG_ENTER ("PrintLibStatTable");
+#ifndef DBUG_OFF
+    printf ("\n\nModule Symbols:\n\n");
+
+    STprint (table);
+#endif
     DBUG_VOID_RETURN;
 }
 
@@ -64,11 +69,11 @@ PrintLibStatCodeAddBodies (module_t *module, node *modnode, node *fundef)
     if (fundef != NULL) {
 
         if (FUNDEF_BODY (fundef) == NULL) {
-            InitDeserialize (modnode);
+            DSinitDeserialize (modnode);
 
-            AddFunctionBodyToHead (fundef);
+            DSdoDeserialize (fundef);
 
-            FinishDeserialize (modnode);
+            DSfinishDeserialize (modnode);
         }
 
         PrintLibStatCodeAddBodies (module, modnode, FUNDEF_NEXT (fundef));
@@ -78,49 +83,46 @@ PrintLibStatCodeAddBodies (module_t *module, node *modnode, node *fundef)
 }
 
 static void
-PrintLibStatCodeReadSymbols (module_t *module, STsymbol_t *symbol, STtable_t *table)
+PrintLibStatCodeReadSymbols (module_t *module, stsymbol_t *symbol, sttable_t *table)
 {
     DBUG_ENTER ("PrintLibStatCodeReadSymbols");
 
-    AddSymbolByName (STSymbolName (symbol), SET_wrapperhead, GetModuleName (module));
-    AddSymbolByName (STSymbolName (symbol), SET_typedef, GetModuleName (module));
-    AddSymbolByName (STSymbolName (symbol), SET_objdef, GetModuleName (module));
+    DSaddSymbolByName (STsymbolName (symbol), SET_wrapperhead,
+                       MODMgetModuleName (module));
+    DSaddSymbolByName (STsymbolName (symbol), SET_typedef, MODMgetModuleName (module));
+    DSaddSymbolByName (STsymbolName (symbol), SET_objdef, MODMgetModuleName (module));
 
     DBUG_VOID_RETURN;
 }
 
 static void
-PrintLibStatCode (module_t *module, STtable_t *table)
+PrintLibStatCode (module_t *module, sttable_t *table)
 {
-    STsymboliterator_t *iterator;
+    stsymboliterator_t *iterator;
     node *syntax_tree;
 
     DBUG_ENTER ("PrintLibStatPrintCode");
 
-    syntax_tree = MakeModul (StringCopy (GetModuleName (module)), F_prog, NULL, NULL,
-                             NULL, NULL, NULL);
+    syntax_tree = TBmakeModule (ILIBstringCopy (MODMgetModuleName (module)), F_prog, NULL,
+                                NULL, NULL, NULL, NULL);
 
-    iterator = STSymbolIteratorGet (table);
+    iterator = STsymbolIteratorGet (table);
 
-    InitDeserialize (syntax_tree);
+    DSinitDeserialize (syntax_tree);
 
-    while (STSymbolIteratorHasMore (iterator)) {
-        PrintLibStatCodeReadSymbols (module, STSymbolIteratorNext (iterator), table);
+    while (STsymbolIteratorHasMore (iterator)) {
+        PrintLibStatCodeReadSymbols (module, STsymbolIteratorNext (iterator), table);
     }
 
-    FinishDeserialize (syntax_tree);
+    DSfinishDeserialize (syntax_tree);
 
-    iterator = STSymbolIteratorRelease (iterator);
+    iterator = STsymbolIteratorRelease (iterator);
 
-    PrintLibStatCodeAddBodies (module, syntax_tree, MODUL_FUNS (syntax_tree));
+    PrintLibStatCodeAddBodies (module, syntax_tree, MODULE_FUNS (syntax_tree));
 
-    Print (syntax_tree);
+    PRTdoPrint (syntax_tree);
 
-    /*
-
-    syntax_tree = FreeTree( syntax_tree);
-
-    */
+    syntax_tree = FREEdoFreeTree (syntax_tree);
 
     DBUG_VOID_RETURN;
 }
@@ -129,29 +131,29 @@ void
 PrintLibStat (char *libname)
 {
     module_t *module;
-    STtable_t *table;
+    sttable_t *table;
 
     DBUG_ENTER ("PrintLibStat");
 
     DBUG_PRINT ("LIBSTAT", ("Loading module `%s'", libname));
 
-    module = LoadModule (libname);
+    module = MODMloadModule (libname);
 
     DBUG_PRINT ("LIBSTAT", ("Getting symbol table"));
 
-    table = GetSymbolTable (module);
+    table = MODMgetSymbolTable (module);
 
     DBUG_PRINT ("LIBSTAT", ("Printing LibStat header\n"));
 
     PrintLibStatHeader (module);
 
     DBUG_PRINT ("LIBSTAT", ("Printing table information"));
-#ifndef DBUG_OFF
-    STPrint (table);
-#endif
+
+    PrintLibStatTable (table);
+
     DBUG_PRINT ("LIBSTAT", ("Printing dependencies"));
 
-    SSPrint (GetDependencyTable (module));
+    PrintLibStatDependencies (module);
 
     DBUG_PRINT ("LIBSTAT", ("Printing code"));
 
@@ -159,11 +161,11 @@ PrintLibStat (char *libname)
 
     DBUG_PRINT ("LIBSTAT", ("Destroying table"));
 
-    table = STDestroy (table);
+    table = STdestroy (table);
 
     DBUG_PRINT ("LIBSTAT", ("Unloading module `%s'", libname));
 
-    module = UnLoadModule (module);
+    module = MODMunLoadModule (module);
 
     DBUG_VOID_RETURN;
 }
