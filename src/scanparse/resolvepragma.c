e9eea84e0f68bd50b4670bb153a85d16b940388a
Stephan Herhut <sah@sac-home.org>
2004-11-25 18:12:34 +0000

    added proper initialisation of REFCOUNTED and LINKSIGN for args and rets

--- src/scanparse/resolvepragma.c
+++ src/scanparse/resolvepragma.c
@@ -1,6 +1,10 @@
 /*
  *
  * $Log$
+ * Revision 1.5  2004/11/25 18:12:34  sah
+ * added proper initialisation of REFCOUNTED and LINKSIGN for
+ * args and rets
+ *
  * Revision 1.4  2004/11/24 11:29:33  sah
  * COMPILES
  *
@@ -36,6 +40,7 @@ struct INFO {
     node *module;
     node *nums;
     int counter;
+    enum { RSP_default, RSP_refcnt, RSP_linksign } travmode;
 };
 
 /*
@@ -44,6 +49,7 @@ struct INFO {
 #define INFO_RSP_MODULE(n) ((n)->module)
 #define INFO_RSP_NUMS(n) ((n)->nums)
 #define INFO_RSP_COUNTER(n) ((n)->counter)
+#define INFO_RSP_TRAVMODE(n) ((n)->travmode)
 
 /*
  * INFO functions
@@ -60,6 +66,7 @@ MakeInfo ()
     INFO_RSP_MODULE (result) = NULL;
     INFO_RSP_NUMS (result) = NULL;
     INFO_RSP_COUNTER (result) = 0;
+    INFO_RSP_TRAVMODE (result) = RSP_default;
 
     DBUG_RETURN (result);
 }
@@ -190,19 +197,70 @@ PRAGMA_READONLY(pragma));
  *
  */
 
-node *
+static node *
+InitFundefRetsForExtern (node *rets)
+{
+    DBUG_ENTER ("InitFundefRetsForExtern");
+
+    if (rets != NULL) {
+        DBUG_ASSERT ((NODE_TYPE (rets) == N_ret), "found a non N_ret node");
+
+        RET_ISREFCOUNTED (rets) = FALSE;
+
+        RET_NEXT (rets) = InitFundefRetsForExtern (RET_NEXT (rets));
+    }
+
+    DBUG_RETURN (rets);
+}
+
+static node *
+InitFundefArgsForExtern (node *args)
+{
+    DBUG_ENTER ("InitFundefArgsForExtern");
+
+    if (args != NULL) {
+        DBUG_ASSERT ((NODE_TYPE (args) == N_arg), "found a non N_arg node");
+
+        ARG_ISREFCOUNTED (args) = FALSE;
+
+        ARG_NEXT (args) = InitFundefArgsForExtern (ARG_NEXT (args));
+    }
+
+    DBUG_RETURN (args);
+}
+
+static node *
 AnnotateRefcounting (node *arg_node, info *arg_info, node *nums)
 {
     DBUG_ENTER ("AnnotateRefcounting");
 
     INFO_RSP_COUNTER (arg_info) = 0;
     INFO_RSP_NUMS (arg_info) = nums;
+    INFO_RSP_TRAVMODE (arg_info) = RSP_refcnt;
 
     FUNDEF_RETS (arg_node) = TRAVdo (FUNDEF_RETS (arg_node), arg_info);
     FUNDEF_ARGS (arg_node) = TRAVdo (FUNDEF_ARGS (arg_node), arg_info);
 
     INFO_RSP_COUNTER (arg_info) = 0;
     INFO_RSP_NUMS (arg_info) = NULL;
+    INFO_RSP_TRAVMODE (arg_info) = RSP_default;
+
+    DBUG_RETURN (arg_node);
+}
+
+static node *
+AnnotateLinksign (node *arg_node, info *arg_info, node *nums)
+{
+    DBUG_ENTER ("AnnotateLinksign");
+
+    INFO_RSP_NUMS (arg_info) = nums;
+    INFO_RSP_TRAVMODE (arg_info) = RSP_linksign;
+
+    FUNDEF_RETS (arg_node) = TRAVdo (FUNDEF_RETS (arg_node), arg_info);
+    FUNDEF_ARGS (arg_node) = TRAVdo (FUNDEF_ARGS (arg_node), arg_info);
+
+    INFO_RSP_NUMS (arg_info) = NULL;
+    INFO_RSP_TRAVMODE (arg_info) = RSP_default;
 
     DBUG_RETURN (arg_node);
 }
@@ -212,13 +270,19 @@ RSPret (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("RSPret");
 
-    if (TCnumsContains (INFO_RSP_COUNTER (arg_info), INFO_RSP_NUMS (arg_info))) {
-        RET_ISREFCOUNTED (arg_node) = TRUE;
-    } else {
-        RET_ISREFCOUNTED (arg_node) = FALSE;
-    }
+    if (INFO_RSP_TRAVMODE (arg_info) == RSP_refcnt) {
+        if (TCnumsContains (INFO_RSP_COUNTER (arg_info), INFO_RSP_NUMS (arg_info))) {
 
-    INFO_RSP_COUNTER (arg_info)++;
+            RET_ISREFCOUNTED (arg_node) = TRUE;
+        }
+
+        INFO_RSP_COUNTER (arg_info)++;
+    } else if (INFO_RSP_TRAVMODE (arg_info) == RSP_linksign) {
+        RET_LINKSIGN (arg_node) = NUMS_VAL (INFO_RSP_NUMS (arg_info));
+        RET_HASLINKSIGNINFO (arg_node) = TRUE;
+
+        INFO_RSP_NUMS (arg_info) = NUMS_NEXT (INFO_RSP_NUMS (arg_info));
+    }
 
     DBUG_RETURN (arg_node);
 }
@@ -228,13 +292,19 @@ RSParg (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("RSParg");
 
-    if (TCnumsContains (INFO_RSP_COUNTER (arg_info), INFO_RSP_NUMS (arg_info))) {
-        ARG_ISREFCOUNTED (arg_node) = TRUE;
-    } else {
-        ARG_ISREFCOUNTED (arg_node) = FALSE;
-    }
+    if (INFO_RSP_TRAVMODE (arg_info) == RSP_refcnt) {
+        if (TCnumsContains (INFO_RSP_COUNTER (arg_info), INFO_RSP_NUMS (arg_info))) {
 
-    INFO_RSP_COUNTER (arg_info)++;
+            ARG_ISREFCOUNTED (arg_node) = TRUE;
+        }
+
+        INFO_RSP_COUNTER (arg_info)++;
+    } else if (INFO_RSP_TRAVMODE (arg_info) == RSP_linksign) {
+        ARG_LINKSIGN (arg_node) = NUMS_VAL (INFO_RSP_NUMS (arg_info));
+        ARG_HASLINKSIGNINFO (arg_node) = TRUE;
+
+        INFO_RSP_NUMS (arg_info) = NUMS_NEXT (INFO_RSP_NUMS (arg_info));
+    }
 
     DBUG_RETURN (arg_node);
 }
@@ -246,8 +316,22 @@ RSPfundef (node *arg_node, info *arg_info)
 
     DBUG_PRINT ("RSP", ("Processing function '%s'...", FUNDEF_NAME (arg_node)));
 
+    /*
+     * first we set an initial state for all external
+     * functions, which is then updated by the pragmas
+     * if they exist.
+     */
+
+    if (FUNDEF_ISEXTERN (arg_node)) {
+        FUNDEF_RETS (arg_node) = InitFundefRetsForExtern (FUNDEF_RETS (arg_node));
+        FUNDEF_ARGS (arg_node) = InitFundefArgsForExtern (FUNDEF_ARGS (arg_node));
+    }
+
     if (FUNDEF_PRAGMA (arg_node) != NULL) {
         node *pragma = FUNDEF_PRAGMA (arg_node);
+
+        DBUG_ASSERT ((FUNDEF_ISEXTERN (arg_node)),
+                     "Found a pragma at a non external function!");
 
         DBUG_PRINT ("RSP",
                     ("Processing pragmas for function '%s'...", FUNDEF_NAME (arg_node)));
@@ -267,6 +351,9 @@ RSPfundef (node *arg_node, info *arg_info)
         if (PRAGMA_LINKSIGN (pragma) != NULL) {
             CheckLinkSignNums (NODE_LINE (arg_node), PRAGMA_NUMPARAMS (pragma),
                                PRAGMA_LINKSIGN (pragma));
+            arg_node = AnnotateLinksign (arg_node, arg_info, PRAGMA_LINKSIGN (pragma));
+
+            PRAGMA_LINKSIGN (pragma) = FREEdoFreeTree (PRAGMA_LINKSIGN (pragma));
         }
 
         if (PRAGMA_REFCOUNTING (pragma) != NULL) {
@@ -308,13 +395,20 @@ RSPfundef (node *arg_node, info *arg_info)
             PRAGMA_LINKOBJ (pragma) = ILIBfree (PRAGMA_LINKOBJ (pragma));
         }
 
-        /*
-         * TODO: implement TOUCH and EFFECT, as soon as classes work
-         */
+        if (PRAGMA_TOUCH (pragma) != NULL) {
+            WARN (NODE_LINE (arg_node), ("Pragma 'touch` has no effect on function"));
+            PRAGMA_TOUCH (pragma) = ILIBfree (PRAGMA_TOUCH (pragma));
+        }
+
+        if (PRAGMA_EFFECT (pragma) != NULL) {
+            WARN (NODE_LINE (arg_node), ("Pragma 'effect` has no effect on function"));
+            PRAGMA_EFFECT (pragma) = ILIBfree (PRAGMA_EFFECT (pragma));
+        }
 
         if ((PRAGMA_LINKNAME (pragma) == NULL) && (PRAGMA_LINKOBJ (pragma) == NULL)
-            && (PRAGMA_LINKNAME (pragma) == NULL) && (PRAGMA_LINKMOD (pragma) == NULL)
-            && (PRAGMA_LINKSIGN (pragma) == NULL)
+            && (PRAGMA_LINKSIGN (pragma) == NULL) && (PRAGMA_LINKMOD (pragma) == NULL)
+            && (PRAGMA_LINKSIGN (pragma) == NULL) && (PRAGMA_TOUCH (pragma) == NULL)
+            && (PRAGMA_EFFECT (pragma) == NULL)
             && (PRAGMA_REFCOUNTING (pragma) == NULL)) {
             FUNDEF_PRAGMA (arg_node) = FREEdoFreeNode (pragma);
         }
