edcd4dfac979a7e41873dd76becbd39905ac4668
Stephan Herhut <sah@sac-home.org>
2004-11-26 03:16:20 +0000

    COMPILES!

--- src/optimize/DeadFunctionRemoval.c
+++ src/optimize/DeadFunctionRemoval.c
@@ -1,73 +1,11 @@
 /*
  *
  * $Log$
+ * Revision 3.15  2004/11/26 03:16:20  sah
+ * COMPILES!
+ *
  * Revision 3.14  2004/11/19 10:17:34  sah
  * objinitfuns are never removed
- *
- * Revision 3.13  2004/10/26 09:31:35  sah
- * uses new PROVIDED/EXPORTED flags in newast mode now
- *
- * Revision 3.12  2004/09/02 17:48:32  skt
- * added warning for use of DFR with SSA-form
- *
- * Revision 3.11  2004/08/09 08:31:47  ktr
- * Fold functions are no longer removed in EMM as they are used by MT.
- *
- * Revision 3.10  2004/08/04 17:06:28  ktr
- * In EMM, Functions can be dead although they are referenced by a fold-withop
- *
- * Revision 3.9  2004/07/18 19:54:54  sah
- * switch to new INFO structure
- * PHASE I
- * (as well some code cleanup)
- *
- * Revision 3.8  2004/06/03 09:08:05  khf
- * DFRwithop(): traverse into NWITHOP_NEXT added
- *
- * Revision 3.7  2004/02/20 08:18:59  mwe
- * now functions with (MODUL_FUNS) and without (MODUL_FUNDECS) body are separated
- * changed tree traversal according to that
- *
- * Revision 3.6  2003/11/18 17:41:30  dkr
- * no changes done
- *
- * Revision 3.5  2001/05/02 11:13:57  nmw
- * missing initialization added
- *
- * Revision 3.4  2001/05/02 09:09:53  nmw
- * change implementation to be more agressive when removing dead functions
- * from SAC programs
- *
- * Revision 3.3  2001/03/22 21:11:25  dkr
- * include of tree.h eliminated
- *
- * Revision 3.2  2000/11/23 16:16:50  sbs
- * mem_dead_fun in DeadFunctionRemoval enclosed in ifndef DBUG_OFF
- * for avoiding a warning when compiling product version.
- *
- * Revision 3.1  2000/11/20 18:00:29  sacbase
- * new release made
- *
- * Revision 2.5  2000/10/31 18:08:09  cg
- * Dead function removal completely re-implemented.
- *
- * Revision 2.4  2000/07/14 12:59:25  dkr
- * DFRfundef: body of function is always traversed now
- *
- * Revision 2.3  2000/07/14 12:05:14  dkr
- * DFRblock() added
- *
- * Revision 2.2  2000/01/26 17:26:59  dkr
- * type of traverse-function-table changed.
- *
- * Revision 2.1  1999/02/23 12:41:13  sacbase
- * new release made
- *
- * Revision 1.3  1999/01/20 09:07:46  cg
- * Dead function removal may now handle programs without any functions.
- *
- * Revision 1.2  1999/01/18 15:46:02  sbs
- * DBUG_PRINT( "OPTMEM",...) inserted for mem-info during optimization
  *
  * Revision 1.1  1999/01/07 17:36:51  sbs
  * Initial revision
@@ -76,22 +14,16 @@
 
 #define NEW_INFO
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "types.h"
+#include "DeadFunctionRemoval.h"
 #include "tree_basic.h"
-#include "tree_compound.h"
 #include "internal_lib.h"
 #include "free.h"
-#include "Error.h"
 #include "dbug.h"
 #include "globals.h"
-#include "my_debug.h"
 #include "traverse.h"
+#include "Error.h"
 
 #include "optimize.h"
-
 /*
  * INFO structure
  */
@@ -114,7 +46,7 @@ MakeInfo ()
 
     DBUG_ENTER ("MakeInfo");
 
-    result = Malloc (sizeof (info));
+    result = ILIBmalloc (sizeof (info));
 
     INFO_DFR_SPINE (result) = 0;
 
@@ -126,7 +58,7 @@ FreeInfo (info *info)
 {
     DBUG_ENTER ("FreeInfo");
 
-    info = Free (info);
+    info = ILIBfree (info);
 
     DBUG_RETURN (info);
 }
@@ -142,29 +74,31 @@ FreeInfo (info *info)
  ******************************************************************************/
 
 node *
-DeadFunctionRemoval (node *arg_node)
+DFRdoDeadFunctionRemoval (node *arg_node)
 {
-    funtab *tmp_tab;
     info *arg_info;
 #ifndef DBUG_OFF
     int mem_dead_fun = dead_fun;
 #endif
 
-    DBUG_ENTER ("DeadFunctionRemoval");
+    DBUG_ENTER ("DFRdoDeadFunctionRemoval");
     DBUG_PRINT ("OPT", ("DEAD FUNCTION REMOVAL"));
-    DBUG_PRINT ("OPTMEM", ("mem currently allocated: %d bytes", current_allocated_mem));
-
-    tmp_tab = act_tab;
-    act_tab = dfr_tab;
+    DBUG_PRINT ("OPTMEM",
+                ("mem currently allocated: %d bytes", global.current_allocated_mem));
 
     arg_info = MakeInfo ();
-    arg_node = Trav (arg_node, arg_info);
+
+    TRAVpush (TR_dfr);
+
+    arg_node = TRAVdo (arg_node, arg_info);
+
+    TRAVpop ();
+
     arg_info = FreeInfo (arg_info);
 
     DBUG_PRINT ("OPT", ("                        result: %d", dead_fun - mem_dead_fun));
-    DBUG_PRINT ("OPTMEM", ("mem currently allocated: %d bytes", current_allocated_mem));
-
-    act_tab = tmp_tab;
+    DBUG_PRINT ("OPTMEM",
+                ("mem currently allocated: %d bytes", global.current_allocated_mem));
 
     DBUG_RETURN (arg_node);
 }
@@ -172,7 +106,7 @@ DeadFunctionRemoval (node *arg_node)
 /******************************************************************************
  *
  * Function:
- *   node *DFRmodul(node *arg_node,info *arg_info)
+ *   node *DFRmodule(node *arg_node,info *arg_info)
  *
  * Description:
  *   Prevents DFR in modules
@@ -181,47 +115,53 @@ DeadFunctionRemoval (node *arg_node)
  ******************************************************************************/
 
 node *
-DFRmodul (node *arg_node, info *arg_info)
+DFRmodule (node *arg_node, info *arg_info)
 {
     node *fun;
     DBUG_ENTER ("DFRmodul");
 
-    if (MODUL_FUNDECS (arg_node) != NULL) {
+    if (MODULE_FUNDECS (arg_node) != NULL) {
         /* clear dfr flag */
-        fun = MODUL_FUNDECS (arg_node);
+        fun = MODULE_FUNDECS (arg_node);
         while (fun != NULL) {
-            FUNDEF_EXPORT (fun) = FALSE;
+            FUNDEF_ISNEEDED (fun) = FALSE;
             fun = FUNDEF_NEXT (fun);
         }
 
+        /*
+         * the following code was disabled for a unknown reason.
+         * as we have no idea why it was disabled, we have
+         * reenabled it for now
+         */
+
         /* check fundefs for applications (only main in programs) */
-        /*    INFO_DFR_SPINE( arg_info) = TRUE;
-            MODUL_FUNDECS( arg_node) = Trav( MODUL_FUNDECS( arg_node), arg_info);*/
+        INFO_DFR_SPINE (arg_info) = TRUE;
+        MODULE_FUNDECS (arg_node) = TRAVdo (MODULE_FUNDECS (arg_node), arg_info);
 
         /* remove all produced zombies */
-        /*    MODUL_FUNDECS(arg_node) = RemoveAllZombies( MODUL_FUNDECS(arg_node));*/
+        MODULE_FUNDECS (arg_node) = FREEremoveAllZombies (MODULE_FUNDECS (arg_node));
     }
 
-    if (MODUL_FUNS (arg_node) != NULL) {
+    if (MODULE_FUNS (arg_node) != NULL) {
         /* clear dfr flag */
-        fun = MODUL_FUNS (arg_node);
+        fun = MODULE_FUNS (arg_node);
         while (fun != NULL) {
-            FUNDEF_EXPORT (fun) = FALSE;
+            FUNDEF_ISNEEDED (fun) = FALSE;
             fun = FUNDEF_NEXT (fun);
         }
 
         /* check fundefs for applications (only main in programs) */
         INFO_DFR_SPINE (arg_info) = TRUE;
-        MODUL_FUNS (arg_node) = Trav (MODUL_FUNS (arg_node), arg_info);
+        MODULE_FUNS (arg_node) = TRAVdo (MODULE_FUNS (arg_node), arg_info);
 
         /* remove all produced zombies */
-        MODUL_FUNS (arg_node) = RemoveAllZombies (MODUL_FUNS (arg_node));
+        MODULE_FUNS (arg_node) = FREEremoveAllZombies (MODULE_FUNS (arg_node));
     }
 
-    if (MODUL_FUNDECS (arg_node) != NULL) {
+    if (MODULE_FUNDECS (arg_node) != NULL) {
 
         /* remove all produced zombies */
-        MODUL_FUNDECS (arg_node) = RemoveAllZombies (MODUL_FUNDECS (arg_node));
+        MODULE_FUNDECS (arg_node) = FREEremoveAllZombies (MODULE_FUNDECS (arg_node));
     }
 
     DBUG_RETURN (arg_node);
@@ -247,79 +187,44 @@ DFRfundef (node *arg_node, info *arg_info)
                     ("Dead Function Removal in function: %s", FUNDEF_NAME (arg_node)));
 
         /* a warning for using DFR with SSA */
-        if ((FUNDEF_STATUS (arg_node) == ST_dofun)
-            || (FUNDEF_STATUS (arg_node) == ST_whilefun)
-            || (FUNDEF_STATUS (arg_node) == ST_condfun)) {
+        if ((FUNDEF_ISDOFUN (arg_node)) || (FUNDEF_ISCONDFUN (arg_node))) {
             if (FUNDEF_USED (arg_node) > 1) {
-                SYSWARN (("Lac-functions, which are used more than once aren't handled "
-                          "correctly by DeadFunctionRemoval"));
+                SYSWARN (("Lac-functions, which are used more than once aren't "
+                          "handled correctly by DeadFunctionRemoval"));
             }
         }
 
-        /* remark: main is always tagged as ST_exported! */
-#ifndef NEW_AST
-        if ((FUNDEF_STATUS (arg_node) == ST_exported)
-            || (FUNDEF_STATUS (arg_node) == ST_objinitfun)) {
-#else
-        if ((GET_FLAG (FUNDEF, arg_node, IS_PROVIDED))
-            || (FUNDEF_STATUS (arg_node) == ST_objinitfun)) {
-#endif
-            FUNDEF_EXPORT (arg_node) = TRUE;
+        /*
+         * remark: main is always tagged as provided
+         */
+        if (FUNDEF_ISPROVIDED (arg_node)) {
+            FUNDEF_ISNEEDED (arg_node) = TRUE;
 
             if (FUNDEF_BODY (arg_node) != NULL) {
                 INFO_DFR_SPINE (arg_info) = FALSE;
-                FUNDEF_BODY (arg_node) = Trav (FUNDEF_BODY (arg_node), arg_info);
+                FUNDEF_BODY (arg_node) = TRAVdo (FUNDEF_BODY (arg_node), arg_info);
                 INFO_DFR_SPINE (arg_info) = TRUE;
             }
         }
 
         /* traverse next fundef */
         if (FUNDEF_NEXT (arg_node) != NULL) {
-            FUNDEF_NEXT (arg_node) = Trav (FUNDEF_NEXT (arg_node), arg_info);
+            FUNDEF_NEXT (arg_node) = TRAVdo (FUNDEF_NEXT (arg_node), arg_info);
         }
 
         /* on bottom up traversal turn unused fundefs into zombies */
-        if (!(FUNDEF_EXPORT (arg_node))) {
+        if (!(FUNDEF_ISNEEDED (arg_node))) {
             dead_fun++;
-            arg_node = FreeNode (arg_node);
+            arg_node = FREEdoFreeNode (arg_node);
         }
     } else {
-        if (!FUNDEF_EXPORT (arg_node)) {
-            FUNDEF_EXPORT (arg_node) = TRUE;
+        if (!FUNDEF_ISNEEDED (arg_node)) {
+            FUNDEF_ISNEEDED (arg_node) = TRUE;
+
             if (FUNDEF_BODY (arg_node) != NULL) {
-                FUNDEF_BODY (arg_node) = Trav (FUNDEF_BODY (arg_node), arg_info);
+                FUNDEF_BODY (arg_node) = TRAVdo (FUNDEF_BODY (arg_node), arg_info);
             }
         }
-    }
-
-    DBUG_RETURN (arg_node);
-}
-
-/******************************************************************************
- *
- * Function:
- *   node *DFRblock( node *arg_node, info *arg_info)
- *
- * Description:
- *   Removes BLOCK_NEEDFUNS!
- *
- ******************************************************************************/
-
-node *
-DFRblock (node *arg_node, info *arg_info)
-{
-    DBUG_ENTER ("DFRblock");
-
-    if (BLOCK_INSTR (arg_node) != NULL) {
-        BLOCK_INSTR (arg_node) = Trav (BLOCK_INSTR (arg_node), arg_info);
-    }
-
-    /*
-     * After DFR the data stored in BLOCK_NEEDFUNS is possibly corrupted!
-     * -> remove it!
-     */
-    if (BLOCK_NEEDFUNS (arg_node) != NULL) {
-        BLOCK_NEEDFUNS (arg_node) = FreeNodelist (BLOCK_NEEDFUNS (arg_node));
     }
 
     DBUG_RETURN (arg_node);
@@ -340,11 +245,11 @@ DFRap (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("DFRap");
 
-    AP_FUNDEF (arg_node) = Trav (AP_FUNDEF (arg_node), arg_info);
-
-#if 0
-  arg_node = TravSons( arg_node, arg_info);
-#endif
+    /*
+     * we have found a function application. SO move into the called
+     * function and mark it as needed.
+     */
+    AP_FUNDEF (arg_node) = TRAVdo (AP_FUNDEF (arg_node), arg_info);
 
     DBUG_RETURN (arg_node);
 }
@@ -352,7 +257,7 @@ DFRap (node *arg_node, info *arg_info)
 /******************************************************************************
  *
  * Function:
- *   node *DFRwithop( node *arg_node, info *arg_info)
+ *   node *DFRfold( node *arg_node, info *arg_info)
  *
  * Description:
  *
@@ -360,21 +265,15 @@ DFRap (node *arg_node, info *arg_info)
  ******************************************************************************/
 
 node *
-DFRwithop (node *arg_node, info *arg_info)
+DFRfold (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("DFRwithop");
 
-    if (NWITHOP_TYPE (arg_node) == WO_foldfun) {
-        NWITHOP_FUNDEF (arg_node) = Trav (NWITHOP_FUNDEF (arg_node), arg_info);
-    }
+    FOLD_FUNDEF (arg_node) = TRAVdo (FOLD_FUNDEF (arg_node), arg_info);
 
-    if (NWITHOP_NEXT (arg_node) != NULL) {
-        NWITHOP_NEXT (arg_node) = Trav (NWITHOP_NEXT (arg_node), arg_info);
+    if (FOLD_NEXT (arg_node) != NULL) {
+        FOLD_NEXT (arg_node) = TRAVdo (FOLD_NEXT (arg_node), arg_info);
     }
-
-#if 0
-  arg_node = TravSons( arg_node, arg_info);
-#endif
 
     DBUG_RETURN (arg_node);
 }
