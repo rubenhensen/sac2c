78d163d96537733a5ac51c77d4e6796eac145272
Kai Trojahner <ktr@sac-home.org>
2004-11-27 02:25:29 +0000

    new version

--- src/compile/functionprecompile.h
+++ src/compile/functionprecompile.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.2  2004/11/27 02:25:29  ktr
+ * new version
+ *
  * Revision 1.1  2004/11/26 23:13:29  sah
  * Initial revision
  *
@@ -27,7 +30,7 @@ extern node *FPCmodule (node *arg_node, info *arg_info);
 extern node *FPCfundef (node *arg_node, info *arg_info);
 extern node *FPCassign (node *arg_node, info *arg_info);
 extern node *FPClet (node *arg_node, info *arg_info);
-extern node *FPCap (node *arg_node, info *arg_info);
-extern node *FPCprf (node *arg_node, info *arg_info);
+extern node *FPCret (node *arg_node, info *arg_info);
+extern node *FPCarg (node *arg_node, info *arg_info);
 
 #endif /* _SAC_FUNCTIONPRECOMPITE_H_ */
