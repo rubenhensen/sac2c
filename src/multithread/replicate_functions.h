f50aae3b8320d3fa61cf4f49157600a3633c7e60
Clemens Grelck <cg@sac-home.org>
2005-03-04 21:21:42 +0000

    Added REPFUNmodule() to adjust initiation of traversal to coding guidelines. This is essential for the new style of silently adding duplicated LaC funs to the fundef chain.

--- src/multithread/replicate_functions.h
+++ src/multithread/replicate_functions.h
@@ -1,5 +1,10 @@
 /*
  * $Log$
+ * Revision 1.4  2005/03/04 21:21:42  cg
+ * Added REPFUNmodule() to adjust initiation of traversal to
+ * coding guidelines. This is essential for the new style of
+ * silently adding duplicated LaC funs to the fundef chain.
+ *
  * Revision 1.3  2004/11/23 20:52:11  skt
  * big compiler brushing during SACDevCampDK 2k4
  *
@@ -27,15 +32,12 @@
 
 extern node *REPFUNdoReplicateFunctions (node *arg_node);
 
+extern node *REPFUNmodule (node *arg_node, info *arg_info);
 extern node *REPFUNfundef (node *arg_node, info *arg_info);
-
 extern node *REPFUNassign (node *arg_node, info *arg_info);
-
 extern node *REPFUNap (node *arg_node, info *arg_info);
-
 extern node *REPFUNex (node *arg_node, info *arg_info);
-
 extern node *REPFUNst (node *arg_node, info *arg_info);
-
 extern node *REPFUNmt (node *arg_node, info *arg_info);
+
 #endif /* _SAC_REPLICATE_FUNCTIONS_H_ */
