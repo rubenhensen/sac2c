ca116073d46f4d48105a78dc061fa527fcb9a53a
Sven-Bodo Scholz <sbs@sac-home.org>
2005-03-19 23:17:01 +0000

    FOLD macros added

--- src/runtime/sac_audwl.h
+++ src/runtime/sac_audwl.h
@@ -2,6 +2,9 @@
 /*
  *
  * $Log$
+ * Revision 1.4  2005/03/19 23:17:01  sbs
+ * FOLD macros added
+ *
  * Revision 1.3  2005/03/16 09:17:38  sbs
  * *** empty log message ***
  *
@@ -40,7 +43,7 @@
  *  - printed output of the index vectors traversed
  */
 
-#define DEBUG_AUD_GENERATOR_CODE 1
+#define DEBUG_AUD_GENERATOR_CODE 0
 
 #if DEBUG_AUD_GENERATOR_CODE
 #define SAC_AUD_WL_DEBUG_PRINT(idx_vec_NT)                                               \
@@ -145,6 +148,67 @@
         SAC_ND_WRITE (idx_vec_NT, SAC_d) = SAC_ND_READ (idx_vec_NT, SAC_d) + 1;          \
     }                                                                                    \
     SAC_WL_OFFSET (res_NT) += SAC_off_inc;                                               \
+    }                                                                                    \
+    }
+
+/*****************************************************************************/
+
+#define SAC_AUD_WL_FOLD_BEGIN(idx_vec_NT, lower_NT, upper_NT)                            \
+    {                                                                                    \
+        int SAC_in_gen;                                                                  \
+        int SAC_max_d = SAC_ND_A_SIZE (idx_vec_NT) - 1;                                  \
+        int SAC_d = 0;                                                                   \
+        int SAC_within_bounds = 1;                                                       \
+                                                                                         \
+        while (SAC_d <= SAC_max_d) {                                                     \
+            SAC_ND_WRITE (idx_vec_NT, SAC_d) = SAC_ND_READ (lower_NT, SAC_d);            \
+            SAC_within_bounds                                                            \
+              = SAC_within_bounds                                                        \
+                && (SAC_ND_READ (lower_NT, SAC_d) < SAC_ND_READ (upper_NT, SAC_d));      \
+            SAC_d++;                                                                     \
+        }                                                                                \
+        while (SAC_within_bounds) {
+
+#define SAC_AUD_WL_FOLD_LU_GEN(lower_NT, idx_vec_NT, upper_NT)                           \
+    SAC_in_gen = 1;                                                                      \
+    SAC_AUD_WL_DEBUG_PRINT (idx_vec_NT)
+
+#define SAC_AUD_WL_FOLD_LUS_GEN(lower_NT, idx_vec_NT, upper_NT, step_NT)                 \
+    SAC_d = SAC_max_d;                                                                   \
+    SAC_in_gen = 1;                                                                      \
+    while (SAC_d >= 0) {                                                                 \
+        SAC_in_gen                                                                       \
+          = SAC_in_gen                                                                   \
+            && (((SAC_ND_READ (idx_vec_NT, SAC_d) - SAC_ND_READ (lower_NT, SAC_d))       \
+                 % SAC_ND_READ (step_NT, SAC_d))                                         \
+                == 0);                                                                   \
+        SAC_d--;                                                                         \
+    }                                                                                    \
+    SAC_AUD_WL_DEBUG_PRINT (idx_vec_NT)
+
+#define SAC_AUD_WL_FOLD_LUSW_GEN(lower_NT, idx_vec_NT, upper_NT, step_NT, width_NT)      \
+    SAC_d = SAC_max_d;                                                                   \
+    SAC_in_gen = 1;                                                                      \
+    while (SAC_d >= 0) {                                                                 \
+        SAC_in_gen                                                                       \
+          = SAC_in_gen                                                                   \
+            && (((SAC_ND_READ (idx_vec_NT, SAC_d) - SAC_ND_READ (lower_NT, SAC_d))       \
+                 % SAC_ND_READ (step_NT, SAC_d))                                         \
+                < SAC_ND_READ (width_NT, SAC_d));                                        \
+        SAC_d--;                                                                         \
+    }                                                                                    \
+    SAC_AUD_WL_DEBUG_PRINT (idx_vec_NT)
+
+#define SAC_AUD_WL_FOLD_END(idx_vec_NT, lower_NT, upper_NT)                              \
+    SAC_d = SAC_max_d;                                                                   \
+    SAC_ND_WRITE (idx_vec_NT, SAC_d) = SAC_ND_READ (idx_vec_NT, SAC_d) + 1;              \
+    while ((SAC_d > 0)                                                                   \
+           && (SAC_ND_READ (idx_vec_NT, SAC_d) == SAC_ND_READ (upper_NT, SAC_d))) {      \
+        SAC_ND_WRITE (idx_vec_NT, SAC_d) = SAC_ND_READ (lower_NT, SAC_d);                \
+        SAC_d--;                                                                         \
+        SAC_ND_WRITE (idx_vec_NT, SAC_d) = SAC_ND_READ (idx_vec_NT, SAC_d) + 1;          \
+    }                                                                                    \
+    SAC_within_bounds = SAC_ND_READ (idx_vec_NT, SAC_d) < SAC_ND_READ (upper_NT, SAC_d); \
     }                                                                                    \
     }
 
