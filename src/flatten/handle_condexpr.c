3316459566379865a32a646b75739a97ed75baef
Kai Trojahner <ktr@sac-home.org>
2005-06-29 08:49:53 +0000

    added rules for N_code, N_with and N_do

--- src/flatten/handle_condexpr.c
+++ src/flatten/handle_condexpr.c
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.2  2005/06/29 08:49:53  ktr
+ * added rules for N_code, N_with and N_do
+ *
  * Revision 1.1  2005/06/28 20:52:13  cg
  * Initial revision
  *
@@ -129,6 +132,41 @@ HCEassign (node *arg_node, info *arg_info)
 
 /** <!--********************************************************************-->
  *
+ * @fn node *HCEcode(node *arg_node, info *arg_info)
+ *
+ * @brief
+ *
+ * @param arg_node
+ * @param arg_info
+ *
+ * @return arg_node
+ *
+ *****************************************************************************/
+node *
+HCEcode (node *arg_node, info *arg_info)
+{
+    DBUG_ENTER ("HCEcode");
+
+    if (CODE_NEXT (arg_node) != NULL) {
+        CODE_NEXT (arg_node) = TRAVdo (CODE_NEXT (arg_node), arg_info);
+    }
+
+    CODE_CEXPRS (arg_node) = TRAVdo (CODE_CEXPRS (arg_node), arg_info);
+
+    if (INFO_HCE_PREASSIGN (arg_info) != NULL) {
+        CODE_CBLOCK_INSTR (arg_node)
+          = TCappendAssign (CODE_CBLOCK_INSTR (arg_node), INFO_HCE_PREASSIGN (arg_info));
+
+        INFO_HCE_PREASSIGN (arg_info) = NULL;
+    }
+
+    CODE_CBLOCK (arg_node) = TRAVdo (CODE_CBLOCK (arg_node), arg_info);
+
+    DBUG_RETURN (arg_node);
+}
+
+/** <!--********************************************************************-->
+ *
  * @fn node *HCEcond(node *arg_node, info *arg_info)
  *
  * @brief
@@ -147,6 +185,37 @@ HCEcond (node *arg_node, info *arg_info)
     COND_THEN (arg_node) = TRAVdo (COND_THEN (arg_node), arg_info);
     COND_ELSE (arg_node) = TRAVdo (COND_ELSE (arg_node), arg_info);
     COND_COND (arg_node) = TRAVdo (COND_COND (arg_node), arg_info);
+
+    DBUG_RETURN (arg_node);
+}
+
+/** <!--********************************************************************-->
+ *
+ * @fn node *HCEdo(node *arg_node, info *arg_info)
+ *
+ * @brief
+ *
+ * @param arg_node
+ * @param arg_info
+ *
+ * @return arg_node
+ *
+ *****************************************************************************/
+node *
+HCEdo (node *arg_node, info *arg_info)
+{
+    DBUG_ENTER ("HCEdo");
+
+    DO_COND (arg_node) = TRAVdo (DO_COND (arg_node), arg_info);
+
+    if (INFO_HCE_PREASSIGN (arg_info) != NULL) {
+        DO_INSTR (arg_node)
+          = TCappendAssign (DO_INSTR (arg_node), INFO_HCE_PREASSIGN (arg_info));
+
+        INFO_HCE_PREASSIGN (arg_info) = NULL;
+    }
+
+    DO_BODY (arg_node) = TRAVdo (DO_BODY (arg_node), arg_info);
 
     DBUG_RETURN (arg_node);
 }
@@ -195,4 +264,28 @@ HCEfuncond (node *arg_node, info *arg_info)
     arg_node = FREEdoFreeNode (arg_node);
 
     DBUG_RETURN (TBmakeSpid (NULL, n));
+}
+
+/** <!--********************************************************************-->
+ *
+ * @fn node *HCEwith(node *arg_node, info *arg_info)
+ *
+ * @brief
+ *
+ * @param arg_node
+ * @param arg_info
+ *
+ * @return arg_node
+ *
+ *****************************************************************************/
+node *
+HCEwith (node *arg_node, info *arg_info)
+{
+    DBUG_ENTER ("HCEwith");
+
+    WITH_CODE (arg_node) = TRAVdo (WITH_CODE (arg_node), arg_info);
+    WITH_PART (arg_node) = TRAVdo (WITH_PART (arg_node), arg_info);
+    WITH_WITHOP (arg_node) = TRAVdo (WITH_WITHOP (arg_node), arg_info);
+
+    DBUG_RETURN (arg_node);
 }
--- src/flatten/handle_condexpr.h
+++ src/flatten/handle_condexpr.h
@@ -1,6 +1,9 @@
 /*
  *
  * $Log$
+ * Revision 1.2  2005/06/29 08:49:53  ktr
+ * added rules for N_code, N_with and N_do
+ *
  * Revision 1.1  2005/06/28 20:53:17  cg
  * Initial revision
  *
@@ -21,7 +24,10 @@
 extern node *HCEdoHandleConditionalExpressions (node *arg_node);
 
 extern node *HCEassign (node *arg_node, info *arg_info);
+extern node *HCEcode (node *arg_node, info *arg_info);
 extern node *HCEcond (node *arg_node, info *arg_info);
+extern node *HCEdo (node *arg_node, info *arg_info);
 extern node *HCEfuncond (node *arg_node, info *arg_info);
+extern node *HCEwith (node *arg_node, info *arg_info);
 
 #endif /* _SAC_HANDLE_CONDEXPR_H_ */
