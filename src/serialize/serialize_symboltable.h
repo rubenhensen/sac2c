4d4ff383bcdd36b0fdf35383f27c9d852f1b1a1a
Stephan Herhut <sah@sac-home.org>
2005-07-16 18:34:08 +0000

    Initial revision

---
+++ src/serialize/serialize_symboltable.h
@@ -0,0 +1,16 @@
+/*
+ * $Log$
+ * Revision 1.1  2005/07/16 18:34:08  sah
+ * Initial revision
+ *
+ *
+ */
+
+#ifndef _SAC_SERIALIZE_SYMBOLTABLE_H_
+#define _SAC_SERIALIZE_SYMBOLTABLE_H_
+
+#include "types.h"
+
+extern void SSTserializeSymbolTable (node *module, sttable_t *table);
+
+#endif
