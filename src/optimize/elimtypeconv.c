06dfa7bfd8d29815243e8365e0b9b2736bb9c8e0
Kai Trojahner <ktr@sac-home.org>
2005-08-20 12:11:41 +0000

    Initial revision

---
+++ src/optimize/elimtypeconv.c
@@ -0,0 +1,73 @@
+/*
+ * $Log$
+ * Revision 1.1  2005/08/20 12:11:28  ktr
+ * Initial revision
+ *
+ */
+#include "tree_basic.h"
+#include "tree_compound.h"
+#include "new_types.h"
+#include "free.h"
+#include "traverse.h"
+#include "dbug.h"
+
+#include "elimtypeconv.h"
+
+/** <!--********************************************************************-->
+ *
+ * @fn node *ETCdoEliminateTypeConversions( node *arg_node)
+ *
+ * @brief starting point of F_type_conv elimination
+ *
+ * @param arg_node
+ *
+ * @return
+ *
+ *****************************************************************************/
+node *
+ETCdoEliminateTypeConversions (node *arg_node)
+{
+    DBUG_ENTER ("ETCdoEliminateTypeConversions");
+
+    TRAVpush (TR_etc);
+    arg_node = TRAVdo (arg_node, NULL);
+    TRAVpop ();
+
+    DBUG_RETURN (arg_node);
+}
+
+/******************************************************************************
+ *
+ * Typeconv Elimination traversal (etc_tab)
+ *
+ * prefix: ETC
+ *
+ *****************************************************************************/
+node *
+ETCfundef (node *arg_node, info *arg_info)
+{
+    DBUG_ENTER ("ETCfundef");
+
+    if (FUNDEF_BODY (arg_node) != NULL) {
+        FUNDEF_BODY (arg_node) = TRAVdo (FUNDEF_BODY (arg_node), arg_info);
+    }
+
+    DBUG_RETURN (arg_node);
+}
+
+node *
+ETCprf (node *arg_node, info *arg_info)
+{
+    DBUG_ENTER ("ETCprf");
+
+    if (PRF_PRF (arg_node) == F_type_conv) {
+        if (TYleTypes (ID_NTYPE (PRF_ARG2 (arg_node)), TYPE_TYPE (PRF_ARG1 (arg_node)))) {
+            node *res = PRF_ARG2 (arg_node);
+            PRF_ARG2 (arg_node) = NULL;
+            arg_node = FREEdoFreeNode (arg_node);
+            arg_node = res;
+        }
+    }
+
+    DBUG_RETURN (arg_node);
+}
---
+++ src/optimize/elimtypeconv.h
@@ -0,0 +1,24 @@
+/*
+ * $Log$
+ * Revision 1.1  2005/08/20 12:11:41  ktr
+ * Initial revision
+ *
+ */
+#ifndef _SAC_ELIMTYPECONV_H_
+#define _SAC_ELIMTYPECONV_H_
+
+#include "types.h"
+
+/******************************************************************************
+ *
+ * Eliminate Type Conversions traversal
+ *
+ * prefix: ETC
+ *
+ *****************************************************************************/
+extern node *ETCdoEliminateTypeConversions (node *arg_node);
+
+extern node *ETCfundef (node *arg_node, info *arg_info);
+extern node *ETCprf (node *arg_node, info *arg_info);
+
+#endif /* _SAC_ELIMTYPECONV_H_ */
