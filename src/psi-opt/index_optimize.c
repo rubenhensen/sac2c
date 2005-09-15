64eb1afbd16379b662a30d4be9e1d47e6d9ab5b2
Stephan Herhut <sah@sac-home.org>
2005-09-15 11:08:52 +0000

    now, wloffsets are used whenever possible

--- src/psi-opt/index_optimize.c
+++ src/psi-opt/index_optimize.c
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.2  2005/09/15 11:08:52  sah
+ * now, wloffsets are used whenever possible
+ *
  * Revision 1.1  2005/09/14 21:26:36  sah
  * Initial revision
  *
@@ -15,16 +18,29 @@
 #include "DupTree.h"
 #include "free.h"
 #include "internal_lib.h"
+#include "type_utils.h"
+#include "new_types.h"
+#include "shape.h"
+
+/**
+ * forward declarations
+ */
+typedef struct OFFSETINFO offsetinfo;
+typedef struct IVINFO ivinfo;
 
 /**
  * INFO structure
  */
 struct INFO {
+    ivinfo *ivinfo;
+    node *lhs;
 };
 
 /**
  * INFO macros
  */
+#define INFO_IVINFO(n) ((n)->ivinfo)
+#define INFO_LHS(n) ((n)->lhs)
 
 /**
  * INFO functions
@@ -37,6 +53,9 @@ MakeInfo ()
     DBUG_ENTER ("MakeInfo");
 
     result = ILIBmalloc (sizeof (info));
+
+    INFO_IVINFO (result) = NULL;
+    INFO_LHS (result) = NULL;
 
     DBUG_RETURN (result);
 }
@@ -52,12 +71,200 @@ FreeInfo (info *info)
 }
 
 /**
+ * WITHIV information structure.
+ *
+ * this structure is used to store withid x shape -> offset
+ * information to ease the lookup of matching withloop
+ * offsets for a given vect2offset operation.
+ *
+ * IV       stores the avis of the indexvector
+ * OFFSETS  contains all offsetvar avis and their corresponding shape
+ * SCALARS  contains the N_ids chain of scalars that can be used instead
+ *          of the index vector
+ */
+
+struct OFFSETINFO {
+    node *avis;
+    shape *shape;
+    offsetinfo *next;
+};
+
+struct IVINFO {
+    node *iv;
+    offsetinfo *offsets;
+    node *scalars;
+    ivinfo *next;
+};
+
+/**
+ * WITHIV macros
+ */
+#define WITHIV_IV(n) ((n)->iv)
+#define WITHIV_OFFSETS(n) ((n)->offsets)
+#define WITHIV_SCALARS(n) ((n)->scalars)
+#define WITHIV_NEXT(n) ((n)->next)
+#define WITHOFFSET_AVIS(n) ((n)->avis)
+#define WITHOFFSET_SHAPE(n) ((n)->shape)
+#define WITHOFFSET_NEXT(n) ((n)->next)
+
+/**
+ * WITHIV functions
+ */
+static offsetinfo *
+GenOffsetInfo (node *lhs, node *withops)
+{
+    offsetinfo *result;
+    offsetinfo *next;
+
+    DBUG_ENTER ("GenOffsetInfo");
+
+    if (lhs != NULL) {
+        DBUG_ASSERT ((withops != NULL), "# withops does not match # lhs ids");
+
+        next = GenOffsetInfo (IDS_NEXT (lhs), WITHOP_NEXT (withops));
+
+        if (TUshapeKnown (IDS_NTYPE (lhs))) {
+            result = ILIBmalloc (sizeof (offsetinfo));
+
+            WITHOFFSET_SHAPE (result) = TYgetShape (IDS_NTYPE (lhs));
+            WITHOFFSET_AVIS (result) = WITHOP_IDX (withops);
+            WITHOFFSET_NEXT (result) = next;
+        } else {
+            result = next;
+        }
+    } else {
+        result = NULL;
+    }
+
+    DBUG_RETURN (result);
+}
+
+static offsetinfo *
+FreeOffsetInfo (offsetinfo *info)
+{
+    DBUG_ENTER ("FreeOffsetInfo");
+
+    if (info != NULL) {
+        WITHOFFSET_NEXT (info) = FreeOffsetInfo (WITHOFFSET_NEXT (info));
+
+        info = ILIBfree (info);
+    }
+
+    DBUG_RETURN (info);
+}
+
+static ivinfo *
+PushIV (ivinfo *info, node *withid, node *lhs, node *withops)
+{
+    ivinfo *result;
+
+    DBUG_ENTER ("PushIV");
+
+    result = ILIBmalloc (sizeof (ivinfo));
+
+    WITHIV_IV (result) = IDS_AVIS (WITHID_VEC (withid));
+    WITHIV_OFFSETS (result) = GenOffsetInfo (lhs, withops);
+    WITHIV_SCALARS (result) = WITHID_IDS (withid);
+    WITHIV_NEXT (result) = info;
+
+    DBUG_RETURN (result);
+}
+
+static ivinfo *
+PopIV (ivinfo *info)
+{
+    ivinfo *result;
+
+    DBUG_ENTER ("PopIV");
+
+    DBUG_ASSERT ((info != NULL), "IVINFO stack already empty!");
+
+    result = WITHIV_NEXT (info);
+
+    WITHIV_OFFSETS (info) = FreeOffsetInfo (WITHIV_OFFSETS (info));
+
+    info = ILIBfree (info);
+
+    DBUG_RETURN (result);
+}
+
+static node *
+FindIVOffset (ivinfo *info, node *iv, shape *shape)
+{
+    node *result = NULL;
+    offsetinfo *oinfo;
+
+    DBUG_ENTER ("FindIVOffset");
+
+    while ((info != NULL) && (WITHIV_IV (info) != iv)) {
+        info = WITHIV_NEXT (info);
+    }
+
+    if (info != NULL) {
+        oinfo = WITHIV_OFFSETS (info);
+
+        while ((oinfo != NULL) && (!SHcompareShapes (shape, WITHOFFSET_SHAPE (oinfo)))) {
+            oinfo = WITHOFFSET_NEXT (oinfo);
+        }
+
+        if (oinfo != NULL) {
+            result = WITHOFFSET_AVIS (oinfo);
+        }
+    }
+
+    DBUG_RETURN (result);
+}
+
+static node *
+FindIVScalars (ivinfo *info, node *iv)
+{
+    node *result = NULL;
+
+    DBUG_ENTER ("FindIVScalars");
+
+    while ((info != NULL) && (WITHIV_IV (info) != iv)) {
+        info = WITHIV_NEXT (info);
+    }
+
+    if (info != NULL) {
+        result = WITHIV_SCALARS (info);
+    }
+
+    DBUG_RETURN (result);
+}
+
+/**
  * optimizer functions
  */
 static node *
 ReplaceByWithOffset (node *arg_node, info *arg_info)
 {
+    shape *shape;
+    node *offset;
+    node *scalars;
+
     DBUG_ENTER ("ReplaceByWithOffset");
+
+    shape = SHarray2Shape (PRF_ARG1 (arg_node));
+
+    offset = FindIVOffset (INFO_IVINFO (arg_info), ID_AVIS (PRF_ARG2 (arg_node)), shape);
+
+    shape = SHfreeShape (shape);
+
+    if (offset != NULL) {
+        arg_node = FREEdoFreeNode (arg_node);
+        arg_node = TBmakeId (offset);
+    } else {
+        scalars = FindIVScalars (INFO_IVINFO (arg_info), ID_AVIS (PRF_ARG2 (arg_node)));
+
+        if (scalars != NULL) {
+            offset
+              = TBmakePrf (F_idxs2offset, TBmakeExprs (DUPdoDupNode (PRF_ARG1 (arg_node)),
+                                                       TCids2Exprs (scalars)));
+            arg_node = FREEdoFreeNode (arg_node);
+            arg_node = offset;
+        }
+    }
 
     DBUG_RETURN (arg_node);
 }
@@ -103,6 +310,33 @@ static node *
 OptimizeComputation (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("OptimizeComputation");
+
+    DBUG_RETURN (arg_node);
+}
+
+node *
+IVEOlet (node *arg_node, info *arg_info)
+{
+    DBUG_ENTER ("IVEOlet");
+
+    INFO_LHS (arg_info) = LET_IDS (arg_node);
+
+    LET_EXPR (arg_node) = TRAVdo (LET_EXPR (arg_node), arg_info);
+
+    DBUG_RETURN (arg_node);
+}
+
+node *
+IVEOwith (node *arg_node, info *arg_info)
+{
+    DBUG_ENTER ("IVEOwith");
+
+    INFO_IVINFO (arg_info) = PushIV (INFO_IVINFO (arg_info), WITH_WITHID (arg_node),
+                                     INFO_LHS (arg_info), WITH_WITHOP (arg_node));
+
+    WITH_CODE (arg_node) = TRAVdo (WITH_CODE (arg_node), arg_info);
+
+    INFO_IVINFO (arg_info) = PopIV (INFO_IVINFO (arg_info));
 
     DBUG_RETURN (arg_node);
 }
--- src/psi-opt/index_optimize.h
+++ src/psi-opt/index_optimize.h
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.2  2005/09/15 11:08:52  sah
+ * now, wloffsets are used whenever possible
+ *
  * Revision 1.1  2005/09/14 21:26:38  sah
  * Initial revision
  *
@@ -11,6 +14,8 @@
 
 #include "types.h"
 
+extern node *IVEOlet (node *arg_node, info *arg_info);
+extern node *IVEOwith (node *arg_node, info *arg_info);
 extern node *IVEOprf (node *arg_node, info *arg_info);
 
 extern node *IVEOdoIndexVectorEliminationOptimisation (node *syntax_tree);
