8e5e7c711a8a07ffd61aa986254c354bbb7a3af0
Jan-Henrik Baumgarten <jhb@sac-home.org>
2005-11-07 14:02:54 +0000

    Check_mem is for space-leak check mechamisn

---
+++ src/tree/check_mem.c
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ */
+
+#include <stdlib.h>
+#include "internal_lib.h"
+
+typedef struct MEMOBJ {
+    int size;
+    void *ptr;
+    //  int subphase;
+    //  int traversal;
+    //  int type;
+    int bit;
+} memobj;
+
+memobj *memtab = NULL;
+
+int memfreeslots = 0;
+int memindex = 0;
+int memtabsize = 0;
+
+void
+CHKmalloc (int bsize, void *aptr)
+{
+
+    if (memindex == memtabsize) {
+
+        if (memtabsize == 0) {
+
+            memtab = (memobj *)malloc (100000 * sizeof (memobj));
+            memtabsize = 100000;
+            memfreeslots = 100000;
+        } else {
+
+            int newtabsize = memtabsize * 2;
+            int newindex = 0;
+            memobj *newtab = (memobj *)malloc (newtabsize * sizeof (memobj));
+
+            for (int i = 0; i < memtabsize; i++) {
+
+                if (memtab[i].ptr != NULL) {
+
+                    newtab[newindex] = memtab[i];
+                    memobj **tmpptr
+                      = (memobj **)(memtab[i].ptr - global.malloc_align_step);
+                    *tmpptr = &memtab[newindex];
+                    newindex++;
+                }
+
+                memtab = newtab;
+                memindex = newindex;
+                memfreeslots = newtabsize - newindex;
+                memtabsize = newtabsize;
+                free (memtab);
+            }
+        }
+    }
+
+    memtab[memindex].size = bsize;
+    memtab[memindex].ptr = aptr;
+    memtab[memindex].bit = 0;
+
+    memfreeslots = -1;
+    memindex = -1;
+}
+
+void *
+CHKfree (void *bptr)
+{
+
+    memtab[memindex].size = 0;
+    memtab[memindex].ptr = NULL;
+
+    memfreeslots = +1;
+    memindex = +1;
+}
