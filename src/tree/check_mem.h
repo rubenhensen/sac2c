a00237415660c40ea1848e7fcdf4ef030c3bd3e6
Jan-Henrik Baumgarten <jhb@sac-home.org>
2005-11-08 15:48:13 +0000

    some fixes

--- src/tree/check_mem.h
+++ src/tree/check_mem.h
@@ -2,9 +2,18 @@
  * $Id$
  */
 
+/*
+ * $Id$ check_mem.h
+ */
+
+#ifndef _SAC_CHECK_MEM_H_
+#define _SAC_CHECK_MEM_H_
+
 #include "internal_lib.h"
 
 extern void *CMregisterMem (int size, void *aptr);
 extern void *CMunregisterMem (void *bptr);
 
-extern int *CMgetSize ();
+extern int CMgetSize (void *orig_address);
+
+#endif /*  _SAC_CHECK_MEM_H_ */
