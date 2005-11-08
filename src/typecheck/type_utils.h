85e88bd56a0b0ce43804cf1293bb60ac0d6b72a4
Kai Trojahner <ktr@sac-home.org>
2005-11-08 11:15:24 +0000

    removed old logs

--- src/typecheck/type_utils.h
+++ src/typecheck/type_utils.h
@@ -1,54 +1,5 @@
 /*
- *
- * $Log$
- * Revision 1.15  2005/09/08 11:03:37  sbs
- * TUrebuildWrapperType added.
- *
- * Revision 1.14  2005/08/19 17:25:06  sbs
- * changed TUrettypes2alpha into TUrettypes2alphaFix, etc.
- *
- * Revision 1.13  2005/07/21 12:02:24  ktr
- * added TUdimKnown
- *
- * Revision 1.12  2005/07/16 19:06:08  sbs
- * TUshapeKnown added.
- *
- * Revision 1.11  2005/07/16 12:30:50  sbs
- * TUisIntVect added.
- *
- * Revision 1.10  2005/06/18 13:52:03  sah
- * moved SignatureMatches and ActualArgs2Ntype from
- * create_wrapper_code to type_utils
- *
- * Revision 1.9  2005/05/24 08:26:34  sbs
- * TUretypes2alpha modified.
- *
- * Revision 1.8  2004/12/09 12:32:27  sbs
- * TUargtypes2AUD and TYrettypes2AUD eliminated
- *
- * Revision 1.7  2004/12/07 14:36:16  sbs
- * added TUtypeSignature2String
- *
- * Revision 1.6  2004/12/05 19:19:55  sbs
- * return type of LaC funs changed into alphas.
- *
- * Revision 1.5  2004/11/26 22:58:51  sbs
- * some new utils added
- * \.
- *
- * Revision 1.4  2004/11/26 21:15:43  cg
- * Added TUisBoxed()
- *
- * Revision 1.3  2004/11/26 20:56:26  sbs
- * TUisHidden added
- *
- * Revision 1.2  2004/11/25 18:01:40  sbs
- * compiles
- *
- * Revision 1.1  2004/11/24 09:50:20  sbs
- * Initial revision
- *
- *
+ * $Id$
  */
 
 #ifndef _SAC_TYPE_UTILS_H_
