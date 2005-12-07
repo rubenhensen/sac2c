904875cfdfd46d2ff5910b3ffdc15023e8f82379
Stephan Herhut <sah@sac-home.org>
2005-12-07 13:56:05 +0000

    minor object brushing

--- src/objects/objinit.c
+++ src/objects/objinit.c
@@ -9,30 +9,25 @@
 #include "types.h"
 #include "tree_basic.h"
 #include "tree_compound.h"
-
 #include "traverse.h"
 #include "dbug.h"
 #include "internal_lib.h"
 #include "free.h"
 #include "new_types.h"
-#include "shape.h"
-
+#include "namespaces.h"
 #include "DupTree.h"
-#include "filemgr.h"
-
-#ifndef OBJI_DEACTIVATED
 
 /*
  * INFO structure
  */
 struct INFO {
-    node *modul;
+    node *module;
 };
 
 /*
  * INFO macros
  */
-#define INFO_OBJINIT_MODULE(n) (n->modul)
+#define INFO_MODULE(n) ((n)->module)
 
 /*
  * INFO functions
@@ -46,7 +41,7 @@ MakeInfo ()
 
     result = ILIBmalloc (sizeof (info));
 
-    INFO_OBJINIT_MODULE (result) = NULL;
+    INFO_MODULE (result) = NULL;
 
     DBUG_RETURN (result);
 }
@@ -62,23 +57,50 @@ FreeInfo (info *info)
 }
 
 /*
- *
- *  functionname  : OImodule
- *  arguments     : 1) pointer to N_modul node
- *                  2) arg_info unused
- *  description   : traverses all global object definitions
- *                  In class implementations, a typedef node for the
- *                  class type is generated as well as generic conversion
- *                  functions.
- *  global vars   : ---
- *  internal funs : ---
- *  external funs : TRAVdo, strcpy, strcat, MakeTypedef, MakeFundef,
- *                  Malloc
- *  macros        : ---
- *
- *  remarks       :
- *
+ * helper functions
  */
+
+static node *
+BuildTypeConversion (const char *name, const namespace_t *ns, ntype *from, ntype *to)
+{
+    node *result;
+    node *avisarg, *avisres;
+    node *assign;
+    node *block;
+
+    DBUG_ENTER ("BuildTypeConversion");
+
+    avisarg = TBmakeAvis (ILIBtmpVar (), TYcopyType (from));
+    avisres = TBmakeAvis (ILIBtmpVar (), TYcopyType (to));
+
+    /*
+     * return( res);
+     */
+    assign = TBmakeAssign (TBmakeReturn (TBmakeExprs (TBmakeId (avisres), NULL)), NULL);
+
+    /*
+     * res = type_conv( restype, arg);
+     */
+    assign
+      = TBmakeAssign (TBmakeLet (TBmakeIds (avisres, NULL),
+                                 TCmakePrf2 (F_type_conv, TBmakeType (TYcopyType (to)),
+                                             TBmakeId (avisarg))),
+                      assign);
+
+    /*
+     * create the fundef body block
+     */
+    block = TBmakeBlock (assign, TBmakeVardec (avisres, NULL));
+
+    /*
+     * create the fundef node
+     */
+    result = TBmakeFundef (ILIBstringCopy (name), NSdupNamespace (ns),
+                           TBmakeRet (TYcopyType (to), NULL), TBmakeArg (avisarg, NULL),
+                           block, NULL);
+
+    DBUG_RETURN (result);
+}
 
 node *
 OImodule (node *arg_node, info *arg_info)
@@ -87,91 +109,77 @@ OImodule (node *arg_node, info *arg_info)
 
     if (MODULE_OBJS (arg_node) != NULL) {
         /* pass on reference to N_modul node */
-        INFO_OBJINIT_MODULE (arg_info) = arg_node;
+        INFO_MODULE (arg_info) = arg_node;
 
         MODULE_OBJS (arg_node) = TRAVdo (MODULE_OBJS (arg_node), arg_info);
 
         /* reset to NULL (mainly for debugging) */
-        INFO_OBJINIT_MODULE (arg_info) = NULL;
+        INFO_MODULE (arg_info) = NULL;
     }
 
     if (MODULE_FILETYPE (arg_node) == F_classimp) {
-        char *toclass;
-        char *fromclass;
+#if 0
+    char *toclass;
+    char *fromclass;
 
-        MODULE_TYPES (arg_node) = TBmakeTypedef (ILIBstringCopy (MODULE_NAME (arg_node)),
-                                                 ILIBstringCopy (MODULE_NAME (arg_node)),
-                                                 TYcopyType (MODULE_CLASSTYPE (arg_node)),
-                                                 MODULE_TYPES (arg_node));
+    MODULE_TYPES(arg_node)
+      =TBmakeTypedef(ILIBstringCopy(MODULE_NAME(arg_node)),
+		     ILIBstringCopy(MODULE_NAME(arg_node)),
+		     TYcopyType(MODULE_CLASSTYPE(arg_node)),
+		     MODULE_TYPES(arg_node));
+    
+    toclass=(char*)ILIBmalloc(MAX_FILE_NAME);
+    fromclass=(char*)ILIBmalloc(MAX_FILE_NAME);
 
-        toclass = (char *)ILIBmalloc (MAX_FILE_NAME);
-        fromclass = (char *)ILIBmalloc (MAX_FILE_NAME);
+    strcpy(toclass, "to_");
+    strcpy(fromclass, "from_");
+    
+    strcat(toclass, MODULE_NAME(arg_node));
+    strcat(fromclass, MODULE_NAME(arg_node));
+    
+    MODULE_FUNDECS(arg_node)
+      =TBmakeFundef(toclass,
+		    ILIBstringCopy( MODULE_NAME( arg_node)),
 
-        strcpy (toclass, "to_");
-        strcpy (fromclass, "from_");
+		    TCreturnTypes2Ret( 
+		      TBmakeTypes(T_user, 0, NULL,
+			   ILIBstringCopy(MODULE_NAME(arg_node)),
+				  ILIBstringCopy(MODULE_NAME(arg_node)))),
 
-        strcat (toclass, MODULE_NAME (arg_node));
-        strcat (fromclass, MODULE_NAME (arg_node));
+		    TBmakeArg( TBmakeAvis( 
+				ILIBstringCopy( "obj"),
+				TYcopyType( MODULE_CLASSTYPE(arg_node))),
+			       NULL),
+		    NULL,
+		    MODULE_FUNDECS(arg_node));
+    
+    FUNDEF_ISCLASSCONVERSIONFUN( MODULE_FUNDECS( arg_node)) = TRUE;     
 
-        MODULE_FUNDECS (arg_node)
-          = TBmakeFundef (toclass, ILIBstringCopy (MODULE_NAME (arg_node)),
+    MODULE_FUNDECS(arg_node)
 
-                          TCreturnTypes2Ret (
-                            TBmakeTypes (T_user, 0, NULL,
-                                         ILIBstringCopy (MODULE_NAME (arg_node)),
-                                         ILIBstringCopy (MODULE_NAME (arg_node)))),
+      =TBmakeFundef(fromclass,
+		    ILIBstringCopy( MODULE_NAME( arg_node)),
 
-                          TBmakeArg (TBmakeAvis (ILIBstringCopy ("obj"),
-                                                 TYcopyType (
-                                                   MODULE_CLASSTYPE (arg_node))),
-                                     NULL),
-                          NULL, MODULE_FUNDECS (arg_node));
+		    TCreturnTypes2Ret( 
+		       TBmakeTypes(T_user, 0, NULL,
+			  ILIBstringCopy(MODULE_NAME(arg_node)),
+			  ILIBstringCopy(MODULE_NAME(arg_node)))),	      
 
-        FUNDEF_ISCLASSCONVERSIONFUN (MODULE_FUNDECS (arg_node)) = TRUE;
+		       TBmakeArg( TBmakeAvis( 
+				    ILIBstringCopy( "obj"),
+			            TYcopyType( MODULE_CLASSTYPE(arg_node))), 
+				  NULL),
+		    NULL,
+		    MODULE_FUNDECS(arg_node));
 
-        MODULE_FUNDECS (arg_node)
-
-          = TBmakeFundef (fromclass, ILIBstringCopy (MODULE_NAME (arg_node)),
-
-                          TCreturnTypes2Ret (
-                            TBmakeTypes (T_user, 0, NULL,
-                                         ILIBstringCopy (MODULE_NAME (arg_node)),
-                                         ILIBstringCopy (MODULE_NAME (arg_node)))),
-
-                          TBmakeArg (TBmakeAvis (ILIBstringCopy ("obj"),
-                                                 TYcopyType (
-                                                   MODULE_CLASSTYPE (arg_node))),
-                                     NULL),
-                          NULL, MODULE_FUNDECS (arg_node));
-
-        FUNDEF_ISCLASSCONVERSIONFUN (MODULE_FUNDECS (arg_node)) = TRUE;
-
-        MODULE_CLASSTYPE (arg_node) = NULL;
+    FUNDEF_ISCLASSCONVERSIONFUN( MODULE_FUNDECS( arg_node)) = TRUE;
+    
+    MODULE_CLASSTYPE(arg_node)=NULL;
+#endif
     }
 
     DBUG_RETURN (arg_node);
 }
-
-/*
- *
- *  functionname  : OIobjdef
- *  arguments     : 1) pointer to objdef node
- *                  2) INFO structure containing pointer to N_modul
- *                     node (head of syntax tree)
- *  description   : A new function definition is generated that has no
- *                  formal parameters, the return type is identical to
- *                  the type of the global object and the initialization
- *                  expression is moved to the return-statement of the
- *                  new function. In the objdef node it's replaced by
- *                  an application of the new function.
- *
- *                  This mechanism allows for arbitrary expression to
- *                  initialize global objects.
- *  external funs : Malloc, strlen, strcpy, strcat,
- *                  TBmakeType, TBmakeExprs, MakeReturn, MakeAssign, MakeBlock,
- *                  MakeFundef, TBmakeAp, TRAVdo
- *
- */
 
 node *
 OIobjdef (node *arg_node, info *arg_info)
@@ -181,39 +189,41 @@ OIobjdef (node *arg_node, info *arg_info)
     char *new_fun_name;
 
     DBUG_ENTER ("OIobjdef");
-    /*
-    if (OBJDEF_EXPR(arg_node)!=NULL) {
-      new_fun_type=TBmakeTypes(TYgetSimpleType(
-                                 TYgetScalar( OBJDEF_TYPE(arg_node))),
-                               TYgetDim( OBJDEF_TYPE(arg_node)),
-                               SHshape2OldShpseg(
-                                 TYgetShape( OBJDEF_TYPE(arg_node))),
-                               ILIBstringCopy(OBJDEF_TNAME(arg_node)),
-                               OBJDEF_TMOD(arg_node));
-    */ /* TODO wrong */
-    new_fun_name = (char *)ILIBmalloc (strlen (OBJDEF_NAME (arg_node)) + 10);
-
-    new_fun_name = strcpy (new_fun_name, "CREATE__");
-    new_fun_name = strcat (new_fun_name, OBJDEF_NAME (arg_node));
-
-    new_node = TBmakeExprs (OBJDEF_EXPR (arg_node), NULL);
-    NODE_LINE (new_node) = NODE_LINE (OBJDEF_EXPR (arg_node));
-
-    new_node = TBmakeReturn (new_node);
-    NODE_LINE (new_node) = NODE_LINE (OBJDEF_EXPR (arg_node));
-
-    new_node = TBmakeAssign (new_node, NULL);
-    NODE_LINE (new_node) = NODE_LINE (OBJDEF_EXPR (arg_node));
-
-    new_node = TBmakeBlock (new_node, NULL);
-    NODE_LINE (new_node) = NODE_LINE (OBJDEF_EXPR (arg_node));
-
-    new_node = TBmakeFundef (new_fun_name, OBJDEF_MOD (arg_node), new_fun_type, NULL,
-                             new_node, MODULE_FUNS (INFO_OBJINIT_MODULE (arg_info)));
-    NODE_LINE (new_node) = NODE_LINE (OBJDEF_EXPR (arg_node));
-
-    FUNDEF_STATUS (new_node) = ST_objinitfun;
-
+#if 0
+  /*
+  if (OBJDEF_EXPR(arg_node)!=NULL) {
+    new_fun_type=TBmakeTypes(TYgetSimpleType( 
+			       TYgetScalar( OBJDEF_TYPE(arg_node))),
+			     TYgetDim( OBJDEF_TYPE(arg_node)),
+			     SHshape2OldShpseg( 
+			       TYgetShape( OBJDEF_TYPE(arg_node))),
+			     ILIBstringCopy(OBJDEF_TNAME(arg_node)),
+			     OBJDEF_TMOD(arg_node));
+  */ /* TODO wrong */
+    new_fun_name=(char*)ILIBmalloc(strlen(OBJDEF_NAME(arg_node)) + 10);
+  
+    new_fun_name=strcpy(new_fun_name, "CREATE__");
+    new_fun_name=strcat(new_fun_name, OBJDEF_NAME(arg_node));
+    
+    new_node=TBmakeExprs(OBJDEF_EXPR(arg_node), NULL);
+    NODE_LINE(new_node)=NODE_LINE(OBJDEF_EXPR(arg_node));
+  
+    new_node=TBmakeReturn(new_node);
+    NODE_LINE(new_node)=NODE_LINE(OBJDEF_EXPR(arg_node));
+  
+    new_node=TBmakeAssign(new_node, NULL);
+    NODE_LINE(new_node)=NODE_LINE(OBJDEF_EXPR(arg_node));
+    
+    new_node=TBmakeBlock(new_node, NULL);
+    NODE_LINE(new_node)=NODE_LINE(OBJDEF_EXPR(arg_node));
+  
+    new_node=TBmakeFundef(new_fun_name, OBJDEF_MOD(arg_node),
+                        new_fun_type, NULL, new_node,
+                        MODULE_FUNS(INFO_MODULE(arg_info)));
+    NODE_LINE(new_node)=NODE_LINE(OBJDEF_EXPR(arg_node));
+ 
+    FUNDEF_STATUS(new_node)=ST_objinitfun;
+  
     /*
      * The new functions have status ST_objinitfun.
      * This tag is needed by the typechecker to ensure that these functions
@@ -221,88 +231,75 @@ OIobjdef (node *arg_node, info *arg_info)
      * a global object initialization of a SAC-program.
      */
 
-    MODULE_FUNS (INFO_OBJINIT_MODULE (arg_info)) = new_node;
+  
+    MODULE_FUNS(INFO_MODULE(arg_info))=new_node;
+  
+    new_node= TBmakeAp(ILIBstringCopy(new_fun_name), 
+		       ILIBstringCopy( OBJDEF_MOD(arg_node)), NULL);
+    OBJDEF_EXPR(arg_node)=new_node;
 
-    new_node = TBmakeAp (ILIBstringCopy (new_fun_name),
-                         ILIBstringCopy (OBJDEF_MOD (arg_node)), NULL);
-    OBJDEF_EXPR (arg_node) = new_node;
+    AP_FUNDEF(OBJDEF_EXPR(arg_node)) =
+      MODULE_FUNS(INFO_MODULE(arg_info));
+    
+  }
+  else {
+    new_fun_type=TBmakeTypes(OBJDEF_BASETYPE(arg_node),
+                          OBJDEF_DIM(arg_node),
+                          DUPdupShpseg(OBJDEF_SHPSEG(arg_node)),
+                          ILIBstringCopy(OBJDEF_TNAME(arg_node)),
+                          OBJDEF_TMOD(arg_node));
 
-    AP_FUNDEF (OBJDEF_EXPR (arg_node)) = MODULE_FUNS (INFO_OBJINIT_MODULE (arg_info));
-}
-else
-{
-    new_fun_type
-      = TBmakeTypes (OBJDEF_BASETYPE (arg_node), OBJDEF_DIM (arg_node),
-                     DUPdupShpseg (OBJDEF_SHPSEG (arg_node)),
-                     ILIBstringCopy (OBJDEF_TNAME (arg_node)), OBJDEF_TMOD (arg_node));
-
-    if (OBJDEF_INITFUN (arg_node) != NULL) {
-        new_fun_name = OBJDEF_INITFUN (arg_node);
-        PRAGMA_INITFUN (OBJDEF_PRAGMA (arg_node)) = NULL;
-    } else {
-        new_fun_name = (char *)ILIBmalloc (strlen (OBJDEF_NAME (arg_node)) + 10);
-
-        if (((sbs == 1) && (strcmp (OBJDEF_MOD (arg_node), EXTERN_MOD_NAME) != 0))
-            || ((sbs == 0) && (OBJDEF_MOD (arg_node) != NULL))) {
-            strcpy (new_fun_name, "CREATE__");
-        } else {
-            strcpy (new_fun_name, "create_");
-        }
-
-        strcat (new_fun_name, OBJDEF_NAME (arg_node));
+    if (OBJDEF_INITFUN(arg_node)!=NULL) {
+      new_fun_name=OBJDEF_INITFUN(arg_node);
+      PRAGMA_INITFUN(OBJDEF_PRAGMA(arg_node))=NULL;
     }
+    else {
+      new_fun_name=(char*)ILIBmalloc(strlen(OBJDEF_NAME(arg_node)) + 10);
+  
+      if (((sbs==1) && (strcmp( OBJDEF_MOD( arg_node), EXTERN_MOD_NAME) != 0)) ||
+          ((sbs==0) && (OBJDEF_MOD( arg_node) != NULL))) {
+        strcpy(new_fun_name, "CREATE__");
+      }
+      else {
+        strcpy(new_fun_name, "create_");
+      }
+      
+      strcat(new_fun_name, OBJDEF_NAME(arg_node));
+    }
+    
+    new_node=TBmakeFundef(new_fun_name, OBJDEF_MOD(arg_node),
+			  new_fun_type, NULL, NULL,
+			  MODULE_FUNDECS(INFO_MODULE(arg_info)));
+ 
+    FUNDEF_STATUS(new_node)=ST_objinitfun;
+  
+    MODULE_FUNDECS(INFO_MODULE(arg_info))=new_node;
 
-    new_node = TBmakeFundef (new_fun_name, OBJDEF_MOD (arg_node), new_fun_type, NULL,
-                             NULL, MODULE_FUNDECS (INFO_OBJINIT_MODULE (arg_info)));
+    new_node=TBmakeAp(ILIBstringCopy(new_fun_name), 
+		      OBJDEF_MOD(arg_node), NULL);
+    
+    OBJDEF_EXPR(arg_node)=new_node;
+    
+    AP_FUNDEF(OBJDEF_EXPR(arg_node)) = 
+      MODULE_FUNDECS(INFO_MODULE(arg_info));
+  }
 
-    FUNDEF_STATUS (new_node) = ST_objinitfun;
-
-    MODULE_FUNDECS (INFO_OBJINIT_MODULE (arg_info)) = new_node;
-
-    new_node = TBmakeAp (ILIBstringCopy (new_fun_name), OBJDEF_MOD (arg_node), NULL);
-
-    OBJDEF_EXPR (arg_node) = new_node;
-
-    AP_FUNDEF (OBJDEF_EXPR (arg_node)) = MODULE_FUNDECS (INFO_OBJINIT_MODULE (arg_info));
+  if ((OBJDEF_PRAGMA(arg_node)!=NULL) &&
+      (PRAGMA_LINKNAME( OBJDEF_PRAGMA(arg_node))==NULL)) {
+    OBJDEF_PRAGMA(arg_node)=FREEdoFreeNode(OBJDEF_PRAGMA(arg_node));
+  }
+  
+  if (OBJDEF_NEXT(arg_node)!=NULL) {
+    OBJDEF_NEXT(arg_node)=TRAVdo(OBJDEF_NEXT(arg_node), arg_info);
+  }
+#endif
+    DBUG_RETURN (arg_node);
 }
-
-if ((OBJDEF_PRAGMA (arg_node) != NULL)
-    && (PRAGMA_LINKNAME (OBJDEF_PRAGMA (arg_node)) == NULL)) {
-    OBJDEF_PRAGMA (arg_node) = FREEdoFreeNode (OBJDEF_PRAGMA (arg_node));
-}
-
-if (OBJDEF_NEXT (arg_node) != NULL) {
-    OBJDEF_NEXT (arg_node) = TRAVdo (OBJDEF_NEXT (arg_node), arg_info);
-}
-
-DBUG_RETURN (arg_node);
-}
-
-/*
- *
- *  functionname  : objinit
- *  arguments     : 1) syntax_tree (pointer to N_modul node)
- *  description   : starts the traversal mechanism for the objinit
- *                  compilation phase. Here, all initialization expressions
- *                  of global objects are moved to new generated functions
- *                  and replaced by applications of the respective functions.
- *  global vars   : act_tab, objinit_tab
- *  internal funs : ---
- *  external funs : TRAVdo
- *  macros        :
- *
- *  remarks       :
- *
- */
-
-#endif /* OBJI_DEACTIVATED */
 
 node *
-objinit (node *syntax_tree)
+OIdoObjectInit (node *syntax_tree)
 {
-    DBUG_ENTER ("objinit");
-
-#ifndef OBJI_DEACTIVATED
+    DBUG_ENTER ("OIdoObjectInit");
 
     info *info;
 
@@ -315,8 +312,6 @@ objinit (node *syntax_tree)
     TRAVpop ();
 
     info = FreeInfo (info);
-
-#endif /* OBJI_DEACTIVATED */
 
     DBUG_RETURN (syntax_tree);
 }
--- src/objects/objinit.h
+++ src/objects/objinit.h
@@ -1,30 +1,5 @@
 /*
- *
- * $Log$
- * Revision 3.4  2004/11/26 21:00:35  jhb
- * compile
- *
- * Revision 3.3  2004/11/22 17:53:27  ktr
- * SacDevCamp 2004 Get Ready for Rumble!
- *
- * Revision 3.2  2004/07/17 14:30:09  sah
- * switch to INFO structure
- * PHASE I
- *
- * Revision 3.1  2000/11/20 18:02:01  sacbase
- * new release made
- *
- * Revision 2.1  1999/02/23 12:43:24  sacbase
- * new release made
- *
- * Revision 1.2  1995/10/31 17:19:07  cg
- * Now, multiple includes of this header file are avoided.
- *
- * Revision 1.1  1995/10/16  12:22:44  cg
- * Initial revision
- *
- *
- *
+ * $Id$
  */
 
 #ifndef _SAC_OBJINIT_H_
@@ -39,12 +14,9 @@
  * Prefix: OI
  *
  *****************************************************************************/
-extern node *OIdoObjInit (node *syntax_tree);
-
-#ifndef OBJI_DEACTIVATED
+extern node *OIdoObjectInit (node *syntax_tree);
 
 extern node *OImodule (node *arg_node, info *arg_info);
 extern node *OIobjdef (node *arg_node, info *arg_info);
 
-#endif /* OBJI_DEACTIVATED */
 #endif /* _SAC_OBJINIT_H_  */
