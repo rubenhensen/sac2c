fc3beed37e698a41a329633f2e4692f8c9ac9489
Jan-Henrik Baumgarten <jhb@sac-home.org>
2005-12-14 14:36:08 +0000

    fixes

--- src/tree/check_mem.h
+++ src/tree/check_mem.h
@@ -15,5 +15,6 @@ extern void *CMregisterMem (int size, void *aptr);
 extern void *CMunregisterMem (void *bptr);
 
 extern int CMgetSize (void *orig_address);
+extern void CMsetNodyType (node *bptr, nodetype newnodetype);
 
 #endif /*  _SAC_CHECK_MEM_H_ */
