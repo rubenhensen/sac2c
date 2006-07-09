294a802c792d2950b25cb29b4baf5246456f6081
Stephan Herhut <sah@sac-home.org>
2006-07-09 16:43:19 +0000

    _MAIN:INIT::init is no more built if there are no objects to initialise.

--- src/objects/generate_object_initialiser.c
+++ src/objects/generate_object_initialiser.c
@@ -310,7 +310,11 @@ GOImodule (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("GOImodule");
 
-    if (MODULE_FILETYPE (arg_node) == F_prog) {
+    /*
+     * we only generate object initialisers in programs
+     * that make use of objects!
+     */
+    if ((MODULE_FILETYPE (arg_node) == F_prog) && (MODULE_OBJS (arg_node) != NULL)) {
         INFO_NS (arg_info) = MODULE_NAMESPACE (arg_node);
 
         if (MODULE_FUNS (arg_node) != NULL) {
