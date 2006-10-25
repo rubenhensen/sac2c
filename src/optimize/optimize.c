2cdc152d177471ac4ee1215da6f885ae5fb72b5b
Stephan Herhut <sah@sac-home.org>
2006-10-25 23:02:03 +0000

    macosx is now darwin

--- src/optimize/optimize.c
+++ src/optimize/optimize.c
@@ -390,6 +390,14 @@ OPTdoOptimize (node *arg_node)
             TRAVsetPreFun (TR_prt, IVEIprintPreFun);
             arg_node = PHrunCompilerSubPhase (SUBPH_ivei, arg_node);
             arg_node = PHrunCompilerSubPhase (SUBPH_ive, arg_node);
+
+            /*
+             * Loop Invariant Removal
+             */
+            if (global.optimize.dolir) {
+                arg_node = PHrunCompilerSubPhase (SUBPH_lirive, arg_node);
+            }
+
             arg_node = PHrunCompilerSubPhase (SUBPH_iveo, arg_node);
             TRAVsetPreFun (TR_prt, NULL);
 
