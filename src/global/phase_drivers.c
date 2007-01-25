a5e6284b471f952f1128ed04c0515397cde9c509
Clemens Grelck <cg@sac-home.org>
2007-01-25 12:38:24 +0000

    Adapted multithreading phase(s) for automated phase drivers.

--- src/global/phase_drivers.c
+++ src/global/phase_drivers.c
@@ -183,7 +183,7 @@ PHDdriveOptimization (node *syntax_tree)
     syntax_tree = TSdoPrintTypeStatistics (syntax_tree);
 
     if (global.doprofile) {
-        syntax_tree = PFdoProfileFunCalls (syntax_tree); /* profile_tab */
+        syntax_tree = PFdoProfileFunCalls (syntax_tree);
     }
 
     DBUG_RETURN (syntax_tree);
@@ -217,121 +217,51 @@ PHDdriveMultithreading (node *syntax_tree)
 {
     DBUG_ENTER ("PHDdriveMultithreading");
 
-    switch (global.mtmode) {
-    case MT_none:
-        CTIstate ("  Multithreaded execution deactivated");
-        break;
-
-    case MT_createjoin:
-    case MT_startstop:
-        if (global.mtmode == MT_createjoin) {
-            CTIstate ("  Using create-join version of multithreading (MT1)");
-        } else {
-            CTIstate ("  Using start-stop version of multithreading (MT2)");
-        }
-
 #ifndef BEMT
 
-        /*
-         * Init SPMD blocks around with-loops
-         */
+    if ((global.mtmode == MT_createjoin) || (global.mtmode == MT_startstop)) {
+
         syntax_tree = PHrunCompilerSubPhase (SUBPH_spmdinit, syntax_tree);
 
-        /*
-         * Create MT-funs for exported and provided functions in modules
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_createmtfuns, syntax_tree);
 
-        /*
-         * Lift SPMD blocks to SPMD functions
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_spmdlift, syntax_tree);
 
-        /*
-         * Remove scheduling information outside of SPMD functions AND
-         * give each segment specification within an SPMD function scheduling
-         * specifications.
-         * These are either infered from the context or extracted from
-         * wlcomp pragma information.
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_sched, syntax_tree);
-
-        /*
-         * Replace SPMD blocks with explicit conditionals
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_rmspmd, syntax_tree);
 
-        /*
-         * Lift SPMD conditionals to special fundefs
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_conclac2fun, syntax_tree);
 
-        /*
-         * Establish SSA form in SPMD conditional functions
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_concssa, syntax_tree);
+    }
+
 #endif
-        break;
 
 #ifndef PRODUCTION
-    case MT_mtstblock:
-        CTIstate ("  Using mt/st-block version of multithreading (MT3)");
-        global.executionmodes_available = TRUE;
+    if (global.mtmode == MT_mtstblock) {
 
-        /*
-         * Tagging execution modes
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_tem, syntax_tree);
 
-        /*
-         * Create with in with
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_crwiw, syntax_tree);
 
-        /*
-         * Propagate execution modes
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_pem, syntax_tree);
 
-        /*
-         * Create data flow graph
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_cdfg, syntax_tree);
 
-        /*
-         * Rearrange assignments
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_asmra, syntax_tree);
 
-        /*
-         * Create cells
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_crece, syntax_tree);
 
-        /*
-         * Cell growth
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_cegro, syntax_tree);
 
-        /*
-         * Replicate functions
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_repfun, syntax_tree);
 
-        /*
-         * Consolidate execution mode cells
-         */
         syntax_tree = PHrunCompilerSubPhase (SUBPH_concel, syntax_tree);
 
-        global.executionmodes_available = FALSE;
-        CTIabort ("MT mode 3 cannot be compiled any further!");
-        break;
-#endif
-
-    default:
-        CTIabort ("Illegal multithreading mode!");
-        break;
+        syntax_tree = PHrunCompilerSubPhase (SUBPH_abort, syntax_tree);
     }
+
+#endif
 
     DBUG_RETURN (syntax_tree);
 }
@@ -423,10 +353,6 @@ PHDdriveMemoryManagement (node *syntax_tree)
 node *
 PHDdrivePrecompilation (node *syntax_tree)
 {
-    /*
-     * Needs adaptation !!
-     */
-
     DBUG_ENTER ("PHDdrivePrecompilation");
 
     /*
@@ -489,12 +415,6 @@ PHDdrivePrecompilation (node *syntax_tree)
      * Resolve Code Sharing in With-Loops
      */
     syntax_tree = PHrunCompilerSubPhase (SUBPH_rcs, syntax_tree);
-
-    /*
-     * WARNING: no phases that duplicate code below this line!
-     *          FPC builds the argtabs which CANNOT be maintained
-     *          by duptree!
-     */
 
     /*
      * Function precompilation
