02558c14acb95c60757de9119f4a25190ee45bcc
Sven-Bodo Scholz <sbs@sac-home.org>
2007-06-22 13:23:08 +0000

    finalized documentation.

--- src/libsac2c/tree/pattern_match.c
+++ src/libsac2c/tree/pattern_match.c
@@ -7,6 +7,120 @@
  *
  * @file pattern_match.c
  *
+ * Overview:
+ * =========
+ *
+ * This module implements some basic functionality for writing
+ * patten-matching-style expression analyses.
+ *
+ * A few examples:
+ * a) we want to match expressions of the form _sub_SxS_( x, x)
+ *    where both x's denote the same variable.
+ *    Using this module, we can analyse a given expression "expr" by
+ *
+ *    node *x=NULL;
+ *
+ *    if(  PM( PMvar( &x, PMvar( &x, PMprf( _sub_SxS_, expr))))  ) {
+ *      ...
+ *    }
+ *
+ *    If the match is sucessfull, "x" will point to the variable
+ *    that matched.
+ *    Note here, that we assume flattened SSA form, i.e., it will
+ *    trace variable definitions as far as possible and necessary.
+ *    Therefore, the above code returns true even if "expr" points to
+ *    some variable "a" within a context of the form:
+ *
+ *    k = ....
+ *    ...
+ *    b = _sub_SxS_( k, k);
+ *    ...
+ *    a = b;
+ *
+ * b) we want to match a more complex constellation of the
+ *    form  _sel_VxA_( iv, _modarray_AxVxS_( A, iv, val))
+ *    where both iv's again denote the same variable.
+ *    This can be matched by:
+ *
+ *    node *iv=NULL:
+ *    node *A=NULL:
+ *    node *val=NULL:
+ *
+ *    if( PM( PMvar( &val, PMvar( &iv, PMvar( &A, PMprf( F_modarray_AxVxS,
+ *             PMvar( &iv, PMprf( F_sel_VxA, expr)))))))  ) {
+ *       ...
+ *    }
+ *
+ *    Again, upon sucessfull match, iv, A, and val will point to
+ *    the respective identifiers. A matching context for a variable "a"
+ *    would be:
+ *
+ *    B = _modarray_AxVxS_( A, iv, val);
+ *    ....
+ *    a = _sel_VxA_( iv, B);
+ *
+ *
+ * How to use it:
+ * ==============
+ *
+ * You can use an arbitrary nesting of the matching functions. They
+ * all adhere to the following structure:
+ *
+ *  node *PM<xyz>( <match-specific-args>, node *stack)
+ *
+ * The argument stack expects the expression to be matched against.
+ * This can either be an expression node (N_num, N_array, N_prf, ...)
+ * or even an expression chain (N_exprs). The remaining expressions are
+ * returned so that they can be passed on to the next matching function.
+ * Matching on "constructors" such as N_prf, N_ap, or N_array extends
+ * these chains. NB: this is done internally by using N_set nodes so that
+ * the AST remains untouched!
+ *
+ * At the time being (22.6.07) we support the following matching functions:
+ *
+ *  PMvar( node **var, ...) which matches N-id nodes, and
+ *  PMprf( prf fun, ...)    which matches anything that is defined as
+ *                          an application of the prf fun.
+ *
+ * Note here, that all variables that you want to match need to be
+ * initialised to NULL prior to matching!
+ *
+ * The nesting of match functions needs to be surrounded by a call to
+ * the function
+ *   bool PM( node *stack)
+ * which interprets the result of the match and yields true or false
+ * accordingly.
+ *
+ * Debugging:
+ * ==========
+ *
+ * You may want to use -#d,PM to observe how your matches perform:-)
+ *
+ *
+ * Some implementation issues:
+ * ===========================
+ *
+ * Their are two main difficulties in this implementation:
+ * a) how to maintain a stack of expressions WITHOUT fiddeling
+ *    around with the existing exprs chains in the AST?
+ * b) how to return a modified stack AND a boolean matching flag
+ *    without using a global variable and an "out-argument"?
+ *
+ * We solve a) by introducing a spine of N_set-nodes whenever
+ * more than one expr or exprs chain is needed in the stack.
+ * This is done dynamically by inserting these whenever we hit
+ * a constructor-match (PushArgs) and by freeing these when pulling
+ * out the last topmost expression of an N_set node (ExtractOneArg).
+ * If the match goes through we expect all exprs to be consumed
+ * and, therefore, all N_set nodes to be freed.
+ * In case the match fails, FailMatch( stack)  is called which
+ * explicitly frees any remaining N_set nodes.
+ *
+ * b) is solved by creating a special static "node" FAIL which we
+ * return instead of a proper stack whenever a match failed. All
+ * matching functions therefore need to check agains a FAIL as stack
+ * and they need to pass-on that FAIL.
+ *
  */
 
 #include "dbug.h"
