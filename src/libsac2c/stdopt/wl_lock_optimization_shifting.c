448b530f934a2081864994c31501dd54ab9a413f
Torben Gerhards <info@sac-home.org>
2007-06-29 16:25:01 +0000

    Corrected withloop lock-optimization shifting.

--- src/libsac2c/stdopt/wl_lock_optimization_shifting.c
+++ src/libsac2c/stdopt/wl_lock_optimization_shifting.c
@@ -25,10 +25,10 @@ struct INFO {
     bool is_prop_obj;
     bool is_prop_obj_in;
     bool is_prop_obj_out;
+    bool found_lock;
     bool wb;
     node *before_lock;
     node *behind_unlock;
-    node *last_assign;
     bool insert_chain_bl;
 };
 
@@ -39,10 +39,10 @@ struct INFO {
 #define INFO_IS_PROP_OBJ(n) ((n)->is_prop_obj)
 #define INFO_IS_PROP_OBJ_IN(n) ((n)->is_prop_obj_in)
 #define INFO_IS_PROP_OBJ_OUT(n) ((n)->is_prop_obj_out)
+#define INFO_FOUND_LOCK(n) ((n)->found_lock)
 #define INFO_WB(n) ((n)->wb)
 #define INFO_BEFORE_LOCK(n) ((n)->before_lock)
 #define INFO_BEHIND_UNLOCK(n) ((n)->behind_unlock)
-#define INFO_LAST_ASSIGN(n) ((n)->last_assign)
 #define INFO_INSERT_CHAIN_BL(n) ((n)->insert_chain_bl)
 
 /**
@@ -61,10 +61,10 @@ MakeInfo ()
     INFO_IS_PROP_OBJ (result) = FALSE;
     INFO_IS_PROP_OBJ_IN (result) = FALSE;
     INFO_IS_PROP_OBJ_OUT (result) = FALSE;
+    INFO_FOUND_LOCK (result) = FALSE;
     INFO_WB (result) = FALSE;
     INFO_BEFORE_LOCK (result) = NULL;
     INFO_BEHIND_UNLOCK (result) = NULL;
-    INFO_LAST_ASSIGN (result) = NULL;
 
     DBUG_RETURN (result);
 }
@@ -126,6 +126,12 @@ WLLOSassign (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("WLLOSassign");
 
+    /*If I shift the current node, I have to return the next and if I shift
+     * the current node I have to set the ->next to NULL, so I have to keep
+     * the next somewhere*/
+    node *ret_node = arg_node;
+    node *next_node = ASSIGN_NEXT (arg_node);
+
     if (INFO_WLLEVEL (arg_info) == 1) {
         /*ask the question if assign is a prop_obj_?*/
         INFO_IS_PROP_OBJ (arg_info) = TRUE;
@@ -141,6 +147,8 @@ WLLOSassign (node *arg_node, info *arg_info)
         if (INFO_IS_PROP_OBJ_OUT (arg_info) == TRUE) {
             INFO_WB (arg_info) = TRUE;
 
+            DBUG_PRINT ("WLLOS", ("Insert BEHIND-CHAIN"));
+
             INFO_BEHIND_UNLOCK (arg_info)
               = TCappendAssign (INFO_BEHIND_UNLOCK (arg_info), ASSIGN_NEXT (arg_node));
             ASSIGN_NEXT (arg_node) = INFO_BEHIND_UNLOCK (arg_info);
@@ -148,65 +156,71 @@ WLLOSassign (node *arg_node, info *arg_info)
 
             INFO_IS_PROP_OBJ_OUT (arg_info) = FALSE;
         } else { /*Some assign, no prop_obj_out*/
-            /*keep prop_obj_in so we dont have to traverse into all the lets and
-             *prf again at the end*/
-            bool old_answer = INFO_IS_PROP_OBJ_IN (arg_info);
-            INFO_IS_PROP_OBJ_IN (arg_info) = FALSE;
+            bool old_answer = FALSE;
+            if (INFO_IS_PROP_OBJ_IN (arg_info) == TRUE) {
 
-            if (ASSIGN_NUP (arg_node) == FALSE) { /*Assign can be moved up*/
-                DBUG_PRINT ("WLLOS", ("^^^Insert Assignment %s in above-chain",
-                                      ASSIGN_NAME (arg_node)));
-                if (INFO_LAST_ASSIGN (arg_info) != NULL) {
-                    ASSIGN_NEXT (INFO_LAST_ASSIGN (arg_info)) = ASSIGN_NEXT (arg_node);
-                }
-                INFO_BEFORE_LOCK (arg_info)
-                  = TCappendAssign (INFO_BEFORE_LOCK (arg_info), arg_node);
-                DBUG_PRINT ("WLLOS", ("Assignment inserted..."));
-                if (ASSIGN_NEXT (arg_node) != NULL) {
-                    ASSIGN_NEXT (arg_node) = TRAVdo (ASSIGN_NEXT (arg_node), arg_info);
-                }
-                ASSIGN_NEXT (arg_node) = NULL;
-            } else if (ASSIGN_NDOWN (arg_node) == FALSE) { /*Assign can be moved down*/
-                DBUG_PRINT ("WLLOS", ("vvvInsert Assignment %s in above-chain",
-                                      ASSIGN_NAME (arg_node)));
-                if (INFO_LAST_ASSIGN (arg_info) != NULL) {
-                    ASSIGN_NEXT (INFO_LAST_ASSIGN (arg_info)) = ASSIGN_NEXT (arg_node);
-                }
-                INFO_BEHIND_UNLOCK (arg_info)
-                  = TCappendAssign (INFO_BEHIND_UNLOCK (arg_info), arg_node);
-                DBUG_PRINT ("WLLOS", ("Assignment inserted..."));
-                if (ASSIGN_NEXT (arg_node) != NULL) {
-                    ASSIGN_NEXT (arg_node) = TRAVdo (ASSIGN_NEXT (arg_node), arg_info);
-                }
-                ASSIGN_NEXT (arg_node) = NULL;
-            } else { /*Assignment cant be moved up nor down*/
-                INFO_LAST_ASSIGN (arg_info) = arg_node;
+                /* If a prop_obj_in is found, there is a lock and therefore there
+                 * are maybe assignments to shift. You also could choose to test
+                 * wether there is a propagate() or not but if somewhen the locks
+                 * will be splitted from the prop_obj it will be easier to change
+                 * this way*/
+                INFO_FOUND_LOCK (arg_info) = TRUE;
+
+                /* keep that this is prop_obj_in for the insertion of the chain
+                 * at the backtraversal part
+                 **/
+                old_answer = INFO_IS_PROP_OBJ_IN (arg_info);
+                INFO_IS_PROP_OBJ_IN (arg_info) = FALSE;
+
+                DBUG_ASSERT (ASSIGN_NEXT (arg_node) != NULL,
+                             "There should be at least a prop_obj_out!");
                 ASSIGN_NEXT (arg_node) = TRAVdo (ASSIGN_NEXT (arg_node), arg_info);
+            } else if ((INFO_FOUND_LOCK (arg_info) == TRUE)
+                       && ((ASSIGN_NUP (arg_node) == FALSE)
+                           || (ASSIGN_NDOWN (arg_node) == FALSE))) {
+
+                next_node = ASSIGN_NEXT (arg_node);
+                ASSIGN_NEXT (arg_node) = NULL;
+
+                if (ASSIGN_NUP (arg_node) == FALSE) {
+                    DBUG_PRINT ("WLLOS", ("^^^Insert %s", ASSIGN_NAME (arg_node)));
+                    INFO_BEFORE_LOCK (arg_info)
+                      = TCappendAssign (INFO_BEFORE_LOCK (arg_info), arg_node);
+                } else {
+                    DBUG_PRINT ("WLLOS", ("vvvInsert %s", ASSIGN_NAME (arg_node)));
+                    INFO_BEHIND_UNLOCK (arg_info)
+                      = TCappendAssign (INFO_BEHIND_UNLOCK (arg_info), arg_node);
+                }
+
+                ret_node = TRAVdo (next_node, arg_info);
+            } else { /*Assignment cant be moved up nor down*/
+                if (ASSIGN_NEXT (arg_node) != NULL) {
+                    ASSIGN_NEXT (arg_node) = TRAVdo (ASSIGN_NEXT (arg_node), arg_info);
+                }
             }
             /*Here starts the backwards traversal*/
             INFO_IS_PROP_OBJ_IN (arg_info) = old_answer;
         }
 
-        /*If we are in prop_obj_in, append it and insert it ohne step above*/
+        /*If we are in prop_obj_in, append it and insert it one step above*/
         if (INFO_IS_PROP_OBJ_IN (arg_info) == TRUE) {
             INFO_BEFORE_LOCK (arg_info)
               = TCappendAssign (INFO_BEFORE_LOCK (arg_info), arg_node);
             INFO_INSERT_CHAIN_BL (arg_info) = TRUE;
             INFO_IS_PROP_OBJ_IN (arg_info) = FALSE;
+            INFO_FOUND_LOCK (arg_info) = FALSE;
         } /*Else if we are in the assignment right above the lock, insert chain*/
         else if (INFO_INSERT_CHAIN_BL (arg_info) == TRUE) {
+            DBUG_PRINT ("WLLOS", ("Insert ABOVE-Chain (ASSIGN)"));
             ASSIGN_NEXT (arg_node) = INFO_BEFORE_LOCK (arg_info);
             INFO_BEFORE_LOCK (arg_info) = NULL;
             INFO_INSERT_CHAIN_BL (arg_info) = FALSE;
         }
     } else if (INFO_WLLEVEL (arg_info) == 0) {
-        DBUG_PRINT ("WLLOS", ("Do TRAVcont..."));
-        DBUG_ASSERT (ASSIGN_INSTR (arg_node) != NULL, "THERE SHOULD BE AN ASSIGN_INSTR!");
         arg_node = TRAVcont (arg_node, arg_info);
-        DBUG_PRINT ("WLLOS", ("Return from TRAVcont..."));
     }
 
-    DBUG_RETURN (arg_node);
+    DBUG_RETURN (ret_node);
 }
 
 /** <!-- ****************************************************************** -->
@@ -228,6 +242,7 @@ WLLOSblock (node *arg_node, info *arg_info)
         BLOCK_INSTR (arg_node) = TRAVdo (BLOCK_INSTR (arg_node), arg_info);
 
         if (INFO_INSERT_CHAIN_BL (arg_info) == TRUE) {
+            DBUG_PRINT ("WLLOS", ("Insert ABOVE-Chain (BLOCK)"));
             BLOCK_INSTR (arg_node) = INFO_BEFORE_LOCK (arg_info);
             INFO_BEFORE_LOCK (arg_info) = NULL;
             INFO_INSERT_CHAIN_BL (arg_info) = FALSE;
@@ -280,21 +295,16 @@ WLLOSdoLockOptimizationShifting (node *syntax_tree)
 
     DBUG_ENTER ("WLLOSdoLockOptimizationShifting");
 
-    DBUG_PRINT ("WLLOS", ("START TRAVERSAL"));
-
     DBUG_ASSERT ((NODE_TYPE (syntax_tree) == N_module),
                  "WLLOSdoLockOptimizationShifting is intended to run on the entire tree");
 
     info = MakeInfo ();
     TRAVpush (TR_wllos);
-    DBUG_PRINT ("WLLOS", ("Enter Traversal"));
     syntax_tree = TRAVdo (syntax_tree, info);
-    DBUG_PRINT ("WLLOS", ("Left Traversal"));
 
     TRAVpop ();
 
     info = FreeInfo (info);
-    DBUG_PRINT ("WLLOS", ("TRAVERSAL DONE"));
 
     DBUG_RETURN (syntax_tree);
 }
