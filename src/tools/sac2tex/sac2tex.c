7148196e40b2b8b86b05d5b28854a56928e5d5d0
Sven-Bodo Scholz <sbs@sac-home.org>
2008-04-21 16:43:09 +0000

    added sac2tex.

---
+++ src/tools/sac2tex/sac2tex.c
@@ -0,0 +1,20 @@
+/*
+ *
+ * $Id:$
+ *
+ */
+
+#include "sactools.h"
+
+#define LIBRARY "/lib/libsac2c.so"
+#define MAINFUN "SACrunSac2tex"
+
+int
+main (int argc, char *argv[])
+{
+    int result;
+
+    LAUNCHFUNCTIONFROMLIB (LIBRARY, MAINFUN, argc, argv, result);
+
+    return (result);
+}
