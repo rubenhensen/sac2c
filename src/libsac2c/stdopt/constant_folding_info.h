109bd826a1ec63d1d3e825eef4c7ff61460182e2
Robert Bernecky <rbe@sac-home.org>
2008-06-08 19:24:26 +0000

    Move booleans down for alignment cleanliness

--- src/libsac2c/stdopt/constant_folding_info.h
+++ src/libsac2c/stdopt/constant_folding_info.h
@@ -9,19 +9,19 @@
  *
  *****************************************************************************/
 struct INFO {
-    bool remassign;
-    bool onefundef;
     node *fundef;
     node *preassign;
     node *postassign;
     node *vardecs;
     node *topblock;
+    bool remassign;
+    bool onefundef;
 };
 
-#define INFO_REMASSIGN(n) (n->remassign)
-#define INFO_ONEFUNDEF(n) (n->onefundef)
 #define INFO_FUNDEF(n) (n->fundef)
 #define INFO_PREASSIGN(n) (n->preassign)
 #define INFO_POSTASSIGN(n) (n->postassign)
 #define INFO_VARDECS(n) (n->vardecs)
 #define INFO_TOPBLOCK(n) (n->topblock)
+#define INFO_REMASSIGN(n) (n->remassign)
+#define INFO_ONEFUNDEF(n) (n->onefundef)
