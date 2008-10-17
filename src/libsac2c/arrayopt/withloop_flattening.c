31b2b22cd139f22db5466fc9f94a2b5e8fd08f36
Sven-Bodo Scholz <sbs@sac-home.org>
2008-10-17 17:37:06 +0000

    .

--- src/libsac2c/arrayopt/withloop_flattening.c
+++ src/libsac2c/arrayopt/withloop_flattening.c
@@ -6,9 +6,49 @@
  *
  * @defgroup wlflt Withloop Flattening Traversal
  *
- * Module description goes here.
+ * This optimization is supposed to turn higher-dimensional
+ * WLs into 1-dimensional ones whenever this can be done easily.
  *
- * For an example, take a look at src/refcount/explicitcopy.c
+ * Currently, we are looking for the following code pattern:
+ *
+ * a0, ..., am = with {
+ *                ( lb <= iv = [ i0,..., in] < shp) {
+ *                   <assignments>
+ *                } : (val0, ..., valm);
+ *              } : ( genarray( shp, def0),
+ *                    ...
+ *                    genarray( shp, defm) );
+ *
+ * where - neither iv nor i0...in are being referenced in <assignments>
+ *         or val0, ..., valm
+ *       - lb is a constant consisting of 0's only
+ *
+ * we then replace the definition of a0, ...., am by:
+ *
+ * lb' = [0];
+ * shp' = sacprelude::prod( shp);
+ * a0', ..., am' = with {
+ *                   ( lb' <= iv' = [ i0' ] < shp') {
+ *                      <assignments>
+ *                   } : (val0, ..., valm);
+ *                 } : ( genarray( shp', def0),
+ *                       ...
+ *                       genarray( shp', defm) );
+ * a0 = _reshape_VxA_( shp, a0');
+ * ...
+ * am = _reshape_VxA_( shp, am');
+ *
+ * and we need to insert new variable declarations:
+ *
+ * <basetype(a)>[.] a0';
+ * ...
+ * <basetype(a)>[.] am';
+ * int[1]{0} lb';
+ * int[1] shp';
+ * int[1] iv';
+ * int i0';
+ *
+ *
  *
  * @ingroup wlflt
  *
@@ -30,9 +70,12 @@
  */
 #include "dbug.h"
 #include "traverse.h"
+#include "compare_tree.h"
+#include "free.h"
 #include "tree_basic.h"
 #include "memory.h"
 #include "new_types.h"
+#include "shape.h"
 #include "constants.h"
 #include "tree_compound.h"
 
@@ -41,12 +84,27 @@
  * @name INFO structure
  * @{
  *
+ *     GENARRAYS : counts the number of N_genarray operators of the
+ *                 current WL ( if any) NB: it does ONLY count those
+ *                 N_genarray nodes whose shapes are identical to the
+ *                 shape of the first N_genarray found.
+ *     SHAPE     : points to the N_avis of the shape descrition used
+ *                 in the first N_genarray found.
+ *     IDSUSED   : communicates from WLFLTwithid to WLFLTwith how many
+ *                 WL identifiers are being used.
+ *     ISFULLPARTITION : communicates from WLFLTgenerator to WLFLTwith
+ *                       whether *all* partitions are full.
+ *
+ *     VARDECS   : accumulates vardec chain created on the fly
+ *     PREASSIGN : accumulates assign chain created on the fly
  *****************************************************************************/
 struct INFO {
     int genarrays;
     node *shape;
     int idsused;
     bool isfullpartition;
+    node *vardecs;
+    node *assigns;
 };
 
 /**
@@ -56,6 +114,8 @@ struct INFO {
 #define INFO_SHAPE(n) ((n)->shape)
 #define INFO_IDSUSED(n) ((n)->idsused)
 #define INFO_ISFULLPARTITION(n) ((n)->isfullpartition)
+#define INFO_VARDECS(n) ((n)->vardecs)
+#define INFO_PREASSIGN(n) ((n)->assigns)
 
 static info *
 MakeInfo ()
@@ -70,6 +130,8 @@ MakeInfo ()
     INFO_SHAPE (result) = NULL;
     INFO_IDSUSED (result) = 0;
     INFO_ISFULLPARTITION (result) = FALSE;
+    INFO_VARDECS (result) = NULL;
+    INFO_PREASSIGN (result) = NULL;
 
     DBUG_RETURN (result);
 }
@@ -132,6 +194,49 @@ WLFLTdoWithloopFlattening (node *syntax_tree)
  * @}  <!-- Static helper functions -->
  *****************************************************************************/
 
+static bool
+equalShapes (node *shp1, node *shp2)
+{
+#ifdef FLATTEN_IS_PROPER
+    return (ID_AVIS (shp1) == ID_AVIS (shp2));
+#else
+    return (CMPTdoCompareTree (shp1, shp2) == CMPT_EQ);
+#endif
+}
+
+static node *
+createLowerBound (info *arg_info)
+{
+    DBUG_ENTER ("createLowerBound");
+
+    node *lb_avis;
+    constant *lb_const;
+    node *lb_assign;
+
+    lb_const = COmakeZero (T_int, SHcreateShape (1, 1));
+    lb_avis = TBmakeAvis (TRAVtmpVar (), TYmakeAKV (TYmakeSimpleType (T_int), lb_const));
+    INFO_VARDECS (arg_info) = TBmakeVardec (lb_avis, INFO_VARDECS (arg_info));
+
+    lb_assign = TBmakeAssign (TBmakeLet (TBmakeIds (lb_avis, NULL),
+                                         TBmakeArray (TYmakeAKS (TYmakeSimpleType (T_int),
+                                                                 SHcreateShape (0)),
+                                                      SHcreateShape (1, 1),
+                                                      TBmakeExprs (TBmakeNum (0), NULL))),
+                              INFO_PREASSIGN (arg_info));
+    INFO_PREASSIGN (arg_info) = lb_assign;
+
+    AVIS_SSAASSIGN (lb_avis) = lb_assign;
+
+    DBUG_RETURN (TBmakeId (lb_avis));
+}
+
+static node *
+createUpperBound (node *bound, info *arg_info)
+{
+    DBUG_ENTER ("createUpperBound");
+    DBUG_RETURN (NULL);
+}
+
 /** <!--********************************************************************-->
  *
  * @name Traversal functions
@@ -151,7 +256,7 @@ WLFLTid (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("WLFLTid");
 
-    DBUG_PRINT ("WLFLT", ("Tagging %s as used.", AVIS_NAME (ID_AVIS (arg_node))));
+    DBUG_PRINT ("WLFLT_TAG", ("Tagging %s as used.", AVIS_NAME (ID_AVIS (arg_node))));
 
     AVIS_ISUSED (ID_AVIS (arg_node)) = TRUE;
 
@@ -220,7 +325,14 @@ WLFLTblock (node *arg_node, info *arg_info)
      * Clear the AVIS_ISUSED flag, again.
      */
     if (BLOCK_VARDEC (arg_node) != NULL) {
+
         BLOCK_VARDEC (arg_node) = TRAVdo (BLOCK_VARDEC (arg_node), arg_info);
+
+        if (INFO_VARDECS (arg_info) != NULL) {
+            BLOCK_VARDEC (arg_node)
+              = TCappendVardec (INFO_VARDECS (arg_info), BLOCK_VARDEC (arg_node));
+            INFO_VARDECS (arg_info) = NULL;
+        }
     }
 
     DBUG_RETURN (arg_node);
@@ -238,7 +350,7 @@ WLFLTavis (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("WLFLTavis");
 
-    DBUG_PRINT ("WLFLT", ("Clearing %s's used flag.", AVIS_NAME (arg_node)));
+    DBUG_PRINT ("WLFLT_TAG", ("Clearing %s's used flag.", AVIS_NAME (arg_node)));
 
     AVIS_ISUSED (arg_node) = FALSE;
 
@@ -266,21 +378,58 @@ WLFLTwith (node *arg_node, info *arg_info)
      * regardless of whether we do the actual optimisation or not.
      */
     WITH_CODE (arg_node) = TRAVdo (WITH_CODE (arg_node), arg_info);
+    /*
+     * Now, we infer INFO_GENARRAYS and INFO_SHAPE
+     */
     WITH_WITHOP (arg_node) = TRAVdo (WITH_WITHOP (arg_node), arg_info);
 
     INFO_IDSUSED (arg_info) = 0;
     INFO_ISFULLPARTITION (arg_info) = TRUE;
+    /**
+     * The traversal of N_part serves 2 purposes:
+     *
+     * - count the number of used(!) N_ids nodes encountered during traversal
+     * of N_withids, i.e., check whether the index vector or its scalarized
+     * components are referred to anywhere in the code (INFO_IDSUSED > 0)!
+     *
+     * - check whether all generators are full partitions while traversing
+     * N_generator.
+     */
     WITH_PART (arg_node) = TRAVdo (WITH_PART (arg_node), arg_info);
 
     wlopsno = TCcountWithops (WITH_WITHOP (arg_node));
     if ((INFO_GENARRAYS (arg_info) == wlopsno) && (INFO_IDSUSED (arg_info) == 0)
         && (INFO_ISFULLPARTITION (arg_info))) {
         DBUG_PRINT ("WLFLT", ("Found victim!"));
-
-        /* do the optimisation */
     }
 
     INFO_GENARRAYS (arg_info) = 0;
+    INFO_ISFULLPARTITION (arg_info) = FALSE;
+    INFO_SHAPE (arg_info) = NULL;
+
+    DBUG_RETURN (arg_node);
+}
+
+/** <!--********************************************************************-->
+ *
+ * @fn node *WLFLTMgenerator(node *arg_node, info *arg_info)
+ *
+ * @brief
+ *
+ *****************************************************************************/
+node *
+WLFLTMgenerator (node *arg_node, info *arg_info)
+{
+    node *lb_id, *ub_id;
+
+    DBUG_ENTER ("WLFLTMgenerator");
+
+    lb_id = createLowerBound (arg_info);
+    ub_id = createUpperBound (GENERATOR_BOUND2 (arg_node), arg_info);
+
+    arg_node = FREEdoFreeTree (arg_node);
+
+    arg_node = TBmakeGenerator (F_wl_le, F_wl_lt, lb_id, ub_id, NULL, NULL);
 
     DBUG_RETURN (arg_node);
 }
@@ -297,15 +446,21 @@ WLFLTgenarray (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("WLFLTgenarray");
 
+#ifdef FLATTEN_IS_PROPER
     DBUG_ASSERT ((NODE_TYPE (GENARRAY_SHAPE (arg_node)) == N_id),
                  "Malformed withloop: non-id node as genarray shape.");
+#else
+    DBUG_ASSERT ((NODE_TYPE (GENARRAY_SHAPE (arg_node)) == N_id)
+                   || (NODE_TYPE (GENARRAY_SHAPE (arg_node)) == N_array),
+                 "Malformed withloop: non-id/array node as genarray shape.");
+#endif
 
     if (INFO_SHAPE (arg_info) == NULL) {
-        INFO_SHAPE (arg_info) = ID_AVIS (GENARRAY_SHAPE (arg_node));
+        INFO_SHAPE (arg_info) = GENARRAY_SHAPE (arg_node);
         INFO_GENARRAYS (arg_info)++;
 
         arg_node = TRAVcont (arg_node, arg_info);
-    } else if (INFO_SHAPE (arg_info) == ID_AVIS (GENARRAY_SHAPE (arg_node))) {
+    } else if (equalShapes (INFO_SHAPE (arg_info), GENARRAY_SHAPE (arg_node))) {
         INFO_GENARRAYS (arg_info)++;
 
         arg_node = TRAVcont (arg_node, arg_info);
@@ -373,18 +528,31 @@ node *
 WLFLTgenerator (node *arg_node, info *arg_info)
 {
     bool stepok, widthok, lowerok, upperok;
-    node *loweravis;
 
     DBUG_ENTER ("WLFLTgenerator");
 
     stepok = (GENERATOR_STEP (arg_node) == NULL);
     widthok = (GENERATOR_WIDTH (arg_node) == NULL);
-    upperok = (ID_AVIS (GENERATOR_BOUND2 (arg_node)) == INFO_SHAPE (arg_info))
-              && (GENERATOR_OP2 (arg_node) == F_lt_VxV);
+    upperok = equalShapes (GENERATOR_BOUND2 (arg_node), INFO_SHAPE (arg_info))
+              && (GENERATOR_OP2 (arg_node) == F_wl_lt);
 
-    loweravis = ID_AVIS (GENERATOR_BOUND1 (arg_node));
-    lowerok = (GENERATOR_OP1 (arg_node) == F_le_VxV) && (TYisAKV (AVIS_TYPE (loweravis)))
-              && (COisZero (TYgetValue (AVIS_TYPE (loweravis)), TRUE));
+#if FLATTEN_IS_PROPER
+    {
+        node *loweravis;
+        loweravis = ID_AVIS (GENERATOR_BOUND1 (arg_node));
+        lowerok = (GENERATOR_OP1 (arg_node) == F_wl_le)
+                  && (TYisAKV (AVIS_TYPE (loweravis)))
+                  && (COisZero (TYgetValue (AVIS_TYPE (loweravis)), TRUE));
+    }
+#else
+    {
+        constant *lower_const = NULL;
+
+        lower_const = COaST2Constant (GENERATOR_BOUND1 (arg_node));
+        lowerok = (lower_const != NULL) && (GENERATOR_OP1 (arg_node) == F_wl_le)
+                  && COisZero (lower_const, TRUE);
+    }
+#endif
 
     INFO_ISFULLPARTITION (arg_info)
       = INFO_ISFULLPARTITION (arg_info) && stepok && widthok && upperok && lowerok;
@@ -404,7 +572,15 @@ WLFLTassign (node *arg_node, info *arg_info)
 {
     DBUG_ENTER ("WLFLTassign");
 
-    arg_node = TRAVcont (arg_node, arg_info);
+    if (ASSIGN_NEXT (arg_node) != NULL) {
+        ASSIGN_NEXT (arg_node) = TRAVdo (ASSIGN_NEXT (arg_node), arg_info);
+    }
+    ASSIGN_INSTR (arg_node) = TRAVdo (ASSIGN_INSTR (arg_node), arg_info);
+
+    if (INFO_PREASSIGN (arg_info) != NULL) {
+        arg_node = TCappendAssign (INFO_PREASSIGN (arg_info), arg_node);
+        INFO_PREASSIGN (arg_info) = NULL;
+    }
 
     DBUG_RETURN (arg_node);
 }
