9bf85933a13437e7b6468d470847c1cc30154573
Carl Joslin <caj@sac-home.org>
2009-01-26 18:00:25 +0000

    Corrections to range of mutc creates.

--- src/runtime/mutc_h/mutc.h
+++ src/runtime/mutc_h/mutc.h
@@ -9,7 +9,7 @@
 #define SAC_MUTC_DECL_FAMILY(name) family name;
 
 #define SAC_MUTC_CREATE(family, place, start, end, inc, bs, ap)                          \
-    create (family; place; start; end; inc; bs;;) ap;
+    create (family; place; start; end - 1; inc; bs;;) ap;
 
 #define SAC_MUTC_SYNC(family) sync (family);
 
