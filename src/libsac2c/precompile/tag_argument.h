fee9eb79e1bf164ca14e5c4bd66a6f04af297cac
Carl Joslin <caj@sac-home.org>
2009-02-24 17:58:29 +0000

    Added a tagging phase to tag arguments of functions as this will be needed in the mutc SL back-end.

---
+++ src/libsac2c/precompile/tag_argument.h
@@ -0,0 +1,14 @@
+/*
+ * $Id: convert_type_representation.h 15838 2008-11-03 00:15:33Z cg $
+ */
+
+#ifndef _SAC_TAG_ARGUMENT_H_
+#define _SAC_TAG_ARGUMENT_H_
+
+#include "types.h"
+
+extern node *TAdoTagArguments (node *syntax_tree);
+
+extern node *TAarg (node *arg_node, info *arg_info);
+
+#endif /* _SAC_TAG_ARGUMENT_H_ */
