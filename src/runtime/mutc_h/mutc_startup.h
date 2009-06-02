d1246dd6c77b1e5f94291c75f8caaec4b636d699
Carl Joslin <caj@sac-home.org>
2009-06-02 16:05:27 +0000

    Mutc/sl int output support

--- src/runtime/mutc_h/mutc_startup.h
+++ src/runtime/mutc_h/mutc_startup.h
@@ -6,7 +6,18 @@
     SAC_MUTC_STARTUP_ANON ()                                                             \
     SAC_MUTC_WORLD_OBJECT                                                                \
     SAC_MUTC_UNIN                                                                        \
-    SAC_MUTC_TOSTRING
+    SAC_MUTC_TOSTRING                                                                    \
+    SAC_MUTC_SAC_SVP_IO_PUTN
+
+#define SAC_MUTC_SAC_SVP_IO_PUTN                                                         \
+    sl_decl (svp_io_putn, void, sl_glparm (long long, a), sl_glparm (int, t));           \
+    sl_def (sac_svp_io_putn, void, sl_glparm (int, a), sl_glparm (int, t))               \
+    {                                                                                    \
+        long long b = (long long)sl_getp (a);                                            \
+        sl_proccall (svp_io_putn, sl_glarg (long long, sl_anon, b),                      \
+                     sl_glarg (int, sl_anon, sl_getp (t)));                              \
+    }                                                                                    \
+    sl_enddef
 
 #define SAC_MUTC_THE_WORLD_TAGS()                                                        \
     T_SHP (SCL, T_HID (NHD, T_UNQ (UNQ, T_REG (INT, T_SCO (GLO, T_USG (PAR, T_EMPTY))))))
