51ed0e5ee6f705be4083dcda9a46239848843d05
Hraban Luyat <hly@sac-home.org>
2009-07-21 12:12:27 +0000

    Simplified the hardcoded prefixes for struct masking (more readable, hopefully)

--- src/libsac2c/scanparse/hidestructs.h
+++ src/libsac2c/scanparse/hidestructs.h
@@ -4,10 +4,10 @@
 #include "types.h"
 
 #define STRUCT_PREFIX "_struct_"
-#define STRUCT_TYPE "_struct_t_"
-#define STRUCT_GET "_struct_t_get_"
-#define STRUCT_SET "_struct_t_set_"
-#define STRUCT_ELEM "_struct_t_elem_"
+#define STRUCT_TYPE "_struct_"
+#define STRUCT_GET "_struct_get_"
+#define STRUCT_SET "_struct_set_"
+#define STRUCT_ELEM "_struct_elem_"
 
 extern node *HSdoDestruct (node *syntax_tree);
 
