e09ba532bc9317337f29b8147bb84db3c712417d
Robert Bernecky <rbe@sac-home.org>
2009-10-09 20:22:52 +0000

    Complete desupport of F_attachextreman.

--- src/libsac2c/typecheck/ct_prf.c
+++ src/libsac2c/typecheck/ct_prf.c
@@ -481,39 +481,6 @@ NTCCTprf_attachextrema (te_info *info, ntype *args)
     res = TYmakeProductType (1, res);
     DBUG_RETURN (res);
 }
-/******************************************************************************
- *
- * function:
- *    ntype *NTCCTprf_attachextreman( te_info *info, ntype *elems)
- *
- * description:
- *   This primitive is used to preserve N_array extrema. The
- *   extrema are attached to the first element of the N_array.
- *   E.g.,  given the following N_array:
- *
- *        narr = [ I, J, K];
- *
- *   This is turned into:
- *
- *        I' = _attachextreman ( I, minnarr, maxnarr);
- *        narr = [ I', J, K];
- *
- *
- ******************************************************************************/
-
-ntype *
-NTCCTprf_attachextreman (te_info *info, ntype *args)
-{
-    ntype *arg;
-    ntype *res;
-
-    DBUG_ENTER ("NTCCTprf_attachextreman");
-
-    arg = TYgetProductMember (args, 0);
-    res = TYcopyType (arg);
-    res = TYmakeProductType (1, res);
-    DBUG_RETURN (res);
-}
 
 /******************************************************************************
  *
@@ -528,6 +495,11 @@ NTCCTprf_attachextreman (te_info *info, ntype *args)
  *   adding ancillary information to the ast.
  *
  *   The semantics of the _attachintersect are:
+ *   If we start with:
+ *
+ *    z   = _sel_VxA_( iv, foldeeWL);
+ *
+ *   We end up with:
  *
  *    iv' = _attachintersect(iv, indexsetmin, indexsetmax)
  *    z   = _sel_VxA_( iv', foldeeWL);
