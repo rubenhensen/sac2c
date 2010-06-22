71f64095977ec985daa3113d3303f29f1501f401
Robert Bernecky <rbe@sac-home.org>
2010-06-22 21:51:39 +0000

    Ahem. Done in by svn.

---
+++ src/libsac2c/stdopt/arithmetic_simplification.h
@@ -0,0 +1,23 @@
+/*
+ * $Id$
+ */
+#ifndef _SAC_ARITHMETIC_SIMPLIFICATION_H_
+#define _SAC_ARITHMETIC_SIMPLIFICATION_H_
+
+#include "types.h"
+
+/******************************************************************************
+ *
+ * arithmetic simplification
+ *
+ * prefix: AS
+ *
+ *****************************************************************************/
+extern node *ASdoArithmeticSimplification (node *arg_node);
+extern node *ASdoArithmeticSimplificationModule (node *arg_node);
+
+extern node *ASfundef (node *arg_node, info *arg_info);
+extern node *ASassign (node *arg_node, info *arg_info);
+extern node *ASprf (node *arg_node, info *arg_info);
+
+#endif /* _SAC_ARITHMETIC_SIMPLIFICATION_H_ */
