15175c5f3b54a48a0dd708a8f644ac1ecac9927f
Nilesh Karavadara <nkk@sac-home.org>
2010-07-15 13:54:43 +0000

    SAC prefix added to function names

--- src/libsac/mutc/reference_counting.c
+++ src/libsac/mutc/reference_counting.c
@@ -17,32 +17,32 @@
 
 #include <sac.h>
 
-sl_def (set_rc, void, sl_glparm (int *, desc), sl_glparm (int, rc))
+sl_def (SAC_set_rc, void, sl_glparm (int *, desc), sl_glparm (int, rc))
 {
     DESC_RC (sl_getp (desc)) = sl_getp (rc);
 }
 sl_enddef
 
-sl_def (inc_rc, void, sl_glparm (int *, desc), sl_glparm (int, rc))
+sl_def (SAC_inc_rc, void, sl_glparm (int *, desc), sl_glparm (int, rc))
 {
     DESC_RC (sl_getp (desc)) += sl_getp (rc);
 }
 sl_enddef
 
-sl_def (dec_rc, void, sl_glparm (int *, desc), sl_glparm (int, rc))
+sl_def (SAC_dec_rc, void, sl_glparm (int *, desc), sl_glparm (int, rc))
 {
     DESC_RC (sl_getp (desc)) -= sl_getp (rc);
 }
 sl_enddef
 
-sl_def (dec_and_get_rc, void, sl_glparm (int *, desc), sl_shparm (int, val))
+sl_def (SAC_dec_and_get_rc, void, sl_glparm (int *, desc), sl_shparm (int, val))
 {
     DESC_RC (sl_getp (desc)) -= sl_getp (val);
     sl_setp (val, DESC_RC (sl_getp (desc)));
 }
 sl_enddef
 
-sl_def (get_rc, void, sl_glparm (int *, desc), sl_shparm (int, val))
+sl_def (SAC_get_rc, void, sl_glparm (int *, desc), sl_shparm (int, val))
 {
     int dummy = sl_getp (val);
     sl_setp (val, DESC_RC (sl_getp (desc)));
