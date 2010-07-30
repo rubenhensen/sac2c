bd06e36df71d38e34977b32668f566fc96aa4969
Zheng Zhangzheng <zzg@sac-home.org>
2010-07-30 14:37:16 +0000

    contains the code only for openmp mt

---
+++ src/runtime/mt_omp_h/mt_omp.h
@@ -0,0 +1,59 @@
+
+
+/*
+ * $Id: omp.h 16792 2010-03-29 09:22:23Z cg $
+ */
+
+/*****************************************************************************
+ *
+ * file:   mt_omp.h
+ *
+ * prefix: SAC_MT_OMP_
+ *
+ * description:
+ *   This file is part of the SAC standard header file sac.h
+ *
+ *   It is the major header file of the implementation of the OpenMP multi-threading
+ *   facilities.
+ *
+ *****************************************************************************/
+
+#ifndef _SAC_MT_OMP_H_
+#define _SAC_MT_OMP_H_
+
+#ifndef SAC_SIMD_COMPILATION
+
+/*****************************************************************************/
+
+#if SAC_DO_MULTITHREAD
+
+#if SAC_DO_MT_OMP
+
+SAC_C_EXTERN void SAC_OMP_SetupInitial (int argc, char *argv[], unsigned int num_threads,
+                                        unsigned int max_threads);
+
+#define SAC_MT_SETUP_INITIAL()                                                           \
+    SAC_OMP_SetupInitial (__argc, __argv, SAC_SET_THREADS, SAC_SET_THREADS_MAX);
+
+#define SAC_OMP_SET_NUM_THREADS() omp_set_num_threads (SAC_MT_THREADS ())
+
+#define SAC_OMP_SET_MAX_ACTIVE_LEVEL()                                                   \
+    omp_set_max_active_levels (SAC_OMP_MAX_ACTIVE_LEVEL ())
+
+#define SAC_OMP_MAX_ACTIVE_LEVEL() SAC_OMP_ACTIVE_LEVEL
+
+#define SAC_MT_SETUP()
+
+#define SAC_MT_DEFINE()
+
+SAC_C_EXTERN unsigned int SAC_Get_ThreadID (pthread_key_t SAC_MT_threadid_key);
+
+#endif /* SAC_DO_MT_OMP */
+
+/*****************************************************************************/
+
+#endif /* SAC_DO_MULTITHREAD */
+
+#endif /* SAC_SIMD_COMPILATION */
+
+#endif /* _SAC_MT_OMP_H_ */
