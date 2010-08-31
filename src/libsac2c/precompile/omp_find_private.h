8f4ea9886b12ed61f3b309567df001ba8e53f1d4
Zheng Zhangzheng <zzg@sac-home.org>
2010-08-31 21:49:37 +0000

    header file for find OpenMP private variables

---
+++ src/libsac2c/precompile/omp_find_private.h
@@ -0,0 +1,23 @@
+/*
+ * Lifting with3 into functions
+ * $Log$
+ *
+ */
+
+#ifndef _SAC_OMP_FIND_PRIVATE_H_
+#define _SAC_OMP_FIND_PRIVATE_H_
+
+#include "types.h"
+
+extern node *OFPdoFindPrivate (node *syntaxtree);
+
+extern node *OFPmodule (node *arg_node, info *arg_info);
+extern node *OFPfundef (node *arg_node, info *arg_info);
+extern node *OFPlet (node *arg_node, info *arg_info);
+extern node *OFPwith2 (node *arg_node, info *arg_info);
+extern node *OFPwith (node *arg_node, info *arg_info);
+extern node *OFPids (node *arg_node, info *arg_info);
+extern node *OFPid (node *arg_node, info *arg_info);
+extern node *OFPwithid (node *arg_node, info *arg_info);
+
+#endif /* _SAC_OMP_FIND_PRIVATE_H_ */
