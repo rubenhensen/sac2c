e585dd39f7ad2efc2bd56b0dac86749d10edf7cd
Jing Guo <jgo@sac-home.org>
2011-07-11 09:39:29 +0000

    wanring added in ccmanager.c if CUDA_ARCH is not defined.

--- src/libsac2c/modules/ccmanager.c
+++ src/libsac2c/modules/ccmanager.c
@@ -28,11 +28,10 @@ GetCCCall ()
     char *result;
 
     DBUG_ENTER ();
-    /*
-      if( global.backend == BE_cuda && STReq( global.config.cuda_arch, "")) {
-        CTIwarn("CUDA architecture cannot be detected, set to default(1.0)\n");
-      }
-    */
+
+    if (global.backend == BE_cuda && STReq (global.config.cuda_arch, "")) {
+        CTIwarn ("CUDA architecture cannot be detected, set to default(1.0)\n");
+    }
 
     buffer = SBUFcreate (128);
 
