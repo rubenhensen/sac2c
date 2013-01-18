9d0c4e9c044000752b7371d8ab1cdefe43d9c3f8
Heinz Wiesinger <hmw@sac-home.org>
2013-01-18 11:12:35 +0100

    Add basic capabilities for runtime optimization tracing.

--- src/libsac2c/codegen/gen_startup_code.c
+++ src/libsac2c/codegen/gen_startup_code.c
@@ -145,6 +145,8 @@ PrintGlobalSwitches (void)
              (global.trace.aa) ? 1 : 0);
     fprintf (global.outfile, "#define SAC_DO_TRACE_MT        %d\n",
              (global.trace.mt) ? 1 : 0);
+    fprintf (global.outfile, "#define SAC_DO_TRACE_RTSPEC    %d\n",
+             (global.trace.rtspec) ? 1 : 0);
     fprintf (global.outfile, "\n");
 
     fprintf (global.outfile, "#define SAC_DO_CACHESIM        %d\n",
