79869b0446086489f77c33b5881bf2170b8b1cf6
Hans-Nikolai Viessmann <hansthespass@googlemail.com>
2015-11-17 21:40:41 +0000

    Added some more functions to the Fortran interface

--- include/sacinterface.f
+++ include/sacinterface.f
@@ -1,6 +1,35 @@
-!
-! Fortran Interface for SAC Runtime System
-!
+!>
+!! Fortran Interface for SAC Runtime System
+!! 
+!!   This file is included as part of the fwrapper.f file that is
+!!   generated through `sac4c -fortran` - it can not be used on it own!
+!!
+!!   It implicitly makes use of the functions declared in sacinterface.h.
+!!
+!!   Last updated Oct 2015
+!!
+
+!************************************************************************
+!> SACARG related functions
+!************************************************************************
+              integer(c_int) function SACARGgetDim
+     &                (sacarg)
+     &                bind(c, name='SACARGgetDim')
+                  import
+                  implicit none
+                  type(c_ptr), value, intent(in) :: sacarg
+              end function SACARGgetDim
+              integer(c_int) function SACARGgetShape
+     &            (sacarg, pos)
+     &            bind(c, name = 'SACARGgetShape')
+                  import
+                  implicit none
+                  type(c_ptr), value, intent(in) :: sacarg
+                  integer(c_int), value, intent(in) :: pos
+              end function SACARGgetShape
+!************************************************************************
+!> SAC Runtime related functions
+!************************************************************************
               subroutine SAC_InitRuntimeSystem
      &                ()
      &                bind(c, name = 'SAC_InitRuntimeSystem')
@@ -13,6 +42,9 @@
                   import
                   implicit none
               end subroutine SAC_FreeRuntimeSystem
+!************************************************************************
+!> SAC Thread-managment related functions
+!************************************************************************
               type(c_ptr) function SAC_AllocHive
      &                (threads, scheduler, places, bdata)
      &                bind(c, name = 'SAC_AllocHive')
@@ -42,6 +74,9 @@
                   import
                   implicit none
               end function SAC_DetachHive
+!************************************************************************
+!> SACARG Conversion functions 
+!************************************************************************
               type(c_ptr) function SACARGconvertFromIntScalar
      &            (num)
      &            bind(c, name = "SACARGconvertFromIntScalar")
@@ -49,35 +84,24 @@
                   implicit none
                   integer(c_int), value, intent(in) :: num
               end function SACARGconvertFromIntScalar
-              type(c_ptr) function SACARGconvertFromIntPointer2
-     &            (matrix, dims, rows, columns)
-     &            bind(c, name = 'SACARGconvertFromIntPointer')
+              type(c_ptr) function SACARGconvertFromIntPointerVect
+     &            (matrix, dims, shape)
+     &            bind(c, name = 'SACARGconvertFromIntPointerVect')
                   import
                   implicit none
                   type(c_ptr), value, intent(in) :: matrix
-                  integer(kind = c_int), value, intent(in) :: dims
-                  integer(kind = c_int), value, intent(in) :: rows
-                  integer(kind = c_int), value, intent(in) :: columns
-              end function SACARGconvertFromIntPointer2
-              type(c_ptr) function SACARGconvertFromDoublePointer1
-     &            (array, dim, elements)
-     &            bind(c, name = "SACARGconvertFromDoublePointer")
+                  type(c_ptr), value, intent(in) :: shape
+                  integer(c_int), value, intent(in) :: dims
+              end function SACARGconvertFromIntPointerVect
+              type(c_ptr) function SACARGconvertFromDoublePointerVect
+     &            (matrix, dims, shape)
+     &            bind(c, name = 'SACARGconvertFromDoublePointerVect')
                   import
                   implicit none
-                  type(c_ptr), value, intent(in) :: array
-                  integer(c_int), value, intent(in) :: dim
-                  integer(c_int), value, intent(in) :: elements
-              end function SACARGconvertFromDoublePointer1
-              type(c_ptr) function SACARGconvertFromDoublePointer2
-     &            (array, dim, element0, element1)
-     &            bind(c, name = "SACARGconvertFromDoublePointer")
-                  import
-                  implicit none
-                  type(c_ptr), value, intent(in) :: array
-                  integer(c_int), value, intent(in) :: dim
-                  integer(c_int), value, intent(in) :: element0
-                  integer(c_int), value, intent(in) :: element1
-              end function SACARGconvertFromDoublePointer2
+                  type(c_ptr), value, intent(in) :: matrix
+                  type(c_ptr), value, intent(in) :: shape
+                  integer(c_int), value, intent(in) :: dims
+              end function SACARGconvertFromDoublePointerVect
               type(c_ptr) function SACARGconvertToDoubleArray
      &            (result)
      &            bind(c, name = "SACARGconvertToDoubleArray")
@@ -92,11 +116,3 @@
                   implicit none
                   type(c_ptr), value, intent(in) :: result
               end function SACARGconvertToIntArray
-              integer(kind = c_int) function SACARGgetShape
-     &            (arg, pos)
-     &            bind(c, name = 'SACARGgetShape')
-                  import
-                  implicit none
-                  type(c_ptr), value, intent(in) :: arg
-                  integer(kind = c_int), value, intent(in) :: pos
-              end function SACARGgetShape
