3841c812d058dcc02f148fbd05e10d01cd2006b4
Hans-Nikolai Viessmann <hv15@hw.ac.uk>
2016-04-19 16:31:58 +0100

    We now can build runtime lib from sac2c cmake
    * We use the ExternalProject module to create a target which builds
      the runtime libraries with a dependency on sac2c/sac2cShared.
    * Added some feature on/off output for convenience

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,6 +1,6 @@
 
 OPTION (DOT       "Build sac with dot-file support for visualising AST"               OFF)
-OPTION (CPUSPLUS  "Use C++ compiler to build sac2c"                                   OFF)
+OPTION (CPLUSPLUS  "Use C++ compiler to build sac2c"                                   OFF)
 OPTION (LTO       "Use Link Time Optimisations if supported by the C compiler"        OFF)
 OPTION (MT        "Build sac2c with thread-based backend support"                     ON)
 OPTION (OMP       "Build sac2c with openmp-based backend support"                     ON)
