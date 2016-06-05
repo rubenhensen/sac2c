0f7b5d040f48a791357d72c3c31d9e7cd8f44a4f
Heinz Wiesinger <hmw@sac-home.org>
2016-06-05 13:31:20 +0200

    Handle runtime specializations of functions with object arguments correctly.

--- src/libsac2c/rtspec/runtime_filtering.c
+++ src/libsac2c/rtspec/runtime_filtering.c
@@ -31,11 +31,15 @@ struct INFO {
 
     node *module;
     node *args;
+    node *rets;
+    node *new_rets;
 };
 
 #define INFO_MODULE(n) ((n)->module)
 #define INFO_ARGS(n) ((n)->args)
+#define INFO_RETS(n) ((n)->rets)
 #define INFO_ARGSFOUND(n) ((n)->args_found)
+#define INFO_NEWRETS(n) ((n)->new_rets)
 
 static info *
 MakeInfo (info *arg_info)
@@ -49,6 +53,8 @@ MakeInfo (info *arg_info)
     INFO_ARGSFOUND (result) = 0;
     INFO_MODULE (result) = NULL;
     INFO_ARGS (result) = NULL;
+    INFO_RETS (result) = NULL;
+    INFO_NEWRETS (result) = NULL;
 
     DBUG_RETURN (result);
 }
@@ -83,6 +89,7 @@ node *
 RTFILTERarg (node *arg_node, info *arg_info)
 {
     ntype *local, *global;
+    ct_res cmp;
 
 #ifndef DBUG_OFF
     char *tmp_str_a, *tmp_str_b;
@@ -100,12 +107,11 @@ RTFILTERarg (node *arg_node, info *arg_info)
 
     DBUG_EXECUTE (tmp_str_a = TYtype2String (local, 0, 0));
     DBUG_EXECUTE (tmp_str_b = TYtype2String (global, 0, 0));
-    DBUG_PRINT ("Searching for: %s, found: %s", tmp_str_a, tmp_str_b);
+    DBUG_PRINT ("Searching for: %s, found: %s", tmp_str_b, tmp_str_a);
 
-    /*
-     * Check if the base types match.
-     */
-    if (TYgetSimpleType (TYgetScalar (local)) == TYgetSimpleType (TYgetScalar (global))) {
+    cmp = TYcmpTypes (local, global);
+
+    if (cmp == TY_gt || cmp == TY_eq) {
         /*
          * Count the number of matching arguments.
          */
@@ -115,6 +121,63 @@ RTFILTERarg (node *arg_node, info *arg_info)
             INFO_ARGS (arg_info) = ARG_NEXT (INFO_ARGS (arg_info));
             ARG_NEXT (arg_node) = TRAVdo (ARG_NEXT (arg_node), arg_info);
         }
+    } else if (cmp == TY_dis) {
+        DBUG_PRINT ("Ignoring non-simple argument.");
+
+        if (ARG_NEXT (arg_node) != NULL) {
+            ARG_NEXT (arg_node) = TRAVdo (ARG_NEXT (arg_node), arg_info);
+        }
+    }
+
+    DBUG_RETURN (arg_node);
+}
+
+/** <!--*******************************************************************-->
+ *
+ * @fn RTFILTERret( node *arg_node, info *arg_info)
+ *
+ * @brief Traverse all the return values of a FUNDEF and check for
+ *        artificial arguments that will get added again later.
+ *
+ * Remove artificial arguments from the return arguments chain to not
+ * end up with duplicate entries later on (rrp phase).
+ *
+ * @param arg_node  The ARG node.
+ * @param arg_info  The info structure.
+ *
+ * @return The unaltered ARG node.
+ *
+ ****************************************************************************/
+node *
+RTFILTERret (node *arg_node, info *arg_info)
+{
+    node *ret = NULL;
+
+    DBUG_ENTER ();
+
+#ifndef DBUG_OFF
+    char *tmp_str_a;
+#endif
+
+    DBUG_PRINT (">>>>>> Checking return values ...");
+
+    DBUG_EXECUTE (tmp_str_a = TYtype2String (RET_TYPE (arg_node), 0, 0));
+    DBUG_PRINT ("Return type: %s", tmp_str_a);
+
+    if (!RET_ISARTIFICIAL (arg_node)) {
+        ret = DUPdoDupNode (arg_node);
+
+        RET_NEXT (ret) = NULL;
+
+        if (INFO_NEWRETS (arg_info) == NULL) {
+            INFO_NEWRETS (arg_info) = ret;
+        } else {
+            RET_NEXT (INFO_NEWRETS (arg_info)) = ret;
+        }
+    }
+
+    if (RET_NEXT (arg_node) != NULL) {
+        RET_NEXT (arg_node) = TRAVdo (RET_NEXT (arg_node), arg_info);
     }
 
     DBUG_RETURN (arg_node);
@@ -142,15 +205,20 @@ node *
 RTFILTERfundef (node *arg_node, info *arg_info)
 {
     node *funspec = NULL;
+    node *funargs = NULL;
 
     DBUG_ENTER ();
-    DBUG_PRINT (">>>> Checking function ...");
+    DBUG_PRINT (">>>> Checking function %s ...", FUNDEF_NAME (arg_node));
 
     /* We're only interested in the function we're specializing. */
-    if (STReq (FUNDEF_NAME (arg_node), global.rt_fun_name)) {
+    if (!FUNDEF_ISWRAPPERFUN (arg_node)
+        && STReq (FUNDEF_NAME (arg_node), global.rt_fun_name)) {
         DBUG_PRINT (">>>> Function found ...");
 
+        funargs = DUPdoDupTree (FUNDEF_ARGS (arg_node));
+
         INFO_ARGS (arg_info) = global.rt_args;
+        INFO_NEWRETS (arg_info) = NULL;
 
         FUNDEF_ARGS (arg_node) = TRAVdo (FUNDEF_ARGS (arg_node), arg_info);
 
@@ -158,15 +226,19 @@ RTFILTERfundef (node *arg_node, info *arg_info)
         if (INFO_ARGSFOUND (arg_info) == global.rt_num_args) {
             DBUG_PRINT ("Arguments match: creating FUNSPEC node.");
 
+            FUNDEF_RETS (arg_node) = TRAVdo (FUNDEF_RETS (arg_node), arg_info);
+
             /* Create a new fundef with the correct signature for specialization. */
             funspec
               = TBmakeFundef (STRcpy (FUNDEF_NAME (arg_node)),
                               NSdupNamespace (MODULE_NAMESPACE (INFO_MODULE (arg_info))),
-                              DUPdoDupTree (FUNDEF_RETS (arg_node)), global.rt_args, NULL,
-                              funspec);
+                              INFO_NEWRETS (arg_info), global.rt_args, NULL, funspec);
 
             /* Designate the fundef for specialization. */
             MODULE_FUNSPECS (INFO_MODULE (arg_info)) = funspec;
+
+            /* No need to process further if we already found a match */
+            // DBUG_RETURN( arg_node);
         }
 
     } else {
--- src/libsac2c/rtspec/runtime_filtering.h
+++ src/libsac2c/rtspec/runtime_filtering.h
@@ -19,6 +19,8 @@ extern node *RTFILTERfundef (node *arg_node, info *arg_info);
 
 extern node *RTFILTERarg (node *arg_node, info *arg_info);
 
+extern node *RTFILTERret (node *arg_node, info *arg_info);
+
 extern node *RTdoFilter (node *syntax_tree);
 
 #endif /* _SAC_RUNTIME_FILTERING_H_ */
