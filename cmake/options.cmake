ca23930711f834642e3826bec019ce9a265bb07b
Hans-Nikolai Viessmann <hv15@hw.ac.uk>
2016-08-05 12:14:00 +0100

    Added most of the distmem related checks
    Still need to do GASNET_INIT and provide a
    detailed printout of what has been configured...

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -6,3 +6,4 @@ OPTION (MT        "Build sac2c with thread-based backend support"               
 OPTION (OMP       "Build sac2c with openmp-based backend support"                     ON)
 OPTION (LPEL      "Build sac2c with LPEL support"                                     OFF)
 OPTION (PHM       "Build sac2c with Private Heap Manager"                             ON)
+OPTION (DISTMEM   "Build sac2c with distributed memory support"                       ON)
--- cmake/sac2c/config.cmake
+++ cmake/sac2c/config.cmake
@@ -1,3 +1,11 @@
+## This files is analogous to configure.ac (*.m4 files) used in the 
+## autotools build system.
+##
+## The configuration is organised as follows:
+##TODO
+
+
+## CMAKE INCLUDES
 INCLUDE (CheckIncludeFiles)
 INCLUDE (CheckCSourceCompiles)
 INCLUDE (CheckFunctionExists)
@@ -5,6 +13,112 @@ INCLUDE (CheckLibraryExists)
 INCLUDE (CheckCSourceRuns)
 INCLUDE (CheckCCompilerFlag)
 
+# Check for support of MPI
+MACRO(CHECK_DISTMEM_MPI)
+  MESSAGE(STATUS "Checking for MPI support")
+  SET(ENABLE_DISTMEM_BACKEND_MPI OFF)
+  FIND_PACKAGE(MPI)
+  #FIXME(hans): we should also handle the case when we
+  # are compiling with CXX.
+  IF(MPI_C_FOUND)
+    MESSAGE(STATUS "MPI Compiler: ${MPI_C_COMPILER}")
+    #XXX(hans): Normally we would use CHECK_C_SOURCE_COMPILES
+    # or similar to do this, but CMAKE is extremely set on using
+    # the project default compiler and does not allow at runtime
+    # changes to this setting. As such we verbosely do in essence
+    # what CHECK_C_SOURCE_COMPILES does...
+    #XXX(hans): perhaps turn this into a macro? Something to push
+    # upstream to CMAKE?
+    FILE(WRITE "${PROJECT_BINARY_DIR}/mpi-test.c"
+      "#include <mpi.h>
+
+      /* This program uses MPI 3 one-sided communication to test
+      whether the MPI installation does support these operations. */
+      int main(int argc, char *argv[]) {
+      static MPI_Win win = NULL;
+      size_t SAC_DISTMEM_pagesz = 0;
+
+      void *local_page_ptr = NULL;
+      size_t owner_rank = 0;
+      size_t remote_page_index;
+
+      MPI_Get( local_page_ptr, SAC_DISTMEM_pagesz,
+        MPI_BYTE, owner_rank,
+        remote_page_index * SAC_DISTMEM_pagesz,
+        SAC_DISTMEM_pagesz, MPI_BYTE, win);
+        }")
+    EXECUTE_PROCESS (COMMAND ${MPI_C_COMPILER} "${PROJECT_BINARY_DIR}/mpi-test.c"
+      WORKING_DIRECTORY "${PROJECT_BINARY_DIR}"
+      RESULT_VARIABLE MPI_COMPILES
+      ERROR_FILE "mpi-error.log")
+    #FILE(REMOVE "${PROJECT_BINARY_DIR}/mpi-test.c" "${PROJECT_BINARY_DIR}/a.out")
+    IF(${MPI_COMPILES} EQUAL 0)
+      SET(ENABLE_DISTMEM_BACKEND_MPI ON)
+    ENDIF ()
+  ENDIF ()
+ENDMACRO ()
+
+# Check for ARMCI for Distmem
+MACRO (CHECK_DISTMEM_ARMCI)
+  MESSAGE (STATUS "Checking for ARMCI")
+  SET (ENABLE_DISTMEM_BACKEND_ARMCI OFF)
+  # Check if ARMCI_HOME is set
+  IF ($ENV{ARMCI_HOME})
+    MESSAGE(STATUS "Using ARMCI_HOME: $ENV{ARMCI_HOME}")
+    IF (EXISTS "$ENV{ARMCI_HOME}")
+      MESSAGE(STATUS "ARMCI_HOME exists...")
+      SET (ENABLE_DISTMEM_BACKEND_ARMCI ON)
+    ELSE ()
+      MESSAGE(STATUS "ARMCI_HOME does not exist.")
+    ENDIF ()
+  ENDIF ()
+ENDMACRO ()
+
+# Check for GPI for Distmem
+MACRO (CHECK_DISTMEM_GPI)
+  MESSAGE(STATUS "Checking for GPI")
+  SET(ENABLE_DISTMEM_BACKEND_GPI OFF)
+  # Check if GPI_HOME is set
+  IF ($ENV{GPI_HOME})
+    MESSAGE(STATUS "Using GPI_HOME: $ENV{GPI_HOME}")
+    IF (EXISTS "$ENV{GPI_HOME}")
+      MESSAGE(STATUS "GPI_HOME exists...")
+      SET (ENABLE_DISTMEM_BACKEND_GPI ON)
+    ELSE ()
+      MESSAGE(STATUS "GPI_HOME does not exist.")
+    ENDIF ()
+  ENDIF ()
+ENDMACRO ()
+
+# Check for GasNet for Distmem
+MACRO (CHECK_DISTMEM_GASNET)
+  MESSAGE(STATUS "Checking for GASNET")
+  SET(ENABLE_DISTMEM_BACKEND_GASNET OFF)
+  # Check if GASNET_HOME is set
+  #FIXME(hans): Should this maybe be $GASNETINCLUDE instead
+  # of GASNET_HOME?
+  IF ($ENV{GASNET_HOME})
+    MESSAGE(STATUS "Using GASNET_HOME: $ENV{GASNET_HOME}")
+    IF (EXISTS "$ENV{GASNET_HOME}")
+      MESSAGE(STATUS "GASNET_HOME exists...")
+      IF(CMAKE_HOST_UNIX)
+        #FIXME(hans): We should try to use CMAKE command mode...
+        EXECUTE_PROCESS (COMMAND ls -d "$ENV{GASNET_HOME}/*-conduit"
+          COMMAND rev
+          COMMAND cut -d/ -f1
+          COMMAND rev
+          COMMAND cut -d- -f1
+          OUTPUT_VARIABLE CONDUITS)
+        IF (${CONDUITS})
+          MESSAGE (STATUS "The following conduits are supported: ${CONDUITS}")
+          SET (ENABLE_DISTMEM_BACKEND_GPI ON)
+        ENDIF ()
+      ENDIF ()
+    ELSE ()
+      MESSAGE(STATUS "GASNET_HOME does not exist.")
+    ENDIF ()
+  ENDIF ()
+ENDMACRO ()
 
 # Checks if the C compiler defines a macro called DEF.
 # In case macro is defined the ${FLAG} will be set on.
@@ -22,8 +136,6 @@ MACRO (CHECK_COMPILER DEF FLAG)
   )
 ENDMACRO ()
 
-
-
 # Enables FLAG in case the COND is enabled.
 MACRO (ENABLE_VAR_IF  FLAG)
   SET (${FLAG} OFF)
@@ -33,7 +145,6 @@ MACRO (ENABLE_VAR_IF  FLAG)
     SET (${FLAG} ON)
   ENDIF ()
 ENDMACRO ()
-
 
 # System-dependent variables.
 SET (OS       "${CMAKE_SYSTEM}")
@@ -54,7 +165,6 @@ SET (SAC2C_VERSION_PATCH  0)
 IF ("${SAC2C_VERSION}" MATCHES "-([0-9]+)(-g[a-f0-9]+)?(-dirty)?$")
     SET (SAC2C_VERSION_PATCH  "${CMAKE_MATCH_1}")
 ENDIF ()
-
 
 # Check for Link Time Optimisation
 SET (HAVE_LTO   OFF)
@@ -83,17 +193,10 @@ CHECK_C_SOURCE_COMPILES ("
     HAVE_GCC_SIMD_OPERATIONS
 )
 
-
 # Check headers.
 CHECK_INCLUDE_FILES (inttypes.h HAVE_INTTYPES_H)
 CHECK_INCLUDE_FILES (malloc.h HAVE_MALLOC_H)
 CHECK_INCLUDE_FILES (memory.h HAVE_MEMORY_H)
-
-# TODO this should be checked by FindThreads and FindOpenmp
-#CHECK_INCLUDE_FILES (pthread.h HAVE_PTHREAD_H)
-#CHECK_INCLUDE_FILES (omp.h HAVE_OMP_H)
-
-
 
 # Check functions
 CHECK_FUNCTION_EXISTS (strtok HAVE_STRTOK)
@@ -170,7 +273,6 @@ IF (LPEL)
     ENDIF ()
 ENDIF ()
 
-
 # If Option MT is set
 IF (MT)
     # Prefer -pthread over -lpthread and stuff.
@@ -184,7 +286,7 @@ IF (MT)
     ENDIF ()
 ENDIF ()
 
-# If oprion OMP is set.
+# If option OMP is set.
 IF (OMP)
     FIND_PACKAGE (OpenMP)
     IF (OPENMP_FOUND)
@@ -196,7 +298,7 @@ IF (OMP)
     ENDIF ()
 ENDIF ()
 
-
+# Check and see if we can enable RTSPEC
 SET (ENABLE_RTSPEC OFF)
 IF (ENABLE_DL AND ENABLE_MT AND ENABLE_UUID AND ENABLE_CRYPT)
   SET (ENABLE_RTSPEC ON)
@@ -215,7 +317,7 @@ IF (HAVE_SBRK)
   SET (SBRK_T      "intptr_t")
 ENDIF ()
 
-# Check if "%p" fomat of printf routines prints "0x"
+# Check if "%p" format of printf routines prints "0x"
 # in the beginning of the number.  NEED_PTR_PREFIX will
 # be set to either ON or OFF.
 CHECK_C_SOURCE_RUNS ("
@@ -249,8 +351,6 @@ FILE *_db_fp_ = stderr;
 CHECK_COMPILER (__SUNPRO_C  SUNC)
 CHECK_COMPILER (__DECC  DECC)
 CHECK_COMPILER (__APPLE__  MACC)
-
-
 
 # Check tools which required for compilation.
 # Find BASH interpreter.
@@ -323,8 +423,6 @@ IF (INDENT_EXEC)
 ENDIF ()
 MESSAGE (STATUS "Using `${CB}' for code-beautification")
 
-
-
 # Set variables depending on the tools.
 SET (ENABLE_MUTC OFF)
 IF (MUTC_EXEC)
@@ -339,6 +437,20 @@ SET (DOT    "${DOT_FLAG}")
 FIND_PACKAGE (Git)
 IF (NOT GIT_FOUND)
     MESSAGE (FATAL_ERROR "Could not find GIT on the system!")
+ENDIF ()
+
+# Check if we should add distmem support...
+MESSAGE(STATUS "Checking for Distmem backends (MPI, ARMCI, GASNET, GPI)")
+CHECK_DISTMEM_MPI()
+CHECK_DISTMEM_ARMCI()
+CHECK_DISTMEM_GASNET()
+CHECK_DISTMEM_GPI()
+
+SET(ENABLE_DISTMEM OFF)
+IF((ENABLE_DISTMEM_BACKEND_MPI OR ENABLE_DISTMEM_BACKEND_ARMCI
+    OR ENABLE_DISTMEM_BACKEND_GASNET OR ENABLE_DISTMEM_BACKEND_GPI)
+    AND DISTMEM)
+  SET(ENABLE_DISTMEM ON)
 ENDIF ()
 
 # Misc variables
@@ -533,7 +645,6 @@ ELSE ()
   SET (LD_FLAGS     "-Wl,-allow-shlib-undefined")
 ENDIF ()
 
-
 # Prpeare C flags for the debug version of the compiler
 SET (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DSANITYCHECKS -DWLAA_DEACTIVATED -DAE_DEACTIVATED -DTSI_DEACTIVATED -DPADT_DEACTIVATED -DCHECK_NODE_ACCESS -DINLINE_MACRO_CHECKS ${MKCCFLAGS}"
 )
@@ -542,11 +653,10 @@ SET (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DSANITYCHECKS -DWLAA_DEACTIVAT
 SET (CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -DDBUG_OFF -DPRODUCTION -DWLAA_DEACTIVATED -DAE_DEACTIVATED -DTSI_DEACTIVATED -DPADT_DEACTIVATED ${PDCCFLAGS}"
 )
 
+# If we don't explicitly set build type, then we default to debug
 IF (NOT CMAKE_BUILD_TYPE)
     SET (CMAKE_BUILD_TYPE DEBUG)
 ENDIF ()
-
-MESSAGE(">> Configuring the `${CMAKE_BUILD_TYPE}' variant")
 
 # These CC flags are always present
 ADD_DEFINITIONS (
@@ -557,7 +667,7 @@ ADD_DEFINITIONS (
 )
 
 # These flags will be used by the linker when creating a shared library
-# FIXME(artem) Revisit this later.  Mayve we don't need -shared and other
+# FIXME(artem) Revisit this later.  Maybe we don't need -shared and other
 #              flags when defining LD_DYNAMIC as CMake should be smart enough
 #              to figure out basic call for building shared library on all
 #              the systems.
@@ -578,3 +688,33 @@ SET (LIBRARY_OUTPUT_PATH "${DLL_BUILD_DIR}")
 # Make sure that this directory exists.
 FILE (MAKE_DIRECTORY "${DLL_BUILD_DIR}")
 
+# Print out configuration information
+#FIXME(hans): we are missing a few details here.
+MESSAGE ("
+* Configuration done.
+*
+* Detected OS string:      ${CMAKE_SYSTEM}
+* Detected CPU string:     ${CMAKE_SYSTEM_PROCESSOR}
+* 
+* CMake Generator:         ${CMAKE_GENERATOR}
+* CMake Varient:           ${CMAKE_BUILD_TYPE}
+*
+* Run-time specialization: ${ENABLE_RTSPEC}
+* Private heap manager:    ${ENABLE_PHM}
+* Back-ends:
+* - MT/pthread:            ${ENABLE_MT}
+* - MT/LPEL:               ${ENABLE_MT_LPEL}
+* - CUDA:                  ${ENABLE_CUDA}
+* - OpenMP:                ${ENABLE_OMP}
+* - SL:                    ${ENABLE_SL}
+* - Distributed memory:    ${ENABLE_DISTMEM}$distmem_details_print
+* - CC:                    ${CMAKE_C_COMPILER} (${CMAKE_C_COMPILER_ID})
+* - SaC compiler CFLAGS:   ${MKCCFLAGS}
+* - SaC programs CFLAGS:   ${RCCCFLAGS}
+*
+* Optional packages:
+* - Integer Set Library (ISL): ${ENABLE_ISL}
+*
+")
+
+# vim: ts=2 sw=2
