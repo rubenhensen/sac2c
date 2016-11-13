5dc129e9afbd6a0d86e54c31157b1a55c9d48f26
Hans-Nikolai Viessmann <hv15@hw.ac.uk>
2016-11-13 18:47:34 +0000

    Done some modularlisation of the CPack stuff

---
+++ cmake/cpack/config.cmake
@@ -0,0 +1,65 @@
+IF (NOT DEFINED SAC2C_VERSION OR "${SAC2C_VERSION}" STREQUAL "")
+  MESSAGE(FATAL_ERROR "Variable SAC2C_VERSION has not been set!")
+ENDIF ()
+
+# FIXME (hans): this is not very clean...
+IF (NOT DEFINED SAC2C_SOURCE_DIR OR "${SAC2C_SOURCE_DIR}" STREQUAL "")
+  MESSAGE(FATAL_ERROR "Variable SAC2C_SOURCE_DIR has not been set!"
+    " One solution would be to make SAC2C_SOURCE_DIR == PROJECT_SOURCE_DIR.")
+ENDIF ()
+
+# We need to make sure that we name the output package sanely.
+IF ("${CMAKE_BUILD_TYPE}" STREQUAL "")
+  # we assume that an empty build type means we are building both.
+  SET (PACKAGE_POSTFIX "omnibus")
+ELSE ()
+  STRING (TOLOWER "${CMAKE_BUILD_TYPE}" PACKAGE_POSTFIX)
+ENDIF ()
+
+# Set default CPack Packaging options
+SET (CPACK_PACKAGE_NAME              "sac2c-compiler")
+SET (CPACK_PACKAGE_VENDOR            "SaC Development Team")
+SET (CPACK_PACKAGE_CONTACT           "info@sac-home.org")
+SET (CPACK_PACKAGE_VERSION           "${SAC2C_VERSION}")
+SET (CPACK_PACKAGE_VERSION_MAJOR     "${SAC2C_VERSION_MAJOR}")
+SET (CPACK_PACKAGE_VERSION_MINOR     "${SAC2C_VERSION_MINOR}")
+SET (CPACK_PACKAGE_VERSION_PATCH     "${SAC2C_VERSION_PATCH}")
+SET (CPACK_PACKAGE_FILE_NAME         "sac2c-${SAC2C_VERSION}-${PACKAGE_POSTFIX}")
+SET (CPACK_PACKAGE_INSTALL_DIRECTORY "sac2c-${SAC2C_VERSION}") # XXX is this really needed?
+SET (CPACK_PACKAGE_ICON              "${SAC2C_SOURCE_DIR}/cmake/cpack/dmg-bundle/sac_logo.png")
+
+# SET (CPACK_PACKAGE_DESCRIPTION_FILE ...)
+SET (CPACK_PACKAGE_DESCRIPTION_SUMMARY "The sac2c compiler for a data-parallel array-based functional language SAC")
+# FIXME(artem) We need to decide on where do we put the stuff on the target system...)
+#SET (CPACK_PACKAGING_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
+SET (CPACK_RESOURCE_FILE_LICENSE "${SAC2C_SOURCE_DIR}/LICENSE")
+
+SET (CPACK_COMPONENTS_ALL applications libraries headers conf)
+# Set the displayed names for each of the components to install.
+# These will be displayed in the list of components inside the installer.
+SET (CPACK_COMPONENT_APPLICATIONS_DISPLAY_NAME "SaC Binaries")
+SET (CPACK_COMPONENT_LIBRARIES_DISPLAY_NAME "SaC Libraries")
+SET (CPACK_COMPONENT_HEADERS_DISPLAY_NAME "SaC Headers")
+SET (CPACK_COMPONENT_CONF_DISPLAY_NAME "SaC Configs")
+
+# DMG Bundle related configuration
+SET (CPACK_BUNDLE_NAME "sac2c-${SAC2C_VERSION}-omnibus")
+## Use Macports 'makeicns' tool to generate MAC OS X icons
+SET (CPACK_BUNDLE_ICON "${SAC2C_SOURCE_DIR}/cmake/cpack/dmg-bundle/sac.icns")
+SET (CPACK_BUNDLE_PLIST "${PROJECT_BINARY_DIR}/Info.plist")
+SET (CPACK_BUNDLE_STARTUP_COMMAND "${PROJECT_BINARY_DIR}/Install.command")
+CONFIGURE_FILE ("${SAC2C_SOURCE_DIR}/cmake/cpack/dmg-bundle/Info.plist.in" "${PROJECT_BINARY_DIR}/Info.plist")
+CONFIGURE_FILE ("${SAC2C_SOURCE_DIR}/cmake/cpack/dmg-bundle/Install.command.in" "${PROJECT_BINARY_DIR}/Install.command")
+
+# Debian-specific variables
+SET (CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_VENDOR} <${CPACK_PACKAGE_CONTACT}>")
+SET (CPACK_DEBIAN_ARCHITECTURE ${CMAKE_SYSTEM_PROCESSOR})
+
+# RPM-specific variables
+# XXX (hans): this may not be exhaustive - does not take into account if the user
+# changes the install prefix
+SET (CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST_ADDITION /usr/local /usr/local/bin /usr/local/include /usr/local/lib /usr/local/libexec /usr/local/share)
+SET (CPACK_RPM_PACKAGE_REQUIRES "gcc")
+
+INCLUDE(CPack)
+# vim: ts=2 sw=2 et:
