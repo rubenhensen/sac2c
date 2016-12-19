bd00c824e3763852d5efc472e4ea60284403de23
Sven-Bodo Scholz <sbs@sac-home.org>
2016-12-19 15:23:25 +0100

    Revert "disabling PHM for package build"
    This reverts commit 26ab3b05166db36c231c8a5a71f6789df9fd50ae.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -8,6 +8,6 @@ OPTION (LTO       "Use Link Time Optimisations if supported by the C compiler"  
 OPTION (MT        "Build sac2c with thread-based backend support"                     ON)
 OPTION (OMP       "Build sac2c with openmp-based backend support"                     OFF)
 OPTION (LPEL      "Build sac2c with LPEL support"                                     OFF)
-OPTION (PHM       "Build sac2c with Private Heap Manager"                             OFF)
+OPTION (PHM       "Build sac2c with Private Heap Manager"                             ON)
 OPTION (DISTMEM   "Build sac2c with distributed memory support"                       OFF)
 OPTION (HWLOC     "Build sac2c with hwloc support"                                    ON)
