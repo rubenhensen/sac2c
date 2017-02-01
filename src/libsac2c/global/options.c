e722fa604b62b6c5102423e8bc8e49e9565089fd
Robert Bernecky <bernecky@snakeisland.com>
2017-02-01 16:20:14 -0500

    Make polyhedral optimizations (PLUR,POGO,PWLF) enable -ecc and -dossawl.

--- src/libsac2c/global/options.c
+++ src/libsac2c/global/options.c
@@ -419,6 +419,18 @@ OPTcheckOptionConsistency (void)
         global.optimize.dowlur = TRUE;
     }
 
+    // Any polyhedral optimizations enabled
+    if ((global.optimize.dopwlf || global.optimize.dopogo || global.optimize.doplur)) {
+        if (!global.insertconformitychecks) {
+            global.insertconformitychecks = TRUE;
+            CTIwarn ("PLUR/POGO/PWLF is enabled: -ecc enabled.");
+        }
+        if (!global.optimize.dossawl) {
+            global.optimize.dossawl = TRUE;
+            CTIwarn ("PLUR/POGO/PWLF is enabled: -dossawl enabled.");
+        }
+    }
+
     if (global.optimize.doawlf && (!global.insertconformitychecks)) {
         global.insertconformitychecks = TRUE;
         CTIwarn ("AWLF is enabled: -ecc enabled.");
