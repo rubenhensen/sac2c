bfedf73f98f12fe06b1bee429fe302b774756983
Hans-Nikolai Viessmann <hv15@hw.ac.uk>
2017-06-27 14:37:52 +0100

    Move CUDA transfer flags into runtime headers

---
+++ src/runtime/essentials_h/cuda_transfer_methods.h
@@ -0,0 +1,22 @@
+/*****************************************************************************
+ *
+ * file:   cuda_transfer_methods.h
+ *
+ * description:
+ *
+ *   This file is part of the SAC standard header file sac.h
+ *
+ *   It provides type and macro definitions for the supported CUDA MemCpy
+ *   mechanisms.
+ *
+ *****************************************************************************/
+
+#ifndef _SAC_CUDA_TRANSFER_METHODS_H
+#define _SAC_CUDA_TRANSFER_METHODS_H
+
+#define SAC_CA_system 0
+#define SAC_CA_cureg 1
+#define SAC_CA_cualloc 2
+#define SAC_CA_cuman 3
+
+#endif /* _SAC_CUDA_TRANSFER_METHODS_H */
