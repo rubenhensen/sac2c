80f0295277dfb934c6602904aed67e6165a36736
Artem Shinkarov <artyom.shinkaroff@gmail.com>
2017-11-26 21:38:29 +0000

    Make SAC_TEST descriptions file-agnostic.
        Introduce meta-tags <file-name> and <file-name-we> that in
        makefiles are substituted with the name of the current file
        and the name of the current file without extension correspondingly.

--- tests/scripts/run.sh
+++ tests/scripts/run.sh
@@ -1,20 +1,37 @@
 #!/bin/sh
+source $(dirname $0)/common.sh
 
+function namewe () {
+    local filename="$1"
+    echo "$filename" | cut -f 1 -d '.'
+}
+
+# Source file that we are about to run tests on.
 SAC_FILE=$1
 
-namewe=$(basename $SAC_FILE)
-makefile=${namewe}.mk
+# Cut the path off.
+base_fname=$(basename $SAC_FILE)
+
+# Generate the name for the extracted makefile.
+makefile=${base_fname}.mk
+
+base_fname_we=$(namewe "$base_fname")
 
 # Generate the Makefile form a sac test:
 cpp -C $SAC_FILE \
      | grep "^// SAC_TEST|" \
      | sed -e 's/^\/\/ SAC_TEST|//g'\
            -e 's/<tab>/\t/g' \
+           -e "s/<file-name>/$base_fname/g" \
+           -e "s/<file-name-we>/$base_fname_we/g" \
            -e 's/<nlslash>/\\/g' > $makefile
 
+if test $? -ne 0; then
+    die "cannot parse SAC_TEST specification in $SAC_FILE"
+fi
+
 if ! make -f $makefile; then
-    echo "test $SAC_FILE failed"
-    exit 1
+    die "test $SAC_FILE failed"
 fi
 
 # else
