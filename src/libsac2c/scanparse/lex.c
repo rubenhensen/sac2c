8e6e2bb833a027271e34a35c30a5cf919a40670f
Artem Shinkarov <artyom.shinkaroff@gmail.com>
2018-10-25 23:26:48 +0000

    [lex] Line comments that end with EOF is ok.

--- src/libsac2c/scanparse/lex.c
+++ src/libsac2c/scanparse/lex.c
@@ -324,10 +324,11 @@ lexer_read_line_comment (struct lexer *lex, char **buf, size_t *size)
     while (true) {
         int c = lexer_getch (lex);
         if (c == EOF) {
-            error_loc (lex->loc, "unexpected end of file in the "
-                                 "middle of line comment");
-            buffer_add_char (buf, &index, size, 0);
-            return tok_unknown;
+            warning_loc (lex->loc, "unexpected end of file in the "
+                                   "middle of line comment");
+            //buffer_add_char (buf, &index, size, 0);
+            //return tok_unknown;
+            break;
         }
 
         buffer_add_char (buf, &index, size, c);
