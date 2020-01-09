4ea77949c5989997b304aa2debca816ea680da9e
Hans-Nikolai Viessmann <hv15@hw.ac.uk>
2020-01-09 21:47:16 +0100

    add missing d2h propogation step (working!)

--- src/libsac2c/cuda/async_delay_expand.c
+++ src/libsac2c/cuda/async_delay_expand.c
@@ -32,7 +32,9 @@
 struct INFO {
     node *curassign;
     node *preassign;
-    node *setassign;
+    node *nextassign;
+    node *upassign;
+    node *downassign;
     node *lhs;
     lut_t *h2d_lut;
     lut_t *d2h_lut;
@@ -44,7 +46,9 @@ struct INFO {
  */
 #define INFO_CURASSIGN(n) ((n)->curassign)
 #define INFO_PREASSIGN(n) ((n)->preassign)
-#define INFO_SETASSIGN(n) ((n)->setassign)
+#define INFO_NEXTASSIGN(n) ((n)->nextassign)
+#define INFO_UPASSIGN(n) ((n)->upassign)
+#define INFO_DOWNASSIGN(n) ((n)->downassign)
 #define INFO_LHS(n) ((n)->lhs)
 #define INFO_H2D_LUT(n) ((n)->h2d_lut)
 #define INFO_D2H_LUT(n) ((n)->d2h_lut)
@@ -66,7 +70,9 @@ MakeInfo (void)
 
     INFO_CURASSIGN (result) = NULL;
     INFO_PREASSIGN (result) = NULL;
-    INFO_SETASSIGN (result) = NULL;
+    INFO_NEXTASSIGN (result) = NULL;
+    INFO_UPASSIGN (result) = NULL;
+    INFO_DOWNASSIGN (result) = NULL;
     INFO_LHS (result) = NULL;
     INFO_H2D_LUT (result) = NULL;
     INFO_D2H_LUT (result) = NULL;
@@ -88,6 +94,9 @@ FreeInfo (info *info)
 
 /**
  *
+ * @param arg_node
+ * @param arg_info
+ * @return N_fundef
  */
 node *
 CUADEfundef (node *arg_node, info *arg_info)
@@ -109,16 +118,46 @@ CUADEfundef (node *arg_node, info *arg_info)
 }
 
 /**
+ * @brief Cause re-ordering of N_assign chain depending on the whether or
+ *        not we need to move a h2d/d2h up or down the chain.
  *
+ * We traverse the N_assign chain bottom-up. We need to maintain some state,
+ * specifically a reference to both the previous N_assign and the next one from
+ * the current N_assign.
+ *
+ * Concretely, if we are at some N_assign (b) where (a) is the N_assign at the
+ * top of the chain:
+ *
+ * ~~~
+ *   ---- top of N_assign chain
+ *  /
+ * (a) -> (b) -> (c) -> (d) -> ...
+ *  |      |      |
+ *  |      |      ---- PREASSIGN
+ *  |      ---- CURASSIGN
+ *  ---- NEXTASSIGN
+ * ~~~
+ *
+ * @param arg_node
+ * @param arg_info
+ * @return N_assign
  */
 node *
 CUADEassign (node *arg_node, info *arg_info)
 {
+    node *old_next;
+
     DBUG_ENTER ();
 
     /* bottom-up */
+    old_next = INFO_NEXTASSIGN (arg_info);
+    INFO_NEXTASSIGN (arg_info) = arg_node;
     ASSIGN_NEXT (arg_node) = TRAVopt (ASSIGN_NEXT (arg_node), arg_info);
+    INFO_NEXTASSIGN (arg_info) = old_next;
 
+    /* Here we remove the reference to a particular N_assign within the
+     * chain. This happens whenever we move a h2d/d2h up or down.
+     */
     if (INFO_DELASSIGN (arg_info))
     {
         ASSIGN_NEXT (arg_node) = ASSIGN_NEXT (INFO_PREASSIGN (arg_info));
@@ -126,17 +165,30 @@ CUADEassign (node *arg_node, info *arg_info)
         INFO_DELASSIGN (arg_info) = false;
     }
 
+    /* in the case of d2h, we move it down, making the next of a N_assign
+     * somewhere lower down in the chain.
+     */
+    if (INFO_DOWNASSIGN (arg_info))
+    {
+        old_next = ASSIGN_NEXT (INFO_DOWNASSIGN (arg_info));
+        ASSIGN_NEXT (INFO_DOWNASSIGN (arg_info)) = INFO_CURASSIGN (arg_info);
+        ASSIGN_NEXT (INFO_CURASSIGN (arg_info)) = old_next;
+        INFO_DOWNASSIGN (arg_info) = NULL;
+    }
+
     INFO_CURASSIGN (arg_info) = arg_node;
     ASSIGN_STMT (arg_node) = TRAVdo (ASSIGN_STMT (arg_node), arg_info);
 
-    if (INFO_SETASSIGN (arg_info))
+    /* in the case of h2d, we move it up, making it the next of a N_assign
+     * somewhere higher up in the chain.
+     */
+    if (INFO_UPASSIGN (arg_info))
     {
-        ASSIGN_NEXT (INFO_SETASSIGN (arg_info)) = INFO_PREASSIGN (arg_info);
-        ASSIGN_NEXT (arg_node) = INFO_SETASSIGN (arg_info);
-        INFO_SETASSIGN (arg_info) = NULL;
+        ASSIGN_NEXT (INFO_UPASSIGN (arg_info)) = INFO_PREASSIGN (arg_info);
+        ASSIGN_NEXT (arg_node) = INFO_UPASSIGN (arg_info);
+        INFO_UPASSIGN (arg_info) = NULL;
     }
 
-    /// FIXME: will break on d2h re-assign
     INFO_PREASSIGN (arg_info) = arg_node;
 
     DBUG_RETURN (arg_node);
@@ -144,6 +196,9 @@ CUADEassign (node *arg_node, info *arg_info)
 
 /**
  *
+ * @param arg_node
+ * @param arg_info
+ * @return N_let
  */
 node *
 CUADElet (node *arg_node, info *arg_info)
@@ -159,7 +214,13 @@ CUADElet (node *arg_node, info *arg_info)
 }
 
 /**
+ * @brief Check if LHS N_avis is an argument to a h2d, if so collect
+ *              the h2d N_assign and prepare to move it to this point
+ *              in the N_assign chain.
  *
+ * @param arg_node
+ * @param arg_info
+ * @return N_ids
  */
 node *
 CUADEids (node *arg_node, info *arg_info)
@@ -171,7 +232,7 @@ CUADEids (node *arg_node, info *arg_info)
     res = LUTsearchInLutPp (INFO_H2D_LUT (arg_info), IDS_AVIS (arg_node));
     if (res != IDS_AVIS (arg_node) && res != NULL)
     {
-        INFO_SETASSIGN (arg_info) = res;
+        INFO_UPASSIGN (arg_info) = res;
         INFO_H2D_LUT (arg_info) = LUTupdateLutP (INFO_H2D_LUT (arg_info), IDS_AVIS (arg_node), NULL, NULL);
     }
 
@@ -181,9 +242,12 @@ CUADEids (node *arg_node, info *arg_info)
 }
 
 /**
+ * @brief Test if a given N_assign contains a N_prf of given
+ *        type on RHS.
  *
  * @param assign An N_assign node
  * @param prf_type A valid prf
+ * @return true if N_prf found, otherwise false
  */
 static bool
 isAssignPrf (node *assign, prf prf_type)
@@ -202,7 +266,12 @@ isAssignPrf (node *assign, prf prf_type)
 }
 
 /**
+ * @brief Check the current N_avis paramater's N_assign, if it contains
+ *        a d2h, we collect N_assign above us to move the d2h down to.
  *
+ * @param arg_node
+ * @param arg_info
+ * @return N_id
  */
 node *
 CUADEid (node *arg_node, info *arg_info)
@@ -214,10 +283,14 @@ CUADEid (node *arg_node, info *arg_info)
     DBUG_PRINT ("Checking if N_assign of N_avis %s has D2H_end on RHS...", ID_NAME (arg_node));
     assign = AVIS_SSAASSIGN (ID_AVIS (arg_node));
 
-    if (assign != NULL && isAssignPrf (assign, F_device2host_end))
+    if (assign != NULL
+        && isAssignPrf (assign, F_device2host_end)
+        && assign != INFO_NEXTASSIGN (arg_info))
     {
         DBUG_PRINT ("Adding N_assign of N_avis %s to LUT...", ID_NAME (arg_node));
-        INFO_D2H_LUT (arg_info) = LUTinsertIntoLutP (INFO_D2H_LUT (arg_info), ID_AVIS (arg_node), INFO_PREASSIGN (arg_info));
+        // We want to only ever use the last referenced point of N_avis, as such we
+        // overwrite the mapping with the new N_assign.
+        INFO_D2H_LUT (arg_info) = LUTupdateLutP (INFO_D2H_LUT (arg_info), ID_AVIS (arg_node), INFO_NEXTASSIGN (arg_info), NULL);
     }
 
     DBUG_RETURN (arg_node);
@@ -225,11 +298,14 @@ CUADEid (node *arg_node, info *arg_info)
 
 /**
  *
+ * @param arg_node
+ * @param arg_info
+ * @return N_prf
  */
 node *
 CUADEprf (node *arg_node, info *arg_info)
 {
-    node *res, *next;
+    node *res;
 
     DBUG_ENTER ();
 
@@ -284,7 +360,7 @@ CUADEprf (node *arg_node, info *arg_info)
          * a_dev = host2device_end (cuad_tmp, a)
          * ~~~
          *
-         * is push START to before assignment OK?
+         * is pushing START to before assignment OK?
          */
 
         INFO_H2D_LUT (arg_info) = LUTinsertIntoLutP (INFO_H2D_LUT (arg_info), ID_AVIS (PRF_ARG1 (arg_node)), INFO_CURASSIGN (arg_info));
@@ -329,13 +405,13 @@ CUADEprf (node *arg_node, info *arg_info)
         DBUG_PRINT ("Searching for %s LHS of D2H_end...", IDS_NAME (INFO_LHS (arg_info)));
         res = LUTsearchInLutPp (INFO_D2H_LUT (arg_info), IDS_AVIS (INFO_LHS (arg_info)));
 
-        DBUG_ASSERT (res != IDS_AVIS (INFO_LHS (arg_info)) && res != NULL, "LHS is missing SSA N_assign!");
-
-        // TODO actually move d2h_end DOWN!
-        //next = ASSIGN_NEXT (res);
-        //ASSIGN_NEXT (res) = INFO_CURASSIGN (arg_info);
-        //ASSIGN_NEXT (INFO_CURASSIGN (arg_info)) = next;
-        //INFO_DELASSIGN (arg_info) = true;
+        if (res != IDS_AVIS (INFO_LHS (arg_info)) && res != NULL)
+        {
+            INFO_D2H_LUT (arg_info) = LUTupdateLutP (INFO_D2H_LUT (arg_info), IDS_AVIS (INFO_LHS (arg_info)), NULL, NULL);
+            DBUG_EXECUTE (PRTdoPrintNode (res););
+            INFO_DOWNASSIGN (arg_info) = res;
+            INFO_DELASSIGN (arg_info) = true;
+        }
 
         break;
 
@@ -349,6 +425,8 @@ CUADEprf (node *arg_node, info *arg_info)
 
 /**
  *
+ * @param syntax_tree
+ * @return AST node
  */
 node *
 CUADEdoAsyncDelayExpand (node *syntax_tree)
