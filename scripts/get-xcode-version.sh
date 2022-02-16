4404febbcc61e24808a884537fe98029e029baf8
Hans-Nikolai Viessmann <hans@viess.mn>
2022-02-16 10:43:08 +0100

    gracefully error out on xcodebuild issue

--- scripts/get-xcode-version.sh
+++ scripts/get-xcode-version.sh
@@ -4,10 +4,16 @@
 # in the environment using `xcodebuild`.
 
 if [ ! -x "/usr/bin/xcodebuild" ]; then
-    echo "ERROR: XCode (and command line tools) doesn't seem to be installed!" >&2
+    echo "ERROR: XCode doesn't seem to be installed!" >&2
     exit 1
 fi
 
+version=$(/usr/bin/xcodebuild -version 2>/dev/null)
+if [ $? -ne 0 ]; then
+    echo "ERROR: \`xcodebuild' commandline tool has failed!" >&2
+    exit 2
+fi
+
 # the output from -version is `XCode VERSION`, we return only the second field
-/usr/bin/xcodebuild -version 2>/dev/null | /usr/bin/grep -E "[0-9]+\.[0-9]+\.[0-9]+" | /usr/bin/cut -d' ' -f2
+echo "${version}" | /usr/bin/grep -E "[0-9]+\.[0-9]+\.[0-9]+" | /usr/bin/cut -d' ' -f2
 exit 0
