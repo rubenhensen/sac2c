6b6f1fba7c09fb755dbdde43feb5e9285039f367
Michiel Verloop <michiel.verloop@gmail.com>
2022-05-06 14:28:27 +0000

    Refactor str_buffer, add tests for it.

--- src/libsac2c/support/str_buffer.c
+++ src/libsac2c/support/str_buffer.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <stdbool.h>
 /* for memcpy */
 
 #define DBUG_PREFIX "SBUF"
@@ -14,28 +15,61 @@
 
 struct STR_BUF {
     char *buf;
-    size_t pos;
+    size_t len;
     size_t size;
 };
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn str_buf *SBUFcreate( size_t size)
+ *
+ *   @brief  Creates a new string buffer with enough memory allocated for 
+ *           size characters and a null byte.
+ * 
+ *   @param  size The number of characters the buffer should allocate, 
+ *                excluding the null byte.
+ *
+ *   @return The newly created string buffer.
+ *
+ ******************************************************************************/
 str_buf *
 SBUFcreate (size_t size)
 {
     str_buf *res;
 
     DBUG_ENTER ();
-
+    size += 1; // +1 for the null byte.
     res = (str_buf *)MEMmalloc (sizeof (str_buf));
+
+    DBUG_PRINT ("allocating buffer with size %zu : %p", size, (void *)res);
+
     res->buf = (char *)MEMmalloc (size * sizeof (char));
     res->buf[0] = '\0';
-    res->pos = 0;
-    res->size = size;
+    res->len = 0; // Length of the stored string, excluding the null byte.
+    res->size = size; // Null byte is included in the allocated size.
 
-    DBUG_PRINT ("allocating buffer size %zu : %p", size, (void *)res);
 
     DBUG_RETURN (res);
 }
 
+/** <!--********************************************************************-->
+ *
+ * @fn str_buf *EnsureStrBufSpace( str_buf *s, size_t len)
+ *
+ *   @brief  Ensures that the buffer has enough room for an 
+ *           additional len characters to be written.
+ *
+ *           NOTE: At this stage, we don't need to allocate space for the null 
+ *                 byte because it already exists. It only has to be moved/rewritten
+ *                 at the end of the string that's written with other functions.
+ * 
+ *   @param  s   The buffer.
+ *   @param  len The number of characters that should be available to write.
+ * 
+ *   @return The given string buffer.
+ *
+ ******************************************************************************/
 static str_buf *
 EnsureStrBufSpace (str_buf *s, size_t len)
 {
@@ -44,14 +78,17 @@ EnsureStrBufSpace (str_buf *s, size_t len)
 
     DBUG_ENTER ();
 
-    if ((len + 1) > (s->size - s->pos)) {
+    // s->size - (s->len + 1) is equivalent to allocated space minus used space, i.e. available space
+    if (len > (s->size - (s->len + 1))) {
 
+        // Guarantees: globally s->size >=1, and in the scope, len >=1
+        // new_size will always fit len characters, a null byte and possibly more.
         new_size = (len >= s->size ? s->size + 2 * len : 2 * s->size);
 
         DBUG_PRINT ("increasing buffer %p from size %zu to size %zu", (void *)s, s->size, new_size);
 
         new_buf = (char *)MEMmalloc (new_size * sizeof (char));
-        memcpy (new_buf, s->buf, s->pos + 1);
+        memcpy (new_buf, s->buf, s->len + 1); // +1 is to also copy over the null byte
         s->buf = MEMfree (s->buf);
         s->buf = new_buf;
         s->size = new_size;
@@ -60,6 +97,19 @@ EnsureStrBufSpace (str_buf *s, size_t len)
     DBUG_RETURN (s);
 }
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn str_buf *SBUFprint( str_buf *s, const char *string)
+ *
+ *   @brief  Append the given string to the buffer.
+ *
+ *   @param  s      The buffer.
+ *   @param  string The string to append to the buffer.
+ * 
+ *   @return The given string buffer.
+ *
+ ******************************************************************************/
 str_buf *
 SBUFprint (str_buf *s, const char *string)
 {
@@ -68,15 +118,27 @@ SBUFprint (str_buf *s, const char *string)
     DBUG_ENTER ();
 
     len = STRlen (string);
-
     s = EnsureStrBufSpace (s, len);
-
-    s->pos += (size_t)sprintf (&s->buf[s->pos], "%s", string);
-    DBUG_PRINT ("pos of buffer %p now is %zu", (void *)s, s->pos);
+    s->len += (size_t)sprintf (&s->buf[s->len], "%s", string);
+    DBUG_PRINT ("len of buffer %p is now %zu", (void *)s, s->len);
 
     DBUG_RETURN (s);
 }
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn str_buf *SBUFprintf( str_buf *s, const char *format, ...)
+ *
+ *   @brief  Applies the extra arguments to the given format string and appends 
+ *           the result to the buffer.
+ *
+ *   @param  s      The buffer.
+ *   @param  format The format string on which the extra arguments are applied.
+ * 
+ *   @return The given string buffer.
+ *
+ ******************************************************************************/
 str_buf *
 SBUFprintf (str_buf *s, const char *format, ...)
 {
@@ -91,42 +153,100 @@ SBUFprintf (str_buf *s, const char *format, ...)
     DBUG_RETURN (s);
 }
 
+/** <!--********************************************************************-->
+ *
+ * @fn str_buf *SBUFvprintf( str_buf *s, const char *format, va_list arg_list)
+ *
+ *   @brief  Applies the arguments captured in arg_list to the format string
+ *           and appends the result to the buffer.
+ *
+ *   @param s        The buffer.
+ *   @param format   The format string on which the arg_list is applied.
+ *   @param arg_list The list of arguments to apply to the format string.
+ * 
+ *   @return The given string buffer.
+ *
+ ******************************************************************************/
 str_buf *
 SBUFvprintf (str_buf *s, const char *format, va_list arg_list)
 {
     va_list arg_list_copy;
-    int len = 0;
-    size_t rem, len_p = 0;
-    bool ok;
+    int required_space;
+    size_t available_space, required_space_p;
+    bool ok = false;
 
     DBUG_ENTER ();
 
-    ok = FALSE;
+    do {
+        available_space = s->size - s->len;
 
-    while (!ok) {
-        rem = s->size - s->pos;
-
-        // determine needed size
+        // Get the required space (excludes null byte) by attempting to append to the buffer.
+        // If there is enough space and no errors occur, this already appends to the buffer.
         va_copy (arg_list_copy, arg_list);
-        len = vsnprintf (&s->buf[s->pos], rem, format, arg_list_copy);
-        len_p = (size_t)len;
+        required_space = vsnprintf (&s->buf[s->len], available_space, format, arg_list_copy);
+        required_space_p = (size_t)required_space;
         va_end (arg_list_copy);
 
-        if ((len >= 0) && (len_p < rem)) {
-            ok = TRUE;
-        } else {
-            if (len < 0) {
-                len_p = 2 * (s->size + 10);
-            }
-            s = EnsureStrBufSpace (s, len_p);
+        // Deal with encoding errors - abort
+        if (required_space < 0) {
+            // We can't print the format string or the arguments since formatting them lead to a failure.
+            CTIerrorInternal ("Encoding of a format string into a string buffer failed.");
+            CTIabortOnError ();
         }
-    }
+        
+        // Make more space if necessary
+        if (required_space_p + 1 > available_space) { // + 1 for the null byte
+            s = EnsureStrBufSpace (s, required_space_p);
+        } else { // No space is necessary, vsnprintf successfully wrote to the buffer.
+            ok = true;
+        }
+    } while (!ok);
+    
+    s->len += required_space_p;
 
-    s->pos += len_p;
+    DBUG_PRINT ("len of buffer %p is now %zu", (void *)s, s->len);
 
     DBUG_RETURN (s);
 }
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn str_buf *SBUFsubstToken( str_buf *s, const char *token, const char *subst)
+ *
+ *   @brief  Substitutes all occurrences of token in the string buffer with subst.
+ *
+ *   @param  s     The buffer.
+ *   @param  token The string to replace.
+ *   @param  subst The string to replace the token with.
+ * 
+ *   @return The given string buffer.
+ *
+ ******************************************************************************/
+str_buf *
+SBUFsubstToken(str_buf *s, const char *token, const char *subst)
+{
+    DBUG_ENTER ();
+
+    // STRsubstTokend deallocates s->buf, so s->buf is not leaked.
+    s->buf = STRsubstTokend (s->buf, token, subst); 
+    s->len = STRlen (s->buf);
+    s->size = s->len + 1; // +1 for the null byte.
+
+    DBUG_RETURN (s);
+}
+
+/** <!--********************************************************************-->
+ *
+ * @fn char *SBUF2str( str_buf *s)
+ *
+ *   @brief  Returns a copy of the string of the string buffer
+ *
+ *   @param  s The buffer.
+ * 
+ *   @return A copy of the string contained in the internal buffer.
+ *
+ ******************************************************************************/
 char *
 SBUF2str (str_buf *s)
 {
@@ -135,33 +255,79 @@ SBUF2str (str_buf *s)
     DBUG_RETURN (STRcpy (s->buf));
 }
 
+/** <!--********************************************************************-->
+ *
+ * @fn size_t *SBUFlen( str_buf *s)
+ *
+ *   @brief  Returns the length of the string buffer.
+ *
+ *   @param  s The buffer.
+ * 
+ *   @return The length (excl. null byte) of the string stored in the string buffer.
+ *
+ ******************************************************************************/
 size_t
 SBUFlen (str_buf *s)
 {
     DBUG_ENTER ();
 
-    DBUG_RETURN (s->pos);
+    DBUG_RETURN (s->len);
 }
 
+/** <!--********************************************************************-->
+ *
+ * @fn void *SBUFflush( str_buf *s)
+ *
+ *   @brief  Resets the string in the buffer so the allocated space can be reused.
+ * 
+ *           NOTE: This does NOT deallocate anything. If you want to free memory,
+ *                 use SBUF2strAndFree or SBUFfree!
+ *
+ *   @param  s The buffer.
+ * 
+ ******************************************************************************/
 void
 SBUFflush (str_buf *s)
 {
     DBUG_ENTER ();
 
-    s->pos = 0;
-    DBUG_PRINT ("pos of buffer %p reset to %zu", (void *)s, s->pos);
+    s->len = 0;
+    s->buf[0] = '\0';
+    DBUG_PRINT ("len of buffer %p reset to %zu", (void *)s, s->len);
 
     DBUG_RETURN ();
 }
 
+/** <!--********************************************************************-->
+ *
+ * @fn bool *SBUFisEmpty( str_buf *s)
+ *
+ *   @brief  Returns whether string in the buffer has a length of 0.
+ *
+ *   @param  s The buffer.
+ * 
+ *   @return True if the string in the buffer has a length of 0, false otherwise.
+ *
+ ******************************************************************************/
 bool
 SBUFisEmpty (str_buf *s)
 {
     DBUG_ENTER ();
 
-    DBUG_RETURN (s->pos == 0);
+    DBUG_RETURN (s->len == 0);
 }
 
+/** <!--********************************************************************-->
+ *
+ * @fn char *SBUFgetBuffer( str_buf *s)
+ *
+ *   @brief  Returns the internal string buffer.
+ *
+ *   @param  s The buffer.
+ * 
+ *   @return The internal string buffer.
+ *
+ ******************************************************************************/
 char *
 SBUFgetBuffer (str_buf *s)
 {
@@ -170,6 +336,18 @@ SBUFgetBuffer (str_buf *s)
     DBUG_RETURN (s->buf);
 }
 
+/** <!--********************************************************************-->
+ *
+ * @fn char *SBUF2strAndFree( str_buf **s)
+ *
+ *   @brief  Frees str_buf, setting the pointer to NULL, and returns the internal 
+ *           string buffer as a string without copying it.
+ *
+ *   @param  s The pointer to the buffer.
+ * 
+ *   @return The internal string buffer.
+ *
+ ******************************************************************************/
 char *
 SBUF2strAndFree (str_buf **s)
 {
@@ -181,6 +359,18 @@ SBUF2strAndFree (str_buf **s)
     DBUG_RETURN (result);
 }
 
+
+/** <!--********************************************************************-->
+ *
+ * @fn str_buf *SBUFfree( str_buf *s)
+ *
+ *   @brief  Frees the str_buf and the internal buffer.
+ *
+ *   @param  s The buffer.
+ * 
+ *   @return NULL.
+ *
+ ******************************************************************************/
 str_buf *
 SBUFfree (str_buf *s)
 {
---
+++ src/tests/test-str-buffer.cpp
@@ -0,0 +1,189 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include "gtest/gtest.h"
+
+extern "C" {
+#define DBUG_PREFIX "TEST-STRBUF"
+#include "debug.h"
+#include "memory.h"
+#include "str.h"
+#include "str_buffer.h"
+}
+
+TEST (StrBuffer, testCreate)
+{
+    str_buf *buf;
+
+    // Neither creation nor freeing should fail
+    buf = SBUFcreate (0);
+    ASSERT_EQ ('\0', SBUFgetBuffer (buf)[0]);
+    SBUFfree (buf);
+
+    // Regardless of size
+    buf = SBUFcreate (128);
+    ASSERT_EQ ('\0', SBUFgetBuffer (buf)[0]);
+    SBUFfree (buf);
+}
+
+TEST (StrBuffer, testPrint)
+{
+    str_buf *buf;
+
+    // An empty buffer should be able to reserve 0 characters.
+    buf = SBUFcreate (0);
+    SBUFprint (buf, "");
+    EXPECT_EQ (0, SBUFlen (buf));
+
+    // Reserving a string containing a nullbyte ignores everything past the nullbyte
+    SBUFprint (buf, "a\0b");
+    EXPECT_EQ (1, SBUFlen (buf));
+
+    // A non-empty buffer should be able to write a string
+    SBUFprint (buf, "bcdefghijklmnopqrstuvwxyz");
+    EXPECT_EQ (26, SBUFlen (buf));
+
+    SBUFfree (buf);
+}
+
+TEST (StrBuffer, testFlush)
+{
+    str_buf *buf;
+
+    // Both the length should be reset, and a null byte should be set at position 0 so all
+    // functions recognize the buffer as an empty string.
+    buf = SBUFcreate (5);
+    SBUFprint (buf, "Data.");
+    SBUFflush (buf);
+    EXPECT_EQ (0, SBUFlen (buf));
+    EXPECT_EQ ('\0', SBUFgetBuffer (buf)[0]);
+    EXPECT_STREQ ("", SBUFgetBuffer (buf));
+
+    SBUFfree (buf);
+}
+
+TEST (StrBuffer, testIsEmpty)
+{
+    str_buf *buf;
+
+    // An empty buffer should be empty.
+    buf = SBUFcreate (0);
+    EXPECT_TRUE (SBUFisEmpty (buf));
+    SBUFfree (buf);
+
+    // An empty buffer with more than 0 allocated memory should still be empty .
+    buf = SBUFcreate (50);
+    EXPECT_TRUE (SBUFisEmpty (buf));
+    
+    // After adding a 0 length string, the buffer should still be empty.
+    SBUFprint (buf, "");
+    EXPECT_TRUE (SBUFisEmpty (buf));
+
+    // After adding a null byte string, the buffer should still be empty.
+    SBUFprint (buf, "\0");
+    EXPECT_TRUE (SBUFisEmpty (buf));
+
+    // After adding a string of length > 1, the buffer should no longer be empty.
+    SBUFprint (buf, ".");
+    EXPECT_FALSE (SBUFisEmpty (buf));
+
+    SBUFfree (buf);
+}
+
+TEST (StrBuffer, testPrintf)
+{
+    str_buf *buf;
+    str_buf *snd_buf;
+
+    // An empty string with no arguments should work.
+    buf = SBUFcreate (0);
+    SBUFprintf (buf, "");
+    EXPECT_TRUE (SBUFisEmpty (buf));
+
+    // An effectively empty string with arguments should work.
+    SBUFprintf (buf, "%0.s", "test");
+    EXPECT_TRUE (SBUFisEmpty (buf));
+
+    // A format without arguments should work.
+    SBUFprintf (buf, "test");
+    EXPECT_EQ (4, SBUFlen (buf));
+
+    // An equivalent option with arguments should give the same result.
+    snd_buf = SBUFcreate (0);
+    SBUFprintf (snd_buf, "%s", "test");
+    EXPECT_EQ (4, SBUFlen (snd_buf));
+
+    SBUFfree (buf);
+    SBUFfree (snd_buf);
+
+    // The null byte should be written properly in a standard case.
+    buf = SBUFcreate (5); 
+    SBUFprintf (buf, "%s", "Test!");
+    EXPECT_EQ ('\0', SBUFgetBuffer (buf)[5]);
+
+    // The null byte should be written properly when new memory is allocated.
+    SBUFprintf (buf, "%s", "Test.");
+    EXPECT_EQ ('\0', SBUFgetBuffer (buf)[10]);
+
+    // The null byte should be written properly after flushing the buffer.
+    SBUFflush (buf);
+    SBUFprintf (buf, "%s", "test");
+    EXPECT_EQ ('\0', SBUFgetBuffer (buf)[4]);
+
+    SBUFfree (buf);
+}
+
+TEST (StrBuffer, testToStr)
+{
+    str_buf *buf;
+    char *str;
+
+    // We should get the same string back that we put into an empty buffer.
+    buf = SBUFcreate (0);
+    SBUFprint (buf, "test");
+    str = SBUF2str (buf);
+    EXPECT_STREQ ("test", str);
+    MEMfree (str);
+
+    // If we append to the buffer, we should get the first string appended with the second.
+    SBUFprint (buf, "123");
+    str = SBUF2str (buf);
+    EXPECT_STREQ ("test123", str);
+
+    SBUFfree (buf);
+    MEMfree (str);
+}
+
+TEST (StrBuffer, testToStrAndFree)
+{
+    str_buf *buf;
+    char *str;
+
+    // We should get the same string back that we put into an empty buffer.
+    buf = SBUFcreate (0);
+    SBUFprint (buf, "test");
+    str = SBUF2strAndFree (&buf);
+    EXPECT_STREQ ("test", str);
+    MEMfree (str);
+
+    // Freeing the buffer at this point should lead to a double free, but that can't be tested.
+}
+
+TEST (StrBuffer, testSubstToken)
+{
+    str_buf *buf;
+    char *str;
+
+    // Applying SBUFsubstToken(args) should give the same result as STRsubstToken(args)
+    buf = SBUFcreate (0);
+    SBUFprint (buf, "A sentence that we will apply a substitution on");
+    str = STRsubstToken (SBUFgetBuffer (buf), "a", "ae");
+    SBUFsubstToken (buf, "a", "ae");
+    EXPECT_STREQ (str, SBUFgetBuffer (buf));
+
+    // Appending something doesn't break things
+    SBUFprint(buf, ".");
+    EXPECT_STREQ ("A sentence thaet we will aepply ae substitution on.", SBUFgetBuffer (buf));
+
+    SBUFfree (buf);
+    MEMfree (str);
+}
