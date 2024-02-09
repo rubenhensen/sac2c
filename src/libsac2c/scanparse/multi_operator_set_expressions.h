875530ad2b065be0c48d2fcdbaecd558619124c3
Sven-Bodo Scholz <sbscholz@gmail.com>
2024-02-09 11:52:45 +0000

    Support for Tensor Comprehensions with arbitrary numbers of return values

---
+++ src/libsac2c/scanparse/multi_operator_set_expressions.h
@@ -0,0 +1,10 @@
+#ifndef _MULTI_OPERATOR_SET_EXPRESSIONS_
+#define _MULTI_OPERATOR_SET_EXPRESSIONS_
+
+#include "types.h"
+
+extern node *MOSEdoMultiOperatorSetExpressions (node *arg_node);
+extern node *MOSElet (node *arg_node, info *arg_info);
+extern node *MOSEsetwl (node *arg_node, info *arg_info);
+
+#endif /* _MULTI_OPERATOR_SET_EXPRESSIONS_ */
